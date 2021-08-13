5c00e8d1ce3af0c3e335c6f33f195a16d73e6ac8
Adnan Yaqoob <adnan@yaqoob.org>
2021-08-13 14:35:32 +0800

    Time: 118 ms (10.66%), Space: 18 MB (69.12%) - LeetHub

--- clone-n-ary-tree/clone-n-ary-tree.py
+++ clone-n-ary-tree/clone-n-ary-tree.py
@@ -8,12 +8,12 @@ class Node:
 
 class Solution:
     def cloneTree(self, root: 'Node') -> 'Node':
-        def clone(current: 'Node', parent: 'Node') -> None:
+        sentinal = Node()
+        stack = [(root, sentinal)]
+        while stack:
+            current, parent = stack.pop()
             if current:
                 parent.children.append(Node(current.val))
-                for child in current.children:
-                    clone(child, parent.children[-1])
-        
-        sentinal = Node()
-        clone(root, sentinal)
+                for child in reversed(current.children):
+                    stack.append((child, parent.children[-1]))
         return sentinal.children[-1] if sentinal.children else None
