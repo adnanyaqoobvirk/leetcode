6ba2766c0f88b0bec51eee138b28185f951af5f6
Adnan Yaqoob <adnan@yaqoob.org>
2021-08-13 15:27:19 +0800

    Time: 376 ms (5.25%), Space: 15.1 MB (59.71%) - LeetHub

--- design-an-ordered-stream/design-an-ordered-stream.py
+++ design-an-ordered-stream/design-an-ordered-stream.py
@@ -2,16 +2,14 @@ class OrderedStream:
 
     def __init__(self, n: int):
         self.chunk = [None] * n
+        self.pos = 0
 
     def insert(self, idKey: int, value: str) -> List[str]:
         self.chunk[idKey - 1] = value
         output = []
-        for i, v in enumerate(self.chunk):
-            if v is None:
-                break
-            if v != -1:
-                output.append(v)
-                self.chunk[i] = -1
+        while self.pos < len(self.chunk) and self.chunk[self.pos] is not None:
+            output.append(self.chunk[self.pos])
+            self.pos += 1
         return output
 
 # Your OrderedStream object will be instantiated and called as such:
