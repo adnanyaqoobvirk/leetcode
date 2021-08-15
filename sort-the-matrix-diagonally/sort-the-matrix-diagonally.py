ae16f752f31b39bc9ab9b63542ae25d5eb973c9f
Adnan Yaqoob <adnan@yaqoob.org>
2021-08-15 12:01:18 +0800

    Time: 161 ms (7.96%), Space: 14.6 MB (53.34%) - LeetHub

--- sort-the-matrix-diagonally/sort-the-matrix-diagonally.py
+++ sort-the-matrix-diagonally/sort-the-matrix-diagonally.py
@@ -2,21 +2,26 @@ from heapq import heapify, heappop
 
 class Solution:
     def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
+        def sortDiagonal(row: int, col: int) -> None:
+            diagonal = []
+            diagonal_size = min(m - row, n - col)
+            
+            for i in range(diagonal_size):
+                diagonal.append(mat[row + i][col + i])
+                
+            diagonal.sort()
+            
+            for i in range(diagonal_size):
+                mat[row + i][col + i] = diagonal[i]
         
         m = len(mat)
         n = len(mat[0])
-        diagonals = {}
         
-        for row in range(m):
-            for col in range(n):
-                diagonals.setdefault(row - col, []).append(mat[row][col])
+        for col in range(n):
+            sortDiagonal(0, col)
+        
+        for row in range(1, m):
+            sortDiagonal(row, 0)
             
-        for diagonal in diagonals.values():
-            heapify(diagonal)
-        
-        for row in range(m):
-            for col in range(n):
-                mat[row][col] = heappop(diagonals[row - col])
-        
         return mat
             
