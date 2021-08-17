e5e02540a723bbdcd523d9ee6e623a23734bba13
Adnan Yaqoob <adnan@yaqoob.org>
2021-08-17 20:05:22 +0800

    Time: 553 ms (14.92%), Space: 0B (100.00%) - LeetHub

--- user-activity-for-the-past-30-days-ii/user-activity-for-the-past-30-days-ii.sql
+++ user-activity-for-the-past-30-days-ii/user-activity-for-the-past-30-days-ii.sql
@@ -1,19 +1,12 @@
 SELECT
     ROUND(
         IFNULL(
-            AVG(session_count), 
+            COUNT(DISTINCT session_id) / COUNT(DISTINCT user_id), 
             0.0
         ), 
         2
     ) AS average_sessions_per_user
 FROM
-    (
-        SELECT
-            COUNT(DISTINCT session_id) AS session_count
-        FROM
-            Activity
-        WHERE
-            activity_date BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27'
-        GROUP BY
-            user_id
-    ) AS T
+    Activity
+WHERE
+    activity_date BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27'
