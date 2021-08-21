47543311637d9eafa5e8b45d93aa3991cbf56fc6
Adnan Yaqoob <adnan@yaqoob.org>
2021-08-21 19:48:15 +0800

    Time: 40 ms (47.36%), Space: 14.3 MB (8.39%) - LeetHub

--- armstrong-number/armstrong-number.py
+++ armstrong-number/armstrong-number.py
@@ -1,6 +1,6 @@
 class Solution:
     def isArmstrong(self, n: int) -> bool:
-        k = math.ceil(math.log(n, 10))
+        k = math.floor(math.log(n, 10)) + 1
         m = n
         total = 0
         while m > 0:
