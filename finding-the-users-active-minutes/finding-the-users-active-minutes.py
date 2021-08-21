8810567f56d1fe6ffac6bb84f6fd38e82362707c
Adnan Yaqoob <adnan@yaqoob.org>
2021-08-21 13:12:39 +0800

    Time: 988 ms (82.77%), Space: 23.9 MB (96.63%) - LeetHub

---
+++ finding-the-users-active-minutes/finding-the-users-active-minutes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findingUsersActiveMinutes(self, logs: List[List[int]], k: int) -> List[int]:
+        uam_map = {}
+        for user, minute in logs:
+            uam_map.setdefault(user, set()).add(minute)
+        
+        result = [0] * k
+        for _, minutes in uam_map.items():
+            result[len(minutes) - 1] += 1
+        
+        return result
