471b2dbc21aad57236159ff341553aefbaaf53df
Adnan Yaqoob <adnan@yaqoob.org>
2021-08-22 09:39:53 +0800

    Time: 73 ms (15.03%), Space: 14.4 MB (23.21%) - LeetHub

--- cells-with-odd-values-in-a-matrix/cells-with-odd-values-in-a-matrix.py
+++ cells-with-odd-values-in-a-matrix/cells-with-odd-values-in-a-matrix.py
@@ -1,15 +1,14 @@
 class Solution:
     def oddCells(self, m: int, n: int, indices: List[List[int]]) -> int:
-        matrix = [[0] * n for _ in range(m)]
+        row_counts = {}
+        col_counts = {}
         for r, c in indices:
-            for i in range(n):
-                matrix[r][i] += 1
-            for j in range(m):
-                matrix[j][c] += 1
+            row_counts[r] = row_counts.get(r, 0) + 1
+            col_counts[c] = col_counts.get(c, 0) + 1
         
         odd_count = 0
         for i in range(m):
             for j in range(n):
-                if matrix[i][j] % 2 != 0:
+                if (row_counts.get(i, 0) + col_counts.get(j, 0)) % 2 != 0:
                     odd_count += 1
         return odd_count
