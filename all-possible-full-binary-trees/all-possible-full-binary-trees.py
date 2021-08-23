020621565b9b05de821483f7242a89f0d63b20eb
Adnan Yaqoob <adnan@yaqoob.org>
2021-08-23 14:42:07 +0800

    Time: 184 ms (89.02%), Space: 17.2 MB (58.09%) - LeetHub

--- all-possible-full-binary-trees/all-possible-full-binary-trees.py
+++ all-possible-full-binary-trees/all-possible-full-binary-trees.py
@@ -5,18 +5,17 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    cache = {1: [TreeNode()]}
+    dp = {1: [TreeNode()]}
     
     def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
         if n % 2 == 0:
             return []
-        elif n in Solution.cache:
-            return Solution.cache[n]
         
-        fbts = []
-        for x in range(1, n, 2):
-            for left in self.allPossibleFBT(x):
-                for right in self.allPossibleFBT(n - 1 - x):
-                    fbts.append(TreeNode(0, left, right))
-        Solution.cache[n] = fbts
-        return Solution.cache[n]               
+        for x in range(3, n + 1, 2):
+            fbts = []
+            for i in range(1, x, 2):
+                for left in Solution.dp[i]:
+                    for right in Solution.dp[x - 1 - i]:
+                        fbts.append(TreeNode(left=left, right=right))
+            Solution.dp[x] = fbts
+        return Solution.dp[n]            
