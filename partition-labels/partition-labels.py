ab0b332e865a664737988f6c14f5611e4fb2f4c1
Adnan Yaqoob <adnan@yaqoob.org>
2021-08-24 13:16:55 +0800

    Time: 40 ms (68.68%), Space: 14.3 MB (55.32%) - LeetHub

--- partition-labels/partition-labels.py
+++ partition-labels/partition-labels.py
@@ -1,22 +1,12 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        pars = []
-        for c in s:
-            npars = []
-            collapse = False
-            for par in pars:
-                if collapse:
-                    npars[-1][0] |= par[0]
-                    npars[-1][1] += par[1]
-                else:
-                    npars.append(par)
-                    if c in par[0]:
-                        collapse = True
-            if collapse:
-                npars[-1][0].add(c)
-                npars[-1][1] += 1
-            else:
-                npars.append([set(c), 1])
-            pars = npars
-        return [count for _, count in pars]
-                    
+        last = {c: i for i, c in enumerate(s)}
+        parts = []
+        start = 0
+        end = 0
+        for i, c in enumerate(s):
+            end = max(end, last[c])
+            if i == end:
+                parts.append(end - start + 1)
+                start = end + 1
+        return parts
