f4d58d5a60151862c438da7260fdf5c66d2ee67f
Adnan Yaqoob <adnan@yaqoob.org>
2021-08-24 18:20:36 +0800

    Time: 332 ms (43.08%), Space: 15.3 MB (33.08%) - LeetHub

--- design-most-recently-used-queue/design-most-recently-used-queue.py
+++ design-most-recently-used-queue/design-most-recently-used-queue.py
@@ -1,11 +1,22 @@
 class MRUQueue:
 
     def __init__(self, n: int):
-        self.q = list(range(1, n + 1))
-
+        self.csize = int(math.sqrt(n))
+        self.chunks = []
+        for i in range(0, n):
+            if i % self.csize == 0:
+                self.chunks.append(deque())
+            self.chunks[-1].append(i + 1)
+            
     def fetch(self, k: int) -> int:
-        e = self.q.pop(k - 1)
-        self.q.append(e)
+        i = (k - 1) // self.csize
+        
+        e = self.chunks[i][(k - 1) % self.csize]
+        self.chunks[i].remove(e)
+        for j in range(i + 1, len(self.chunks)):
+            self.chunks[j - 1].append(self.chunks[j].popleft())
+        self.chunks[-1].append(e)
+        
         return e
 
 # Your MRUQueue object will be instantiated and called as such:
