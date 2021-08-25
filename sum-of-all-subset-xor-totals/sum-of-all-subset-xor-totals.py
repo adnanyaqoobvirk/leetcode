4985a5a1d7351be5149922b04cd1c0e1c157700e
Adnan Yaqoob <adnan@yaqoob.org>
2021-08-25 13:35:19 +0800

    Time: 16 ms (99.88%), Space: 14.2 MB (80.54%) - LeetHub

--- sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
+++ sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
@@ -1,10 +1,6 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        total = 0
-        for i in range(1, 1 << len(nums)):
-            xor = 0
-            for j in range(len(nums)):
-                if (1 << j) & i:
-                    xor ^= nums[j]
-            total += xor
-        return total
+        bits = 0
+        for num in nums:
+            bits |= num
+        return (1 << len(nums) - 1) * bits
