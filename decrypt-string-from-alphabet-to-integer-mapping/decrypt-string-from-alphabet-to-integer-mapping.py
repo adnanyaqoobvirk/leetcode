2e681cd8047f816071a7071bd9a5a29f6fb4361f
Adnan Yaqoob <adnan@yaqoob.org>
2021-08-25 15:07:13 +0800

    Time: 32 ms (60.69%), Space: 14.1 MB (80.34%) - LeetHub

--- decrypt-string-from-alphabet-to-integer-mapping/decrypt-string-from-alphabet-to-integer-mapping.py
+++ decrypt-string-from-alphabet-to-integer-mapping/decrypt-string-from-alphabet-to-integer-mapping.py
@@ -1,12 +1,19 @@
 class Solution:
     def freqAlphabets(self, s: str) -> str:
+        n = len(s)
         output = []
         start = ord('a') - 1
-        for i in range(len(s)):
-            if s[i] == '#':
-                output.pop()
-                output.pop()
-                output.append(chr(start + int(s[i - 1]) + int(s[i - 2]) * 10))
+        left = 0
+        right = 2
+        while left < n:
+            if right < n and s[right] == '#':
+                output.append(
+                    chr(start + int(s[left]) * 10 + int(s[left + 1]))
+                )
+                left += 3
+                right += 3
             else:
-                output.append(chr(start + int(s[i])))
+                output.append(chr(start + int(s[left])))
+                left += 1
+                right += 1
         return "".join(output)
