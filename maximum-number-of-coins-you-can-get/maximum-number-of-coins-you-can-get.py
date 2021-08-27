89e7a6f6f740d23377fe34c8851eff8ebb6da786
Adnan Yaqoob <adnan@yaqoob.org>
2021-08-27 15:20:23 +0800

    Time: 1520 ms (6.63%), Space: 26.6 MB (35.03%) - LeetHub

--- maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.py
+++ maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.py
@@ -1,8 +1,18 @@
 class Solution:
     def maxCoins(self, piles: List[int]) -> int:
+        def countingSort(nums: List[int]) -> List[int]:
+            counts = Counter(nums)
+            result = []
+            for num in range(1, 10001):
+                count = counts.get(num, None)
+                if count:
+                    for _ in range(count):
+                        result.append(num)
+            return result
+                    
         n = len(piles)
-        piles.sort()
+        sorted_piles = countingSort(piles)
         total = 0
         for i in range(1, n // 3 + 1):
-            total += piles[n - 2 * i]
+            total += sorted_piles[n - 2 * i]
         return total
