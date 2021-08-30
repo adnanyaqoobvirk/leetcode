5e49b2a3ccc9e34a9ab2f8e4d6f80eabafd1ff46
Adnan Yaqoob <adnan@yaqoob.org>
2021-08-30 17:19:04 +0800

    Time: 167 ms (5.31%), Space: 14.6 MB (50.34%) - LeetHub

--- high-five/high-five.py
+++ high-five/high-five.py
@@ -1,11 +1,17 @@
 class Solution:
     def highFive(self, items: List[List[int]]) -> List[List[int]]:
-        student_scores = {}
-        for i, score in items:
-            student_scores.setdefault(i, []).append(score)
+        items.sort(key=lambda x: (x[0], -x[1]))
         
         res = []
-        for i in sorted(student_scores.keys()):
-            student_scores[i].sort()
-            res.append([i, sum(student_scores[i][-1:-6:-1]) // 5])
+        prev_id = id_sum = id_count = 0
+        for i, score in items:
+            id_count += 1
+            id_sum += score
+            if prev_id != i:
+                prev_id = i
+                id_sum = score
+                id_count = 1
+            elif id_count == 5:
+                res.append([i, id_sum // 5])
         return res
+                
