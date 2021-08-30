62747fe4e98dbd5477f7a35b2be9cae6d0ecd721
Adnan Yaqoob <adnan@yaqoob.org>
2021-08-30 17:35:58 +0800

    Time: 60 ms (99.64%), Space: 14.9 MB (18.06%) - LeetHub

--- high-five/high-five.py
+++ high-five/high-five.py
@@ -1,17 +1,14 @@
+from heapq import heappush, heappushpop
+
 class Solution:
     def highFive(self, items: List[List[int]]) -> List[List[int]]:
-        items.sort(key=lambda x: (x[0], -x[1]))
-        
-        res = []
-        prev_id = id_sum = id_count = 0
+        student_scores = {}
         for i, score in items:
-            id_count += 1
-            id_sum += score
-            if prev_id != i:
-                prev_id = i
-                id_sum = score
-                id_count = 1
-            elif id_count == 5:
-                res.append([i, id_sum // 5])
-        return res
+            scores = student_scores.setdefault(i, [])
+            
+            if len(scores) >= 5:
+                heappushpop(scores, score)
+            else:
+                heappush(scores, score)
                 
+        return [[i, sum(student_scores[i]) // 5] for i in sorted(student_scores)]
