f8e7692538081d20ef66c8cde374bdee92f226d2
Adnan Yaqoob <adnan@yaqoob.org>
2021-08-31 19:03:54 +0800

    Time: 196 ms (20.26%), Space: 23.3 MB (25.93%) - LeetHub

--- letter-tile-possibilities/letter-tile-possibilities.py
+++ letter-tile-possibilities/letter-tile-possibilities.py
@@ -1,15 +1,19 @@
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
-        def backtrack(t: List[int], chars: List[str]) -> None:
+        def backtrack(t: set[int], chars: List[str]) -> None:
             if chars:
                 combs.add("".join(chars))
             
-            for i in range(len(t)):
-                chars.append(t[i])
-                backtrack(t[:i] + t[i + 1:], chars)
-                chars.pop()
+            if len(t) < n:
+                for i in range(len(tiles)):
+                    if i not in t:
+                        chars.append(tiles[i])
+                        t.add(i)
+                        backtrack(t, chars)
+                        t.remove(i)
+                        chars.pop()
         
         n = len(tiles)
         combs = set()
-        backtrack(tiles, [])
+        backtrack(set(), [])
         return len(combs)
