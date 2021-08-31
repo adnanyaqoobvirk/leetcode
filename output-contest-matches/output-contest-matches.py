b7b4b2f47788dd601e392de6fc81ede441855655
Adnan Yaqoob <adnan@yaqoob.org>
2021-08-31 14:43:19 +0800

    Time: 68 ms (5.41%), Space: 14.7 MB (67.57%) - LeetHub

--- output-contest-matches/output-contest-matches.py
+++ output-contest-matches/output-contest-matches.py
@@ -2,11 +2,6 @@ class Solution:
     def findContestMatch(self, n: int) -> str:
         teams = list(range(1, n + 1))
         while len(teams) > 1:
-            start, end = 0, len(teams) - 1
-            new_teams = []
-            while start < end:
-                new_teams.append(f"({teams[start]},{teams[end]})")
-                start += 1
-                end -= 1
-            teams = new_teams
+            m = len(teams) - 1
+            teams = [f"({teams[i]},{teams[m - i]})" for i in range((m + 1) // 2)]
         return teams[0]
