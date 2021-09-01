f148c4e9fde62595b9a1806a7b5026974269ed70
Adnan Yaqoob <adnan@yaqoob.org>
2021-09-01 15:35:24 +0800

    Time: 73 ms (5.25%), Space: 14.3 MB (61.13%) - LeetHub

--- find-and-replace-pattern/find-and-replace-pattern.py
+++ find-and-replace-pattern/find-and-replace-pattern.py
@@ -1,13 +1,8 @@
 class Solution:
     def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
-        res = []
         pat_count = len(set(pattern))
-        for word in words:
-            if pat_count == len(set(word)):
-                mapping = {}
-                for i, c in enumerate(word):
-                    if mapping.setdefault(pattern[i], c) != c:
-                        break
-                else:
-                    res.append(word)
-        return res
+        return [
+            word 
+            for word in words 
+            if pat_count == len(set(word)) == len(set(zip(word, pattern)))
+        ]
