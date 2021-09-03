9a6be2e68abcd62c10d431013d09edc9a9e0a3ec
Adnan Yaqoob <adnan@yaqoob.org>
2021-09-03 16:22:35 +0800

    Time: 112 ms (30.57%), Space: 18.4 MB (27.07%) - LeetHub

--- 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.py
+++ 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.py
@@ -5,20 +5,19 @@
 #         self.left = left
 #         self.right = right
 class FindElements:
-
     def __init__(self, root: Optional[TreeNode]):
-        def recurse(current: TreeNode, val: int) -> None:
-            if current:
-                if current.left:
-                    left = val * 2 + 1
-                    self.vals.add(left)
-                    recurse(current.left, left)
-                if current.right:
-                    right = val * 2 + 2
-                    self.vals.add(right)
-                    recurse(current.right, right)
-        self.vals = {0}
-        recurse(root, 0)
+        root.val = 0
+        self.vals = set()
+        stack = [root]
+        while stack:
+            current = stack.pop()
+            self.vals.add(current.val)
+            if current.left:
+                current.left.val = current.val * 2 + 1
+                stack.append(current.left)
+            if current.right:
+                current.right.val = current.val * 2 + 2
+                stack.append(current.right)
         
     def find(self, target: int) -> bool:
         return target in self.vals
