7865ff723e7b37e464e8eda3ab53901298cd3bd1
Adnan Yaqoob <adnan@yaqoob.org>
2021-09-04 15:04:59 +0800

    Time: 55 ms (28.95%), Space: 16.2 MB (25.23%) - LeetHub

--- 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.py
+++ 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.py
@@ -10,15 +10,10 @@ class Solution:
     def postorder(self, root: 'Node') -> List[int]:
         ans = []
         if root:
-            stack = [[root, False]]
+            stack = [root]
             while stack:
-                current, processed = stack[-1]
-                if not processed:
-                    stack[-1][1] = True
-                    if current.children:
-                        for i in range(len(current.children) - 1, -1, -1):
-                            stack.append([current.children[i], False])
-                else:
-                    ans.append(current.val)
-                    stack.pop()
-        return ans
+                current = stack.pop()
+                ans.append(current.val)
+                for child in current.children:
+                    stack.append(child)
+        return ans[::-1]
