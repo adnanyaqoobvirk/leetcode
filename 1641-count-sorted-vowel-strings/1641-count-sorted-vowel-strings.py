00bc060260900c4293eff240e55103f30f895fa7
Adnan Yaqoob <adnan@yaqoob.org>
2021-09-05 13:58:02 +0800

    Time: 576 ms (17.71%), Space: 14.1 MB (80.36%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
@@ -1,16 +1,12 @@
 class Solution:
     def countVowelStrings(self, n: int) -> int:
-        dp = [
-            [
-                1 if j == n else 0
-                for j in range(n + 1)
-            ] 
-            for i in range(6)
-        ]
-                    
-        for i in range(4, -1, -1):
-            for j in range(n - 1, -1, -1):
-                for k in range(i, 5):
-                    dp[i][j] += dp[k][j + 1]
-        return dp[0][0]
-                    
+        def recurse(c: int, pos: int) -> int:
+            if c == 1:
+                return pos
+            
+            if pos == 1:
+                return 1
+            
+            return recurse(c - 1, pos) + recurse(c, pos - 1)
+        
+        return recurse(n, 5)
