2bcd70385f005c64cba75db95f2f4c162ddc1e62
Adnan Yaqoob <adnan@yaqoob.org>
2021-09-05 09:53:32 +0800

    Time: 61 ms (20.09%), Space: 14.1 MB (92.56%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
@@ -1,12 +1,16 @@
 class Solution:
     def countVowelStrings(self, n: int) -> int:
-        @cache
-        def backtrack(comb: int, pos: int) -> int:
-            if comb == n:
-                return 1
-            else:
-                ans = 0
-                for i in range(pos, 5):
-                    ans += backtrack(comb + 1, i)
-                return ans
-        return backtrack(0, 0)
+        dp = [
+            [
+                1 if j == n else 0
+                for j in range(n + 1)
+            ] 
+            for i in range(6)
+        ]
+                    
+        for i in range(4, -1, -1):
+            for j in range(n - 1, -1, -1):
+                for k in range(i, 5):
+                    dp[i][j] += dp[k][j + 1]
+        return dp[0][0]
+                    
