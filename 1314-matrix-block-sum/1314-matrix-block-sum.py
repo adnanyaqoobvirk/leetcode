c2425f2fb149ffd9706199622dc9dd9634ff4cbf
Adnan Yaqoob <adnan@yaqoob.org>
2021-09-09 20:36:41 +0800

    Time: 104 ms (81.19%), Space: 15.3 MB (59.95%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.py
+++ 1314-matrix-block-sum/1314-matrix-block-sum.py
@@ -1,28 +1,16 @@
 class Solution:
     def matrixBlockSum(self, mat: List[List[int]], k: int) -> List[List[int]]:
-        h, w = len(mat), len(mat[0])
+        h, w = len(mat) + 1, len(mat[0]) + 1
         
         ii = [[0] * w for _ in range(h)]
-        for y in range(h):
-            for x in range(w):
-                ii[y][x] = mat[y][x]
-                if x > 0:
-                    ii[y][x] += ii[y][x - 1]
-                if y > 0:
-                    ii[y][x] += ii[y - 1][x]
-                if x > 0 and y > 0:
-                    ii[y][x] -= ii[y - 1][x - 1]
+        for y in range(1, h):
+            for x in range(1, w):
+                ii[y][x] = mat[y - 1][x - 1] + ii[y][x - 1] + ii[y - 1][x] - ii[y - 1][x - 1]
         
-        ans = [[0] * w for _ in range(h)]
-        for y in range(h):
-            by_min, by_max = max(y - k, 0), min(y + k, h - 1)
-            for x in range(w):
-                bx_min, bx_max = max(x - k, 0), min(x + k, w - 1)
-                ans[y][x] = ii[by_max][bx_max]
-                if by_min > 0:
-                    ans[y][x] -= ii[by_min - 1][bx_max]
-                if bx_min > 0:
-                    ans[y][x] -= ii[by_max][bx_min - 1]
-                if by_min > 0 and bx_min > 0:
-                    ans[y][x] += ii[by_min - 1][bx_min - 1]
+        ans = [[0] * (w - 1) for _ in range(h - 1)]
+        for y in range(1, h):
+            by_min, by_max = max(y - k, 1), min(y + k, h - 1)
+            for x in range(1, w):
+                bx_min, bx_max = max(x - k, 1), min(x + k, w - 1)
+                ans[y - 1][x - 1] = ii[by_max][bx_max] - ii[by_min - 1][bx_max] - ii[by_max][bx_min - 1] + ii[by_min - 1][bx_min - 1]
         return ans
