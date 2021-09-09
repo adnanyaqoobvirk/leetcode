92e7abf0c24886fdf75331e6cf1c1d005dc4b142
Adnan Yaqoob <adnan@yaqoob.org>
2021-09-09 20:30:05 +0800

    Time: 142 ms (41.54%), Space: 15.4 MB (40.19%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.py
+++ 1314-matrix-block-sum/1314-matrix-block-sum.py
@@ -4,27 +4,25 @@ class Solution:
         
         ii = [[0] * w for _ in range(h)]
         for y in range(h):
-            total = 0
             for x in range(w):
-                total += mat[y][x]
-                ii[y][x] = total
+                ii[y][x] = mat[y][x]
+                if x > 0:
+                    ii[y][x] += ii[y][x - 1]
                 if y > 0:
                     ii[y][x] += ii[y - 1][x]
+                if x > 0 and y > 0:
+                    ii[y][x] -= ii[y - 1][x - 1]
         
         ans = [[0] * w for _ in range(h)]
         for y in range(h):
+            by_min, by_max = max(y - k, 0), min(y + k, h - 1)
             for x in range(w):
-                bx_min, by_min = max(x - k, 0), max(y - k, 0)
-                bx_max, by_max = min(x + k, w - 1), min(y + k, h - 1)
-                
+                bx_min, bx_max = max(x - k, 0), min(x + k, w - 1)
                 ans[y][x] = ii[by_max][bx_max]
-                
                 if by_min > 0:
                     ans[y][x] -= ii[by_min - 1][bx_max]
-                
                 if bx_min > 0:
                     ans[y][x] -= ii[by_max][bx_min - 1]
-                
                 if by_min > 0 and bx_min > 0:
                     ans[y][x] += ii[by_min - 1][bx_min - 1]
         return ans
