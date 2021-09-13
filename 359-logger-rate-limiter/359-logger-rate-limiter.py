bdc12b1294a1c0a45fe66425573cb947accb29b7
Adnan Yaqoob <adnan@yaqoob.org>
2021-09-13 15:10:34 +0800

    Time: 148 ms (52.71%), Space: 20.1 MB (34.45%) - LeetHub

--- 359-logger-rate-limiter/359-logger-rate-limiter.py
+++ 359-logger-rate-limiter/359-logger-rate-limiter.py
@@ -4,7 +4,9 @@ class Logger:
         """
         Initialize your data structure here.
         """
-        self.messages = {}
+        self.old_msgs = {}
+        self.curr_msgs = {}
+        self.min_time = 0
         
     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
         """
@@ -12,12 +14,19 @@ class Logger:
         If this method returns false, the message will not be printed.
         The timestamp is in seconds granularity.
         """
-        if timestamp < self.messages.get(message, timestamp - 10) + 10:
+        if timestamp >= self.min_time + 10:
+            self.old_msgs = self.curr_msgs
+            self.curr_msgs = {}
+            self.min_time = timestamp
+        
+        if message in self.curr_msgs or timestamp < self.old_msgs.get(message, timestamp - 10) + 10:
             return False
         
-        self.messages[message] = timestamp
+        self.curr_msgs[message] = timestamp
         return True
         
+        
+
 
 
 # Your Logger object will be instantiated and called as such:
