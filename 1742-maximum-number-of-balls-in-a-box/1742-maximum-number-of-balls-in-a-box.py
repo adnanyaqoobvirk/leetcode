e7744d50b8478def64c215e1b51175b17a5dcbfb
Adnan Yaqoob <adnan@yaqoob.org>
2021-09-15 12:21:25 +0800

    Time: 771 ms (24.44%), Space: 14.2 MB (57.14%) - LeetHub

--- 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.py
+++ 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.py
@@ -1,13 +1,10 @@
 class Solution:
     def countBalls(self, lowLimit: int, highLimit: int) -> int:
-        ballCounts = {}
-        maxBallCount = 0
+        ballCounts = [0] * 46
         for ball in range(lowLimit, highLimit + 1):
             box = 0
             while ball > 0:
                 ball, digit = divmod(ball, 10)
                 box += digit
-            ballCounts[box] = ballCounts.get(box, 0) + 1
-            maxBallCount = max(maxBallCount, ballCounts[box])
-        return maxBallCount
-        
+            ballCounts[box] += 1
+        return max(ballCounts)
