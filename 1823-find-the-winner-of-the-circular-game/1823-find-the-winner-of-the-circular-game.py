015ca59f91871765c8670b3df3b8281f8dedcb91
Adnan Yaqoob <adnan@yaqoob.org>
2021-09-15 18:52:44 +0800

    Time: 28 ms (95.06%), Space: 14.2 MB (72.08%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,18 +1,8 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
         friends = [i for i in range(1, n + 1)]
-        loserCount = pos = 0
-        while loserCount < n - 1:
-            i = 0
-            while i < k:
-                if friends[pos]:
-                    i += 1
-                pos += 1
-                if pos == n:
-                    pos = 0
-            loserCount += 1
-            friends[pos - 1] = 0
-        
-        for f in friends:
-            if f:
-                return f
+        pos = 0
+        for i in range(n - 1):
+            pos = (pos + k - 1) % len(friends)
+            friends.pop(pos)
+        return friends[0]
