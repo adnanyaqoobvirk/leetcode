98fa8c441bbc9f232d3118e453f80341eec376fe
Adnan Yaqoob <adnan@yaqoob.org>
2021-09-19 17:08:32 +0800

    Time: 280 ms (26.89%), Space: 33.5 MB (45.89%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -7,13 +7,13 @@
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         def recurse(current: TreeNode, maximum: int) -> int:
-            ans = 0
-            if current:
-                maximum = max(current.val, maximum)
-                if current.val == maximum:
-                    ans += 1
-                
-                ans += recurse(current.left, maximum)
-                ans += recurse(current.right, maximum)
-            return ans
+            if not current:
+                return 0
+            
+            maximum = max(current.val, maximum)
+            return (
+                int(current.val == maximum) + 
+                recurse(current.left, maximum) + 
+                recurse(current.right, maximum)
+            )
         return recurse(root, float('-inf'))
