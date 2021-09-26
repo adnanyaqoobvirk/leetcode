f872d6d69945d8aa6fa53cd10dfc8ca9fd7cf2dc
Adnan Yaqoob <adnan@yaqoob.org>
2021-09-26 10:01:35 +0800

    Time: 480 ms (89.23%), Space: 19.7 MB (18.46%) - LeetHub

--- 1063-number-of-valid-subarrays/1063-number-of-valid-subarrays.py
+++ 1063-number-of-valid-subarrays/1063-number-of-valid-subarrays.py
@@ -1,11 +1,10 @@
 class Solution:
     def validSubarrays(self, nums: List[int]) -> int:
         ans = 0
-        for i in range(len(nums)):
-            left = nums[i]
-            for j in range(i, len(nums)):
-                if left <= nums[j]:
-                    ans += 1
-                else:
-                    break
+        stack = []
+        for num in nums:
+            while stack and stack[-1] > num:
+                stack.pop()
+            stack.append(num)
+            ans += len(stack)
         return ans
