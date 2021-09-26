1bf78a818df93ead99ce70f29f38454e18631b3e
Adnan Yaqoob <adnan@yaqoob.org>
2021-09-26 16:51:20 +0800

    Time: 32 ms (97.41%), Space: 14.3 MB (77.63%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -1,15 +1,10 @@
 class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
-        @cache
-        def recurse(pos: int, time: int) -> int:
-            if pos == n:
-                return 0
-            
-            return max(
-                satisfaction[pos] * time + recurse(pos + 1, time + 1),
-                recurse(pos + 1, time)
-            )
-        
-        n = len(satisfaction)
         satisfaction.sort()
-        return recurse(0, 1)
+        ans = runSum = 0
+        for num in reversed(satisfaction):
+            runSum += num
+            if runSum <= 0:
+                break
+            ans += runSum
+        return ans
