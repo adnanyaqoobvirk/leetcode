2e18eb92890869b22f04be9b8b68e673d3f684d7
Adnan Yaqoob <adnan@yaqoob.org>
2021-09-27 16:49:45 +0800

    Time: 112 ms (50.24%), Space: 14.8 MB (18.34%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -1,12 +1,10 @@
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
         n, m = len(mat), len(mat[0])
-        done = set()
-        ans = []
+        ans = {}
         for j in range(m + 1):
             for i in range(n):
-                if i not in done and (j == m or mat[i][j] == 0):
-                    ans.append(i)
-                    done.add(i)
+                if i not in ans and (j == m or mat[i][j] == 0):
+                    ans[i] = True
                 if len(ans) == k:
-                    return ans
+                    return [k for k in ans.keys()]
