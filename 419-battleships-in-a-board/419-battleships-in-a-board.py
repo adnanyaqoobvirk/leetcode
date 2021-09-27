8e77128952faffef438950adc7d76eb9008c2383
Adnan Yaqoob <adnan@yaqoob.org>
2021-09-27 11:31:04 +0800

    Time: 72 ms (76.93%), Space: 14.7 MB (79.21%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.py
+++ 419-battleships-in-a-board/419-battleships-in-a-board.py
@@ -5,14 +5,12 @@ class Solution:
         ships = 0
         for i in range(n):
             for j in range(m):
-                if board[i][j] == 'X':
-                    isShip = True
-                    if i < n - 1 and board[i + 1][j] == 'X':
-                        isShip = False
-                    
-                    if j < m - 1 and board[i][j + 1] == 'X':
-                        isShip = False
-                    
-                    if isShip:
-                        ships += 1
+                if (
+                    board[i][j] == 'X'
+                    and
+                    (not (i < n - 1 and board[i + 1][j] == 'X'))
+                    and
+                    (not (j < m - 1 and board[i][j + 1] == 'X'))
+                ):
+                    ships += 1
         return ships
