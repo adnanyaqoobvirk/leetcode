1bf4142fa76ac225db1a3737e72145389f843ba1
Adnan Yaqoob <adnan@yaqoob.org>
2021-09-28 12:50:33 +0800

    Time: 724 ms (100.00%), Space: 0B (100.00%) - LeetHub

--- 2020-number-of-accounts-that-did-not-stream/2020-number-of-accounts-that-did-not-stream.sql
+++ 2020-number-of-accounts-that-did-not-stream/2020-number-of-accounts-that-did-not-stream.sql
@@ -1,25 +1,15 @@
 SELECT
-    COUNT(S.account_id) AS accounts_count
+    COUNT(DISTINCT account_id) AS accounts_count
 FROM
-    (
+    Subscriptions
+WHERE
+    YEAR(end_date) = 2021
+    AND
+    account_id NOT IN (
         SELECT
-            DISTINCT account_id
-        FROM
-            Subscriptions
-        WHERE
-            YEAR(start_date) = 2021 OR YEAR(end_date) = 2021
-    ) AS S
-    LEFT JOIN
-    (
-        SELECT
-            DISTINCT account_id
+            account_id
         FROM
             Streams
         WHERE
             YEAR(stream_date) = 2021
-    ) AS ST
-    ON
-        S.account_id = ST.account_id
-WHERE
-    ST.account_id IS NULL
-    
+    )
