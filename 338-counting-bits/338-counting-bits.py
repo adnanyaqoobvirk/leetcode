f033045157c799ea9ea2a2d5e30cc82f1d728d77
Adnan Yaqoob <adnan@yaqoob.org>
2021-09-28 15:14:09 +0800

    Time: 80 ms (83.63%), Space: 20.9 MB (34.54%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -2,6 +2,7 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         ans = [0] * (n + 1)
         for i in range(1, n + 1):
-            ans[i] = ans[i // 2] + (i & 1)
+            idx, r = divmod(i, 2)
+            ans[i] = ans[idx] + r
         return ans
             
