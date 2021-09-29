911c9222cc2c4b28027e6385677403e095d4466b
Adnan Yaqoob <adnan@yaqoob.org>
2021-09-29 16:17:29 +0800

    Time: 28 ms (88.52%), Space: 14.3 MB (25.53%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -1,24 +1,21 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        def recurse(x: int, y:int, n: int, m: int) -> None:
-            if n > 0 and m > 0:
-                for j in range(y, y + m):
-                    ans.append(matrix[x][j])
-
-                for i in range(x + 1, x + n):
-                    ans.append(matrix[i][y + m - 1])
-
-                if n > 1:
-                    for j in range(y + m - 2, y - 1, -1):
-                        ans.append(matrix[x + n - 1][j])
-
-                if m > 1:
-                    for i in range(x + n - 2, x, -1):
-                        ans.append(matrix[i][y])
-
-                recurse(x + 1, y + 1, n - 2, m - 2)
-        
         ans = []
-        recurse(0, 0, len(matrix), len(matrix[0]))
+        pos = 0
+        for n, m in zip(range(len(matrix), 0, -2), range(len(matrix[0]), 0, -2)):
+            for j in range(pos, pos + m):
+                ans.append(matrix[pos][j])
+
+            for i in range(pos + 1, pos + n):
+                ans.append(matrix[i][pos + m - 1])
+
+            if n > 1:
+                for j in range(pos + m - 2, pos - 1, -1):
+                    ans.append(matrix[pos + n - 1][j])
+
+            if m > 1:
+                for i in range(pos + n - 2, pos, -1):
+                    ans.append(matrix[i][pos])
+            pos += 1
         return ans
         
