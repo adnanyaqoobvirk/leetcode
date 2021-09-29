2d106b172d644cd8ad86edf7d37b16f02f2bd990
Adnan Yaqoob <adnan@yaqoob.org>
2021-09-29 15:17:24 +0800

    Time: 236 ms (84.58%), Space: 25.5 MB (30.77%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.py
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -5,7 +5,7 @@ class UndergroundSystem:
         self.times = {}
 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
-        self.customers[id] = [stationName, t]
+        self.customers[id] = (stationName, t)
 
     def checkOut(self, id: int, stationName: str, t: int) -> None:
         startStation, startTime = self.customers[id]
@@ -13,8 +13,6 @@ class UndergroundSystem:
         travelStats = self.times.setdefault((startStation, stationName), [0, 0])
         travelStats[0] += t - startTime
         travelStats[1] += 1
-        
-        del self.customers[id]
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
         totalTime, totalTrips = self.times[(startStation, endStation)]
