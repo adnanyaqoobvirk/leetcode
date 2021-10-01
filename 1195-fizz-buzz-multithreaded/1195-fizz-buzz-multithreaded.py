25a5b03976286cc8c1c5f38698e9f6147396e2d6
Adnan Yaqoob <adnan@yaqoob.org>
2021-10-01 09:55:07 +0800

    Time: 24 ms (96.73%), Space: 14.9 MB (46.91%) - LeetHub

--- 1195-fizz-buzz-multithreaded/1195-fizz-buzz-multithreaded.py
+++ 1195-fizz-buzz-multithreaded/1195-fizz-buzz-multithreaded.py
@@ -1,51 +1,60 @@
-from threading import Event
+from threading import Lock
 
 class FizzBuzz:
     def __init__(self, n: int):
         self.n = n
-        self.event = Event()
-        self.count = 1
+        self.f = Lock()
+        self.b = Lock()
+        self.fb = Lock()
+        self.num = Lock()
+        
+        self.f.acquire()
+        self.b.acquire()
+        self.fb.acquire()
 
     # printFizz() outputs "fizz"
     def fizz(self, printFizz: 'Callable[[], None]') -> None:
-        while self.count <= self.n:
-            if self.count % 3 == 0 and self.count % 5 != 0:
-                printFizz()
-                self.count += 1
-                self.event.set()
-            else:
-                self.event.clear()
-                self.event.wait()
+        while True:
+            self.f.acquire()
+            if self.n == 0:
+                break
+            printFizz()
+            self.num.release()
 
     # printBuzz() outputs "buzz"
     def buzz(self, printBuzz: 'Callable[[], None]') -> None:
-        while self.count <= self.n:
-            if self.count % 3 != 0 and self.count % 5 == 0:
-                printBuzz()
-                self.count += 1
-                self.event.set()
-            else:
-                self.event.clear()
-                self.event.wait()
+        while True:
+            self.b.acquire()
+            if self.n == 0:
+                break
+            printBuzz()
+            self.num.release()
 
     # printFizzBuzz() outputs "fizzbuzz"
     def fizzbuzz(self, printFizzBuzz: 'Callable[[], None]') -> None:
-        while self.count <= self.n:
-            if self.count % 3 == 0 and self.count % 5 == 0:
-                printFizzBuzz()
-                self.count += 1
-                self.event.set()
-            else:
-                self.event.clear()
-                self.event.wait()
+        while True:
+            self.fb.acquire()
+            if self.n == 0:
+                break
+            printFizzBuzz()
+            self.num.release()
 
     # printNumber(x) outputs "x", where x is an integer.
     def number(self, printNumber: 'Callable[[int], None]') -> None:
-        while self.count <= self.n:
-            if self.count % 3 != 0 and self.count % 5 != 0:
-                printNumber(self.count)
-                self.count += 1
-                self.event.set()
+        for i in range(1, self.n + 1):
+            self.num.acquire()
+            if i % 3 == 0 and i % 5 == 0:
+                self.fb.release()
+            elif i % 3 == 0:
+                self.f.release()
+            elif i % 5 == 0:
+                self.b.release()
             else:
-                self.event.clear()
-                self.event.wait()
+                printNumber(i)
+                self.num.release()
+            
+        self.num.acquire()
+        self.n = 0
+        self.fb.release()
+        self.b.release()
+        self.f.release()
