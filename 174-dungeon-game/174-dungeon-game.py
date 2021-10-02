7fe0452be9cd2f5b51aa5a004db1df7fbbf9cf0d
Adnan Yaqoob <adnan@yaqoob.org>
2021-10-02 18:05:11 +0800

    Time: 70 ms (80.85%), Space: 15.2 MB (71.93%) - LeetHub

--- 174-dungeon-game/174-dungeon-game.py
+++ 174-dungeon-game/174-dungeon-game.py
@@ -1,19 +1,16 @@
 class Solution:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
-        @cache
-        def recurse(i: int, j: int) -> int:
-            if i > n or j > m:
-                return float('inf')
-            
-            if i == n and j == m:
-                return -dungeon[i][j] + 1 if dungeon[i][j] < 0 else 1
-            
-            health = min(
-                recurse(i + 1, j),
-                recurse(i, j + 1)
-            ) - dungeon[i][j]
-            
-            return 1 if health <= 0 else health 
-        
         n, m = len(dungeon) - 1, len(dungeon[0]) - 1
-        return recurse(0, 0)
+        dp = [[float('inf')] * (m + 2) for _ in range(n + 2)]
+        
+        for i in range(n, -1, -1):
+            for j in range(m, -1, -1):
+                if i == n and j == m:
+                    dp[i][j] = -dungeon[i][j] + 1 if dungeon[i][j] < 0 else 1
+                else:
+                    health = min(
+                        dp[i + 1][j],
+                        dp[i][j + 1]
+                    ) - dungeon[i][j]
+                    dp[i][j] = 1 if health <= 0 else health
+        return dp[0][0]
