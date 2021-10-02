072b33d531bc33a154814bbff08664d88fe7c579
Adnan Yaqoob <adnan@yaqoob.org>
2021-10-02 18:11:13 +0800

    Time: 68 ms (92.14%), Space: 15.2 MB (71.93%) - LeetHub

--- 174-dungeon-game/174-dungeon-game.py
+++ 174-dungeon-game/174-dungeon-game.py
@@ -1,16 +1,18 @@
 class Solution:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
         n, m = len(dungeon) - 1, len(dungeon[0]) - 1
-        dp = [[float('inf')] * (m + 2) for _ in range(n + 2)]
-        
+
+        prev_row = [float('inf')] * (m + 2)
+        curr_row = [float('inf')] * (m + 2)
         for i in range(n, -1, -1):
             for j in range(m, -1, -1):
                 if i == n and j == m:
-                    dp[i][j] = -dungeon[i][j] + 1 if dungeon[i][j] < 0 else 1
+                    curr_row[j] = -dungeon[i][j] + 1 if dungeon[i][j] < 0 else 1
                 else:
                     health = min(
-                        dp[i + 1][j],
-                        dp[i][j + 1]
+                        prev_row[j],
+                        curr_row[j + 1]
                     ) - dungeon[i][j]
-                    dp[i][j] = 1 if health <= 0 else health
-        return dp[0][0]
+                    curr_row[j] = 1 if health <= 0 else health
+            prev_row, curr_row = curr_row, prev_row
+        return prev_row[0]
