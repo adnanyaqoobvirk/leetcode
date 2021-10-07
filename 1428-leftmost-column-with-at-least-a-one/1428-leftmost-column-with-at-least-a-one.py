9408b31adda2dedbe13f329786a454c6616e77de
Adnan Yaqoob <adnan@yaqoob.org>
2021-10-07 18:35:37 +0800

    Time: 100 ms (90.91%), Space: 14.6 MB (31.11%) - LeetHub

--- 1428-leftmost-column-with-at-least-a-one/1428-leftmost-column-with-at-least-a-one.py
+++ 1428-leftmost-column-with-at-least-a-one/1428-leftmost-column-with-at-least-a-one.py
@@ -11,6 +11,13 @@ class Solution:
         n, m = binaryMatrix.dimensions()
         j = m - 1
         for i in range(n):
-            while j >= 0 and binaryMatrix.get(i, j):
-                j -= 1
+            if j >= 0 and binaryMatrix.get(i, j):
+                left, right = 0, j + 1
+                while left < right:
+                    mid = left + (right - left) // 2
+                    if binaryMatrix.get(i, mid):
+                        right = mid
+                    else:
+                        left = mid + 1
+                j = left - 1
         return j + 1 if j != m - 1 else -1
