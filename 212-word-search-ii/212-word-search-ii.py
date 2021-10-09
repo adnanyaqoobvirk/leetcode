b2fd3c3567cdc90e9e2f46c4cf78e176b394606a
Adnan Yaqoob <adnan@yaqoob.org>
2021-10-09 19:11:32 +0800

    Time: 440 ms (79.94%), Space: 14.5 MB (65.56%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.py
+++ 212-word-search-ii/212-word-search-ii.py
@@ -4,14 +4,17 @@ class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         def backtrack(i: int, j: int, t: Any, w: List[str]):
             if 0 <= i < n and 0 <= j < m and board[i][j] in t[0]:
-                w.append(board[i][j])
-                nt = t[0][board[i][j]]
-                if nt[1]:
-                    ans.add("".join(w[::]))
+                l = board[i][j]
+                w.append(l)
                 board[i][j] = '#'
                 for x, y in [(i + 1, j), (i, j + 1), (i - 1, j), (i, j - 1)]:
-                    backtrack(x, y, nt, w)
-                board[i][j] = w.pop()
+                    backtrack(x, y, t[0][l], w)
+                board[i][j] = l
+                if t[0][l][1]:
+                    ans.add("".join(w[::]))
+                    if not t[0][l][0]:
+                        del t[0][l]
+                w.pop()
         
         trie = [{}, False]
         for word in words:
