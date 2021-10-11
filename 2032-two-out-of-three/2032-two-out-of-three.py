b8ecbca4bf224fa3c2f84a941108f5809658d480
Adnan Yaqoob <adnan@yaqoob.org>
2021-10-11 15:23:35 +0800

    Time: 99 ms (12.50%), Space: 14.3 MB (100.00%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.py
+++ 2032-two-out-of-three/2032-two-out-of-three.py
@@ -1,4 +1,14 @@
 class Solution:
     def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
-        s1, s2, s3 = set(nums1), set(nums2), set(nums3)
-        return s1 & (s2 | s3) | s2 & s3
+        freq = {}
+        ans = []
+        for nums in [nums1, nums2, nums3]:
+            nums = set(nums)
+            for num in nums:
+                count = freq.get(num, 0)
+                if count == 1:
+                    ans.append(num)
+                    freq[num] = float('-inf')
+                else:
+                    freq[num] = count + 1
+        return ans
