133d6742718a0d469c3c38caa47564ce3a314a0f
Adnan Yaqoob <adnan@yaqoob.org>
2021-10-11 17:48:12 +0800

    Time: 2002 ms (14.16%), Space: 14.2 MB (97.76%) - LeetHub

---
+++ 1395-count-number-of-teams/1395-count-number-of-teams.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numTeams(self, rating: List[int]) -> int:
+        ans = 0
+        for i in range(len(rating)):
+            less, more = [0, 0], [0, 0]
+            for j in range(len(rating)):
+                if rating[j] < rating[i]:
+                    less[j > i] += 1
+                if rating[j] > rating[i]:
+                    more[j > i] += 1
+            ans += less[0] * more[1] + less[1] * more[0]
+        return ans
