4856c760dcaabc7e43679aac7d1537197bd05141
Adnan Yaqoob <adnan@yaqoob.org>
2021-10-19 20:36:07 +0800

    Time: 591 ms (30.38%), Space: 15 MB (54.99%) - LeetHub

--- 302-smallest-rectangle-enclosing-black-pixels/302-smallest-rectangle-enclosing-black-pixels.py
+++ 302-smallest-rectangle-enclosing-black-pixels/302-smallest-rectangle-enclosing-black-pixels.py
@@ -1,11 +1,28 @@
 class Solution:
     def minArea(self, image: List[List[str]], x: int, y: int) -> int:
         n, m = len(image), len(image[0])
+        
         mini, maxi = n - 1, 0
         minj, maxj = m - 1, 0
-        for i in range(n):
-            for j in range(m):
-                if image[i][j] == '1':
-                    mini, maxi = min(mini, i), max(maxi, i)
-                    minj, maxj = min(minj, j), max(maxj, j)
+        
+        stack = [(x, y)]
+        while stack:
+            i, j = stack.pop()
+            
+            mini, maxi = min(mini, i), max(maxi, i)
+            minj, maxj = min(minj, j), max(maxj, j)
+            image[i][j] = "0"
+            
+            if i - 1 >= 0 and image[i - 1][j] == '1':
+                stack.append((i - 1, j))
+            
+            if i + 1 < n and image[i + 1][j] == '1':
+                stack.append((i + 1, j))
+            
+            if j - 1 >= 0 and image[i][j - 1] == '1':
+                stack.append((i, j - 1))
+            
+            if j + 1 < m and image[i][j + 1] == '1':
+                stack.append((i, j + 1))
+            
         return (maxi - mini + 1) * (maxj - minj + 1)
