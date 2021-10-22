83df52734fb985cb9e7ec352ce84e196c2c20ab6
Adnan Yaqoob <adnan@yaqoob.org>
2021-10-22 16:48:55 +0800

    Time: 132 ms (5.13%), Space: 20.8 MB (10.92%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
@@ -1,6 +1,17 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
+        freq = defaultdict(int)
+        max_freq = 0
+        for c in s:
+            freq[c] += 1
+            max_freq = max(max_freq, freq[c])
+        
+        buckets = [[] for _ in range(max_freq)]
+        for c, i in freq.items():
+            buckets[i - 1].append(c)
+        
         ans = []
-        for c, freq in sorted(Counter(s).items(), key = lambda t: t[1], reverse = True):
-            ans.extend([c] * freq)
+        for i in reversed(range(1, max_freq + 1)):
+            for c in buckets[i - 1]:
+                ans.extend([c] * i)
         return "".join(ans)
