df20fc3a446448a1cfaa0487bc72a7d1bf3e463d
Adnan Yaqoob <adnan@yaqoob.org>
2021-11-06 11:11:19 +0800

    Time: 56 ms (83.18%), Space: 16.2 MB (29.00%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.py
+++ 137-single-number-ii/137-single-number-ii.py
@@ -1,9 +1,3 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        seen = defaultdict(int)
-        for num in nums:
-            if seen[num] == 2:
-                del seen[num]
-            else:
-                seen[num] += 1
-        return next(iter(seen))
+        return (3 * sum(set(nums)) - sum(nums)) // 2
