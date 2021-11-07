ba3b2d52bae59cb2ed5cf11e2a9841b34daa5bbd
Adnan Yaqoob <adnan@yaqoob.org>
2021-11-07 18:44:27 +0800

    Time: 232 ms (78.12%), Space: 15.6 MB (73.47%) - LeetHub

--- 1231-divide-chocolate/1231-divide-chocolate.py
+++ 1231-divide-chocolate/1231-divide-chocolate.py
@@ -1,21 +1,19 @@
 class Solution:
     def maximizeSweetness(self, sweetness: List[int], k: int) -> int:
-        def valid_plan(plan: int) -> bool:
-            run_sum = cuts = 0
-            for sweet in sweetness:
-                run_sum += sweet
-                if run_sum >= plan:
-                    cuts += 1
-                    run_sum = 0
-            if run_sum >= plan:
-                cuts += 1
-            return cuts >= k + 1
-    
         left, right = min(sweetness), sum(sweetness) // (k + 1)
         while left <= right:
             mid = left + (right - left >> 1)
             
-            if valid_plan(mid):
+            run_sum = cuts = 0
+            for sweet in sweetness:
+                run_sum += sweet
+                if run_sum >= mid:
+                    cuts += 1
+                    run_sum = 0
+            if run_sum >= mid:
+                cuts += 1
+                
+            if cuts >= k + 1:
                 left = mid + 1
             else:
                 right = mid - 1
