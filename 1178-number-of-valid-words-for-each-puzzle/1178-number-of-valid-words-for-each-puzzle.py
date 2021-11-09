02749a945f184a86b0eadfc9d5b8d1f3be33fb63
Adnan Yaqoob <adnan@yaqoob.org>
2021-11-09 17:39:33 +0800

    Time: 936 ms (46.23%), Space: 51.9 MB (48.11%) - LeetHub

--- 1178-number-of-valid-words-for-each-puzzle/1178-number-of-valid-words-for-each-puzzle.py
+++ 1178-number-of-valid-words-for-each-puzzle/1178-number-of-valid-words-for-each-puzzle.py
@@ -1,23 +1,24 @@
 class Solution:
     def findNumOfValidWords(self, words: List[str], puzzles: List[str]) -> List[int]:
-        ans = []
-        wmasks = defaultdict(int)
-        for word in words:
-            wmask = 0
-            for c in word:
-                wmask |= (1 << ord(c) - ord('a'))
-            wmasks[wmask] += 1
-        
-        for puzzle in puzzles:
-            pmask = 0
-            for c in puzzle[1:]:
-                pmask |= (1 << ord(c) - ord('a'))
+        def helper(t: Dict[str, int], has_first: int) -> int:
+            valids = t['#'] if has_first else 0
+            for c in puzzle:
+                if c in t:
+                    valids += helper(t[c], has_first or c == puzzle[0])
+            return valids
             
-            fmask = 1 << ord(puzzle[0]) - ord('a')
-            valids = wmasks[fmask]
-            subset = pmask
-            while subset:
-                valids += wmasks[subset | fmask]
-                subset = (subset - 1) & pmask
-            ans.append(valids)
+        trie = {'#': 0}
+        for word in words:
+            word = set(word)
+            if len(word) <= 7:
+                t = trie
+                for c in sorted(word):
+                    if c not in t:
+                        t[c] = {"#": 0}
+                    t = t[c]
+                t['#'] += 1
+                
+        ans = []
+        for puzzle in puzzles:
+            ans.append(helper(trie, False))
         return ans
