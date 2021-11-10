ba0aeade162d0feee9d2f641125ea6d2042fb012
Adnan Yaqoob <adnan@yaqoob.org>
2021-11-10 15:45:00 +0800

    Time: 32 ms (63.25%), Space: 14.3 MB (44.50%) - LeetHub

--- 266-palindrome-permutation/266-palindrome-permutation.py
+++ 266-palindrome-permutation/266-palindrome-permutation.py
@@ -1,11 +1,10 @@
 class Solution:
     def canPermutePalindrome(self, s: str) -> bool:
-        odds = 0
-        for c, count in Counter(s).items():
-            if count & 1:
-                odds += 1
-            
-            if odds > 1:
-                return False
-        return True
+        seen = set()
+        for c in s:
+            if c in seen:
+                seen.remove(c)
+            else:
+                seen.add(c)
+        return len(seen) <= 1
                 
