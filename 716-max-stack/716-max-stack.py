6c3f7eb4d555e6358abce1a13244d635bb61368b
Adnan Yaqoob <adnan@yaqoob.org>
2021-11-12 10:36:04 +0800

    Time: 136 ms (92.90%), Space: 16.6 MB (74.92%) - LeetHub

--- 716-max-stack/716-max-stack.py
+++ 716-max-stack/716-max-stack.py
@@ -2,26 +2,31 @@ class MaxStack:
 
     def __init__(self):
         self.stack = []
+        self.max = float('-inf')
 
     def push(self, x: int) -> None:
         self.stack.append(x)
+        if x > self.max:
+            self.max = x
 
     def pop(self) -> int:
-        return self.stack.pop()
+        v = self.stack.pop()
+        if v == self.max:
+            self.max = max(self.stack) if self.stack else float('-inf')
+        return v
 
     def top(self) -> int:
         return self.stack[-1]
 
     def peekMax(self) -> int:
-        return max(self.stack)
+        return self.max
 
     def popMax(self) -> int:
-        maximum = self.peekMax()
         for i in reversed(range(len(self.stack))):
-            if self.stack[i] == maximum:
-                self.stack.pop(i)
-                break
-        return maximum
+            if self.stack[i] == self.max:
+                v = self.stack.pop(i)
+                self.max = max(self.stack) if self.stack else float('-inf')
+                return v
 
 
 # Your MaxStack object will be instantiated and called as such:
