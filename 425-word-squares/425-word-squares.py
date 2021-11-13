2a1760bafa512059addb61bae2826abc6fc3da47
Adnan Yaqoob <adnan@yaqoob.org>
2021-11-13 19:39:57 +0800

    Time: 468 ms (53.00%), Space: 17.9 MB (36.91%) - LeetHub

--- 425-word-squares/425-word-squares.py
+++ 425-word-squares/425-word-squares.py
@@ -1,23 +1,35 @@
 class Solution:
     def wordSquares(self, words: List[str]) -> List[List[str]]:
+        def getWords(p: str) -> List[str]:
+            if not p:
+                return words
+            
+            t = prefixes
+            for c in p:
+                if c not in t:
+                    return []
+                t = t[c]
+            return [words[i] for i in t['#']]
+        
         def backtrack() -> None:
             sn = len(square)
             if sn == n:
                 ans.append(square[::])
             else:
                 prefix = "".join([word[sn] for word in square])
-                for word in prefixes[prefix]:
+                for word in getWords(prefix):
                     square.append(word)
                     backtrack()
                     square.pop()
         
-        prefixes = defaultdict(list)
-        prefixes[""] = words
-        for word in words:
-            prefix = ""
+        prefixes = {"#": []}
+        for i, word in enumerate(words):
+            p = prefixes
             for c in word:
-                prefix += c
-                prefixes[prefix].append(word)
+                if c not in p:
+                    p[c] = {'#': []}
+                p = p[c]
+                p['#'].append(i)
                 
         n = len(words[0])
         ans = []
