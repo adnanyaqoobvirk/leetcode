1704589563bf8d933f5504312a63e88e7d06e958
Adnan Yaqoob <adnan@yaqoob.org>
2021-11-14 17:51:49 +0800

    Time: 192 ms (40.89%), Space: 17.7 MB (6.94%) - LeetHub

--- 716-max-stack/716-max-stack.py
+++ 716-max-stack/716-max-stack.py
@@ -1,36 +1,49 @@
+from sortedcontainers import SortedDict
+
+class Node:
+    def __init__(self, val: int = 0, prev: 'Node' = None, next: 'Node' = None) -> None:
+        self.val = val
+        self.prev = prev
+        self.next = next
+
 class MaxStack:
 
     def __init__(self):
-        self.stack = []
-        self.max, self.maxi = float('-inf'), None
+        self.map = SortedDict()
+        self.tail = Node(float('inf'))
         
-    def setMax(self) -> None:
-        self.max, self.maxi = float('-inf'), None
-        for i in reversed(range(len(self.stack))):
-            if self.stack[i] > self.max:
-                self.max, self.maxi = self.stack[i], i
-
     def push(self, x: int) -> None:
-        self.stack.append(x)
-        if x >= self.max:
-            self.max, self.maxi = x, len(self.stack) - 1
+        self.tail.next = Node(x, self.tail)
+        self.tail = self.tail.next
+        self.map.setdefault(x, []).append(self.tail)
 
     def pop(self) -> int:
-        v = self.stack.pop()
-        if v == self.max:
-            self.setMax()
+        v = self.tail.val
+        l = self.map[v]
+        l.pop()
+        if not l:
+            self.map.pop(v)
+        self.tail, self.tail.next = self.tail.prev, None
         return v
 
     def top(self) -> int:
-        return self.stack[-1]
-
+        return self.tail.val
+    
     def peekMax(self) -> int:
-        return self.max
-
+        return self.map.peekitem()[1][-1].val
+    
     def popMax(self) -> int:
-        v = self.stack.pop(self.maxi)
-        self.setMax()
-        return v
+        l = self.map.peekitem()[1]
+        node = l[-1]
+        if node == self.tail:
+            self.tail = node.prev
+        else:
+            node.next.prev = node.prev
+        node.prev.next, node.next, node.prev = node.next, None, None
+        l.pop()
+        if not l:
+            self.map.popitem()
+        return node.val
 
 
 # Your MaxStack object will be instantiated and called as such:
