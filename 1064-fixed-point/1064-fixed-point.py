4c9e2d1eba4079a8c467924085c3a4b670b25751
Adnan Yaqoob <adnan@yaqoob.org>
2021-11-15 18:23:41 +0800

    Time: 64 ms (66.94%), Space: 15.4 MB (49.59%) - LeetHub

--- 1064-fixed-point/1064-fixed-point.py
+++ 1064-fixed-point/1064-fixed-point.py
@@ -1,6 +1,10 @@
 class Solution:
     def fixedPoint(self, arr: List[int]) -> int:
-        for i, num in enumerate(arr):
-            if num == i:
-                return i
-        return -1
+        left, right = 0, len(arr) - 1
+        while left < right:
+            mid = left + (right - left) // 2
+            if arr[mid] >= mid:
+                right = mid
+            else:
+                left = mid + 1
+        return left if arr[left] == left else -1
