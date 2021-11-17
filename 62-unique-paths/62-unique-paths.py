dfa7a79f4ddb041e04596d594304e12052dbed14
Adnan Yaqoob <adnan@yaqoob.org>
2021-11-17 15:42:44 +0800

    Time: 36 ms (35.38%), Space: 14.9 MB (5.19%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,12 +1,10 @@
 class Solution:
+    @cache
     def uniquePaths(self, m: int, n: int) -> int:
-        @cache
-        def helper(x: int, y: int) -> int:
-            if x >= m or y >= n:
-                return 0
-            
-            if x == m - 1 and y == n - 1:
-                return 1
-            
-            return helper(x + 1, y) + helper(x, y + 1)
-        return helper(0, 0)
+        if m <= 0 or n <= 0:
+            return 0
+
+        if m == 1 and n == 1:
+            return 1
+
+        return self.uniquePaths(m - 1, n) + self.uniquePaths(m, n - 1)
