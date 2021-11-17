2120585a90364422908f12f496c4ff26278a2100
Adnan Yaqoob <adnan@yaqoob.org>
2021-11-17 16:44:39 +0800

    Time: 36 ms (41.51%), Space: 14.4 MB (25.94%) - LeetHub

--- 1427-perform-string-shifts/1427-perform-string-shifts.py
+++ 1427-perform-string-shifts/1427-perform-string-shifts.py
@@ -1,13 +1,5 @@
 class Solution:
     def stringShift(self, s: str, shift: List[List[int]]) -> str:
-        q = deque(s)
-        
-        ops = sum(amount if direction else -amount for direction, amount in shift)
-        if ops > 0:
-            for _ in range(ops):
-                q.appendleft(q.pop())
-        else:
-            for _ in range(-ops):
-                q.append(q.popleft())
-                
-        return "".join(q)
+        ops = sum(-amount if direction else amount for direction, amount in shift)
+        ops %= len(s)
+        return s[ops:] + s[:ops]
