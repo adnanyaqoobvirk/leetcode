728a3c158f27bf5e0fc9f1b7b5e6e572ce8eee7b
Adnan Yaqoob <adnan@yaqoob.org>
2021-11-17 15:44:48 +0800

    Time: 28 ms (88.36%), Space: 14.3 MB (67.53%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,10 +1,16 @@
 class Solution:
-    @cache
+    cache = {}
+    
     def uniquePaths(self, m: int, n: int) -> int:
+        key = (m, n)
+        if key in Solution.cache:
+            return Solution.cache[key]
+        
         if m <= 0 or n <= 0:
             return 0
 
         if m == 1 and n == 1:
             return 1
 
-        return self.uniquePaths(m - 1, n) + self.uniquePaths(m, n - 1)
+        Solution.cache[key] = self.uniquePaths(m - 1, n) + self.uniquePaths(m, n - 1)
+        return Solution.cache[key]
