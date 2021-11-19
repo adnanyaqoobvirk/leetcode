f7f64b6a43f2bb390ba719fd00be6531296c7e94
Adnan Yaqoob <adnan@yaqoob.org>
2021-11-20 07:09:26 +0800

    Time: 48 ms (68.89%), Space: 16.1 MB (6.41%) - LeetHub

--- 1522-diameter-of-n-ary-tree/1522-diameter-of-n-ary-tree.py
+++ 1522-diameter-of-n-ary-tree/1522-diameter-of-n-ary-tree.py
@@ -7,22 +7,25 @@ class Node:
 """
 
 class Solution:
-    def dfs(self, current: int) -> int:
-        if not current:
-            return 0
-
-        first = second = 0
-        for child in current.children:
-            height = self.dfs(child)
-            if height > first:
-                second, first = first, height
-            elif height > second:
-                second = height
-                
-        self.dia = max(self.dia, first + second)
-        return 1 + first
-    
     def diameter(self, root: 'Node') -> int:
-        self.dia = 0
-        self.dfs(root)
-        return self.dia
+        def helper(current: 'Node') -> int:
+            nonlocal dia
+            
+            if not current:
+                return 0
+            
+            first_max = second_max = 0
+            for node in current.children:
+                curr = helper(node)
+                if curr > first_max:
+                    second_max, first_max = first_max, curr
+                elif curr > second_max:
+                    second_max = curr
+
+            dia = max(dia, first_max + second_max)
+
+            return 1 + first_max
+        
+        dia = 0
+        helper(root)
+        return dia
