483b75d1330b979cc523c7d625a8e62553281f45
Adnan Yaqoob <adnan@yaqoob.org>
2021-11-19 19:25:31 +0800

    Time: 740 ms (95.12%), Space: 37.3 MB (55.40%) - LeetHub

--- 245-shortest-word-distance-iii/245-shortest-word-distance-iii.py
+++ 245-shortest-word-distance-iii/245-shortest-word-distance-iii.py
@@ -1,30 +1,11 @@
 class Solution:
     def shortestWordDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
         w1hash, w2hash = hash(word1), hash(word2)
-        w1loc = []
-        w2loc = []
-        for i, whash in enumerate(map(hash, wordsDict)):
-            if whash == w1hash:
-                w1loc.append(i)
-            elif whash == w2hash:
-                w2loc.append(i)
-        
-        p0 = p1 = 0
+        p = phash = None
         distance = float('inf')
-        
-        if w1hash == w2hash:
-            w2loc = w1loc
-            p1 = 1
-        
-        ll0, ll1 = len(w1loc), len(w2loc)
-        while p0 < ll0 and p1 < ll1:
-            distance = min(abs(w1loc[p0] - w2loc[p1]), distance)
-            
-            if w1hash != w2hash:
-                if w1loc[p0] < w2loc[p1]:
-                    p0 += 1
-                else:
-                    p1 += 1
-            else:
-                p0, p1 = p1, p1 + 1
+        for i, whash in enumerate(map(hash, wordsDict)):
+            if whash == w1hash or whash == w2hash:
+                if p is not None and (w1hash == w2hash or whash != phash):
+                    distance = min(abs(i - p), distance)
+                p, phash = i, whash
         return distance
