c8373ae6e32526349716a202394a432c40195df8
Adnan Yaqoob <adnan@yaqoob.org>
2021-11-21 12:10:57 +0800

    Time: 2307 ms (100.00%), Space: 53.3 MB (100.00%) - LeetHub

---
+++ 2080-range-frequency-queries/2080-range-frequency-queries.py
@@ -0,0 +1,19 @@
+from bisect import bisect_left, bisect_right
+
+class RangeFreqQuery:
+
+    def __init__(self, arr: List[int]):
+        self.ftable = defaultdict(list)
+        for i, num in enumerate(arr):
+            self.ftable[num].append(i)
+    
+    def query(self, left: int, right: int, value: int) -> int:
+        positions = self.ftable[value]
+        if not positions:
+            return 0
+        return bisect_right(positions, right) - bisect_left(positions, left)
+
+
+# Your RangeFreqQuery object will be instantiated and called as such:
+# obj = RangeFreqQuery(arr)
+# param_1 = obj.query(left,right,value)
