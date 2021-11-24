78c1b7b540839861bd56b9977ce843517298056a
Adnan Yaqoob <adnan@yaqoob.org>
2021-11-24 11:43:18 +0800

    Time: 237 ms (12.94%), Space: 15.1 MB (39.75%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.py
+++ 986-interval-list-intersections/986-interval-list-intersections.py
@@ -1,18 +1,25 @@
 class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
-        firstList.extend(secondList)
-        firstList.sort()
-    
+        n, m = len(firstList), len(secondList)
         ans = []
-        prev = 0
-        for i in range(1, len(firstList)):
-            pstart, pend = firstList[prev]
-            cstart, cend = firstList[i]
-            if pstart <= cstart <= pend:
-                if cend <= pend:
-                    ans.append([cstart, cend])
-                else:
-                    ans.append([cstart, pend])
-            if cend >= pend:
-                prev = i
+        p1 = p2 = 0
+        while p1 < n and p2 < m:
+            p1start, p1end = firstList[p1]
+            p2start, p2end = secondList[p2]
+            if p1start <= p2start:
+                if p1start <= p2start <= p1end:
+                    if p2end <= p1end:
+                        ans.append([p2start, p2end])
+                    else:
+                        ans.append([p2start, p1end])
+            else:
+                if p2start <= p1start <= p2end:
+                    if p1end <= p2end:
+                        ans.append([p1start, p1end])
+                    else:
+                        ans.append([p1start, p2end])
+            if p1end >= p2end:
+                p2 += 1
+            else:
+                p1 += 1
         return ans
