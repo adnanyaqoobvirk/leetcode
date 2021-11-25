96c9bbcf5d736adc764aaf42e1600b13eae09a7f
Adnan Yaqoob <adnan@yaqoob.org>
2021-11-25 19:55:33 +0800

    Time: 110 ms (27.96%), Space: 15.4 MB (88.74%) - LeetHub

---
+++ 261-graph-valid-tree/261-graph-valid-tree.py
@@ -0,0 +1,34 @@
+class DisjointSet:
+    def __init__(self: Any, n: int) -> None:
+        self.nodes = [-1 for i in range(n)]
+        self.roots = n
+        
+    def find(self, x) -> int:
+        if self.nodes[x] >= 0:
+            self.nodes[x] = self.find(self.nodes[x])
+            return self.nodes[x]
+        else:
+            return x
+    
+    def union(self, x, y) -> None:
+        xp, yp = self.find(x), self.find(y)
+        if xp != yp:
+            if self.nodes[xp] < self.nodes[yp]:
+                self.nodes[yp] = xp
+            elif self.nodes[xp] > self.nodes[yp]:
+                self.nodes[xp] = yp
+            else:
+                self.nodes[xp] = yp
+                self.nodes[yp] -= 1
+            self.roots -= 1
+
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) > n - 1:
+            return False
+        
+        ds = DisjointSet(n)
+        for i, j in edges:
+            ds.union(i, j)
+        
+        return ds.roots == 1
