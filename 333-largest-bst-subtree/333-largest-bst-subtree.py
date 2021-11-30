fdcd8c55cb4b9e45953d24abb541f3d68fd1ff6a
Adnan Yaqoob <adnan@yaqoob.org>
2021-11-30 16:44:47 +0800

    Time: 48 ms (77.54%), Space: 17.5 MB (6.26%) - LeetHub

--- 333-largest-bst-subtree/333-largest-bst-subtree.py
+++ 333-largest-bst-subtree/333-largest-bst-subtree.py
@@ -6,23 +6,32 @@
 #         self.right = right
 class Solution:
     def largestBSTSubtree(self, root: Optional[TreeNode]) -> int:
-        def helper(curr: Optional[TreeNode]) -> int:
-            if not curr:
-                return 0, float('inf'), float('-inf') 
-            
-            left, left_min, left_max = helper(curr.left)
-            right, right_min, right_max = helper(curr.right)
+        if not root:
+            return 0
         
-            if left == -1 or right == -1 or curr.val <= left_max or curr.val >= right_min:
-                nodes = -1
+        null = [0, float('inf'), float('-inf')]
+        ans, stack, done = 0, [root], {}
+        while stack:
+            curr = stack[-1]
+
+            if curr not in done:
+                if curr.right:
+                    stack.append(curr.right)
+
+                if curr.left:
+                    stack.append(curr.left)
+                done[curr] = null
             else:
-                nodes = 1 + left + right
-                nonlocal ans
-                ans = max(ans, nodes)
-            
-            return nodes, min(curr.val, left_min, right_min), max(curr.val, left_max, right_max)
-        
-        ans = 0
-        helper(root)
+                stack.pop()
+                
+                left, left_min, left_max = done[curr.left] if curr.left else null
+                right, right_min, right_max = done[curr.right] if curr.right else null
+
+                if left == -1 or right == -1 or curr.val <= left_max or curr.val >= right_min:
+                    nodes = -1
+                else:
+                    nodes = 1 + left + right
+                    ans = max(ans, nodes)
+
+                done[curr] = [nodes, min(curr.val, left_min, right_min), max(curr.val, left_max, right_max)]
         return ans
-        
