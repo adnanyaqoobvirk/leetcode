1c6cfc23ffba311a2068f58ecbc603a341c8cf4d
Adnan Yaqoob <adnan@yaqoob.org>
2021-11-30 22:02:20 +0800

    Time: 424 ms (20.44%), Space: 15.6 MB (44.58%) - LeetHub

--- 85-maximal-rectangle/85-maximal-rectangle.py
+++ 85-maximal-rectangle/85-maximal-rectangle.py
@@ -2,21 +2,16 @@ class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
         n, m = len(matrix), 0 if len(matrix) == 0 else len(matrix[0])
         
+        ans = 0
         dp = [[0] * m for _ in range(n)]
         for i in range(n):
             for j in range(m):
                 if matrix[i][j] == '1':
                     dp[i][j] = dp[i][j - 1] + 1
-        
-        ans = 0
-        for k in reversed(range(n)):
-            for j in reversed(range(m)):
-                col_min = 200
-                for i in reversed(range(n - k)):
-                    col_min = min(dp[i][j], col_min)
-                    if col_min == 0:
-                        break
-                    ans = max(ans, col_min * (n - k - i))
+                    
+                    minarea = float('inf')
+                    for k in reversed(range(i + 1)):
+                        minarea = min(dp[k][j], minarea)
+                        if minarea == 0: break
+                        ans = max(ans, minarea * (i - k + 1))
         return ans
-                    
-                    
