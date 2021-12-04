adbf66846815912f04a74eb0486081bf71666ac4
Adnan Yaqoob <adnan@yaqoob.org>
2021-12-04 18:35:14 +0800

    Time: 56 ms (88.20%), Space: 14.3 MB (58.15%) - LeetHub

--- 256-paint-house/256-paint-house.py
+++ 256-paint-house/256-paint-house.py
@@ -1,12 +1,12 @@
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:    
-        n = len(costs)
-        dp = [[0] * 3 for _ in range(n + 1)]
-        for i in range(n):
+        prev, curr = [0] * 3, [0] * 3
+        for i in range(len(costs)):
             for j in range(3):
                 cost = float('inf')
                 for k in range(3):
                     if k != j:
-                        cost = min(cost, costs[i][k] + dp[i - 1][k])
-                dp[i][j] = cost
-        return min(dp[n - 1])
+                        cost = min(cost, costs[i][k] + prev[k])
+                curr[j] = cost
+            prev, curr = curr, prev
+        return min(prev)
