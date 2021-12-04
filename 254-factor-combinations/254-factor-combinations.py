05663f1ea0eaeb7953826738d82dc90fc07f227b
Adnan Yaqoob <adnan@yaqoob.org>
2021-12-04 17:37:08 +0800

    Time: 92 ms (86.27%), Space: 15.1 MB (52.31%) - LeetHub

--- 254-factor-combinations/254-factor-combinations.py
+++ 254-factor-combinations/254-factor-combinations.py
@@ -1,21 +1,13 @@
 class Solution:
     def getFactors(self, n: int) -> List[List[int]]:
-        def backtrack(pos: int, product: int) -> None:
+        def backtrack(start: int, product: int) -> None:
             if product > 1:
-                for i in range(pos, len(factors)):
-                    f = factors[i]
-                    if f <= int(math.sqrt(product)) and product % f == 0:
+                for f in range(start, int(math.sqrt(product)) + 1):
+                    if product % f == 0:
                         ans.append(comb + [f, product // f])
                         comb.append(f)
-                        backtrack(i, product // f)
+                        backtrack(f, product // f)
                         comb.pop()
-                    
-        factors = []
-        for f in range(2, int(math.sqrt(n)) + 1):
-            if n % f == 0:
-                factors.append(f)
-        
-        ans = []
-        comb = []
-        backtrack(0, n)
+        ans, comb = [], []
+        backtrack(2, n)
         return ans
