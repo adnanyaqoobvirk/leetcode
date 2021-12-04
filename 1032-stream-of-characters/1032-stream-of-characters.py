8c74cc6088eb2a4e0d1384862e252334924e3544
Adnan Yaqoob <adnan@yaqoob.org>
2021-12-04 10:49:45 +0800

    Time: 576 ms (83.23%), Space: 39.4 MB (40.57%) - LeetHub

--- 1032-stream-of-characters/1032-stream-of-characters.py
+++ 1032-stream-of-characters/1032-stream-of-characters.py
@@ -2,12 +2,10 @@ class StreamChecker:
 
     def __init__(self, words: List[str]):
         self.trie = {}
-        self.maxlen = 0
-        self.stream = deque()
+        self.stream = []
         
         for word in words:
             t = self.trie
-            self.maxlen = max(self.maxlen, len(word))
             for c in reversed(word):
                 if c not in t:
                     t[c] = {}
@@ -15,9 +13,6 @@ class StreamChecker:
             t['#'] = True
 
     def query(self, letter: str) -> bool:
-        if self.maxlen == len(self.stream):
-            self.stream.popleft()
-            
         self.stream.append(letter)
         
         t = self.trie
@@ -29,8 +24,3 @@ class StreamChecker:
             if '#' in t:
                 return True
         return False
-
-
-# Your StreamChecker object will be instantiated and called as such:
-# obj = StreamChecker(words)
-# param_1 = obj.query(letter)
