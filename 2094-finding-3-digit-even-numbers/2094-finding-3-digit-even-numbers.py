2b7a0481cd4252bca9c0655f7954e4c7d8aa9d60
Adnan Yaqoob <adnan@yaqoob.org>
2021-12-05 16:55:31 +0800

    Time: 48 ms (88.89%), Space: 14.4 MB (66.67%) - LeetHub

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.py
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.py
@@ -1,21 +1,9 @@
 class Solution:
     def findEvenNumbers(self, digits: List[int]) -> List[int]:
-        def get_digits(n: int) -> List[int]:
-            counts = [0] * 10
-            n = num
-            while n > 0:
-                n, r = divmod(n, 10)
-                counts[r] += 1
-            return counts
-                
-        counts = Counter(digits)
-        ans = []
-        for num in range(100, 1000):
-            if num % 2 == 0:
-                dcounts = get_digits(num)
-                for d in range(10):
-                    if counts[d] < dcounts[d]:
-                        break
-                else:
-                    ans.append(num)
+        ans, counts = [], Counter(digits)
+        for d1 in range(1, 10):
+            for d2 in range(10):
+                for d3 in range(0, 10, 2):
+                    if counts[d1] > 0 and counts[d2] > (d1 == d2) and counts[d3] > (d3 == d1) + (d3 == d2):
+                        ans.append(d1 * 100 + d2 * 10 + d3)
         return ans
