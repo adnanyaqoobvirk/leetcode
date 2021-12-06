cac454b454ee13ab1e20a5c563f5fbc146f8eae1
Adnan Yaqoob <adnan@yaqoob.org>
2021-12-06 18:39:44 +0800

    Time: 64 ms (72.30%), Space: 14.8 MB (27.92%) - LeetHub

--- 271-encode-and-decode-strings/271-encode-and-decode-strings.py
+++ 271-encode-and-decode-strings/271-encode-and-decode-strings.py
@@ -1,30 +1,18 @@
 class Codec:
-    def __init__(self):
-        self.map = {chr(i): f"a{i}" for i in range(256)}
-        self.rmap = {f"{i}": chr(i)  for i in range(256)}
-            
     def encode(self, strs: [str]) -> str:
         """Encodes a list of strings to a single string.
         """
         estr = []
         for s in strs:
-            for c in s:
-                estr.append(self.map[c])
-            estr.append('|')
+            estr.append(s)
+            estr.append(chr(257))
         estr.pop()
         return "".join(estr)
 
     def decode(self, s: str) -> [str]:
         """Decodes a single string to a list of strings.
         """
-        dstr = []
-        for ss in s.split('|'):
-            dss = []
-            for c in ss.split('a'):
-                if c:
-                    dss.append(self.rmap[c])
-            dstr.append("".join(dss))
-        return dstr
+        return s.split(chr(257))
 
 
 # Your Codec object will be instantiated and called as such:
