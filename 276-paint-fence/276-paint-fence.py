21b729baaa688b18dc173711d0cca3795fa82ce3
Adnan Yaqoob <adnan@yaqoob.org>
2021-12-07 11:20:05 +0800

    Time: 32 ms (57.48%), Space: 14.2 MB (83.95%) - LeetHub

--- 276-paint-fence/276-paint-fence.py
+++ 276-paint-fence/276-paint-fence.py
@@ -1,11 +1,9 @@
 class Solution:
     def numWays(self, n: int, k: int) -> int:
-        @cache
-        def helper(f: int) -> int:
-            if f == 1:
-                return k
-            elif f == 2:
-                return k * k
+        if n == 1:
+            return k
         
-            return (k - 1) * (helper(f - 1) + helper(f - 2))
-        return helper(n)
+        prev, curr = k, k * k
+        for i in range(3, n + 1):
+            prev, curr = curr, (k - 1) * (curr + prev)
+        return curr
