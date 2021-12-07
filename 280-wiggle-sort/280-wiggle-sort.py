75d67a1da1475ea55485fa7baebc09c950344a39
Adnan Yaqoob <adnan@yaqoob.org>
2021-12-07 20:00:06 +0800

    Time: 96 ms (54.87%), Space: 15.3 MB (20.51%) - LeetHub

--- 280-wiggle-sort/280-wiggle-sort.py
+++ 280-wiggle-sort/280-wiggle-sort.py
@@ -3,8 +3,6 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        less = True
         for i in range(len(nums) - 1):
-            if (less and nums[i] > nums[i + 1]) or (not less and nums[i] < nums[i + 1]):
+            if (not i & 1 and nums[i] > nums[i + 1]) or (i & 1 and nums[i] < nums[i + 1]):
                 nums[i], nums[i + 1] = nums[i + 1], nums[i]
-            less = not less
