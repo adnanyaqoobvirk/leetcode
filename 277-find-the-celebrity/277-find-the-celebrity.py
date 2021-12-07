302779d6b6a8cba3fd3782b880cf0db77008be0a
Adnan Yaqoob <adnan@yaqoob.org>
2021-12-07 17:19:21 +0800

    Time: 1560 ms (96.71%), Space: 14.7 MB (5.54%) - LeetHub

--- 277-find-the-celebrity/277-find-the-celebrity.py
+++ 277-find-the-celebrity/277-find-the-celebrity.py
@@ -4,14 +4,19 @@
 
 class Solution:
     def findCelebrity(self, n: int) -> int:
+        @cache
+        def helper(a: int, b: int) -> bool:
+            return knows(a, b)
+        
         p1, p2 = 0, 1
         while p1 < n and p2 < n:
-            if knows(p1, p2):
+            if helper(p1, p2):
                 p1 = max(p1, p2) + 1
             else:
                 p2 = max(p1, p2) + 1
+                
         p = min(p1, p2)
         for i in range(n):
-            if i != p and (knows(p, i) or not knows(i, p)):
+            if i != p and (helper(p, i) or not helper(i, p)):
                 return -1
         return p
