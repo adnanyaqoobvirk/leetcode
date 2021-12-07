ff76513a6876cfc1954f11813c7e75ff61a0f7e2
Adnan Yaqoob <adnan@yaqoob.org>
2021-12-07 20:02:26 +0800

    Time: 156 ms (10.26%), Space: 15.1 MB (51.03%) - LeetHub

--- 280-wiggle-sort/280-wiggle-sort.py
+++ 280-wiggle-sort/280-wiggle-sort.py
@@ -4,5 +4,5 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         for i in range(len(nums) - 1):
-            if (not i & 1 and nums[i] > nums[i + 1]) or (i & 1 and nums[i] < nums[i + 1]):
+            if (i & 1) ^ (nums[i] > nums[i + 1]):
                 nums[i], nums[i + 1] = nums[i + 1], nums[i]
