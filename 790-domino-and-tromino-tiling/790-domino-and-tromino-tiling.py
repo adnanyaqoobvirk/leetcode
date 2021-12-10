d2c14fe2a4b53f979dd52e81afb85226378f5cef
Adnan Yaqoob <adnan@yaqoob.org>
2021-12-10 17:46:05 +0800

    Time: 44 ms (36.67%), Space: 17.6 MB (5.90%) - LeetHub

--- 790-domino-and-tromino-tiling/790-domino-and-tromino-tiling.py
+++ 790-domino-and-tromino-tiling/790-domino-and-tromino-tiling.py
@@ -1,7 +1,17 @@
 class Solution:
     def numTilings(self, n: int) -> int:
-        if n == 1: return 1
-        mod, pg, cg, pu, cu = 10**9 + 7, 1, 2, 1, 2
-        for _ in range(3, n + 1):
-            pg, cg, pu, cu = cg, (cg + pg + 2 * pu) % mod, cu, (cu + cg) % mod
-        return cg
+        @cache
+        def domino(k: int) -> int:
+            if k <= 2:
+                return k
+            
+            return domino(k - 1) + domino(k - 2) + 2 * tromino(k - 2)
+        
+        @cache
+        def tromino(k: int) -> int:
+            if k <= 2:
+                return k
+            
+            return tromino(k - 1) + domino(k - 1)
+        
+        return domino(n) % (10**9 + 7)
