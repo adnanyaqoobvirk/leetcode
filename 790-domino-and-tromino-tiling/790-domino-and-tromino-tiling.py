897e3741d02a6d80fd7a98ee3c617b086a4cc2e9
Adnan Yaqoob <adnan@yaqoob.org>
2021-12-10 16:45:46 +0800

    Time: 36 ms (63.08%), Space: 14.2 MB (87.44%) - LeetHub

--- 790-domino-and-tromino-tiling/790-domino-and-tromino-tiling.py
+++ 790-domino-and-tromino-tiling/790-domino-and-tromino-tiling.py
@@ -3,11 +3,8 @@ class Solution:
         if n == 1:
             return 1
         
-        g, u = [0] * (n + 1), [0] * (n + 1)
-        g[1], g[2] = 1, 2
-        u[1], u[2] = 1, 2
-        for i in range(3, n + 1):
-            g[i] = g[i - 1] + g[i - 2] + 2 * u[i - 2]
-            u[i] = u[i - 1] + g[i - 1]
+        pg, cg, pu, cu = 1, 2, 1, 2
+        for _ in range(3, n + 1):
+            pg, cg, pu, cu = cg, cg + pg + 2 * pu, cu, cu + cg
             
-        return g[n] % (10**9 + 7)
+        return cg % (10**9 + 7)
