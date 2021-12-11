347ed2c0f020ffc2c56d551647bbacbbbb6ad24e
Adnan Yaqoob <adnan@yaqoob.org>
2021-12-11 13:39:09 +0800

    Time: 32 ms (72.73%), Space: 14.4 MB (31.31%) - LeetHub

--- 878-nth-magical-number/878-nth-magical-number.py
+++ 878-nth-magical-number/878-nth-magical-number.py
@@ -1,9 +1,20 @@
 class Solution:
     def nthMagicalNumber(self, n: int, a: int, b: int) -> int:
-        lo, hi, l = min(a, b), n * min(a, b), a * b // gcd(a, b)
+        def gcd(x: int, y: int) -> int:
+            while y > 0:
+                x, y = y, x % y
+            return x
+        
+        def lcm(x: int, y: int) -> int:
+            return x * y // gcd(x, y)
+        
+        def magicalNumbers(x: int) -> int:
+            return x // a + x // b - x // l
+        
+        lo, hi, l = min(a, b), n * min(a, b), lcm(a, b)
         while lo < hi:
             mid = lo + (hi - lo) // 2
-            if mid // a + mid // b - mid // l < n:
+            if magicalNumbers(mid) < n:
                 lo = mid + 1
             else:
                 hi = mid
