1cf19504feeeb1a7e04392076e6421389989a084
Adnan Yaqoob <adnan@yaqoob.org>
2021-12-11 17:48:02 +0800

    Time: 76 ms (48.63%), Space: 18.3 MB (12.47%) - LeetHub

--- 285-inorder-successor-in-bst/285-inorder-successor-in-bst.py
+++ 285-inorder-successor-in-bst/285-inorder-successor-in-bst.py
@@ -7,13 +7,18 @@
 
 class Solution:
     def inorderSuccessor(self, root: 'TreeNode', p: 'TreeNode') -> 'Optional[TreeNode]':
-        stack, successor = [root], TreeNode(float('inf'))
-        while stack:
-            curr = stack.pop()
-            if curr.val > p.val and curr.val < successor.val:
-                successor = curr
-            if curr.right:
-                stack.append(curr.right)
-            if curr.left:
-                stack.append(curr.left)
-        return None if successor.val == float('inf') else successor
+        def helper(curr: TreeNode) -> None:
+            nonlocal successor, foundp
+            if curr:
+                helper(curr.left)
+                
+                if not successor and foundp:
+                    successor = curr
+                else:
+                    if curr == p:
+                        foundp = True
+
+                    helper(curr.right)
+        successor, foundp = None, False
+        helper(root)
+        return successor
