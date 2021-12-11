d02e729b7146c5287fdcb11688779b61dacdb724
Adnan Yaqoob <adnan@yaqoob.org>
2021-12-11 18:59:09 +0800

    Time: 96 ms (88.11%), Space: 16 MB (9.43%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
@@ -1,13 +1,15 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        def helper(pos: int) -> None:
-            if pos == n:
-                ans.append(path[::])
+        q, n, ans = [[0]], len(graph) - 1, []
+        while True:
+            nq = []
+            for p in q:
+                if p[-1] != n:
+                    for v in graph[p[-1]]:
+                        nq.append(p + [v])
+                else:
+                    ans.append(p)
+            if not nq:
+                return ans
             else:
-                for i in graph[pos]:
-                    path.append(i)
-                    helper(i)
-                    path.pop()
-        n, ans, path = len(graph) - 1, [], [0]
-        helper(0)
-        return ans
+                q = nq
