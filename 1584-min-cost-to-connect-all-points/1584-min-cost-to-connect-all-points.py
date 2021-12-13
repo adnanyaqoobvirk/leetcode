43ed670abcd9e943de88e3baa8469793d4572c94
Adnan Yaqoob <adnan@yaqoob.org>
2021-12-13 16:32:54 +0800

    Time: 892 ms (96.98%), Space: 83.3 MB (71.73%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,3 +1,4 @@
+from heapq import heapify, heappop
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         def find(x: int) -> int:
@@ -6,17 +7,20 @@ class Solution:
             return nodes[x]
         
         n = len(points)
-        nodes = [i for i in range(n)]
-        min_cost = edge_count = 0
-        for d, i, j in sorted(
+        edges = [            
             (abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]), i, j)
             for i in range(n)
             for j in range(i + 1, n)
-        ):
+        ]
+        heapify(edges)
+        
+        nodes = [i for i in range(n)]
+        min_cost = edge_count = 0
+        while edge_count < n - 1:
+            d, i, j = heappop(edges)
             pi, pj = find(i), find(j)
             if pi != pj:
                 nodes[pj] = pi
                 min_cost += d
                 edge_count += 1
-                if edge_count == n - 1: break
         return min_cost
