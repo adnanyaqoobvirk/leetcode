0d005437ccc7f3c735965f248d518817781840e4
Adnan Yaqoob <adnan@yaqoob.org>
2021-12-13 16:27:01 +0800

    Time: 1416 ms (79.70%), Space: 83.2 MB (72.09%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,34 +1,22 @@
-class UF:
-    def __init__(self, n):
-        self.nodes = [i for i in range(n)]
-    
-    def find(self, i) -> int:
-        if self.nodes[i] == i:
-            return i
-        else:
-            self.nodes[i] = self.find(self.nodes[i])
-            return self.nodes[i]
-    
-    def union(self, i, j):
-        pi, pj = self.find(i), self.find(j)
-        if pi != pj: self.nodes[pj] = pi
-            
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        edges = []
-        for i in range(len(points)):
-            for j in range(i + 1, len(points)):
-                (pix, piy), (pjx, pjy) = points[i], points[j]
-                edges.append((abs(pix - pjx) + abs(piy - pjy), (i, j)))
-        edges.sort(key=lambda x: x[0])
+        def find(x: int) -> int:
+            if nodes[x] != x:
+                nodes[x] = find(nodes[x])
+            return nodes[x]
         
-        ans = ecount = 0
-        uf = UF(len(points))
-        for d, (i, j) in edges:
-            if uf.find(i) != uf.find(j):
-                uf.union(i, j)
-                ans += d
-                ecount += 1
-                if ecount == len(points) - 1:
-                    break
-        return ans
+        n = len(points)
+        nodes = [i for i in range(n)]
+        min_cost = edge_count = 0
+        for d, i, j in sorted(
+            (abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]), i, j)
+            for i in range(n)
+            for j in range(i + 1, n)
+        ):
+            pi, pj = find(i), find(j)
+            if pi != pj:
+                nodes[pj] = pi
+                min_cost += d
+                edge_count += 1
+                if edge_count == n - 1: break
+        return min_cost
