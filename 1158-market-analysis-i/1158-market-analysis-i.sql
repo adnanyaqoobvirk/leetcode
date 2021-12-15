67e4cdcee8a24b78db47a5c272c3b8fda8a5fb78
Adnan Yaqoob <adnan@yaqoob.org>
2021-12-15 17:51:51 +0800

    Time: 873 ms (98.24%), Space: 0B (100.00%) - LeetHub

---
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -0,0 +1,20 @@
+SELECT
+    user_id AS buyer_id,
+    join_date,
+    COUNT(order_id) AS orders_in_2019
+FROM
+    Users
+    LEFT JOIN
+    (
+        SELECT
+            *
+        FROM
+            Orders
+        WHERE
+            YEAR(order_date) = 2019
+    ) AS T
+    ON
+        user_id = buyer_id
+GROUP BY
+    user_id,
+    join_date
