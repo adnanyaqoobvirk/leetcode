a7ba04207d4125dface54724637d7eff76878800
Adnan Yaqoob <adnan@yaqoob.org>
2021-12-17 18:01:28 +0800

    Time: 568 ms (31.86%), Space: 16.3 MB (24.89%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -4,19 +4,18 @@ class Solution:
         for src, dst, time in times:
             graph[src].append((dst, time))
         
-        nodes, visited = {i: float('inf') for i in range(1, n + 1)}, set()
+        nodes, visited, minheap = {i: float('inf') for i in range(1, n + 1)}, set(), [(0, k)]
         nodes[k] = 0
-        while len(visited) < n:
-            curr_node, distance = -1, float('inf')
-            for node, d in nodes.items():
-                if node not in visited and d < distance:
-                    curr_node, distance = node, d
-            
-            if distance == float('inf'):
-                return -1
-            
-            for dst, time in graph[curr_node]:
-                nodes[dst] = min(nodes[dst], nodes[curr_node] + time)
-            
-            visited.add(curr_node)
-        return max(nodes.values())
+        while minheap:
+            distance, curr_node = heappop(minheap)
+            if curr_node not in visited:
+                if distance == float('inf'):
+                    return -1
+
+                for dst, time in graph[curr_node]:
+                    nodes[dst] = min(nodes[dst], nodes[curr_node] + time)
+                    heappush(minheap, (nodes[dst], dst))
+
+                visited.add(curr_node)
+        ans = max(nodes.values())
+        return -1 if ans == float('inf') else ans
