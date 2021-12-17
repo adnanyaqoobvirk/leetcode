97a0c44fd8a0d24087c1250fee0e1eb9e6a56c7f
Adnan Yaqoob <adnan@yaqoob.org>
2021-12-17 11:42:13 +0800

    Time: 271 ms (30.43%), Space: 15.6 MB (54.93%) - LeetHub

--- 221-maximal-square/221-maximal-square.py
+++ 221-maximal-square/221-maximal-square.py
@@ -1,12 +1,14 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         n, m, max_square = len(matrix), len(matrix[0]), 0
+        prev_row, curr_row = [0] * m, [0] * m
         for i in range(n):
             for j in range(m):
-                matrix[i][j] = int(matrix[i][j])
-                if matrix[i][j]:
+                curr_row[j] = int(matrix[i][j])
+                if curr_row[j]:
                     pi, pj = i - 1, j - 1
                     if 0 <= pi < n and 0 <= pj < m:
-                        matrix[i][j] += min(matrix[pi][j], matrix[i][pj], matrix[pi][pj])
-                max_square = max(max_square, matrix[i][j])
+                        curr_row[j] += min(prev_row[j], curr_row[pj], prev_row[pj])
+                max_square = max(max_square, curr_row[j])
+            prev_row, curr_row = curr_row, prev_row
         return max_square * max_square
