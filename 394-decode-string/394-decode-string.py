a8e0c61a0a83624beef8923ab7dcd185ef97b144
Adnan Yaqoob <adnan@yaqoob.org>
2021-12-19 17:34:15 +0800

    Time: 32 ms (59.36%), Space: 14.3 MB (52.98%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -1,28 +1,21 @@
 class Solution:
-    def __init__(self):
-        self.digits = {str(i): i for i in range(10)}
-        
     def decodeString(self, s: str) -> str:
-        ans, r, bcount, ss = [], 0, 0, []
-        for c in s:
-            if bcount == 0:
+        def helper(i: int) -> Tuple[int, str]:
+            ans, r = [], 0
+            while i < n:
+                c = s[i]
                 if c == '[':
-                    bcount += 1
-                elif c in self.digits:
-                    r = r * 10 + self.digits[c]
+                    i, ss = helper(i + 1)
+                    while r > 0:
+                        ans.extend(ss)
+                        r -= 1
+                elif c == ']':
+                    break
+                elif c in digits:
+                    r = r * 10 + digits[c]
                 else:
                     ans.append(c)
-            else:
-                ss.append(c)
-                
-                if c == '[':
-                    bcount += 1
-                elif c == ']':
-                    bcount -= 1
-                
-                if bcount == 0:
-                    ss.pop()
-            if c == ']' and bcount == 0:
-                ans.append(r * self.decodeString("".join(ss)))
-                ss, r = [], 0
-        return "".join(ans)
+                i += 1
+            return i, ans
+        digits, n = {str(i): i for i in range(10)}, len(s)
+        return "".join(helper(0)[1])
