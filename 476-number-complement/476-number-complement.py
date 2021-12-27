d76db3c938dc53c401f9b42524a080b429cb7662
Adnan Yaqoob <adnan@yaqoob.org>
2021-12-27 09:26:27 +0800

    Time: 28 ms (81.72%), Space: 14.3 MB (7.36%) - LeetHub

---
+++ 476-number-complement/476-number-complement.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        ans = count = 0
+        while num > 0:
+            if not (num & 1): ans += (1 << count)
+            num >>= 1
+            count += 1
+        return ans
