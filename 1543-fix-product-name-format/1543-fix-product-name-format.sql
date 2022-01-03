0f9dbbc643a4400ec889d9b3babb7148cb90b5c1
Adnan Yaqoob <adnan@yaqoob.org>
2022-01-03 14:54:47 +0800

    Time: 268 ms (81.45%), Space: 0B (100.00%) - LeetHub

---
+++ 1543-fix-product-name-format/1543-fix-product-name-format.sql
@@ -0,0 +1,12 @@
+SELECT
+    LOWER(TRIM(product_name)) AS product_name,
+    DATE_FORMAT(sale_date, '%Y-%m') AS sale_date,
+    COUNT(product_name) AS total
+FROM
+    Sales
+GROUP BY
+    LOWER(TRIM(product_name)),
+    DATE_FORMAT(sale_date, '%Y-%m')
+ORDER BY
+    product_name,
+    sale_date
