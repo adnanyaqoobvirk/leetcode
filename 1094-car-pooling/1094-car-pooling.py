8343810a866c1780e6e73b76e1d11838b4fc5079
Adnan Yaqoob <adnan@yaqoob.org>
2022-01-06 19:14:18 +0800

    Time: 121 ms (14.20%), Space: 14.9 MB (18.85%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,14 +1,14 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        events = []
-        for pcount, f, t in trips:
-            events.append((f, pcount))
-            events.append((t, -pcount))
-        events.sort()
+        events = defaultdict(list)
+        for pcount, pick, drop in trips:
+            events[pick].append(pcount)
+            events[drop].append(-pcount)
         
         curr_cap = 0
-        for t, pcount in events:
-            curr_cap += pcount
+        for i in range(1001):
+            for pcount in events[i]:
+                curr_cap += pcount
             if curr_cap > capacity:
                 return False
         return True
