5931132b879d30d58facd090f073956b4f85f6a8
Adnan Yaqoob <adnan@yaqoob.org>
2022-01-08 13:29:50 +0800

    Time: 1345 ms (58.81%), Space: 19.3 MB (77.35%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
@@ -1,18 +1,14 @@
 class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
-        @cache
-        def helper(i: int, j: int, k: int) -> int:
-            if i >= n or j < 0 or j >= m or k < 0 or k >= m or j >= k:
-                return 0
-
-            cherries = grid[i][j] + grid[i][k]
-            
-            max_cherries = 0
-            for a in [j, j + 1, j - 1]:
-                for b in [k, k + 1, k - 1]:
-                    max_cherries = max(max_cherries, helper(i + 1, a, b))
-            
-            return cherries + max_cherries
-        
         n, m = len(grid), len(grid[0])
-        return helper(0, 0, m - 1)
+        dp = [[[0] * (m + 1) for _ in range(m + 1)] for _ in range(n + 1)]
+        for i in reversed(range(n)):
+            for j in reversed(range(m)):
+                for k in range(m):
+                    max_cherries = 0
+                    for a in [j, j + 1, j - 1]:
+                        for b in [k, k + 1, k - 1]:
+                            if b > a:
+                                max_cherries = max(max_cherries, dp[i + 1][a][b])
+                    dp[i][j][k] = grid[i][j] + grid[i][k] + max_cherries
+        return dp[0][0][m - 1]
