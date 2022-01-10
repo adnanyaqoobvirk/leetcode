494f3b7bc73f14979841116ac6002d1299e480d3
Adnan Yaqoob <adnan@yaqoob.org>
2022-01-10 11:27:00 +0800

    Time: 746 ms (23.60%), Space: 0B (100.00%) - LeetHub

--- 1555-bank-account-summary/1555-bank-account-summary.sql
+++ 1555-bank-account-summary/1555-bank-account-summary.sql
@@ -1,18 +1,8 @@
-SELECT
-    U.user_id,
-    U.user_name,
-    credit + IFNULL(
-        SUM(
-            IF(
-                T.paid_by = U.user_id,
-                -T.amount, 
-                T.amount
-            )
-        ),
-        0
-    )AS credit,
-    IF(
-        credit + IFNULL(
+WITH CurrentCredit AS (
+    SELECT
+        U.user_id,
+        U.user_name,
+        U.credit + IFNULL(
             SUM(
                 IF(
                     T.paid_by = U.user_id,
@@ -21,17 +11,25 @@ SELECT
                 )
             ),
             0
-        ) < 0, 
-        'Yes', 
-        'No'
-    ) AS credit_limit_breached
+        ) AS credit
+    FROM
+        Users AS U
+        LEFT JOIN
+        Transactions AS T
+        ON
+            U.user_id = T.paid_by 
+            OR 
+            U.user_id = T.paid_to
+    GROUP BY
+        U.user_id,
+        U.user_name,
+        U.credit
+)
+
+SELECT
+    user_id,
+    user_name,
+    credit,
+    IF(credit < 0, 'Yes', 'No') AS credit_limit_breached
 FROM
-    Users AS U
-    LEFT JOIN
-    Transactions AS T
-    ON
-        U.user_id = T.paid_by 
-        OR 
-        U.user_id = T.paid_to
-GROUP BY
-    U.user_id
+    CurrentCredit
