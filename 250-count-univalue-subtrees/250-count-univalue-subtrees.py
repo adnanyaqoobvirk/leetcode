3be27ea0d450d1e084805052aeaaebee448d0c3c
Adnan Yaqoob <adnan@yaqoob.org>
2022-01-13 14:01:09 +0800

    Time: 75 ms (5.24%), Space: 14.6 MB (11.95%) - LeetHub

--- 250-count-univalue-subtrees/250-count-univalue-subtrees.py
+++ 250-count-univalue-subtrees/250-count-univalue-subtrees.py
@@ -6,27 +6,23 @@
 #         self.right = right
 class Solution:
     def countUnivalSubtrees(self, root: Optional[TreeNode]) -> int:
-        def helper(current: TreeNode) -> Tuple[int, bool]:
-            nonlocal count
+        def helper(curr: TreeNode) -> Tuple[int, int]:
+            if not curr:
+                return 0, 0
 
-            if not current:
-                return (None, True)
-            
-            left, luni = helper(current.left)
-            right, runi = helper(current.right)
+            if not curr.left and not curr.right:
+                return 1, curr.val
 
-            uni = True
-            if left and current.val != left:
-                uni = False
+            left_cnt, left_val = helper(curr.left)
+            right_cnt, right_val = helper(curr.right)
 
-            if right and current.val != right:
-                uni = False
+            if curr.left and curr.right:
+                if curr.val == left_val == right_val:
+                    return 1 + left_cnt + right_cnt, curr.val
+            elif curr.left and curr.val == left_val:
+                return 1 + left_cnt, curr.val
+            elif curr.right and curr.val == right_val:
+                return 1 + right_cnt, curr.val
 
-            if uni and luni and runi:
-                count += 1
-
-            return (current.val, uni and luni and runi)
-
-        count = 0
-        helper(root)
-        return count
+            return left_cnt + right_cnt, -2000
+        return helper(root)[0]
