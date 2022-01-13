529d97f6a04688e70b47fde6ae1663af9b803d4f
Adnan Yaqoob <adnan@yaqoob.org>
2022-01-13 15:36:41 +0800

    Time: 422 ms (7.53%), Space: 18.8 MB (86.05%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.py
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -1,14 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
-        def helper(istart: int, iend: int) -> TreeNode:
-            if istart <= iend:
-                current = TreeNode(postorder.pop())
-                
-                ipos = inorder_map[current.val]
-                
-                current.right = helper(ipos + 1, iend)
-                current.left = helper(istart, ipos - 1)
-                
-                return current
-        inorder_map = {v: i for i, v in enumerate(inorder)}
+        def helper(i: int, j: int) -> TreeNode:
+            for p in range(i, j + 1):
+                if inorder[p] == postorder[-1]:
+                    curr = TreeNode(postorder.pop())
+                    curr.right = helper(p + 1, j)
+                    curr.left = helper(i, p - 1)
+                    return curr
         return helper(0, len(inorder) - 1)
