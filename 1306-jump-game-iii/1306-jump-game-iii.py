db5753a32b36e7f3f15a788d80fc7596ccef251c
Adnan Yaqoob <adnan@yaqoob.org>
2022-01-15 18:33:28 +0800

    Time: 629 ms (5.05%), Space: 20.9 MB (82.28%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.py
+++ 1306-jump-game-iii/1306-jump-game-iii.py
@@ -1,15 +1,15 @@
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
-        q, seen = [start], set()
+        n, q, seen = len(arr), [start], set()
         while q:
             nq = []
             for i in q:
-                if 0 <= i < len(arr) and i not in seen:
-                    if arr[i] == 0:
-                        return True
-                    
-                    seen.add(i)
-                    nq.append(i + arr[i])
-                    nq.append(i - arr[i])
+                if i in seen or i < 0 or i >= n:
+                    continue
+                if arr[i] == 0:
+                    return True
+                seen.add(i)
+                nq.append(i + arr[i])
+                nq.append(i - arr[i])
             q = nq
         return False
