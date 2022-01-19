755d24c6ed28c90fb5640768154a3395c24cde19
Adnan Yaqoob <adnan@yaqoob.org>
2022-01-19 16:12:52 +0800

    Time: 45 ms (36.54%), Space: 15.7 MB (25.27%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -5,19 +5,12 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        def helper(curr: Optional[ListNode]) -> Optional[ListNode]:
-            if not curr:
-                return None
-
-            if not curr.next:
-                nonlocal nhead
-                nhead = curr
-                return curr
-
-            node = helper(curr.next)
-            node.next, curr.next = curr, None
-            return curr
-    
-        nhead = None
-        curr = helper(head)
-        return nhead
+        if not head or not head.next:
+            return head
+        
+        prev, curr, head.next = head, head.next, None
+        while curr:
+            tmp = curr.next
+            curr.next, prev = prev, curr
+            curr = tmp
+        return prev
