127826bffe233ec42225a8e23fa594d2614fc287
Adnan Yaqoob <adnan@yaqoob.org>
2022-01-21 22:32:34 +0800

    Time: 66 ms (5.33%), Space: 14.2 MB (53.43%) - LeetHub

--- 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.py
+++ 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.py
@@ -2,5 +2,5 @@ class Solution:
     def kthGrammar(self, n: int, k: int) -> int:
         if k == 1: return 0
         
-        if k & 1: return self.kthGrammar(n - 1, math.ceil(k / 2))
-        else: return int(not self.kthGrammar(n - 1, k // 2))
+        if k & 1: return self.kthGrammar(n, math.ceil(k / 2))
+        else: return int(not self.kthGrammar(n, k // 2))
