04cd0fb6a43191148c33ea67c0ada9a78dbcee14
Adnan Yaqoob <adnan@yaqoob.org>
2022-01-23 14:58:03 +0800

    Time: 551 ms (8.72%), Space: 52.3 MB (48.99%) - LeetHub

--- 1602-find-nearest-right-node-in-binary-tree/1602-find-nearest-right-node-in-binary-tree.py
+++ 1602-find-nearest-right-node-in-binary-tree/1602-find-nearest-right-node-in-binary-tree.py
@@ -6,15 +6,18 @@
 #         self.right = right
 class Solution:
     def findNearestRightNode(self, root: TreeNode, u: TreeNode) -> TreeNode:
-        def recurse(current: TreeNode, depth: int) -> TreeNode:
-            nonlocal target
-            if current:
-                if target == depth:
-                    return current
-                
-                if current == u:
-                    target = depth
-                else:
-                    return recurse(current.left, depth + 1) or recurse(current.right, depth + 1)
-        target = -1
-        return recurse(root, 0)
+        q = [root]
+        while q:
+            nq = []
+            for i in range(len(q)):
+                node = q[i]
+                if node == u:
+                    if i + 1 < len(q):
+                        return q[i + 1]
+                    else:
+                        return None
+                if node.left:
+                    nq.append(node.left)
+                if node.right:
+                    nq.append(node.right)
+            q = nq
