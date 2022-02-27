4e4a4628f021f4f5b55ec9a8da8beb2e1b8b8c35
Adnan Yaqoob <adnan@yaqoob.org>
2022-02-27 22:06:53 +0800

    Time: 120 ms (25.43%), Space: 14.5 MB (80.27%) - LeetHub

--- 622-design-circular-queue/622-design-circular-queue.py
+++ 622-design-circular-queue/622-design-circular-queue.py
@@ -1,35 +1,24 @@
 class MyCircularQueue:
 
     def __init__(self, k: int):
-        self.k = k - 1
+        self.cap = k
+        self.count = self.head = 0
         self.q = [None] * k
-        self.head = self.tail = -1
-
+        
     def enQueue(self, value: int) -> bool:
         if self.isFull():
             return False
         
-        if self.tail == self.k:
-            self.tail = 0
-        else:
-            self.tail += 1
-            
-        self.q[self.tail] = value
-        if self.head == -1:
-            self.head = 0
+        self.q[(self.head + self.count) % self.cap] = value
+        self.count += 1
         return True
 
     def deQueue(self) -> bool:
         if self.isEmpty():
             return False
         
-        if self.head == self.tail:
-            self.head = self.tail = -1
-        else:
-            if self.head == self.k:
-                self.head = 0
-            else:
-                self.head += 1
+        self.head = (self.head + 1) % self.cap
+        self.count -= 1
         return True
 
     def Front(self) -> int:
@@ -42,13 +31,13 @@ class MyCircularQueue:
         if self.isEmpty():
             return -1
         
-        return self.q[self.tail]
+        return self.q[(self.head + self.count - 1) % self.cap]
 
     def isEmpty(self) -> bool:
-        return self.head == -1
+        return self.count == 0
 
     def isFull(self) -> bool:
-        return (self.tail == self.k and self.head == 0) or self.tail + 1 == self.head
+        return self.count == self.cap
 
 
 # Your MyCircularQueue object will be instantiated and called as such:
