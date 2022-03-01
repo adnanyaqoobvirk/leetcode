1a8ec6d4c6cac9797dfebfb5579f3c065050ac09
Adnan Yaqoob <adnan@yaqoob.org>
2022-03-01 13:13:42 +0800

    Time: 72 ms (90.99%), Space: 18 MB (87.00%) - LeetHub

---
+++ 1490-clone-n-ary-tree/1490-clone-n-ary-tree.py
@@ -0,0 +1,17 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children if children is not None else []
+"""
+
+class Solution:
+    def cloneTree(self, root: 'Node') -> 'Node':
+        if not root:
+            return None
+
+        nroot = Node(root.val)
+        for child in root.children:
+            nroot.children.append(self.cloneTree(child))
+        return nroot
