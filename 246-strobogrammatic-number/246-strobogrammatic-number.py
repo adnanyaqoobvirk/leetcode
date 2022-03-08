bfed3f1ce253c22b6f1676e5eb7f9639f9c1c971
Adnan Yaqoob <adnan@yaqoob.org>
2022-03-08 19:54:57 +0800

    Time: 33 ms (73.25%), Space: 13.8 MB (76.99%) - LeetHub

--- 246-strobogrammatic-number/246-strobogrammatic-number.py
+++ 246-strobogrammatic-number/246-strobogrammatic-number.py
@@ -1,17 +1,9 @@
 class Solution:
     def isStrobogrammatic(self, num: str) -> bool:
-        invalids = {'2', '3', '4', '5', '7'}
-        six_nine = {'6', '9'}
+        valids = {"0":"0", "1":"1", "6":"9", "8":"8", "9":"6"}
         lo, hi = 0, len(num) - 1
         while lo <= hi:
-            if num[lo] in invalids or num[hi] in invalids:
+            if num[lo] not in valids or valids[num[lo]] != num[hi]:
                 return False
-            
-            if num[lo] == num[hi] and (num[lo] == '6' or num[lo] == '9'):
-                return False
-            
-            if num[lo] != num[hi] and (num[lo] not in six_nine or num[hi] not in six_nine):
-                return False
-            
             lo, hi = lo + 1, hi - 1
         return True
