3b11b25217ec158d6f0d9355903363cc323d896c
Adnan Yaqoob <adnan@yaqoob.org>
2022-03-08 20:12:12 +0800

    Time: 324 ms (34.11%), Space: 25.7 MB (73.05%) - LeetHub

--- 247-strobogrammatic-number-ii/247-strobogrammatic-number-ii.py
+++ 247-strobogrammatic-number-ii/247-strobogrammatic-number-ii.py
@@ -1,16 +1,15 @@
 class Solution:
     def findStrobogrammatic(self, n: int) -> List[str]:
         if n & 1:
+            ans = ["0", "1", "8"]
             n -= 1
-            ans = ["0", "1", "8"] 
         else:
             ans = [""]
             
-        for i in range(n, 0, -2):
+        for i in range(0, n, 2):
             nans = []
-            for s in ans:
-                if i != 2:
-                    nans.append(f"0{s}0")
-                nans.extend([f"1{s}1", f"8{s}8", f"6{s}9", f"9{s}6"])
+            for left, right in ["00", "11", "88", "69", "96"]:
+                for num in ans:
+                    nans.append(f"{left}{num}{right}")
             ans = nans
-        return ans
+        return [num for num in ans if len(num) == 1 or num[0] != "0"]
