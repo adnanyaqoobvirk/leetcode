50b86570d8f765f478e52544946ad83512dcf1ee
Adnan Yaqoob <adnan@yaqoob.org>
2022-03-09 15:26:38 +0800

    Time: 278 ms (86.03%), Space: 24.5 MB (9.13%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.py
+++ 286-walls-and-gates/286-walls-and-gates.py
@@ -3,19 +3,28 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        n, m = len(rooms), len(rooms[0])
-        q = []
+        def queue_adjacent(
+            queue: List[Tuple[int, int, int]], 
+            x: int, y: int, distance: int
+        ):
+            queue.append((x + 1, y, distance))
+            queue.append((x, y + 1, distance))
+            queue.append((x - 1, y, distance))
+            queue.append((x, y - 1, distance))
+        
+        q, n, m = [], len(rooms), len(rooms[0])
         for i in range(n):
             for j in range(m):
                 if rooms[i][j] == 0:
-                    q.append((i, j))
-        d = 0
+                    queue_adjacent(q, i, j, 0)
+        
         while q:
-            d += 1
             nq = []
-            for i, j in q:
-                for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
-                    if 0 <= x < n and 0 <= y < m and rooms[x][y] == 2147483647:
-                        rooms[x][y] = d
-                        nq.append((x, y))
+            for i, j, d in q:
+                d += 1
+                if 0 <= i < n and 0 <= j < m and rooms[i][j] == 2147483647:
+                    rooms[i][j] = d
+                    queue_adjacent(nq, i, j, d)
             q = nq
+                    
+                    
