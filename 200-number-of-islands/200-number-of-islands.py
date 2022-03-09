22b612b3f5058e06758bd7d4dbffd8d4738dd8af
Adnan Yaqoob <adnan@yaqoob.org>
2022-03-09 16:52:03 +0800

    Time: 333 ms (80.42%), Space: 16.3 MB (94.26%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,21 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        def exploreIsland() -> None:
+            q = [(i, j)]
+            while q:
+                nq = []
+                for x, y in q:
+                    for a, b in [(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)]:
+                        if 0 <= a < n and 0 <= b < m and grid[a][b] == "1":    
+                            grid[a][b] = "2"
+                            nq.append((a, b))
+                q = nq
+                
         n, m, islands = len(grid), len(grid[0]), 0
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == "1":
                     islands += 1
                     grid[i][j] = "2"
-                    
-                    q = [(i,j)]
-                    while q:
-                        nq = []
-                        for x, y in q:
-                            for a, b in [(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)]:
-                                if 0 <= a < n and 0 <= b < m and grid[a][b] == "1":    
-                                    grid[a][b] = "2"
-                                    nq.append((a, b))
-                        q = nq
+                    exploreIsland()
         return islands
                                 
                     
