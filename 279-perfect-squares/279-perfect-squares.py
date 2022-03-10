060ed887a66a3c0e883705b70770fb44ee0ffeae
Adnan Yaqoob <adnan@yaqoob.org>
2022-03-10 18:02:42 +0800

    Time: 3868 ms (54.37%), Space: 13.9 MB (82.19%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.py
+++ 279-perfect-squares/279-perfect-squares.py
@@ -1,11 +1,10 @@
 class Solution:
     def numSquares(self, n: int) -> int:
-        squares = [num ** 2 for num in range(1, math.ceil(math.sqrt(n)) + 1)]
-        dp = [float('inf')] * (n + 1)
-        dp[n] = 0
-        for total in reversed(range(n)):
-            for square in reversed(squares):
-                ntotal = total + square
-                if ntotal <= n:
-                    dp[total] = min(dp[total], 1 + dp[ntotal])
-        return dp[0]
+        squares = [i**2 for i in range(int(math.sqrt(n)) + 1)]
+        dp = [0] * (n + 1)
+        for i in range(1, n + 1):
+            min_square = float('inf')
+            for j in range(1, int(math.sqrt(i)) + 1):
+                min_square = min(dp[i - squares[j]] + 1, min_square)
+            dp[i] = min_square
+        return dp[n]
