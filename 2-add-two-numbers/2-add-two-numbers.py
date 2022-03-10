75e28a5e8e8a43a852ddf60518287d348fd9da8b
Adnan Yaqoob <adnan@yaqoob.org>
2022-03-10 10:45:21 +0800

    Time: 105 ms (41.33%), Space: 13.9 MB (65.79%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -5,20 +5,25 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        sentinal, carry = ListNode(), 0
-        curr, curr1, curr2 = sentinal, l1, l2
-        while curr1 and curr2:
-            carry, d = divmod(curr1.val + curr2.val + carry, 10)
-            curr.next = ListNode(d)
-            curr, curr1, curr2 = curr.next, curr1.next, curr2.next
+        if not l1:
+            return l2
+        if not l2:
+            return l1
         
-        l = curr1 if curr1 else curr2
-        while l:
-            carry, d = divmod(l.val + carry, 10)
-            curr.next = ListNode(d)
-            curr, l = curr.next, l.next
+        dummy = curr = ListNode()
+        p1, p2, carry = l1, l2, 0
+        while p1 and p2:
+            carry, digit = divmod(p1.val + p2.val + carry, 10)
+            curr.next = ListNode(digit)
+            p1, p2, curr = p1.next, p2.next, curr.next
+        
+        p = p1 if p1 else p2
+        while p:
+            carry, digit = divmod(p.val + carry, 10)
+            curr.next = ListNode(digit)
+            p, curr = p.next, curr.next
         
         if carry:
             curr.next = ListNode(carry)
             
-        return sentinal.next
+        return dummy.next
