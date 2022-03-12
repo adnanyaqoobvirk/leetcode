42a9b51f7be9f9dff97b235b220f6e418cbb0024
Adnan Yaqoob <adnan@yaqoob.org>
2022-03-12 18:18:50 +0800

    Time: 47 ms (63.20%), Space: 15.4 MB (14.60%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
@@ -9,22 +9,12 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        if not head: return head
-        
-        nodes = defaultdict(Node)
-        ncurr = sentinal = Node(0)
-        curr, nodes[head] = head, Node(head.val)
-        while curr:
-            node = nodes[curr]
-            if curr.random:
-                if curr.random not in nodes:
-                    nodes[curr.random] = Node(curr.random.val)
-                node.random = nodes[curr.random] = nodes[curr.random]
-                
-            if curr.next:
-                if curr.next not in nodes:
-                    nodes[curr.next] = Node(curr.next.val)
-                node.next = nodes[curr.next] = nodes[curr.next]
-            
-            curr, ncurr.next, ncurr = curr.next, node, node
-        return sentinal.next
+        def helper(curr: 'Optional[Node]') -> 'Optional[Node]':
+            if curr:
+                node_map[curr] = node = Node(curr.val)
+                node.next = helper(curr.next)
+                if curr.random:
+                    node.random = node_map[curr.random]
+                return node
+        node_map = defaultdict(Node)
+        return helper(head)
