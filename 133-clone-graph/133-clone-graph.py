deb06bf2f5dd83c339c5dbc6a542d8ac3e06526c
Adnan Yaqoob <adnan@yaqoob.org>
2022-03-13 00:21:14 +0800

    Time: 65 ms (28.54%), Space: 14.4 MB (43.12%) - LeetHub

--- 133-clone-graph/133-clone-graph.py
+++ 133-clone-graph/133-clone-graph.py
@@ -7,14 +7,17 @@ class Node:
 """
 
 class Solution:
-    def __init__(self) -> None:
-        self.seen = {}
+    def __init__(self):
+        self.processed = {}
         
-    def cloneGraph(self, root: 'Node') -> 'Node':
-        if not root: return None
-
-        if root not in self.seen:
-            nroot = self.seen[root] = Node(root.val)
-            for child in root.neighbors:
-                nroot.neighbors.append(self.cloneGraph(child))
-        return self.seen[root]
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if node:
+            if node in self.processed:
+                return self.processed[node]
+            
+            self.processed[node] = curr = Node(node.val)
+            for neighbor in node.neighbors:
+                n = self.cloneGraph(neighbor)
+                if n:
+                    curr.neighbors.append(n)
+            return curr
