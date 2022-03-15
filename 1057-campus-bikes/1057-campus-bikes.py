54f1eea4a308f51bc815c68097abe47e70bfe60c
Adnan Yaqoob <adnan@yaqoob.org>
2022-03-15 15:04:46 +0800

    Time: 1319 ms (52.29%), Space: 110.4 MB (77.34%) - LeetHub

--- 1057-campus-bikes/1057-campus-bikes.py
+++ 1057-campus-bikes/1057-campus-bikes.py
@@ -3,16 +3,19 @@ class Solution:
         def manhattan(x1, x2, y1, y2) -> int:
             return abs(x1 - x2) + abs(y1 - y2)
         
-        pairs = []
+        min_d, max_d = float('inf'), float('-inf')
+        pairs = defaultdict(list)
         for i, (wx, wy) in enumerate(workers):
             for j, (bx, by) in enumerate(bikes):
-                pairs.append((manhattan(wx, bx, wy, by), i, j))
+                d = manhattan(wx, bx, wy, by)
+                pairs[d].append((i, j))
+                min_d, max_d = min(min_d, d), max(max_d, d)
         
-        pairs.sort()
         ans = [-1] * len(workers)
         bs = [-1] * len(bikes)
-        for _, wi, bi in pairs:
-            if ans[wi] == -1 and bs[bi] == -1:
-                ans[wi] = bi
-                bs[bi] = wi
+        for d in range(min_d, max_d + 1):
+            for wi, bi in pairs[d]:
+                if ans[wi] == -1 and bs[bi] == -1:
+                    ans[wi] = bi
+                    bs[bi] = wi
         return ans
