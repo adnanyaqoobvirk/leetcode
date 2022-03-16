b33e80b3d5101096837f00c7399386f911e9ad51
Adnan Yaqoob <adnan@yaqoob.org>
2022-03-16 13:59:50 +0800

    Time: 556 ms (82.01%), Space: 21.5 MB (58.97%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.py
+++ 912-sort-an-array/912-sort-an-array.py
@@ -1,25 +1,7 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        
-        if n <= 1:
-            return nums
-        
-        left_nums, right_nums = self.sortArray(nums[0: n // 2]), self.sortArray(nums[n // 2:])
-        
+        heapify(nums)
         sorted_nums = []
-        lp = rp = 0
-        while lp < len(left_nums) and rp < len(right_nums):
-            if left_nums[lp] <= right_nums[rp]:
-                sorted_nums.append(left_nums[lp])
-                lp += 1
-            else:
-                sorted_nums.append(right_nums[rp])
-                rp += 1
-        
-        extended_nums, p = (left_nums, lp) if lp < len(left_nums) else (right_nums, rp)
-        while p < len(extended_nums):
-            sorted_nums.append(extended_nums[p])
-            p += 1
-        
+        while nums:
+            sorted_nums.append(heappop(nums))
         return sorted_nums
