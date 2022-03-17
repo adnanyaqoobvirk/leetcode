2f00c6f2792941d52bd18e2e0b49ad33d7303a8b
Adnan Yaqoob <adnan@yaqoob.org>
2022-03-17 21:18:12 +0800

    Time: 180 ms, Memory: 20.7 MB - LeetHub

--- search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
+++ search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
@@ -1,26 +1,17 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        m, n = len(matrix), len(matrix[0])
-        for i in range(min(m, n)):
-            # searching in the row
-            lo, hi = i, n - 1
-            while lo <= hi:
-                mid = lo + (hi - lo) // 2
-                if matrix[i][mid] == target:
-                    return True
-                elif matrix[i][mid] < target:
-                    lo = mid + 1
-                else:
-                    hi = mid - 1
+        def helper(top: int, left: int, bottom: int, right: int) -> bool:
+            if top > bottom or left > right:
+                return False
+            elif matrix[top][left] > target or matrix[bottom][right] < target:
+                return False
             
-            # searching in the column
-            lo, hi = i, m - 1
-            while lo <= hi:
-                mid = lo + (hi - lo) // 2
-                if matrix[mid][i] == target:
+            mid = left + (right - left) // 2
+            for row in range(top, bottom + 1):
+                if matrix[row][mid] == target:
                     return True
-                elif matrix[mid][i] < target:
-                    lo = mid + 1
-                else:
-                    hi = mid - 1
-        return False
+                elif matrix[row][mid] > target:
+                    break
+            return helper(row, left, bottom, mid) or helper(top, mid + 1, row, right)
+        m, n = len(matrix), len(matrix[0])
+        return helper(0, 0, m - 1, n - 1)
