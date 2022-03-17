9f8e7449064a5a244201642e6518aa506cf5ff4d
Adnan Yaqoob <adnan@yaqoob.org>
2022-03-17 18:42:49 +0800

    Time: 32 ms (86.28%), Space: 13.8 MB (70.86%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.py
+++ 856-score-of-parentheses/856-score-of-parentheses.py
@@ -1,16 +1,12 @@
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
-        n, i, stack = len(s), 0, [0]
-        while i < n:
-            if s[i] == '(':
-                if s[i + 1] == ')':
-                    stack[-1] += 1
-                    i += 1
-                else:
-                    stack.append(0)
+        score = depth = 0
+        for i, c in enumerate(s):
+            if c == '(':
+                depth += 1
             else:
-                if s[i - 1] != '(':
-                    score = 2 * stack.pop()
-                    stack[-1] += score
-            i += 1
-        return stack[0]
+                depth -= 1
+                if s[i - 1] == '(':
+                    score += 2 ** depth
+        return score
+            
