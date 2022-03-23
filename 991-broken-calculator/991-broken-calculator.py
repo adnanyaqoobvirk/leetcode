f76524834c11a41d246348cd2a922c75c918c78e
Adnan Yaqoob <adnan@yaqoob.org>
2022-03-23 11:57:37 +0800

    Time: 24 ms (99.09%), Space: 13.9 MB (25.11%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.py
@@ -0,0 +1,10 @@
+class Solution:
+    def brokenCalc(self, startValue: int, target: int) -> int:
+        ops = 0
+        while target > startValue:
+            if target & 1:
+                target += 1
+            else:
+                target //= 2
+            ops += 1
+        return ops + (startValue - target)
