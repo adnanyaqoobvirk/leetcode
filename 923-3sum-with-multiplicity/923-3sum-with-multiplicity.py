3c87ec500aca836d0ce26e7232e3156dcc012c5d
Adnan Yaqoob <adnan@yaqoob.org>
2022-04-07 12:32:47 +0800

    Time: 637 ms (40.70%), Space: 14.2 MB (22.09%) - LeetHub

---
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.py
@@ -0,0 +1,25 @@
+class Solution:
+    def threeSumMulti(self, arr: List[int], target: int) -> int:
+        ans, counts = 0, Counter(arr)
+        for i in range(101):
+            if i not in counts:
+                continue
+            for j in range(i, 101):
+                if j not in counts:
+                    continue
+                for k in range(j, 101):
+                    if k not in counts:
+                        continue
+                    if i + j + k == target:
+                        if i == j == k:
+                            if counts[i] >= 3:
+                                ans += comb(counts[i], 3)
+                        elif i == j:
+                            if counts[i] >= 2:
+                                ans += comb(counts[i], 2) * counts[k]
+                        elif j == k:
+                            if counts[j] >= 2:
+                                ans += comb(counts[j], 2) * counts[i]
+                        else:
+                            ans += counts[i] * counts[j] * counts[k]
+        return ans % (10**9 + 7)
