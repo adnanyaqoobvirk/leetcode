548e355dae52b3cf53ece46405730ee131ff22e2
Adnan Yaqoob <adnan@yaqoob.org>
2022-04-07 15:20:56 +0800

    Time: 2855 ms (25.53%), Space: 18.6 MB (15.43%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,13 +1,15 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        ans, dups = set(), set()
-        seen = {}
+        nums.sort()
+        
+        vmap = defaultdict(int)
+        for i, num in enumerate(nums):
+            vmap[-num] = i
+        
+        ans = set()
         for i in range(len(nums)):
-            if nums[i] not in dups:
-                dups.add(nums[i])
-                for j in range(i + 1, len(nums)):
-                    comps = -nums[i] - nums[j]
-                    if seen.get(comps, -1) == i:
-                        ans.add(tuple(sorted([nums[i], nums[j], comps])))
-                    seen[nums[j]] = i
+            for j in range(i + 1, len(nums)):
+                k = nums[i] + nums[j]
+                if k in vmap and i < vmap[k] and j < vmap[k]:
+                    ans.add((nums[i], nums[j], -k))
         return ans
