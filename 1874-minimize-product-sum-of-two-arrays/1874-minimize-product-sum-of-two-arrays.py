37cabf2af32cf373189690c35ddfdb8cd1919b5d
Adnan Yaqoob <adnan@yaqoob.org>
2022-04-08 12:51:02 +0800

    Time: 1240 ms (82.52%), Space: 19.2 MB (54.29%) - LeetHub

--- 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.py
+++ 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.py
@@ -1,5 +1,9 @@
 class Solution:
     def minProductSum(self, nums1: List[int], nums2: List[int]) -> int:
-        nums1.sort()
-        nums2.sort(reverse=True)
-        return sum(n1 * n2 for n1, n2 in zip(nums1, nums2))
+        return sum(
+            n1 * n2 
+            for n1, n2 in zip(
+                sorted(nums1), 
+                sorted(nums2, reverse=True)
+            )
+        )
