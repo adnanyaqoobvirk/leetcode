781562c3c93a98322f8cc21a31ff4087767a4097
Adnan Yaqoob <adnan@yaqoob.org>
2022-04-13 13:02:08 +0800

    Time: 40 ms, Memory: 13.9 MB - LeetHub

---
+++ sqrtx/sqrtx.py
@@ -0,0 +1,10 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        lo, hi = 0, x
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+            if mid * mid > x:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        return lo if lo * lo <= x else lo - 1
