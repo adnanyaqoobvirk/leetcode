e59f97f8bb9b8b5f5e78eea1c04dbd39b77fdf9b
Adnan Yaqoob <adnan@yaqoob.org>
2022-04-16 00:10:57 +0800

    Time: 617 ms (95.63%), Space: 27.9 MB (93.70%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,19 +1,14 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        n = len(nums) - 1
-        max_bits = int(math.log2(n)) + 1
-        masks = [(1 << i) for i in range(max_bits)]
+        slow = nums[nums[0]]
+        fast = nums[slow]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
         
-        original_bit_count = [0] * max_bits
-        bit_count = [0] * max_bits
-        for i in range(1, n + 2):
-            for j in range(max_bits):
-                if i <= n and i & masks[j]:
-                    original_bit_count[j] += 1
-                if nums[i - 1] & masks[j]:
-                    bit_count[j] += 1
-        ans = 0
-        for i in range(max_bits):
-            if bit_count[i] - original_bit_count[i] > 0:
-                ans |= (1 << i)
-        return ans
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+        return slow
+            
