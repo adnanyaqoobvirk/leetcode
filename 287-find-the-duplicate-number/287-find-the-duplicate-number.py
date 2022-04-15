833445e056be341e8d5b96def11422231e695541
Adnan Yaqoob <adnan@yaqoob.org>
2022-04-15 21:05:47 +0800

    Time: 921 ms (39.88%), Space: 28.3 MB (31.30%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,9 +1,17 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        heapify(nums)
-        prev = heappop(nums)
-        while len(nums) > 0:
-            curr = heappop(nums)
-            if curr == prev:
-                return curr
-            prev = curr
+        def numCount(num: int) -> int:
+            count = 0
+            for n in nums:
+                if n <= num:
+                    count += 1
+            return count
+        
+        lo, hi = 1, len(nums) - 1
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+            if numCount(mid) <= mid:
+                lo = mid + 1
+            else:
+                hi = mid
+        return lo
