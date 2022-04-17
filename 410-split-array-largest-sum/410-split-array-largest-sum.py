63d8fb84a4f14b86c328e3237ecbac07a8049543
Adnan Yaqoob <adnan@yaqoob.org>
2022-04-18 00:25:45 +0800

    Time: 41 ms (80.30%), Space: 13.9 MB (96.45%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.py
+++ 410-split-array-largest-sum/410-split-array-largest-sum.py
@@ -1,20 +1,19 @@
 class Solution:
     def splitArray(self, nums: List[int], m: int) -> int:
-        def splitPossible(total: int) -> bool:
-            curr = splits = 0
+        def possible(guess: int) -> bool:
+            count, total = 1, 0
             for num in nums:
-                if curr + num > total:
-                    curr = num
-                    splits += 1
-                else:
-                    curr += num
-            return curr <= total and splits < m
+                total += num
+                if total > guess:
+                    count += 1
+                    total = num
+            return count <= m
         
-        n, lo, hi = len(nums), max(nums), sum(nums)
+        lo, hi = max(nums), sum(nums)
         while lo < hi:
-            mid = lo + (hi - lo) // 2
-            if splitPossible(mid):
-                hi = mid
+            guess = lo + (hi - lo) // 2
+            if possible(guess):
+                hi = guess
             else:
-                lo = mid + 1
+                lo = guess + 1
         return lo
