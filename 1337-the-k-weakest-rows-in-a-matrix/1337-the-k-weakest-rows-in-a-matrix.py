f23b1df70ba09eab9cfb906b1feb78de512a689f
Adnan Yaqoob <adnan@yaqoob.org>
2022-04-18 23:31:25 +0800

    Time: 120 ms (79.46%), Space: 14.2 MB (51.98%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -1,8 +1,16 @@
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        return [
-            j for _, j in sorted(
-                (sum(row), i)
-                for i, row in enumerate(mat)
-            )[:k]
-        ]
+        def soldierCount(row: int) -> int:
+            lo, hi = 0, n - 1
+            while lo < hi:
+                mid = lo + (hi - lo) // 2
+                if row[mid] == 1:
+                    lo = mid + 1
+                else:
+                    hi = mid - 1
+            return lo if row[lo] == 0 else lo + 1
+        
+        m, n = len(mat), len(mat[0])
+        h = [(soldierCount(row), i) for i, row in enumerate(mat)]
+        heapify(h)
+        return [heappop(h)[1] for _ in range(k)]
