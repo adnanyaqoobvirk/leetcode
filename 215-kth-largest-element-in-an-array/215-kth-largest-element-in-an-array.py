048773120356f2a5208095ebeb65b7feb0b4a35d
Adnan Yaqoob <adnan@yaqoob.org>
2022-04-18 20:14:16 +0800

    Time: 66 ms (90.08%), Space: 15.5 MB (12.12%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,10 +1,25 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        for i in range(len(nums)):
-            nums[i] = -nums[i]
-        heapify(nums)
+        def partition(left: int, right: int, pivot_idx: int) -> int:
+            nums[right], nums[pivot_idx] = nums[pivot_idx], nums[right]
+            
+            store_idx = left
+            for i in range(left, right):
+                if nums[i] < nums[right]:
+                    nums[store_idx], nums[i] = nums[i], nums[store_idx]
+                    store_idx += 1
+            nums[store_idx], nums[right] = nums[right], nums[store_idx]
+            return store_idx
         
-        for _ in range(1, k):
-            heappop(nums)
-        return -heappop(nums)
-        
+        def select(left: int, right: int, nsmallest: int) -> int:
+            if left == right:
+                return nums[left]
+            
+            pivot_idx = partition(left, right, randint(left, right))
+            if pivot_idx == nsmallest:
+                return nums[pivot_idx]
+            elif pivot_idx > nsmallest:
+                return select(left, pivot_idx - 1, nsmallest)
+            else:
+                return select(pivot_idx + 1, right, nsmallest)
+        return select(0, len(nums) - 1, len(nums) - k)
