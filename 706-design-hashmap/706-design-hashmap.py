1d82c58459e984e8d36da0ad46cf6c435e7f7e2b
Adnan Yaqoob <adnan@yaqoob.org>
2022-04-22 14:32:04 +0800

    Time: 434 ms (39.53%), Space: 17.2 MB (83.70%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.py
+++ 706-design-hashmap/706-design-hashmap.py
@@ -1,13 +1,7 @@
-class Node:
-    def __init__(self, val: int = -1, key: int = -1, next: 'Node' = None):
-        self.key = key
-        self.val = val
-        self.next = next
-        
 class MyHashMap:
 
     def __init__(self):
-        self.keys = [None] * 31
+        self.keys = [[] for _ in range(2069)]
         self.size = 0
         
     def _loadfactor(self) -> float:
@@ -18,61 +12,37 @@ class MyHashMap:
     
     def _resize(self):
         keys = self.keys
-        self.keys = [None] * (len(self.keys) * 3)
-        for key in keys:
-            while key:
-                h = self._hash(key.key)
-                head = self.keys[h]
-                if not head:
-                    self.keys[h] = Node(key.val, key.key)
-                else:
-                    curr = head
-                    while curr.next:
-                        curr = curr.next
-                    curr.next = Node(key.val, key.key)
-                key = key.next
+        self.keys = [[] * (len(self.keys) * 3)]
+        for kl in keys:
+            for k, v in kl:
+                self.keys[self._hash(k)].append((k, v))
         
     def put(self, key: int, value: int) -> None:
-        if self._loadfactor() >= 0.8:
-            self._resize()
+        # if self._loadfactor() >= 0.8:
+        #     self._resize()
             
         h = self._hash(key)
-        head = self.keys[h]
-        if not head:
-            self.keys[h] = Node(value, key)
+        for i, (k, v) in enumerate(self.keys[h]):
+            if k == key:
+                self.keys[h][i] = (key, value) 
+                break
         else:
-            prev, curr = None, head
-            while curr:
-                if curr.key == key:
-                    curr.val = value
-                    return
-                prev, curr = curr, curr.next
-            prev.next = Node(value, key)
-        self.size += 1
+            self.keys[h].append((key, value))
+            self.size += 1
 
     def get(self, key: int) -> int:
         h = self._hash(key)
-        head = self.keys[h]
-        while head and head.key != key:
-            head = head.next
-        return head.val if head else -1
+        for k, v in self.keys[h]:
+            if k == key:
+                return v
+        return -1
 
     def remove(self, key: int) -> None:
         h = self._hash(key)
-        head = self.keys[h]
-        if head:
-            if head.key == key:
-                self.keys[h] = head.next
+        for i, (k, v) in enumerate(self.keys[h]):
+            if k == key:
+                del self.keys[h][i]
                 self.size -= 1
-            else:
-                prev, curr = head, head.next
-                while curr:
-                    if curr.key == key:
-                        prev.next = curr.next
-                        self.size -= 1
-                        break
-                    prev, curr = curr, curr.next
-
 
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()
