147e8dbde04c337ef6d5eaf620ca0a69b8e192dc
Adnan Yaqoob <adnan@yaqoob.org>
2022-04-26 00:31:42 +0800

    Time: 924 ms (66.14%), Space: 20.6 MB (21.65%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,10 +1,33 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        h = []
-        for point in points:
-            t = (-1 * (point[0]**2 + point[1]**2), point)
-            if len(h) == k:
-                heappushpop(h, t)
+        def getCounts(guess: int) -> Tuple[List[Tuple[int, int]], List[Tuple[int, int]]]:
+            closer, farther = [], []
+            for d, i in dpoints:
+                if d <= guess:
+                    closer.append((d, i))
+                else:
+                    farther.append((d, i))
+            return closer, farther
+        
+        dpoints, maxd = [], float('-inf')
+        for i, (x, y) in enumerate(points):
+            d = x**2 + y**2
+            maxd = max(maxd, d)
+            dpoints.append((d, i))
+        
+        lo, hi, ans = 0, maxd, []
+        while k:
+            mid = lo + (hi - lo) / 2
+            closer, farther = getCounts(mid)
+            if len(closer) > k:
+                hi = mid
+                dpoints = closer
             else:
-                heappush(h, t)
-        return [point for _, point in h]
+                lo = mid
+                for _, i in closer:
+                    ans.append(points[i])
+                k -= len(closer)
+                dpoints = farther
+        return ans
+        
+        
