4417419c805053c4cd49730900462421cc580d64
Adnan Yaqoob <adnan@yaqoob.org>
2022-04-25 21:40:47 +0800

    Time: 219 ms (75.19%), Space: 18.7 MB (40.36%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,13 +1,28 @@
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        h = [(row[0], i, 1) for i, row in enumerate(matrix)]
-        heapify(h)
+        def getCount(guess: int) -> Tuple[int, int]:
+            count, maxval, i, j = 0, float('-inf'), n - 1, 0
+            for j in range(n):
+                for i in reversed(range(n)):
+                    if matrix[i][j] > guess:
+                        continue
+                    else:
+                        maxval = max(maxval, matrix[i][j])
+                        count += i + 1
+                        break
+            return count, maxval
         
-        n, count, k = len(matrix), 0, k - 1
-        while count < k:
-            _, r, c = heappop(h)
-            count = count + 1
-            if c < n:
-                heappush(h, (matrix[r][c], r, c + 1))
-        return h[0][0]
-        
+        n, lo, hi = len(matrix), matrix[0][0], matrix[-1][-1]
+        while lo <= hi:
+            guess = lo + (hi - lo) // 2
+            count, _ = getCount(guess)
+            
+            if count > k:
+                hi = guess - 1
+            else:
+                lo = guess + 1
+        count, val = getCount(hi)
+        if count >= k:
+            return val
+        else:
+            return getCount(lo)[1]
