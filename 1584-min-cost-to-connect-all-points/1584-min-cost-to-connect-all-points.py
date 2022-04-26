0405b7f9b8f3c3294721a29ba254436b703abb10
Adnan Yaqoob <adnan@yaqoob.org>
2022-04-27 00:38:51 +0800

    Time: 1217 ms (90.92%), Space: 78.6 MB (82.15%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,23 +1,30 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         n = len(points)
+        
         mst = [False] * n
+        mst[0] = True
+        
         h = [
-            (abs(points[0][0] - points[i][0]) + abs(points[0][1] - points[i][1]), 0, i)
+            (abs(points[0][0] - points[i][0]) +\
+             abs(points[0][1] - points[i][1]), i)
             for i in range(1, n)
         ]
         heapify(h)
         
         ecount = cost = 0
         while ecount < n - 1:
-            while mst[h[0][1]] and mst[h[0][2]]:
+            while mst[h[0][1]]:
                 heappop(h)
-            d, i, j = heappop(h)
-            mst[i] = mst[j] = True
+            
+            d, i = heappop(h)
+            mst[i] = True
             cost += d
             ecount += 1
-            for k in range(n):
-                if k != j and not mst[k]:
-                    heappush(h, (abs(points[j][0] - points[k][0]) + abs(points[j][1] - points[k][1]), j, k))
+            
+            for j in range(n):
+                if not mst[j]:
+                    heappush(h, (abs(points[i][0] - points[j][0]) +\
+                                 abs(points[i][1] - points[j][1]), j))
         return cost
             
