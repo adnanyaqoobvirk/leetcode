fdbc2eafc4cda53fdd34b1ff4c12916ccf9ba91a
Adnan Yaqoob <adnan@yaqoob.org>
2022-04-27 00:34:05 +0800

    Time: 1203 ms (91.30%), Space: 78.7 MB (82.15%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,41 +1,23 @@
-class UF:
-    def __init__(self, n: int) -> None:
-        self.root = [-1] * n
-        
-    def find(self, node: int) -> int:
-        if self.root[node] >= 0:
-            self.root[node] = self.find(self.root[node])
-            return self.root[node]
-        return node
-        
-    def union(self, node1: int, node2: int) -> bool:
-        root1, root2 = self.find(node1), self.find(node2)
-        if root1 == root2:
-            return False
-        
-        if self.root[root1] < self.root[root2]:
-            self.root[root2] = root1
-        elif self.root[root1] > self.root[root2]:
-            self.root[root1] = root2
-        else:
-            self.root[root1] = root2
-            self.root[root2] -= 1
-        return True
-        
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        edges, n = [], len(points)
-        for i in range(n):
-            for j in range(i + 1, n):
-                d = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
-                edges.append((d, i, j))
-        edges.sort()
+        n = len(points)
+        mst = [False] * n
+        h = [
+            (abs(points[0][0] - points[i][0]) + abs(points[0][1] - points[i][1]), 0, i)
+            for i in range(1, n)
+        ]
+        heapify(h)
         
-        uf, cost, ecount = UF(n), 0, 0
-        for d, i, j in edges:
-            if uf.union(i, j):
-                cost += d
-                ecount += 1
-                if ecount == n - 1:
-                    break
+        ecount = cost = 0
+        while ecount < n - 1:
+            while mst[h[0][1]] and mst[h[0][2]]:
+                heappop(h)
+            d, i, j = heappop(h)
+            mst[i] = mst[j] = True
+            cost += d
+            ecount += 1
+            for k in range(n):
+                if k != j and not mst[k]:
+                    heappush(h, (abs(points[j][0] - points[k][0]) + abs(points[j][1] - points[k][1]), j, k))
         return cost
+            
