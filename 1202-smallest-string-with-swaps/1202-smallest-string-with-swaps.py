3913685e3257de0a4267d3b3d48a72fbb42b0848
Adnan Yaqoob <adnan@yaqoob.org>
2022-04-27 22:45:04 +0800

    Time: 1196 ms (21.82%), Space: 78.4 MB (5.13%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.py
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.py
@@ -1,38 +1,30 @@
-class DisjointSet:
-    def __init__(self: 'DisjointSet', n: int) -> None:
-        self.nodes = [-1 for _ in range(n)]
-    
-    def find(self: 'DisjoinSet', x: int) -> int:
-        if self.nodes[x] >= 0:
-            self.nodes[x] = self.find(self.nodes[x])
-            return self.nodes[x]
-        else:
-            return x
-
-    def union(self: 'DisjoinSet', x: int, y: int) -> None:
-        px, py = self.find(x), self.find(y)
-        if px != py:
-            if self.nodes[px] < self.nodes[py]:
-                self.nodes[py] = px
-            elif self.nodes[px] > self.nodes[py]:
-                self.nodes[px] = py
-            else:
-                self.nodes[px] = py
-                self.nodes[py] -= 1
-                
 class Solution:
     def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
-        ds = DisjointSet(len(s))
-        for i, j in pairs:
-            ds.union(i, j)
+        def helper(node: int) -> None:
+            for child in graph[node]:
+                if child not in seen:
+                    seen.add(child)
+                    unseen.remove(child)
+                    helper(child)
         
-        imap = defaultdict(list)
-        for i in range(len(s)):
-            imap[ds.find(i)].append(s[i])
-        for k in imap:
-            imap[k].sort(reverse=True)
+        graph, unseen = defaultdict(list), set()
+        for src, dst in pairs:
+            graph[src].append(dst)
+            graph[dst].append(src)
+            unseen.add(dst)
+            unseen.add(src)
         
-        ans = []
-        for i in range(len(s)):
-            ans.append(imap[ds.find(i)].pop())
-        return "".join(ans)
+        groups = []
+        while unseen:
+            seen = {unseen.pop()}
+            helper(next(iter(seen)))
+            groups.append(sorted(seen))
+        
+        slist = list(s)
+        for group in groups:
+            cgroup = sorted(slist[i] for i in group)
+            for i, idx in enumerate(group):
+                slist[idx] = cgroup[i]
+        return "".join(slist)
+        
+        
