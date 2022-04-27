5d45c09a224c2afd550287f5920ee06d0cc0f770
Adnan Yaqoob <adnan@yaqoob.org>
2022-04-28 00:06:23 +0800

    Time: 957 ms (47.69%), Space: 50.7 MB (31.77%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.py
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.py
@@ -1,26 +1,34 @@
+class UF:
+    def __init__(self, nodes: list[int]) -> None:
+        self.root = {node: node for node in nodes}
+        
+    def find(self, node) -> None:
+        if self.root[node] != node:
+            self.root[node] = self.find(self.root[node])
+        return self.root[node]
+
+    def union(self, node1: int, node2: int) -> None:
+        root1, root2 = self.find(node1), self.find(node2)
+        if root1 != root2:
+            self.root[root1] = root2
+            
+    def groups(self) -> List[List[int]]:
+        ans = defaultdict(set)
+        for k in self.root:
+            ans[self.find(k)].add(k)
+        return [sorted(v) for v in ans.values()]
+            
 class Solution:
     def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
-        graph, unseen = defaultdict(list), set()
+        nodes = set()
         for src, dst in pairs:
-            graph[src].append(dst)
-            graph[dst].append(src)
-            unseen.add(dst)
-            unseen.add(src)
+            nodes.add(dst)
+            nodes.add(src)
         
-        groups = []
-        while unseen:
-            group = [unseen.pop()]
-            q = [group[0]]
-            while q:
-                nq = []
-                for node in q:
-                    for child in graph[node]:
-                        if child in unseen:
-                            group.append(child)
-                            unseen.remove(child)
-                            nq.append(child)
-                q = nq
-            groups.append(sorted(group))
+        uf = UF(nodes)
+        for src, dst in pairs:
+            uf.union(src, dst)
+        groups = uf.groups()
         
         slist = list(s)
         for group in groups:
@@ -28,5 +36,3 @@ class Solution:
             for i, idx in enumerate(group):
                 slist[idx] = cgroup[i]
         return "".join(slist)
-        
-        
