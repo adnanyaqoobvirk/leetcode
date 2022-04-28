fe916c5a98594da8469047bcdb92604c3559d2ac
Adnan Yaqoob <adnan@yaqoob.org>
2022-04-28 12:42:35 +0800

    Time: 3539 ms (16.61%), Space: 16.1 MB (51.72%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,22 +1,20 @@
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
         def possible() -> bool:
-            q, seen = [(0, 0)], {(0, 0)}
-            while q:
-                nq = []
-                for i, j in q:
-                    if i == m - 1 and j == n - 1:
-                        return True
-                    
-                    for x, y in [(i, j - 1), (i, j + 1), (i - 1, j), (i + 1, j)]:
-                        if (
-                            0 <= x < m and 0 <= y < n and
-                            (x, y) not in seen and
-                            abs(heights[x][y] - heights[i][j]) <= k
-                        ):
-                            seen.add((x, y))
-                            nq.append((x, y))
-                q = nq
+            stack, seen = [(0, 0)], {(0, 0)}
+            while stack:
+                i, j = stack.pop()
+                if i == m - 1 and j == n - 1:
+                    return True
+
+                for x, y in [(i, j - 1), (i, j + 1), (i - 1, j), (i + 1, j)]:
+                    if (
+                        0 <= x < m and 0 <= y < n and
+                        (x, y) not in seen and
+                        abs(heights[x][y] - heights[i][j]) <= k
+                    ):
+                        seen.add((x, y))
+                        stack.append((x, y))
             return False
                             
         m, n = len(heights), len(heights[0])
