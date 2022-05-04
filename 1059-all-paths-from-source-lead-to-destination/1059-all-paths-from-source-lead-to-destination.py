31da52d27044c69b556102b02f585e273157aa34
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-05 02:29:09 +0800

    Time: 724 ms (5.03%), Space: 18.7 MB (97.69%) - LeetHub

--- 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.py
+++ 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.py
@@ -1,23 +1,28 @@
 class Solution:
     def leadsToDestination(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        def helper(curr: int) -> bool:
-            if not graph[curr] and curr != destination:
-                return False
-            
-            visited[curr] = False
-            for node in graph[curr]:
-                if node in visited and not visited[node]:
-                    return False
-                
-                if not helper(node):
-                    return False
-            visited[curr] = True
-            return True
-        
         graph = defaultdict(set)
         for src, dst in edges:
             graph[src].add(dst)
         
-        visited = {}
-        return helper(source)
+        visited, stack = {}, [source]
+        while stack:
+            curr = stack[-1]
+            
+            if not graph[curr] and curr != destination:
+                return False
+            
+            for node in graph[curr]:
+                if node not in visited or not visited[node]:
+                    break
+            else:
+                visited[curr] = True
+                stack.pop()
+                continue
+
+            visited[curr] = False
+            for node in graph[curr]:
+                if node in visited and not visited[node]:
+                    return False
+                stack.append(node)
+        return True
         
