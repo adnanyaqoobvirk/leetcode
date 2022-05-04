82f6dde58fb6b981d800cded58a87355bb252ca2
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-04 20:59:20 +0800

    Time: 1078 ms (5.15%), Space: 21 MB (51.87%) - LeetHub

--- 1168-optimize-water-distribution-in-a-village/1168-optimize-water-distribution-in-a-village.py
+++ 1168-optimize-water-distribution-in-a-village/1168-optimize-water-distribution-in-a-village.py
@@ -1,36 +1,22 @@
-class UF:
-    def __init__(self, size: int) -> None:
-        self.roots = [i for i in range(size)]
-        self.rank = [1] * size
-        
-    def find(self, node: int) -> int:
-        if self.roots[node] != node:
-            self.roots[node] = self.find(self.roots[node])
-        return self.roots[node]
-    
-    def union(self, node1: int, node2: int) -> bool:
-        root1, root2 = self.find(node1), self.find(node2)
-        if root1 == root2:
-            return False
-        
-        if self.rank[root1] > self.rank[root2]:
-            self.roots[root2] = root1
-        elif self.rank[root1] < self.rank[root2]:
-            self.roots[root1] = root2
-        else:
-            self.roots[root1] = root2
-            self.rank[root2] += 1
-        return True
-    
 class Solution:
     def minCostToSupplyWater(self, n: int, wells: List[int], pipes: List[List[int]]) -> int:
-        edges = [(wells[i], 0, i + 1) for i in range(n)]
+        graph = defaultdict(list)
         for src, dest, cost in pipes:
-            edges.append((cost, src, dest))
-        edges.sort()
+            graph[src].append((dest, cost))
+            graph[dest].append((src, cost))
+            
+        heap = [(wells[i], i + 1) for i in range(n)]
+        heapify(heap)
         
-        uf, min_cost = UF(n + 1), 0
-        for cost, src, dest in edges:
-            if uf.union(src, dest):
+        min_cost, mst = 0, {0}
+        while heap:
+            cost, dest = heappop(heap)
+            
+            if dest not in mst:
                 min_cost += cost
+                mst.add(dest)
+                
+                for n, c in graph[dest]:
+                    if n not in mst:
+                        heappush(heap, (c, n))
         return min_cost
