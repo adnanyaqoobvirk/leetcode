6fca1225cb45caa98756b07de56386dc72756695
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-05 09:44:00 +0800

    Time: 61 ms (66.89%), Space: 13.9 MB (92.03%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -1,43 +1,26 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        n, m = len(grid), len(grid[0])
+        m, n = len(grid), len(grid[0])
+        fresh_nodes, q = set(), []
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    fresh_nodes.add((i, j))
+                elif grid[i][j] == 2:
+                    q.append((i, j))
+                    
+        if not fresh_nodes:
+            return 0
         
-        minutes = 0
-        stop = False
-        all_rotten = False
-        while not stop and not all_rotten:
-            all_rotten = True
-            stop = True
-            any_fresh = False
-            ignore = set()
-            for i in range(n):
-                for j in range(m):
-                    if (i, j) not in ignore and grid[i][j] == 2:
-                        if i - 1 >= 0 and grid[i - 1][j] == 1:
-                            grid[i - 1][j] = 2
-                            ignore.add((i - 1, j))
-                            any_fresh = True
-                        
-                        if i + 1 < n and grid[i + 1][j] == 1:
-                            grid[i + 1][j] = 2
-                            ignore.add((i + 1, j))
-                            any_fresh = True
-                        
-                        if j - 1 >= 0 and grid[i][j - 1] == 1:
-                            grid[i][j - 1] = 2
-                            ignore.add((i, j - 1))
-                            any_fresh = True
-                        
-                        if j + 1 < m and grid[i][j + 1] == 1:
-                            grid[i][j + 1] = 2
-                            ignore.add((i, j + 1))
-                            any_fresh = True
-                        
-                        grid[i][j] = -2
-                        stop = False
-                    elif grid[i][j] == 1:
-                        all_rotten = False
-            if any_fresh:
-                minutes += 1
-        return minutes if all_rotten else -1
-                            
+        minutes = -1
+        while q:
+            nq = []
+            
+            for i, j in q:
+                for indices in [(i, j - 1), (i, j + 1), (i - 1, j), (i + 1, j)]:
+                    if 0 <= indices[0] < m and 0 <= indices[1] < n and indices in fresh_nodes:
+                        fresh_nodes.remove(indices)
+                        nq.append(indices)
+            q = nq
+            minutes += 1
+        return -1 if fresh_nodes else minutes
