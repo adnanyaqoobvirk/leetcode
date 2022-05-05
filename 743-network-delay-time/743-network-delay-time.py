6cb6d3e551832edcad13c2e036c19903bc06e8eb
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-05 16:08:23 +0800

    Time: 514 ms (72.51%), Space: 16.6 MB (48.73%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -4,16 +4,14 @@ class Solution:
         for src, dst, time in times:
             graph[src].append((dst, time))
             
-        q, node_times = [k], defaultdict(lambda: float('inf'))
+        stack, node_times = [k], defaultdict(lambda: float('inf'))
         node_times[k] = 0
-        while q:
-            nq = []
-            for src in q:
-                for dst, time in graph[src]:
-                    if node_times[dst] > (node_times[src] + time):
-                        node_times[dst] = node_times[src] + time
-                        nq.append(dst)
-            q = nq
+        while stack:
+            src = stack.pop()
+            for dst, time in graph[src]:
+                if node_times[dst] > (node_times[src] + time):
+                    node_times[dst] = node_times[src] + time
+                    stack.append(dst)
         
         if len(node_times) != n:
             return -1
