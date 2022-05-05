1dc8c910ea3b01ca6cacdaf19343def4599919dc
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-05 15:57:09 +0800

    Time: 696 ms (33.92%), Space: 16.7 MB (38.71%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -3,13 +3,19 @@ class Solution:
         graph = defaultdict(list)
         for src, dst, time in times:
             graph[src].append((dst, time))
+            
+        q, node_times = [k], defaultdict(lambda: float('inf'))
+        node_times[k] = 0
+        while q:
+            nq = []
+            for src in q:
+                for dst, time in graph[src]:
+                    if node_times[dst] > (node_times[src] + time):
+                        node_times[dst] = node_times[src] + time
+                        nq.append(dst)
+            q = nq
         
-        nodes, minheap = {}, [(0, k)]
-        while minheap:
-            distance, curr_node = heappop(minheap)
-            if curr_node not in nodes:
-                nodes[curr_node] = distance
-                for dst, time in graph[curr_node]:
-                    if dst not in nodes:
-                        heappush(minheap, (distance + time, dst))
-        return max(nodes.values()) if len(nodes) == n else -1
+        if len(node_times) != n:
+            return -1
+        
+        return max(node_times.values())
