a136e4f7ccdd2818682c770e3f24506b455e4ad3
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-07 17:59:42 +0800

    Time: 1159 ms (51.46%), Space: 16.9 MB (27.44%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,29 +1,19 @@
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        def possible() -> bool:
-            stack, seen = [(0, 0)], {(0, 0)}
-            while stack:
-                i, j = stack.pop()
-                if i == m - 1 and j == n - 1:
-                    return True
-
-                for x, y in [(i, j - 1), (i, j + 1), (i - 1, j), (i + 1, j)]:
-                    if (
-                        0 <= x < m and 0 <= y < n and
-                        (x, y) not in seen and
-                        abs(heights[x][y] - heights[i][j]) <= k
-                    ):
-                        seen.add((x, y))
-                        stack.append((x, y))
-            return False
-                            
         m, n = len(heights), len(heights[0])
-        lo, hi = 0, max(max(heights[i]) for i in range(m))
-        while lo < hi:
-            k = lo + (hi - lo) // 2
-            if possible():
-                hi = k
-            else:
-                lo = k + 1
-        return lo
-                
+        heap, nodes = [(0, 0, 0)], {}
+        while heap:
+            effort, i, j = heappop(heap)
+            if (i, j) not in nodes:
+                nodes[(i, j)] = effort
+                for x, y in [(i, j - 1), (i, j + 1), (i - 1, j), (i + 1, j)]:
+                    if 0 <= x < m and 0 <= y < n:
+                        heappush(
+                            heap, 
+                            (
+                                max(effort, abs(heights[i][j] - heights[x][y])),
+                                x,
+                                y
+                            )
+                        )
+        return nodes[(m - 1, n - 1)]
