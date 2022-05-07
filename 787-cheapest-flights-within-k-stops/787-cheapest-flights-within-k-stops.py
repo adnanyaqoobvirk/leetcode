3671e5c1fd844df23a47ceede07859a761e4b434
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-07 17:45:38 +0800

    Time: 435 ms (14.64%), Space: 15.2 MB (77.24%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -1,22 +1,11 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        graph = defaultdict(list)
-        for f, t, p in flights:
-            graph[f].append((t, p))
-        
-        heap, node_prices = [(0, 0, src)], defaultdict(lambda: (float('inf'), float('inf')))
-        node_prices[src] = (0, 0)
-        while heap:
-            price, stops, s = heappop(heap)
-            
-            if s == dst:
-                break
-            
-            if stops <= k:
-                for d, p in graph[s]:
-                    if price + p < node_prices[d][0]:
-                        node_prices[d] = (price + p, stops + 1)
-                        heappush(heap, (price + p, stops + 1, d))
-                    elif stops <= node_prices[d][1]:
-                        heappush(heap, (price + p, stops + 1, d))
-        return node_prices[dst][0] if dst in node_prices else -1
+        prev = defaultdict(lambda: float('inf'))
+        prev[src] = 0
+        for _ in range(k + 1):
+            curr = defaultdict(lambda: float('inf'))
+            curr[src] = 0
+            for s, d, p in flights:
+                curr[d] = min(curr[d], prev[s] + p)
+            prev = curr
+        return -1 if prev[dst] == float('inf') else prev[dst]
