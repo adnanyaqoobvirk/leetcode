7b5ec938932bdca38400ba6671d178acf1449cbc
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-07 16:52:43 +0800

    Time: 393 ms (75.91%), Space: 32 MB (95.87%) - LeetHub

--- 456-132-pattern/456-132-pattern.py
+++ 456-132-pattern/456-132-pattern.py
@@ -7,15 +7,13 @@ class Solution:
         
         stack = []
         for j in reversed(range(len(nums))):
-            if nums[j] <= minis[j]:
-                continue
-                
             while stack and stack[-1] <= minis[j]:
                 stack.pop()
             
             if stack and stack[-1] < nums[j]:
                 return True
-
-            stack.append(nums[j])
+            
+            if nums[j] > minis[j]:
+                stack.append(nums[j])
 
         return False
