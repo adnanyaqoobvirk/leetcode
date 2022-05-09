6f5312476a222512af75b1ad9d70e77f313bb13f
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-09 13:25:12 +0800

    Time: 95 ms (56.82%), Space: 13.9 MB (90.61%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -4,13 +4,12 @@ class Solution:
         if oldColor == newColor:
             return image
         
-        m, n, q= len(image), len(image[0]), [(sr, sc)]
-        while q:
-            nq = []
-            for i, j in q:
-                image[i][j] = newColor
-                for x, y in [(i, j - 1), (i, j + 1), (i - 1, j), (i + 1, j)]:
-                    if 0 <= x < m and 0 <= y < n and image[x][y] == oldColor:
-                        nq.append((x, y))
-            q = nq
+        m, n, stack = len(image), len(image[0]), [(sr, sc)]
+        while stack:
+            i, j = stack.pop()
+            image[i][j] = newColor
+            
+            for x, y in [(i, j - 1), (i, j + 1), (i - 1, j), (i + 1, j)]:
+                if 0 <= x < m and 0 <= y < n and image[x][y] == oldColor:
+                    stack.append((x, y))
         return image
