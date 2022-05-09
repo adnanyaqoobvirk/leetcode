11624e4cb7200a4922b4274be2f945f82264fbc5
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-09 12:26:37 +0800

    Time: 148 ms (10.46%), Space: 14 MB (66.73%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,14 +1,15 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        oldColor = image[sr][sc]
-        if newColor == oldColor:
-            return image
-        
-        def helper(i: int, j: int) -> None:
-            image[i][j] = newColor
-            for x, y in [(i, j - 1), (i, j + 1), (i + 1, j), (i - 1, j)]:
-                if 0 <= x < n and 0 <= y < m and image[x][y] == oldColor:
-                    helper(x, y)
-        n, m = len(image), len(image[0])
-        helper(sr, sc)
-        return image
+        m, n = len(image), len(image[0])
+        q, nimage, visited = [(sr, sc)], [row[:] for row in image], {(sr, sc)}
+        nimage[sr][sc] = newColor
+        while q:
+            nq = []
+            for i, j in q:
+                for x, y in [(i, j - 1), (i, j + 1), (i - 1, j), (i + 1, j)]:
+                    if 0 <= x < m and 0 <= y < n and (x, y) not in visited and image[x][y] == image[sr][sc]:
+                        visited.add((x, y))
+                        nimage[x][y] = newColor
+                        nq.append((x, y))
+            q = nq
+        return nimage
