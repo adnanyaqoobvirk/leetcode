04df764d8b377a150d46183d27218c5fdbbfa27b
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-09 16:16:01 +0800

    Time: 288 ms (96.16%), Space: 16.3 MB (80.14%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,23 +1,19 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m, n = len(grid), len(grid[0])
-        lands = {
-            (i, j) 
-            for i in range(m) 
-            for j in range(n) 
-            if grid[i][j] == '1'
-        }
-        
-        ans = 0
-        while lands:
-            q = [lands.pop()]
-            while q:
-                nq = []
-                for i, j in q:
-                    for x, y in [(i, j - 1), (i, j + 1), (i - 1, j), (i + 1, j)]:
-                        if 0 <= x < m and 0 <= y < n and (x, y) in lands:
-                            lands.discard((x, y))
-                            nq.append((x, y))
-                q = nq
-            ans += 1
-        return ans
+        m, n, islands = len(grid), len(grid[0]), 0
+        for a in range(m):
+            for b in range(n):
+                if grid[a][b] != '1':
+                    continue
+                grid[a][b] = '0'
+                q = [(a, b)]
+                while q:
+                    nq = []
+                    for i, j in q:
+                        for x, y in [(i, j - 1), (i, j + 1), (i - 1, j), (i + 1, j)]:
+                            if 0 <= x < m and 0 <= y < n and grid[x][y] == '1':
+                                grid[x][y] = '0'
+                                nq.append((x, y))
+                    q = nq
+                islands += 1
+        return islands
