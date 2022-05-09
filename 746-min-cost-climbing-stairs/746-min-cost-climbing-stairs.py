7d44e22ff854a3c837474e663660dddbce514ff3
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-09 22:25:07 +0800

    Time: 71 ms (64.32%), Space: 17.1 MB (5.37%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -2,12 +2,15 @@ class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         @cache
         def helper(pos: int) -> int:
-            if pos >= n:
+            if pos < 0:
                 return 0
             
+            if pos == 1:
+                return cost[pos]
+            
             return min(
-                cost[pos] + helper(pos + 2),
-                cost[pos] + helper(pos + 1)
+                cost[pos] + helper(pos - 2),
+                cost[pos] + helper(pos - 1)
             )
         n = len(cost)
-        return min(helper(0), helper(1))
+        return min(helper(n - 1), helper(n - 2))
