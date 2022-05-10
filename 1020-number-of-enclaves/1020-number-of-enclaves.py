23cb2d7802168f330a1335a91476265b60e2c787
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-10 13:25:06 +0800

    Time: 639 ms (76.74%), Space: 15.1 MB (99.51%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -5,23 +5,21 @@ class Solution:
             for j in range(n):
                 if grid[i][j] != 1:
                     continue
-                q, lands, grid[i][j] = [(i, j)], 1, 0
+                stack, lands, grid[i][j] = [(i, j)], 0, 0
                 boundry = True if i == 0 or i == m - 1 or j == 0 or j == n - 1 else False
-                while q:
-                    nq = []
-                    for a, b in q:
-                        for x, y in [(a, b - 1), (a, b + 1), (a - 1, b), (a + 1, b)]:
-                            if 0 <= x < m and 0 <= y < n and grid[x][y] == 1:
-                                if not boundry:
-                                    boundry = (
-                                        True 
-                                        if x == 0 or x == m - 1 or y == 0 or y == n - 1 
-                                        else False
-                                    )
-                                grid[x][y] = 0
-                                nq.append((x, y))
-                    q = nq
-                    lands += len(q)
+                while stack:
+                    a, b = stack.pop()
+                    for x, y in [(a, b - 1), (a, b + 1), (a - 1, b), (a + 1, b)]:
+                        if 0 <= x < m and 0 <= y < n and grid[x][y] == 1:
+                            if not boundry:
+                                boundry = (
+                                    True 
+                                    if x == 0 or x == m - 1 or y == 0 or y == n - 1 
+                                    else False
+                                )
+                            grid[x][y] = 0
+                            stack.append((x, y))
+                    lands += 1
                 if not boundry:
                     ans += lands
         return ans
