1aa138132524bdee59be10c3a156ed0ed2599ade
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-15 21:34:23 +0800

    Time: 308 ms (40.04%), Space: 14.2 MB (98.50%) - LeetHub

--- 694-number-of-distinct-islands/694-number-of-distinct-islands.py
+++ 694-number-of-distinct-islands/694-number-of-distinct-islands.py
@@ -5,21 +5,19 @@ class Solution:
             for j in range(n):
                 if grid[i][j] != 1:
                     continue
-                q, path, grid[i][j] = [(i, j)], [], 0
-                while q:
-                    nq = []
-                    for a, b in q:
-                        for x, y, p in [
-                            (a, b - 1, 'L'), 
-                            (a, b + 1, 'R'), 
-                            (a - 1, b, 'U'), 
-                            (a + 1, b, 'D')
-                        ]:
-                            if 0 <= x < m and 0 <= y < n and grid[x][y] == 1:
-                                grid[x][y] = 0
-                                path.append(p)
-                                nq.append((x, y))
-                        path.append('|')
-                    q = nq
+                stack, path, grid[i][j] = [(i, j)], [], 0
+                while stack:
+                    a, b = stack.pop()
+                    for x, y, p in [
+                        (a, b - 1, 'L'), 
+                        (a, b + 1, 'R'), 
+                        (a - 1, b, 'U'), 
+                        (a + 1, b, 'D')
+                    ]:
+                        if 0 <= x < m and 0 <= y < n and grid[x][y] == 1:
+                            grid[x][y] = 0
+                            path.append(p)
+                            stack.append((x, y))
+                    path.append('|')
                 paths.add("".join(path))
         return len(paths)
