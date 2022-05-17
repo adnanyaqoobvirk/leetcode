dfe949180a32e488572bf78613b1248e390fbe98
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-17 21:59:11 +0800

    Time: 737 ms (73.35%), Space: 17.1 MB (58.44%) - LeetHub

--- 542-01-matrix/542-01-matrix.py
+++ 542-01-matrix/542-01-matrix.py
@@ -1,19 +1,22 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         m, n = len(mat), len(mat[0])
-        ans, q = [[float('inf')] * n for _ in range(m)], []
+        ans = [[float('inf')] * n for _ in range(m)]
         for i in range(m):
             for j in range(n):
                 if mat[i][j] == 0:
-                    q.append((i, j, 0))
                     ans[i][j] = 0
-        while q:
-            nq = []
-            for i, j, d in q:
-                for x, y in [(i, j - 1), (i, j + 1), (i - 1, j), (i + 1, j)]:
-                    if 0 <= x < m and 0 <= y < n and ans[x][y] == float('inf'):
-                        if mat[x][y] == 1:
-                            ans[x][y] = d + 1
-                            nq.append((x, y, d + 1))
-            q = nq
+                else:
+                    minval = float('inf')
+                    for x, y in [(i, j - 1), (i - 1, j)]:
+                        if 0 <= x < m and 0 <= y < n:
+                            minval = min(minval, ans[x][y])
+                    ans[i][j] = min(ans[i][j], minval + 1)
+        for i in reversed(range(m)):
+            for j in reversed(range(n)):
+                minval = float('inf')
+                for x, y in [(i, j + 1), (i + 1, j)]:
+                    if 0 <= x < m and 0 <= y < n:
+                        minval = min(minval, ans[x][y])
+                ans[i][j] = min(ans[i][j], minval + 1)
         return ans
