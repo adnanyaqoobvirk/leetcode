b006bd61259d34d0957bc064abfba313f4058431
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-17 20:34:00 +0800

    Time: 1266 ms (14.77%), Space: 17.5 MB (33.28%) - LeetHub

--- 542-01-matrix/542-01-matrix.py
+++ 542-01-matrix/542-01-matrix.py
@@ -1,19 +1,19 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        q, n, m = [], len(mat), len(mat[0])
-        for i in range(n):
-            for j in range(m):
+        m, n = len(mat), len(mat[0])
+        ans, q = [[float('inf')] * n for _ in range(m)], []
+        for i in range(m):
+            for j in range(n):
                 if mat[i][j] == 0:
                     q.append((i, j, 0))
-                else:
-                    mat[i][j] = -1
+                    ans[i][j] = 0
         while q:
             nq = []
             for i, j, d in q:
-                d += 1
                 for x, y in [(i, j - 1), (i, j + 1), (i - 1, j), (i + 1, j)]:
-                    if 0 <= x < n and 0 <= y < m and mat[x][y] == -1:
-                        mat[x][y] = d
-                        nq.append((x, y, d))
+                    if 0 <= x < m and 0 <= y < n and ans[x][y] == float('inf'):
+                        if mat[x][y] == 1:
+                            ans[x][y] = d + 1
+                            nq.append((x, y, d + 1))
             q = nq
-        return mat
+        return ans
