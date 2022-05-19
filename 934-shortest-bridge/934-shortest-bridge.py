08054fab20f008a7743b248a2f3b8c55342a4380
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-19 19:18:06 +0800

    Time: 347 ms (99.78%), Space: 14.5 MB (96.32%) - LeetHub

--- 934-shortest-bridge/934-shortest-bridge.py
+++ 934-shortest-bridge/934-shortest-bridge.py
@@ -1,10 +1,7 @@
 class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-        
-        stack = []
-        for i in range(m):
+        n, directions, stack = len(grid), [(0, -1), (0, 1), (-1, 0), (1, 0)], []
+        for i in range(n):
             for j in range(n):
                 if grid[i][j] == 1:
                     stack.append((i, j))
@@ -20,7 +17,7 @@ class Solution:
             grid[i][j] = -1
             for x, y in directions:
                 x, y = i + x, j + y
-                if 0 <= x < m and 0 <= y < n:
+                if 0 <= x < n and 0 <= y < n:
                     if grid[x][y] == 1:
                         grid[x][y] = 0
                         stack.append((x, y))
@@ -30,7 +27,7 @@ class Solution:
             for i, j, d in q:
                 for x, y in directions:
                     x, y = i + x, j + y
-                    if 0 <= x < m and 0 <= y < n and grid[x][y] != -1:
+                    if 0 <= x < n and 0 <= y < n and grid[x][y] != -1:
                         if grid[x][y] == 1:
                             return d
                         grid[x][y] = -1
