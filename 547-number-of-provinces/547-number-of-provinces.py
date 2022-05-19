7b25e2152118751758e6e18ea95aea04b744ef0e
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-19 23:42:46 +0800

    Time: 283 ms (43.14%), Space: 14.3 MB (51.91%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -1,36 +1,19 @@
-class DisjoinSet:
-    def __init__(self: Any, n: int) -> None:
-        self.nodes = [-1 for i in range(n)]
-        self.roots = n
-        
-    def find(self, x) -> int:
-        if self.nodes[x] >= 0:
-            self.nodes[x] = self.find(self.nodes[x])
-            return self.nodes[x]
-        else:
-            return x
-    
-    def union(self, x, y) -> None:
-        xp, yp = self.find(x), self.find(y)
-        if xp != yp:
-            if self.nodes[xp] < self.nodes[yp]:
-                self.nodes[yp] = xp
-            elif self.nodes[xp] > self.nodes[yp]:
-                self.nodes[xp] = yp
-            else:
-                self.nodes[xp] = yp
-                self.nodes[yp] -= 1
-            self.roots -= 1
-                
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        n, m = len(isConnected), len(isConnected[0])
-        
-        ds = DisjoinSet(n)
+        n, graph = len(isConnected), defaultdict(list)
         for i in range(n):
-            for j in range(m):
-                if i != j and isConnected[i][j]:
-                    ds.union(i, j)
-        
-        return ds.roots
-        
+            for j in range(n):
+                if isConnected[i][j] == 1:
+                    graph[i].append(j)
+
+        nodes, ans = {i for i in range(n)}, 0
+        while nodes:
+            stack = [nodes.pop()]
+            while stack:
+                src = stack.pop()
+                for dst in graph[src]:
+                    if dst in nodes:
+                        stack.append(dst)
+                        nodes.remove(dst)
+            ans += 1
+        return ans
