6659018d657978f9b9ab4bb6e4b9af63b9fceeef
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-19 23:45:20 +0800

    Time: 218 ms (71.91%), Space: 14.4 MB (51.91%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -1,18 +1,13 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        n, graph = len(isConnected), defaultdict(list)
-        for i in range(n):
-            for j in range(n):
-                if isConnected[i][j] == 1:
-                    graph[i].append(j)
-
+        n = len(isConnected)
         nodes, ans = {i for i in range(n)}, 0
         while nodes:
             stack = [nodes.pop()]
             while stack:
                 src = stack.pop()
-                for dst in graph[src]:
-                    if dst in nodes:
+                for dst in range(n):
+                    if isConnected[src][dst] == 1 and dst in nodes:
                         stack.append(dst)
                         nodes.remove(dst)
             ans += 1
