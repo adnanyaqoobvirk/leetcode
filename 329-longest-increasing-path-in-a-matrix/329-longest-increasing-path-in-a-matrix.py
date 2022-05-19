4aefa5575c88fbedee63af4d9b9fb104f33bd54a
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-19 17:08:28 +0800

    Time: 928 ms (12.96%), Space: 20.7 MB (9.43%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -1,19 +1,30 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        @cache
-        def helper(a: int, b: int) -> int:
-            ans = 0
-            for x, y in directions:
-                x, y = a + x, b + y
-                if 0 <= x < m and 0 <= y < n and matrix[x][y] > matrix[a][b]:
-                    ans = max(ans, helper(x, y))
-            return ans + 1
-        
         m, n = len(matrix), len(matrix[0])
         directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-        return max(
-            helper(i, j)
-            for i in range(m)
-            for j in range(n)
-        )
-                
+        graph, outdegrees = defaultdict(list), defaultdict(int)
+        for i in range(m):
+            for j in range(n):
+                for x, y in directions:
+                    x, y = i + x, j + y
+                    if 0 <= x < m and 0 <= y < n and matrix[x][y] > matrix[i][j]:
+                        graph[(x, y)].append((i, j))
+                        outdegrees[(i, j)] += 1
+        
+        q = [
+            (i, j) 
+            for i in range(m) 
+            for j in range(n) 
+            if outdegrees[(i, j)] == 0
+        ]
+        ans = 0
+        while q:
+            nq = []
+            for i, j in q:
+                for x, y in graph[(i, j)]:
+                    outdegrees[(x, y)] -= 1
+                    if outdegrees[(x, y)] == 0:
+                        nq.append((x, y))
+            q = nq
+            ans += 1
+        return ans
