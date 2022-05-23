3d4fb85f397d363150123be6f195b6db533c1857
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-23 15:25:58 +0800

    Time: 103 ms (96.92%), Space: 13.9 MB (40.86%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -1,23 +1,15 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        n = len(s)
-        pmap = defaultdict(bool)
-        pcount = 0
+        def getPCounts(lo: int, hi: int) -> int:
+            counts = 0
+            while lo >= 0 and hi < n:
+                if s[lo] != s[hi]:
+                    break
+                counts += 1
+                lo, hi = lo - 1, hi + 1
+            return counts
         
-        for c in s:
-            pmap[c] = True
-            pcount += 1
-            
-        for i in range(1, n):
-            if s[i - 1] == s[i]:
-                pmap[f"{s[i - 1]}{s[i]}"] = True
-                pcount += 1
-        
-        for i in range(3, n + 1):
-            for j in range(n - i + 1):
-                ss = s[j + 1:j + i - 1]
-                if s[j] == s[j + i - 1] and pmap[ss]:
-                    pmap[s[j:j + i]] = True
-                    pcount += 1
-                    
-        return pcount
+        n, ans = len(s), 0
+        for i in range(n):
+            ans += getPCounts(i, i) + getPCounts(i - 1, i)
+        return ans
