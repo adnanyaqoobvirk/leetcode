28e6c9119a467c616901528b4e4fdeed544b93a7
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-23 19:06:21 +0800

    Time: 127 ms (44.79%), Space: 24 MB (18.43%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.py
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -1,15 +1,19 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        num_map = defaultdict(int)
+        @cache
+        def helper(num: int) -> int:
+            if num > max_num:
+                return 0
+            
+            return max(
+                num_map[num] + helper(num + 2),
+                helper(num + 1)
+            )
+        
+        max_num, num_map = 0, defaultdict(int)
         for num in nums:
             num_map[num] += num
-            
-        elems = sorted(num_map.keys())
-        prev, curr = 0, num_map[elems[0]]
-        for i in range(1, len(elems)):
-            num = elems[i]
-            if num == elems[i - 1] + 1:
-                prev, curr = curr, max(curr, num_map[num] + prev)
-            else:
-                prev, curr = curr, curr + num_map[num]
-        return curr
+            max_num = max(max_num, num)
+        
+        return helper(0)
+        
