25848ca7775d9853778f402606b2e0eeb09ef317
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-30 21:31:22 +0800

    Time: 8522 ms (5.00%), Space: 547.8 MB (5.13%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,18 +1,21 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         @cache
-        def helper(pos: int, total: int) -> int:
-            if pos >= n or total > amount:
-                return float('inf')
-            
-            if total == amount:
+        def helper(pos: int, remaining: int) -> int:
+            if remaining == 0:
                 return 0
             
-            return min(
-                helper(pos, coins[pos] + total) + 1,
-                helper(pos + 1, coins[pos] + total) + 1,
-                helper(pos + 1, total)
-            )
+            if pos >= n or remaining < 0:
+                return float('inf')
+        
+            coin_count = float('inf')
+            for i in range(pos, n):
+                coin_count = min(
+                    coin_count,
+                    helper(i, remaining - coins[i]) + 1
+                )
+            return coin_count
+        
         n = len(coins)
-        ans = helper(0, 0)
+        ans = helper(0, amount)
         return -1 if ans == float('inf') else ans
