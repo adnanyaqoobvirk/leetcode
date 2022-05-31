03f88e6fa93e99868798189648f81b1cc0a45629
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-01 01:29:58 +0800

    Time: 57 ms (60.21%), Space: 14.4 MB (50.30%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,15 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        @cache
-        def recurse(pos: int, bought: int) -> int:
-            if pos >= n:
-                return 0
-            
-            return max(
-                -prices[pos] + recurse(pos + 1, 1),
-                recurse(pos + 1, bought),
-                (prices[pos] + recurse(pos + 2, 0)) if bought else 0
-            )
-        
         n = len(prices)
-        return recurse(0, 0)
+        dp = [[0] * 2 for _ in range(n + 2)]
+        for pos in reversed(range(n)):
+            for bought in reversed(range(2)):
+                dp[pos][bought] = max(
+                    -prices[pos] + dp[pos + 1][1],
+                    dp[pos + 1][bought],
+                    (prices[pos] + dp[pos + 2][0]) if bought else 0
+                )
+        return dp[0][0]
