132b16b8d84a2fd286da6289528c156ac8937b93
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-01 01:21:16 +0800

    Time: 259 ms (28.33%), Space: 14 MB (76.30%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,23 +1,24 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
         n = len(prices)
-        dp = [[[0] * (k + 1) for _ in range(2)] for _ in range(n + 1)]
+        prev, curr = [[0] * (k + 1) for _ in range(2)], [[0] * (k + 1) for _ in range(2)]
         for pos in reversed(range(n)):
             for bought in reversed(range(2)):
                 for t in range(1, k + 1):
                     buy_stock = 0
                     if not bought:
-                        buy_stock = -prices[pos] + dp[pos + 1][1][t]
+                        buy_stock = -prices[pos] + prev[1][t]
 
-                    skip = dp[pos + 1][bought][t]
+                    skip = prev[bought][t]
 
                     sell_stock = 0
                     if bought:
-                        sell_stock = prices[pos] + dp[pos + 1][0][t - 1]
+                        sell_stock = prices[pos] + prev[0][t - 1]
 
-                    dp[pos][bought][t] = max(
+                    curr[bought][t] = max(
                         buy_stock,
                         skip,
                         sell_stock
                     )
-        return dp[0][0][k]
+            prev, curr = curr, prev
+        return prev[0][k]
