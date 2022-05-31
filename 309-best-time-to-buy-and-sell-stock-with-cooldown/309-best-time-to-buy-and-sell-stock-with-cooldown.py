a19ba622955cd2d2b4507766e6cec179eea97a42
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-01 01:32:33 +0800

    Time: 54 ms (66.40%), Space: 14.2 MB (78.43%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,12 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        dp = [[0] * 2 for _ in range(n + 2)]
+        pprev, prev, curr = [0] * 2, [0] * 2, [0] * 2
         for pos in reversed(range(n)):
             for bought in reversed(range(2)):
-                dp[pos][bought] = max(
-                    -prices[pos] + dp[pos + 1][1],
-                    dp[pos + 1][bought],
-                    (prices[pos] + dp[pos + 2][0]) if bought else 0
+                curr[bought] = max(
+                    -prices[pos] + prev[1],
+                    prev[bought],
+                    (prices[pos] + pprev[0]) if bought else 0
                 )
-        return dp[0][0]
+            pprev, prev, curr = prev, curr, pprev
+        return prev[0]
