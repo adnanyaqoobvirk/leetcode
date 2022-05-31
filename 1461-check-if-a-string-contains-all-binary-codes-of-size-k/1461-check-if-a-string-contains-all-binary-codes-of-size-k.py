b7049737a4f781cab6e103275dbb133feb19d182
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-31 13:59:51 +0800

    Time: 529 ms (49.11%), Space: 23.4 MB (76.55%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -4,11 +4,7 @@ class Solution:
         
         ss = set()
         for i in range(len(s) - k + 1):
-            if len(ss) == MAX_SET_SIZE:
-                break
             ss.add(int(s[i:i + k], 2))
-        
-        for code in range(MAX_SET_SIZE):
-            if code not in ss:
-                return False
-        return True
+            if len(ss) == MAX_SET_SIZE:
+                return True
+        return False
