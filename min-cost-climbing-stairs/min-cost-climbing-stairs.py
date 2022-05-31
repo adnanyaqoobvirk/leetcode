063ce1f270854e51384d9e52abdd64470ff4b6c1
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-01 02:35:39 +0800

    Time: 65 ms, Memory: 13.9 MB - LeetHub

---
+++ min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cost.append(0)
+        n = len(cost)
+        
+        prev, curr = cost[0], cost[1]
+        for pos in range(2, n):
+            prev, curr = curr, cost[pos] + min(
+                curr,
+                prev
+            )
+        return curr
+    
+        
