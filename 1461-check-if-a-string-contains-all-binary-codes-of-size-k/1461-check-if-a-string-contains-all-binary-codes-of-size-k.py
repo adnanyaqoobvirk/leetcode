594f0936708d1cc685cdd89c8bfba47198d33f8c
Adnan Yaqoob <adnan@yaqoob.org>
2022-05-31 14:22:43 +0800

    Time: 703 ms (26.10%), Space: 23.5 MB (67.70%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -3,10 +3,9 @@ class Solution:
         MAX_SET_SIZE = 1 << k
         BIT_MASK = MAX_SET_SIZE - 1
         
-        codes = [False] * MAX_SET_SIZE
         code = int(s[0:k], 2)
-        codes[code] = True
+        codes = {code}
         for i in range(k, len(s)):
             code = (code << 1) & BIT_MASK | int(s[i])
-            codes[code] = True
-        return all(codes)
+            codes.add(code)
+        return len(codes) == MAX_SET_SIZE
