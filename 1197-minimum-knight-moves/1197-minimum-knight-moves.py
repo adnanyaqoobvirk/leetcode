e31e1fb879ecf1ba5eeab6c3f68000c56a6d4ac5
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-01 20:39:13 +0800

    Time: 109 ms (82.50%), Space: 15.4 MB (89.60%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.py
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.py
@@ -1,24 +1,15 @@
 class Solution:
     def minKnightMoves(self, x: int, y: int) -> int:
-        possible_moves = [
-            (-2, 1), (-2, -1),
-            (-1, 2), (-1, -2),
-            (1, 2), (1, -2),
-            (2, 1), (2, -1)
-        ]
-        
-        x, y = abs(x), abs(y)
-        q, moves, seen = [(0, 0)], 0, {(0, 0)}
-        while q:
-            nq = []
-            for i, j in q:
-                if i == x and j == y:
-                    return moves
-                for a, b in possible_moves:
-                    ab = abs(a + i), abs(b + j)
-                    if ab not in seen:
-                        seen.add(ab)
-                        nq.append(ab)
-            q = nq
-            moves += 1
-        return -1
+        @cache
+        def helper(i: int, j: int) -> int:
+            if i == 0 and j == 0:
+                return 0
+            
+            if i + j == 2:
+                return 2
+            
+            return 1 + min(
+                helper(abs(i - 1), abs(j - 2)),
+                helper(abs(i - 2), abs(j - 1))
+            )
+        return helper(abs(x), abs(y))
