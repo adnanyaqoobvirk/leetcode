411aeb8c86fc908354ccf0891485659060387f72
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-03 15:27:42 +0800

    Time: 43 ms (48.70%), Space: 13.9 MB (22.42%) - LeetHub

--- 276-paint-fence/276-paint-fence.py
+++ 276-paint-fence/276-paint-fence.py
@@ -1,8 +1,6 @@
 class Solution:
     def numWays(self, n: int, k: int) -> int:
-        dp = [0] * (n + 2)
-        dp[1] = k
-        dp[2] = k * k
-        for pos in range(3, n + 1):
-            dp[pos] = (k - 1) * (dp[pos - 1] + dp[pos - 2])
-        return dp[n]
+        prev, curr = k, k * k
+        for pos in range(n - 1):
+            prev, curr = curr, (k - 1) * (curr + prev)
+        return prev
