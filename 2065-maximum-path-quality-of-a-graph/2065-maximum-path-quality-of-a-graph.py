659f689675d98ff3efa3fd2f84496d3e341a4f32
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-04 00:08:47 +0800

    Time: 7935 ms (5.23%), Space: 17.1 MB (34.44%) - LeetHub

--- 2065-maximum-path-quality-of-a-graph/2065-maximum-path-quality-of-a-graph.py
+++ 2065-maximum-path-quality-of-a-graph/2065-maximum-path-quality-of-a-graph.py
@@ -1,27 +1,28 @@
 class Solution:
     def maximalPathQuality(self, values: List[int], edges: List[List[int]], maxTime: int) -> int:
-        def helper(curr: int, time: int, quality: int) -> None:
-            if time <= maxTime:
-                if curr == 0:
-                    nonlocal max_quality
-                    max_quality = max(max_quality, quality)
-
-                for neighbor, ntime in graph[curr]:
-                    if neighbor not in path:
-                        quality += values[neighbor]
-                    path[neighbor] += 1
-                    helper(neighbor, time + ntime, quality)
-                    path[neighbor] -= 1
-                    if path[neighbor] == 0:
-                        del path[neighbor]
-                        quality -= values[neighbor]
-                        
+        def helper(curr: int, time: int, quality: int) -> int:
+            if time > maxTime:
+                return 0
+            
+            max_quality = quality if curr == 0 else 0
+            for neighbor, ntime in graph[curr]:
+                if neighbor not in path:
+                    quality += values[neighbor]
+                    path[neighbor] = 0
+                    
+                path[neighbor] += 1
+                max_quality = max(max_quality, helper(neighbor, time + ntime, quality))
+                path[neighbor] -= 1
+                
+                if path[neighbor] == 0:
+                    del path[neighbor]
+                    quality -= values[neighbor]
+            return max_quality
+        
         graph = defaultdict(list)
         for src, dst, time in edges:
             graph[src].append((dst, time))
             graph[dst].append((src, time))
         
-        max_quality, path = 0, defaultdict(int)
-        path[0] += 1
-        helper(0, 0, values[0])
-        return max_quality
+        path = {0: 1}
+        return helper(0, 0, values[0])
