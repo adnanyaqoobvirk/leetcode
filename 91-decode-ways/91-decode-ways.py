5df8e18367cdecfb5222d19b97a211d92f7acd48
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-04 13:26:23 +0800

    Time: 32 ms (90.81%), Space: 14.2 MB (10.64%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -5,10 +5,12 @@ class Solution:
             if pos == n:
                 return 1
             
-            return (
-                helper(pos + 1) if s[pos] != '0' else 0
-            ) + (
-                helper(pos + 2) if pos < n - 1 and 10 <= int(f"{s[pos]}{s[pos + 1]}") <= 26 else 0
-            )
+            count = 0
+            if s[pos] != '0':
+                count += helper(pos + 1)
+            
+            if pos < n - 1 and 10 <= int(f"{s[pos]}{s[pos + 1]}") <= 26:
+                count += helper(pos + 2)
+            return count
         n = len(s)
         return helper(0)
