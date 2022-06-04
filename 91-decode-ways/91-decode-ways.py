ceebe3490f0f128d8383a64e6df2450bdae19944
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-04 13:34:29 +0800

    Time: 68 ms (9.18%), Space: 13.8 MB (79.03%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,14 +1,13 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         n = len(s)
-        dp = [0] * (n + 2)
-        dp[n] = 1
+        prev, curr = 0, 1
         for pos in reversed(range(n)):
             count = 0
             if s[pos] != '0':
-                count += dp[pos + 1]
+                count += curr
             
             if pos < n - 1 and 10 <= int(f"{s[pos]}{s[pos + 1]}") <= 26:
-                count += dp[pos + 2]
-            dp[pos] = count
-        return dp[0]
+                count += prev
+            prev, curr = curr, count
+        return curr
