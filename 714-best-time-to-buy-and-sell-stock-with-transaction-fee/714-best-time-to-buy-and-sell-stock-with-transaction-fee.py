9617b116a858a5df81fc11beb2b273ed7eb7d327
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-07 15:51:11 +0800

    Time: 1176 ms (39.96%), Space: 21.3 MB (55.40%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,20 +1,19 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        @cache
-        def helper(pos: int, bought: bool) -> int:
-            if pos == n:
-                return 0
-            
-            do_nothing = helper(pos + 1, bought)
-            
-            if bought:
-                do_something = prices[pos] + helper(pos + 1, False)
-            else:
-                do_something = helper(pos + 1, True) - prices[pos] - fee
-            
-            return max(
-                do_nothing,
-                do_something
-            )
         n = len(prices)
-        return helper(0, False)
+        prev, curr = [0, 0], [0, 0]
+        for pos in reversed(range(n)):
+            for bought in range(2):
+                do_nothing = prev[bought]
+
+                if bought:
+                    do_something = prices[pos] + prev[0]
+                else:
+                    do_something = prev[1] - prices[pos] - fee
+
+                curr[bought] = max(
+                    do_nothing,
+                    do_something
+                )
+            prev, curr = curr, prev
+        return prev[0]
