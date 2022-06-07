73ecb63715a08565ae0fffb065c0776fa4038911
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-07 22:16:58 +0800

    Time: 353 ms, Memory: 14 MB - LeetHub

---
+++ paint-house-ii/paint-house-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minCostII(self, costs: List[List[int]]) -> int:
+        n, k = len(costs), len(costs[0])
+        prev, curr = [0] * k, [0] * k
+        for house in reversed(range(n)):
+            for rc in range(k):
+                curr[rc] = float('inf')
+                for c in range(k):
+                    if c == rc:
+                        continue
+                    curr[rc] = min(curr[rc], costs[house][c] + prev[c])
+            prev, curr = curr, prev
+        return min(prev)
