d6a8dd44b8b609efa8fddd1ce08e7c8a479459cd
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-07 10:56:43 +0800

    Time: 969 ms (92.44%), Space: 35.5 MB (32.14%) - LeetHub

--- 2296-design-a-text-editor/2296-design-a-text-editor.py
+++ 2296-design-a-text-editor/2296-design-a-text-editor.py
@@ -1,69 +1,35 @@
-class Node:
-    def __init__(self, ch: str, prev: 'Node' = None, nxt: 'Node' = None) -> None:
-        self.ch = ch
-        self.prev = prev
-        self.next = nxt
-    
-    def __repr__(self):
-        ans, cursor = [], self
-        while cursor:
-            ans.append(cursor.ch)
-            cursor = cursor.next
-        return "".join(ans)
-        
 class TextEditor:
 
     def __init__(self):
-        self.head = self.cursor = Node('#')
-        
+        self.prefix = []
+        self.suffix = []
+
     def addText(self, text: str) -> None:
         for c in text:
-            next_prev = self.cursor.next
-            self.cursor.next = Node(c, self.cursor, self.cursor.next)
-            self.cursor = self.cursor.next
-            if next_prev:
-                next_prev.prev = self.cursor
-
+            self.prefix.append(c)
+            
     def deleteText(self, k: int) -> int:
         deleted = 0
         for _ in range(k):
-            if self.cursor.ch == '#':
+            if not self.prefix:
                 break
-            prev, nxt = self.cursor.prev, self.cursor.next
-            self.cursor.next = self.cursor.prev = None
-            self.cursor = prev
-            self.cursor.next = nxt
-            if self.cursor.next:
-                self.cursor.next.prev = self.cursor
+            self.prefix.pop()
             deleted += 1
         return deleted
-        
+
     def cursorLeft(self, k: int) -> str:
         for _ in range(k):
-            if self.cursor.ch == '#':
-                return ""
-            self.cursor = self.cursor.prev
-        ans, cursor = [], self.cursor
-        for _ in range(10):
-            if cursor.ch == '#':
+            if not self.prefix:
                 break
-            ans.append(cursor.ch)
-            cursor = cursor.prev
-        return "".join(reversed(ans))
-
+            self.suffix.append(self.prefix.pop())
+        return "".join(self.prefix[max(0, len(self.prefix) - 10):])
+    
     def cursorRight(self, k: int) -> str:
         for _ in range(k):
-            if not self.cursor.next:
+            if not self.suffix:
                 break
-            self.cursor = self.cursor.next
-        ans, cursor = [], self.cursor
-        for _ in range(10):
-            if cursor.ch == '#':
-                break
-            ans.append(cursor.ch)
-            cursor = cursor.prev
-        return "".join(reversed(ans))
-
+            self.prefix.append(self.suffix.pop())
+        return "".join(self.prefix[max(0, len(self.prefix) - 10):])
 
 # Your TextEditor object will be instantiated and called as such:
 # obj = TextEditor()
