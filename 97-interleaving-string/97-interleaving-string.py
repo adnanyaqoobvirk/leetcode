07cc9448851a5ffc07eb548c1e424419b1bb5095
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-10 22:41:08 +0800

    Time: 69 ms (36.44%), Space: 15.8 MB (11.02%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -1,7 +1,9 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         @cache
-        def helper(p1: int, p2: int, p3: int) -> bool:
+        def helper(p1: int, p2: int) -> bool:
+            p3 = p1 + p2
+            
             if p1 == n1 and p2 == n2 and p3 == n3:
                 return True
             
@@ -10,10 +12,12 @@ class Solution:
             
             ans = False
             if p1 < n1 and s1[p1] == s3[p3]:
-                ans |= helper(p1 + 1, p2, p3 + 1)
+                ans |= helper(p1 + 1, p2)
             
             if p2 < n2 and s2[p2] == s3[p3]:
-                ans |= helper(p1, p2 + 1, p3 + 1)
+                ans |= helper(p1, p2 + 1)
             return ans
         n1, n2, n3 = len(s1), len(s2), len(s3)
-        return helper(0, 0, 0)
+        return helper(0, 0)
+    
+        
