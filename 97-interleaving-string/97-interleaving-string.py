cac5f6d581a7d0a1681d161a7a5c9715f81a46dd
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-10 22:54:10 +0800

    Time: 51 ms (71.50%), Space: 14 MB (91.09%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -1,23 +1,24 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        @cache
-        def helper(p1: int, p2: int) -> bool:
-            p3 = p1 + p2
-            
-            if p1 == n1 and p2 == n2 and p3 == n3:
-                return True
-            
-            if p3 == n3:
-                return False
-            
-            ans = False
-            if p1 < n1 and s1[p1] == s3[p3]:
-                ans |= helper(p1 + 1, p2)
-            
-            if p2 < n2 and s2[p2] == s3[p3]:
-                ans |= helper(p1, p2 + 1)
-            return ans
         n1, n2, n3 = len(s1), len(s2), len(s3)
-        return helper(0, 0)
-    
+        
+        if n1 + n2 != n3:
+            return False
+        
+        dp = [[False] * (n2 + 1) for _ in range(n1 + 1)]
+        dp[n1][n2] = True
+        for p1 in reversed(range(n1 + 1)):
+            for p2 in reversed(range(n2 + 1)):
+                p3 = p1 + p2
+                
+                if p1 == n1 and p2 == n2 and p3 == n3:
+                    dp[p1][p2] = True
+                else:
+                    dp[p1][p2] = False
+                    if p1 < n1 and s1[p1] == s3[p3]:
+                        dp[p1][p2] |= dp[p1 + 1][p2]
+
+                    if p2 < n2 and s2[p2] == s3[p3]:
+                        dp[p1][p2] |= dp[p1][p2 + 1]
+        return dp[0][0]
         
