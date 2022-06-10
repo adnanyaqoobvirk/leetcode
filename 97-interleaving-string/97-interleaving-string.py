6b2ecfb2ff2c52500d0b2d47d3339e51b195205d
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-10 22:56:54 +0800

    Time: 101 ms (8.98%), Space: 14 MB (91.09%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -5,20 +5,20 @@ class Solution:
         if n1 + n2 != n3:
             return False
         
-        dp = [[False] * (n2 + 1) for _ in range(n1 + 1)]
-        dp[n1][n2] = True
+        prev, curr = [False] * (n2 + 1), [False] * (n2 + 1)
         for p1 in reversed(range(n1 + 1)):
             for p2 in reversed(range(n2 + 1)):
                 p3 = p1 + p2
                 
                 if p1 == n1 and p2 == n2 and p3 == n3:
-                    dp[p1][p2] = True
+                    curr[p2] = True
                 else:
-                    dp[p1][p2] = False
+                    curr[p2] = False
                     if p1 < n1 and s1[p1] == s3[p3]:
-                        dp[p1][p2] |= dp[p1 + 1][p2]
+                        curr[p2] |= prev[p2]
 
                     if p2 < n2 and s2[p2] == s3[p3]:
-                        dp[p1][p2] |= dp[p1][p2 + 1]
-        return dp[0][0]
+                        curr[p2] |= curr[p2 + 1]
+            prev, curr = curr, prev
+        return prev[0]
         
