e803a0d780965e68966b3071b92ed161de099cee
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-13 21:39:39 +0800

    Time: 806 ms, Memory: 20.7 MB - LeetHub

---
+++ number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+        @cache
+        def helper(dice: int, remaining: int) -> int:
+            if dice == n:
+                return remaining == 0
+            
+            ways = 0
+            for face in range(1, k + 1):
+                ways += helper(dice + 1, remaining - face)
+            return ways % MOD
+        MOD = 10**9 + 7
+        return helper(0, target)
