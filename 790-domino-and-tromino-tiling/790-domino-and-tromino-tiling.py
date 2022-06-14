fda049bd722ff640dc529abfd033e2b93a02e636
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-14 13:34:16 +0800

    Time: 111 ms (7.47%), Space: 16.6 MB (10.80%) - LeetHub

--- 790-domino-and-tromino-tiling/790-domino-and-tromino-tiling.py
+++ 790-domino-and-tromino-tiling/790-domino-and-tromino-tiling.py
@@ -1,17 +1,20 @@
 class Solution:
     def numTilings(self, n: int) -> int:
         @cache
-        def domino(pos: int) -> int:
+        def helper(pos: int, domino: bool) -> int:
             if pos <= 2:
                 return pos
             
-            return (domino(pos - 1) + domino(pos - 2) + 2 * tromino(pos - 2)) % MOD
-        
-        @cache
-        def tromino(pos: int) -> int:
-            if pos <= 2:
-                return pos
-            
-            return (tromino(pos - 1) + domino(pos - 1)) % MOD
-        MOD = 10**9+7
-        return domino(n)
+            if domino:
+                return (
+                    helper(pos - 1, True) + 
+                    helper(pos - 2, True) + 
+                    2 * helper(pos - 2, False)
+                ) % MOD
+            else:
+                return (
+                    helper(pos - 1, False) +
+                    helper(pos - 1, True)
+                ) % MOD
+        MOD = 10**9 + 7
+        return helper(n, True)
