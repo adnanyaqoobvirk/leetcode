4446fef3e0675f60b5877050a43aab74f49c1763
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-14 13:59:36 +0800

    Time: 56 ms (45.73%), Space: 13.8 MB (83.20%) - LeetHub

--- 790-domino-and-tromino-tiling/790-domino-and-tromino-tiling.py
+++ 790-domino-and-tromino-tiling/790-domino-and-tromino-tiling.py
@@ -1,20 +1,19 @@
 class Solution:
     def numTilings(self, n: int) -> int:
-        @cache
-        def helper(pos: int, domino: bool) -> int:
-            if pos <= 2:
-                return pos
-            
-            if domino:
-                return (
-                    helper(pos - 1, True) + 
-                    helper(pos - 2, True) + 
-                    2 * helper(pos - 2, False)
-                ) % MOD
-            else:
-                return (
-                    helper(pos - 1, False) +
-                    helper(pos - 1, True)
-                ) % MOD
+        if n <= 2:
+            return n
+    
         MOD = 10**9 + 7
-        return helper(n, True)
+        pprev, prev, curr = [1, 1], [2, 2], [0, 0]
+        for pos in range(n - 2):
+            curr[0] = (
+                prev[0] +
+                prev[1]
+            ) % MOD 
+            curr[1] = (
+                prev[1] + 
+                pprev[1] + 
+                2 * pprev[0]
+            ) % MOD
+            pprev, prev, curr = prev, curr, [0, 0]
+        return prev[1]
