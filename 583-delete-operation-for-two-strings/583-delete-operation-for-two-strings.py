33774cea1af582d580139ae7a8121c055ee54482
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-14 10:31:43 +0800

    Time: 422 ms (41.32%), Space: 14 MB (89.05%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
@@ -1,18 +1,18 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        @cache
-        def helper(p1: int, p2: int) -> int:
-            if p1 == m:
-                return n - p2
-            
-            if p2 == n:
-                return m - p1
-            
-            return min(
-                1 + helper(p1 + 1, p2),
-                1 + helper(p1, p2 + 1),
-                helper(p1 + 1, p2 + 1) if word1[p1] == word2[p2] else inf
-            )
-        
         m, n = len(word1), len(word2)
-        return helper(0, 0)
+        prev, curr = [inf] * (n + 1), [inf] * (n + 1)
+        for p1 in reversed(range(m + 1)):
+            for p2 in reversed(range(n + 1)):
+                if p1 == m:
+                    curr[p2] = n - p2
+                elif p2 == n:
+                    curr[p2] = m - p1
+                else:
+                    curr[p2] = min(
+                        1 + prev[p2],
+                        1 + curr[p2 + 1],
+                        prev[p2 + 1] if word1[p1] == word2[p2] else inf
+                    )
+            prev, curr = curr, [inf] * (n + 1)
+        return prev[0]
