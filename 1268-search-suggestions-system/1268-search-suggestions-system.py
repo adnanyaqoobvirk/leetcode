8d000a235f1940ec2edba15f2b12e517ef3f9166
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-19 21:43:30 +0800

    Time: 196 ms (61.91%), Space: 19.9 MB (42.58%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,40 +1,28 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        def search(lo: int, hi: int, j: int) -> Tuple[int, int]:
-            if hi < lo:
-                return lo, hi
-            
-            l, r = lo, hi
-            while l < r:
-                mid = l + (r - l) // 2
-                if j < len(products[mid]) and products[mid][j] >= searchWord[j]:
-                    r = mid
-                else:
-                    l = mid + 1
-            
-            lo = l
-            
-            l, r = lo, hi
-            while l < r:
-                mid = l + (r - l) // 2
-                if j < len(products[mid]) and products[mid][j] <= searchWord[j]:
-                    l = mid + 1
-                else:
-                    r = mid
-            hi = l if j < len(products[l]) and products[l][j] == searchWord[j] else l - 1
-            
-            return lo, hi
-        
         products.sort()
         
-        ans, left, right = [], 0, len(products) - 1
-        for i in range(len(searchWord)):
-            left, right = search(left, right, i)
-            
-            ss = []
-            for k in range(left, right + 1):
-                if len(ss) == 3:
-                    break
-                ss.append(products[k])
-            ans.append(ss)
+        trie = {}
+        for i, p in enumerate(products):
+            t = trie
+            for c in p:
+                if c not in t:
+                    t[c] = {'$':[]}
+                t = t[c]
+                if len(t['$']) < 3:
+                    t['$'].append(i)
+        
+        ans = []
+        t = trie
+        for i, c in enumerate(searchWord):
+            if c not in t:
+                for j in range(i, len(searchWord)):
+                    ans.append([])
+                break
+            else:
+                t = t[c]
+                ss = []
+                for j in t['$']:
+                    ss.append(products[j])
+                ans.append(ss)
         return ans
