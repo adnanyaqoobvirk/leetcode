3b1f92dd486d48495d935ad8ce2535d2c4ceba0c
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-20 15:47:04 +0800

    Time: 1189 ms (67.00%), Space: 0B (100.00%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -4,13 +4,15 @@ from
 (
     select
         customer_id,
-        sum(if(amount is null, 1, 0)) as count_no_trans
+        count(customer_id) as count_no_trans
     from
         Visits v
         left join
         Transactions tr
         on
             v.visit_id = tr.visit_id
+    where
+        amount is null
     group by
         customer_id
 ) t
