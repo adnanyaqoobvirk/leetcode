e576920cc162d1a8ea69b78a37db8620cbe56625
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-20 14:13:09 +0800

    Time: 238 ms (63.72%), Space: 16.3 MB (55.81%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.py
+++ 820-short-encoding-of-words/820-short-encoding-of-words.py
@@ -1,16 +1,16 @@
 class Solution:
     def minimumLengthEncoding(self, words: List[str]) -> int:
-        words.sort(key=lambda x: len(x), reverse=True)
-        
         trie, enc_len = {}, 0
         for word in words:
             t = trie
             for c in reversed(word):
+                if '$' in t:
+                    enc_len -= t['$']
+                    del t['$']
                 if c not in t:
                     t[c] = {}
                 t = t[c]
-            if '$' not in t:
+            if not t:
                 t['$'] = len(word) + 1
-                if len(t) == 1:
-                    enc_len += t['$']
+                enc_len += t['$']
         return enc_len
