04e8a6de8d3105f7c292fd2e8431809ee50fa263
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-20 17:02:43 +0800

    Time: 178 ms (7.95%), Space: 15.4 MB (49.98%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,26 +1,25 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        ans = [-1, -1]
-        if not nums: return ans
+        lo, hi = 0, len(nums)
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+            if nums[mid] > target:
+                hi = mid
+            else:
+                lo = mid + 1
         
-        lo, hi = 0, len(nums) - 1
-        while lo <= hi:
+        right = hi - 1
+        if right < 0 or nums[hi - 1] != target:
+            return [-1, -1]
+
+        lo = 0
+        while lo < hi:
             mid = lo + (hi - lo) // 2
             if nums[mid] >= target:
-                hi = mid - 1
+                hi = mid
             else:
                 lo = mid + 1
-        if lo == len(nums) or nums[lo] != target:
-            return ans
-        ans[0] = lo
-        
-        lo, hi = 0, len(nums) - 1
-        while lo <= hi:
-            mid = lo + (hi - lo) // 2
-            if nums[mid] <= target: 
-                lo = mid + 1
-            else:
-                hi = mid - 1
-        ans[1] = hi
-        
-        return ans
+                
+        left = lo
+        return [left, right]
+            
