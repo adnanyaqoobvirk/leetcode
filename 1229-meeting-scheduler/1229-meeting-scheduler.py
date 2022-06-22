fd546368d01e7b315c11317bc145c618393cb799
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-22 21:27:26 +0800

    Time: 910 ms (25.08%), Space: 21.5 MB (90.13%) - LeetHub

--- 1229-meeting-scheduler/1229-meeting-scheduler.py
+++ 1229-meeting-scheduler/1229-meeting-scheduler.py
@@ -1,18 +1,21 @@
 class Solution:
     def minAvailableDuration(self, slots1: List[List[int]], slots2: List[List[int]], duration: int) -> List[int]:
-        slots1.sort()
-        slots2.sort()
+        slots = [
+            slot
+            for slot in slots1
+            if slot[1] - slot[0] >= duration
+        ]
+        for slot in slots2:
+            if slot[1] - slot[0] >= duration:
+                slots.append(slot)
         
-        m, n = len(slots1), len(slots2)
+        heapify(slots)
         
-        p1 = p2 = 0
-        while p1 < m and p2 < n:
-            start = max(slots1[p1][0], slots2[p2][0])
-            end = min(slots1[p1][1], slots2[p2][1])
-            if end - start >= duration:
-                return [start, start + duration]
-            elif slots1[p1][1] < slots2[p2][1]:
-                p1 += 1
-            else:
-                p2 += 1
+        while len(slots) > 1:
+            start, end = heappop(slots)
+            cstart, cend = slots[0]
+            
+            if end >= cstart + duration:
+                return [cstart, cstart + duration]
         return []
+            
