24402d7bb012405643c490dd33813e37604ddacb
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-22 21:37:26 +0800

    Time: 920 ms (7.99%), Space: 0B (100.00%) - LeetHub

--- 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
@@ -1,9 +1,16 @@
 select
     customer_number
 from
-    Orders
-group by
-    customer_number
-order by
-    count(order_number) desc
-limit 1
+(
+    select
+        customer_number,
+        rank() over(
+            order by count(order_number) desc
+        ) as orank
+    from
+        Orders
+    group by
+        customer_number
+) t
+where
+    t.orank = 1
