a0df8002eb20e63bd087f903d7cba190dac495cb
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-22 11:29:44 +0800

    Time: 62 ms (98.06%), Space: 14.9 MB (40.87%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,25 +1,4 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        def partition(left: int, right: int, pivot_idx: int) -> int:
-            nums[right], nums[pivot_idx] = nums[pivot_idx], nums[right]
-            
-            store_idx = left
-            for i in range(left, right):
-                if nums[i] < nums[right]:
-                    nums[store_idx], nums[i] = nums[i], nums[store_idx]
-                    store_idx += 1
-            nums[store_idx], nums[right] = nums[right], nums[store_idx]
-            return store_idx
-        
-        def select(left: int, right: int, nsmallest: int) -> int:
-            if left == right:
-                return nums[left]
-            
-            pivot_idx = partition(left, right, randint(left, right))
-            if pivot_idx == nsmallest:
-                return nums[pivot_idx]
-            elif pivot_idx > nsmallest:
-                return select(left, pivot_idx - 1, nsmallest)
-            else:
-                return select(pivot_idx + 1, right, nsmallest)
-        return select(0, len(nums) - 1, len(nums) - k)
+        nums.sort()
+        return nums[-k]
