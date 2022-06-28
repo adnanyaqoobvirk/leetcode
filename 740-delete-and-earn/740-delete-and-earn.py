b5f8848f61d9baf6dd7fd6098f379414dfc54c7e
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-28 17:52:01 +0800

    Time: 145 ms (42.59%), Space: 16 MB (76.45%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.py
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -1,17 +1,15 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        @cache
-        def helper(num: int) -> int:
-            if num > max_val:
-                return 0
-            
-            if num not in counts:
-                return helper(num + 1)
-            
-            return max(
-                num * counts[num] + helper(num + 2),
-                helper(num + 1)
-            )
-        
         min_val, max_val, counts = min(nums), max(nums), Counter(nums)
-        return helper(min_val)
+        prev = curr = 0
+        for num in reversed(range(min_val, max_val + 2)):
+            if num not in counts:
+                prev = curr
+            else:
+                prev, curr = curr, max(
+                    num * counts[num] + prev,
+                    curr
+                )
+        return curr
+            
+            
