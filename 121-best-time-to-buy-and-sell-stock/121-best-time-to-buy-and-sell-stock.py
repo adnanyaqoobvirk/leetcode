4db67db809ef841a2277c18c20e8de7919258584
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-30 18:30:53 +0800

    Time: 2209 ms (7.16%), Space: 25.1 MB (37.78%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,26 +1,7 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-#         @cache
-#         def helper(pos: int, bought: int) -> int:
-#             if pos == n:
-#                 return -inf
-            
-#             if bought:
-#                 return max(
-#                     prices[pos],
-#                     helper(pos + 1, True)
-#                 )
-#             else:
-#                 return max(
-#                     helper(pos + 1, False),
-#                     -prices[pos] + helper(pos + 1, True)
-#                 )
-        
-#         n = len(prices)
-#         ans = helper(0, 0)
-#         return 0 if ans < 0 else ans
-    
         n = len(prices)
+        
         curr_bought = curr_not_bought = -inf
         for pos in reversed(range(n)):
             curr_bought, curr_not_bought = max(
