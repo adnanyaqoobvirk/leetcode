d488df4fc0f9ec07546c6231cc58808d315e2e10
Adnan Yaqoob <adnan@yaqoob.org>
2022-06-30 18:16:28 +0800

    Time: 4134 ms (5.01%), Space: 399 MB (6.48%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,9 +1,21 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_price = float('inf')
-        profit = 0
-        for price in prices:
-            profit = max(profit, price - min_price)
-            if price < min_price:
-                min_price = price
-        return profit
+        @cache
+        def helper(pos: int, bought: int) -> int:
+            if pos == n:
+                return -inf
+            
+            if bought:
+                return max(
+                    prices[pos],
+                    helper(pos + 1, True)
+                )
+            else:
+                return max(
+                    helper(pos + 1, False),
+                    -prices[pos] + helper(pos + 1, True)
+                )
+        
+        n = len(prices)
+        ans = helper(0, 0)
+        return 0 if ans < 0 else ans
