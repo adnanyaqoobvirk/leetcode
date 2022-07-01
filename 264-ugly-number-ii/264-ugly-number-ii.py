b743fba1d76fda7531af0868bd1ec76c46287bbf
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-01 19:50:12 +0800

    Time: 369 ms (24.34%), Space: 14.2 MB (17.31%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.py
+++ 264-ugly-number-ii/264-ugly-number-ii.py
@@ -1,14 +1,15 @@
-from sortedcontainers import SortedSet
-
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
         factors = [2, 3, 5]
         
-        seen = SortedSet([1])
+        seen = {1}
+        h = [1]
+        
         for _ in range(n - 1):
-            num = seen.pop(0)
+            num = heappop(h)
             for f in factors:
                 ugly = f * num
                 if ugly not in seen:
+                    heappush(h, ugly)
                     seen.add(ugly)
-        return seen.pop(0)
+        return heappop(h)
