1d2f3485197571f0e8e91c40db8b879d81c400e3
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-01 20:59:07 +0800

    Time: 70 ms (5.37%), Space: 14 MB (12.76%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.py
@@ -1,17 +1,13 @@
 class Solution:
     def numTrees(self, n: int) -> int:
+        @cache
         def helper(left: int, right: int) -> int:
-            if (left, right) in cache:
-                return cache[(left, right)]
-            
             if right - left <= 0:
                 return 1
             
             ans = 0
             for i in range(left, right + 1):
                 ans += helper(left, i - 1) * helper(i + 1, right)
-            cache[(left, right)] = ans
+                
             return ans
-        
-        cache = {}
         return helper(1, n)
