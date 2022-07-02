58087ab83585caafc893e14155a9aedec63cded7
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-02 14:01:08 +0800

    Time: 103 ms (54.42%), Space: 14.9 MB (92.29%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -3,14 +3,14 @@ class Solution:
         n = len(triangle)
         
         prev = triangle[-1][::]
-        curr = [0] * (len(triangle[-1]) - 1)
+        curr = [0] * len(triangle[-1])
         for row in reversed(range(n - 1)):
             for col in range(row + 1):
                 curr[col] = triangle[row][col] + min(
                     prev[col],
                     prev[col + 1]
                 )
-            prev, curr = curr, [0] * row
+            prev, curr = curr, prev
         return prev[0]
     
         
