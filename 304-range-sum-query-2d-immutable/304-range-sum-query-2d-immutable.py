b8dca2122d5ec3660a994fa9bc4cc4ba901eced3
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-02 19:34:58 +0800

    Time: 1516 ms (94.68%), Space: 25.1 MB (36.67%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
@@ -1,35 +1,21 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        m, n = len(matrix), len(matrix[0])
-        for i in range(m):
-            for j in range(n):
-                total = matrix[i][j]
-                if i > 0:
-                    total += matrix[i - 1][j]
-                    if j > 0:
-                        total -= matrix[i - 1][j - 1]
-                if j > 0:
-                    total += matrix[i][j - 1]
-                    if i > 0:
-                        total -= matrix[i - 1][j - 1]
-                if i > 0 and j > 0:
-                    total += matrix[i - 1][j - 1]
-                matrix[i][j] = total
-        self.matrix = matrix
+        self.m, self.n = len(matrix), len(matrix[0])
+        
+        self.cmat = [[0] * (self.n + 1) for _ in range(self.m + 1)]
+        for i in range(1, self.m + 1):
+            for j in range(1, self.n + 1):
+                self.cmat[i][j] = (
+                    matrix[i - 1][j - 1] + self.cmat[i - 1][j] + 
+                    self.cmat[i][j - 1] - self.cmat[i - 1][j - 1]
+                )
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        ans = self.matrix[row2][col2]
-        if row1 > 0:
-            ans -= self.matrix[row1 - 1][col2]
-        
-        if col1 > 0:
-            ans -= self.matrix[row2][col1 - 1]
-        
-        if row1 > 0 and col1 > 0:
-            ans += self.matrix[row1 - 1][col1 - 1]
-        return ans
-
+        return (
+            self.cmat[row2 + 1][col2 + 1] - self.cmat[row1][col2 + 1] - 
+            self.cmat[row2 + 1][col1] + self.cmat[row1][col1]
+        )
 
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
