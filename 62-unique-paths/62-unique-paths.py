474dc0d7410d21390923a3b4bf5e96579d89e403
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-02 20:29:58 +0800

    Time: 55 ms (31.11%), Space: 14.3 MB (7.00%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,10 +1,12 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        prev = [0] * (n + 1)
-        curr = [0] * (n + 1)
-        curr[1] = 1
-        for _ in range(m):
-            for j in range(1, n + 1):
-                curr[j] += prev[j] + curr[j - 1]
-            prev, curr = curr, [0] * (n + 1)
-        return prev[-1]
+        @cache
+        def helper(i: int, j: int) -> int:
+            if i < 0 or i >= m or j < 0 or j >= n:
+                return 0
+            
+            if i == m - 1 and j == n - 1:
+                return 1
+            
+            return helper(i + 1, j) + helper(i, j + 1)
+        return helper(0, 0)
