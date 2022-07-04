ef1b5b10de470e67b9a5bfff9e55d1d79fe1fd3e
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-04 23:15:22 +0800

    Time: 930 ms (8.70%), Space: 0B (100.00%) - LeetHub

--- 2084-drop-type-1-orders-for-customers-with-type-0-orders/2084-drop-type-1-orders-for-customers-with-type-0-orders.sql
+++ 2084-drop-type-1-orders-for-customers-with-type-0-orders/2084-drop-type-1-orders-for-customers-with-type-0-orders.sql
@@ -3,29 +3,15 @@ select
 from
     Orders
 where
-    customer_id not in (
-        select
-            customer_id
-        from
-            Orders
-        where
-            order_type = 0
+    not (
+        order_type = 1 
+        and 
+        customer_id in (
+            select
+                customer_id
+            from
+                Orders
+            where
+                order_type = 0
+        )
     )
-
-union all
-
-select
-    *
-from
-    Orders
-where
-    customer_id in (
-        select
-            customer_id
-        from
-            Orders
-        where
-            order_type = 0
-    )
-    and
-    order_type != 1
