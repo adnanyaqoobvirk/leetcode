7ea481a906514cf8fad75d799f2904892243e814
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-05 00:26:05 +0800

    Time: 70 ms (23.13%), Space: 14.2 MB (48.85%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.py
+++ 413-arithmetic-slices/413-arithmetic-slices.py
@@ -4,12 +4,8 @@ class Solution:
         
         ans = 0
         for i in range(n - 2):
-            diff = nums[i + 2] - nums[i + 1]
-            if diff != nums[i + 1] - nums[i]:
-                continue
-            ans += 1
-            
-            for j in range(i + 3, n):
+            diff = nums[i + 1] - nums[i]
+            for j in range(i + 2, n):
                 if diff != nums[j] - nums[j - 1]:
                     break
                 ans += 1
