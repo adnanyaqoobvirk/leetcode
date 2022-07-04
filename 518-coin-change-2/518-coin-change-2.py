f7c092e6e88125860108af685349291a56b9c7c2
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-04 21:50:02 +0800

    Time: 565 ms (42.90%), Space: 14.2 MB (52.22%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -1,13 +1,15 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
+        n = len(coins)
+        
         prev, curr = [0] * (amount + 1), [0] * (amount + 1)
         prev[0] = curr[0] = 1
-        for pos in reversed(range(len(coins))):
-            for remaining in range(1, amount + 1):
-                curr[remaining] = (
-                    ((curr[remaining - coins[pos]]) if remaining >= coins[pos] else 0)
-                    + prev[remaining]
-                )
-            prev, curr = curr, [0] * (amount + 1)
-            curr[0] = 1
+        for pos in reversed(range(n)):
+            for remaining in range(amount + 1):
+                if remaining - coins[pos] < 0:
+                    curr[remaining] = prev[remaining]
+                else:
+                    curr[remaining] = curr[remaining - coins[pos]] + prev[remaining]
+            prev, curr = curr, prev
         return prev[amount]
+                
