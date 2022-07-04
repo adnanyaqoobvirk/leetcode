3d672ccdf0caa362559405662b9e077f838d2753
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-04 14:35:17 +0800

    Time: 55 ms (21.71%), Space: 45.7 MB (32.15%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,22 +1,26 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        double ans = helper(coins, 0, amount, new double[coins.length + 1][amount + 1]);
-        return ans == Double.POSITIVE_INFINITY ? -1 : (int) ans;
+        int ans = helper(
+            coins, amount, new int[amount + 1]
+        );
+        
+        return (ans == Integer.MAX_VALUE) ? -1 : ans;
     }
     
-    private double helper(int[] coins, int i, int remaining, double[][] dp) {
-        if (i == coins.length || remaining < 0) 
-            return Double.POSITIVE_INFINITY;
+    private int helper(int[] coins, int remaining, int[] dp) {
+        if (remaining < 0) 
+            return Integer.MAX_VALUE;
         
         if (remaining == 0)
-            return 0.0;
+            return 0;
         
-        if (dp[i][remaining] == 0.0)
-            dp[i][remaining] = Math.min(
-                1 + helper(coins, i, remaining - coins[i], dp),
-                helper(coins, i + 1, remaining, dp)
-            );
+        if (dp[remaining] == 0){
+            int ans = Integer.MAX_VALUE;
+            for (int coin : coins)
+                ans = Math.min(ans, helper(coins, remaining - coin, dp));
+            dp[remaining] = (ans == Integer.MAX_VALUE) ? Integer.MAX_VALUE : ans + 1;
+        }
         
-        return dp[i][remaining];
+        return dp[remaining];
     }
 }
