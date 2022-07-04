215a5b04ac6471c786d6a08006893a3ae1fd1de6
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-04 22:41:53 +0800

    Time: 5 ms (71.55%), Space: 41.9 MB (68.21%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -3,11 +3,11 @@ class Solution {
         int[] prev = new int[amount + 1], curr = new int[amount + 1];
         prev[0] = curr[0] = 1;
         for(int pos = coins.length - 1; pos >= 0; pos--){
-            for(int remaining = 0; remaining <= amount; remaining++)
-                if(remaining - coins[pos] < 0)
-                    curr[remaining] = prev[remaining];
-                else
-                    curr[remaining] = curr[remaining - coins[pos]] + prev[remaining];
+            for(int remaining = 0; remaining <= amount; remaining++){
+                curr[remaining] = prev[remaining];
+                if(remaining - coins[pos] >= 0)
+                    curr[remaining] += curr[remaining - coins[pos]];
+            }
             int[] tmp = prev;
             prev = curr;
             curr = tmp;
