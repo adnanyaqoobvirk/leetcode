ef618c7ee86ecc987fcc56c62bcd71ae29224a58
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-04 15:06:38 +0800

    Time: 688 ms (97.31%), Space: 14.9 MB (33.35%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,20 +1,20 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        @cache
-        def helper(remaining: int) -> int:
-            if remaining < 0:
-                return inf
-            
-            if remaining == 0:
-                return 0
-            
-            ans = inf
-            for coin in coins:
-                ans = min(
-                    ans, helper(remaining - coin)
-                )
-            return ans + 1
-        
-        n = len(coins)
-        ans = helper(amount)
-        return ans if ans != inf else -1
+        q = [amount]
+        seen = {amount}
+        ans = 0
+        while q:
+            nq = []
+            for r in q:
+                if r == 0:
+                    return ans
+                
+                if r > 0:
+                    for coin in coins:
+                        if r - coin in seen:
+                            continue
+                        nq.append(r - coin)
+                        seen.add(r - coin)
+            q = nq
+            ans += 1
+        return -1
