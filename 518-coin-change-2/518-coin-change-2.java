a5c936baa9321e74c590ddebfb5402aa0b88550b
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-04 22:40:04 +0800

    Time: 5 ms (71.55%), Space: 40 MB (92.28%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -1,26 +1,17 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int[][] dp = new int[coins.length + 1][amount + 1];
-        for(int i = 0; i < coins.length + 1; i++)
-            Arrays.fill(dp[i], -1);
-            
-        return helper(
-            coins, 0, amount, dp
-        );
-    }
-    
-    private int helper(int[] coins, int pos, int remaining, int[][] dp){
-        if (remaining == 0)
-            return 1;
-        
-        if (pos == coins.length || remaining < 0)
-            return 0;
-        
-        if (dp[pos][remaining] == -1)
-            dp[pos][remaining] = helper(
-                coins, pos, remaining - coins[pos], dp
-            ) + helper(coins, pos + 1, remaining, dp);
-        
-        return dp[pos][remaining];
+        int[] prev = new int[amount + 1], curr = new int[amount + 1];
+        prev[0] = curr[0] = 1;
+        for(int pos = coins.length - 1; pos >= 0; pos--){
+            for(int remaining = 0; remaining <= amount; remaining++)
+                if(remaining - coins[pos] < 0)
+                    curr[remaining] = prev[remaining];
+                else
+                    curr[remaining] = curr[remaining - coins[pos]] + prev[remaining];
+            int[] tmp = prev;
+            prev = curr;
+            curr = tmp;
+        }
+        return prev[amount];
     }
 }
