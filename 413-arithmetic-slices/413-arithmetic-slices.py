17ab79bf60e8b75f68ec79aff82f5fed87d059a1
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-05 00:21:50 +0800

    Time: 67 ms (27.72%), Space: 14.1 MB (92.25%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.py
+++ 413-arithmetic-slices/413-arithmetic-slices.py
@@ -1,9 +1,17 @@
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        n = len(nums)
+        
         ans = 0
-        for i in range(2, len(nums)):
-            for j in range(i, len(nums)):
-                if nums[j] - nums[j - 1] != nums[j - 1] - nums[j - 2]:
+        for i in range(n - 2):
+            diff = nums[i + 2] - nums[i + 1]
+            if diff != nums[i + 1] - nums[i]:
+                continue
+            ans += 1
+            
+            for j in range(i + 3, n):
+                if diff != nums[j] - nums[j - 1]:
                     break
                 ans += 1
         return ans
+                
