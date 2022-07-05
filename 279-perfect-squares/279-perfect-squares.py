afa2d65fdd001c2fdcab0cbfc2a0572b2ccca786
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-05 17:50:23 +0800

    Time: 1161 ms (68.82%), Space: 15.6 MB (19.80%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.py
+++ 279-perfect-squares/279-perfect-squares.py
@@ -2,20 +2,18 @@ class Solution:
     def numSquares(self, n: int) -> int:
         squares = [num**2 for num in range(1, int(sqrt(n)) + 1)]
         
-        q = [n]
-        seen = {n}
+        q = {n}
         ans = 0
         while q:
-            nq = []
+            nq = set()
             for remaining in q:
                 if remaining == 0:
                     return ans
                 
                 for j in range(int(sqrt(remaining))):
                     nr = remaining - squares[j]
-                    if nr not in seen:
-                        seen.add(nr)
-                        nq.append(nr)
+                    if nr not in nq:
+                        nq.add(nr)
             q = nq
             ans += 1
         return ans
