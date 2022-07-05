b01ca88c588031e512f50d38d4a856ced4766dfa
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-05 15:35:16 +0800

    Time: 2206 ms (34.29%), Space: 28.6 MB (42.76%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -4,17 +4,11 @@ class Solution:
         
         ans = 0
         for num in nums:
-            count = 0
-            nnum = num
-            while nnum in num_set:
-                num_set.remove(nnum)
-                count += 1
-                nnum -= 1
-            
-            nnum = num + 1
-            while nnum in num_set:
-                num_set.remove(nnum)
-                count += 1
-                nnum += 1
-            ans = max(ans, count)
+            if num - 1 not in num_set:
+                count = 0
+                nnum = num
+                while nnum in num_set:
+                    count += 1
+                    nnum += 1
+                ans = max(ans, count)
         return ans
