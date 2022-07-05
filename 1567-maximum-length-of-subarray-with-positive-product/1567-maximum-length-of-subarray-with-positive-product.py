f380d64c1fba3f7ee5e56ff72328bcb353c22abc
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-06 00:42:45 +0800

    Time: 683 ms (63.31%), Space: 24.6 MB (83.87%) - LeetHub

--- 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.py
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.py
@@ -6,28 +6,27 @@ class Solution(object):
         """
         nums.append(0)
         
-        ans = 0
-        neg_cnt = 0
-        left = 0
+        ans = neg_cnt = left = 0
+        first_neg = last_neg = -1
         for right, num in enumerate(nums):
             if num == 0:
                 if neg_cnt & 1:
-                    left_pos_cnt = 1
-                    for i in range(left, right):
-                        if nums[i] < 0:
-                            break
-                        left_pos_cnt += 1
-                    
-                    right_pos_cnt = 1
-                    for i in reversed(range(left, right)):
-                        if nums[i] < 0:
-                            break
-                        right_pos_cnt += 1
-                    ans = max(ans, right - left - min(left_pos_cnt, right_pos_cnt))
+                    ans = max(
+                        ans, 
+                        right - left - min(
+                            first_neg - left + 1, right - last_neg
+                        )
+                    )
                 else:
                     ans = max(ans, right - left)
+                
                 neg_cnt = 0
                 left = right + 1
+                first_neg = last_neg = -1
             elif num < 0:
+                if first_neg < 0:
+                    first_neg = last_neg = right
+                else:
+                    last_neg = right
                 neg_cnt += 1
         return ans
