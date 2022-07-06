51dac39dddbebc895e5b7e60bc82ba1a3d7cf038
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-06 19:40:50 +0800

    Time: 1006 ms (18.02%), Space: 0B (100.00%) - LeetHub

--- 1699-number-of-calls-between-two-persons/1699-number-of-calls-between-two-persons.sql
+++ 1699-number-of-calls-between-two-persons/1699-number-of-calls-between-two-persons.sql
@@ -1,19 +1,10 @@
-with PersonCalls as (
-    select
-        if(from_id < to_id, from_id, to_id) as person1,
-        if(from_id > to_id, from_id, to_id) as person2,
-        duration
-    from
-        Calls
-)
-
 select
-    person1,
-    person2,
+    if(from_id < to_id, from_id, to_id) as person1,
+    if(from_id > to_id, from_id, to_id) as person2,
     count(duration) as call_count,
     sum(duration) as total_duration
 from
-    PersonCalls
+    Calls
 group by
     person1,
     person2
