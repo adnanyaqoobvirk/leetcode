e15b2edf5c3c48b19a464eaf0ff1de50411efae4
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-06 23:02:17 +0800

    Time: 244 ms (34.87%), Space: 27.1 MB (13.90%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
@@ -1,23 +1,20 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        def isSumEqualGreater(l: int) -> bool:
-            total = 0
-            for i in range(l):
-                total += nums[i]
-            
-            for j in range(i + 1, len(nums)):
-                if total >= target:
-                    return True
-                
-                total -= nums[j - l]
-                total += nums[j]
-            return total >= target
+        nums.append(0)
         
-        lo, hi = 0, len(nums)
-        while lo < hi:
-            mid = lo + (hi - lo) // 2
-            if isSumEqualGreater(mid + 1):
-                hi = mid
-            else:
-                lo = mid + 1
-        return 0 if lo == len(nums) else lo + 1
+        n = len(nums)
+        
+        left = total = 0
+        for right in range(n):
+            total += nums[right]
+            if total >= target:
+                break
+        
+        ans = inf
+        for right in range(right + 1, n):
+            while total >= target:
+                total -= nums[left]
+                left += 1
+            ans = min(ans, right - left + 1)
+            total += nums[right]
+        return 0 if ans == inf else ans
