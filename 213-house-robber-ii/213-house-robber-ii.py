8f0a1a341e254c7ddb24721abd27a2fc9fc1eb06
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-07 18:09:11 +0800

    Time: 87 ms (5.60%), Space: 14.1 MB (6.25%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -1,19 +1,19 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        
         @cache
-        def helper(pos: int, first: bool) -> int:
-            if first and pos >= n - 1:
-                return 0
-            
-            if not first and pos >= n:
+        def helper(pos: int, m: int) -> int:
+            if pos >= m:
                 return 0
             
             return max(
-                nums[pos] + helper(pos + 2, first),
-                helper(pos + 1, first)
+                nums[pos] + helper(pos + 2, m),
+                helper(pos + 1, m)
             )
         n = len(nums)
         return max(
-            nums[0] + helper(2, True),
-            helper(1, False)
+            helper(0, n - 1),
+            helper(1, n)
         )
