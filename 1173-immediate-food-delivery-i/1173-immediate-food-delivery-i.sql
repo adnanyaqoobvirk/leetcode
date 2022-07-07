fc3a5be0d1c642fde5738a0be177230d260d90a6
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-07 15:16:46 +0800

    Time: 380 ms (94.51%), Space: 0B (100.00%) - LeetHub

---
+++ 1173-immediate-food-delivery-i/1173-immediate-food-delivery-i.sql
@@ -0,0 +1,9 @@
+select
+    round(
+        sum(
+            if(order_date = customer_pref_delivery_date, 1, 0)
+        ) / count(*) * 100,
+        2
+    ) as immediate_percentage
+from
+    Delivery
