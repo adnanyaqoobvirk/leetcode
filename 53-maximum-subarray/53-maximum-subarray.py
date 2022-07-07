071b708c2b5d2b5514e930a8bd720523d68a39b5
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-07 17:49:12 +0800

    Time: 4662 ms (5.00%), Space: 42.2 MB (9.70%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,15 +1,27 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        n = len(nums)
-        curr_picked, curr_not_picked = 0, -inf
-        for pos in reversed(range(n)):
-            curr_picked, curr_not_picked = max(
-                nums[pos] + curr_picked,
-                0
-            ), max(
-                nums[pos] + curr_picked,
-                curr_not_picked
+        def helper(left: int, right: int) -> int:
+            if left > right:
+                return -inf
+            
+            mid = left + (right - left) // 2
+            
+            best_left_sum = curr = 0
+            for i in reversed(range(left, mid)):
+                curr += nums[i]
+                best_left_sum = max(curr, best_left_sum)
+
+            best_right_sum = curr = 0
+            for i in range(mid + 1, right + 1):
+                curr += nums[i]
+                best_right_sum = max(curr, best_right_sum)
+                
+            left_max = helper(left, mid - 1)
+            right_max = helper(mid + 1, right)
+            
+            return max(
+                left_max,
+                right_max,
+                best_left_sum + nums[mid] + best_right_sum
             )
-        return curr_not_picked
-            
-            
+        return helper(0, len(nums) - 1)
