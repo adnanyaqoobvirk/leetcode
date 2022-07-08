d0b5c2a3a8c45fd7b0e9952b64131efdde0ee52a
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-08 19:55:34 +0800

    Time: 69 ms (31.56%), Space: 49 MB (48.44%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.java
+++ 1473-paint-house-iii/1473-paint-house-iii.java
@@ -1,47 +1,45 @@
 class Solution {
     public int minCost(int[] houses, int[][] cost, int m, int n, int target) {
-        int[][][] dp = new int[m + 1][target + 1][n + 1];
-        for(int i = 0; i <= m; i++)
-            for(int j = 0; j <= target; j++)
-                for(int k = 0; k <= n; k++)
-                    dp[i][j][k] = -1;
-        
-        int res = helper(houses, cost, m, n, target, 0, 0, dp);
-        return res == Integer.MAX_VALUE ? -1 : res;
-    }
+        int[][] prev = new int[target + 2][n + 1];
+        int[][] curr = new int[target + 2][n + 1];
+        for(int i = 0; i <= target + 1; i++)
+            for(int j = 0; j <= n; j++){
+                if(i == target)
+                    prev[i][j] = 0;
+                else
+                    prev[i][j] = Integer.MAX_VALUE;
+                curr[i][j] = Integer.MAX_VALUE;
+            }
     
-    private int helper(int[] houses, int[][] cost, int m, int n, int nhs, int i, int pcolor, int[][][] dp){
-        if(i == m)
-            return nhs == 0 ? 0 : Integer.MAX_VALUE;
-        
-        if(nhs < 0)
-            return Integer.MAX_VALUE;
-        
-        if(dp[i][nhs][pcolor] != -1)
-            return dp[i][nhs][pcolor];
-        
-        if(houses[i] != 0)
-            dp[i][nhs][pcolor] = helper(
-                houses, cost, m, n,
-                pcolor != houses[i] ? nhs - 1 : nhs,
-                i + 1, houses[i], dp
-            );
-        else{
-            dp[i][nhs][pcolor] = Integer.MAX_VALUE;
-            for(int j = 0; j < n; j++){
-                int res = helper(
-                    houses, cost, m, n,
-                    pcolor != j + 1 ? nhs - 1 : nhs,
-                    i + 1, j + 1, dp
-                );
-
-                if(res != Integer.MAX_VALUE)
-                    dp[i][nhs][pcolor] = Math.min(
-                        dp[i][nhs][pcolor],
-                        cost[i][j] + res
-                    );
-            } 
-        }
-        return dp[i][nhs][pcolor];
+        for(int i = m - 1; i >= 0; i--){
+            for(int nhs = target; nhs >= 0; nhs--){
+                for(int pcolor = n; pcolor >= 0; pcolor--){
+                    if(houses[i] != 0){
+                        curr[nhs][pcolor] = prev[
+                            pcolor != houses[i] ? nhs + 1 : nhs
+                        ][houses[i]];
+                        continue;
+                    }
+                    
+                    for(int j = 0; j < n; j++){
+                        int res = prev[
+                            pcolor != j + 1 ? nhs + 1 : nhs
+                        ][j + 1];
+                        if(res != Integer.MAX_VALUE)
+                            curr[nhs][pcolor] = Math.min(
+                                curr[nhs][pcolor],
+                                cost[i][j] + res
+                            );
+                    }
+                }
+            }
+            prev = curr;
+            curr = new int[target + 2][n + 1];
+            for(int k = 0; k <= target + 1; k++)
+                for(int l = 0; l <= n; l++){
+                    curr[k][l] = Integer.MAX_VALUE;
+                }
+            }
+        return prev[0][0] == Integer.MAX_VALUE ? -1 : prev[0][0];
     }
 }
