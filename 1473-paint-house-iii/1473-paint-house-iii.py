0a31e9e4b9aa4de520b6d8a1899b9b96dd9292e3
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-08 19:35:20 +0800

    Time: 7501 ms (5.17%), Space: 19.7 MB (76.23%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.py
+++ 1473-paint-house-iii/1473-paint-house-iii.py
@@ -1,31 +1,5 @@
 class Solution:
     def minCost(self, houses: List[int], cost: List[List[int]], m: int, n: int, target: int) -> int:
-#         @cache
-#         def helper(i: int, nhs: int, pcolor: int) -> int:
-#             if i == m:
-#                 return inf if nhs != target else 0
-            
-#             if houses[i] != 0:
-#                 return helper(
-#                     i + 1, 
-#                     nhs + 1 if pcolor != houses[i] else nhs,
-#                     houses[i]
-#                 )
-            
-#             ans = inf
-#             for j in range(n):
-#                 ans = min(
-#                     ans, 
-#                     cost[i][j] + helper(
-#                         i + 1, 
-#                         nhs + 1 if pcolor != j + 1 else nhs,
-#                         j + 1
-#                     )
-#                 )
-#             return ans
-#         res = helper(0, 0, 0)
-#         return -1 if res == inf else res
-    
         dp = [
             [[inf] * (n + 1) for _ in range(target + 2)]
             for _ in range(m + 1)
