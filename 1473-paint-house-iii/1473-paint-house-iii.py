8a5f8f9ed68695ab6bf792ccfd97b58d3586b3d1
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-08 19:37:37 +0800

    Time: 5815 ms (5.94%), Space: 14.2 MB (100.00%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.py
+++ 1473-paint-house-iii/1473-paint-house-iii.py
@@ -1,28 +1,26 @@
 class Solution:
     def minCost(self, houses: List[int], cost: List[List[int]], m: int, n: int, target: int) -> int:
-        dp = [
-            [[inf] * (n + 1) for _ in range(target + 2)]
-            for _ in range(m + 1)
-        ]
-    
+        prev = [[inf] * (n + 1) for _ in range(target + 2)]
+        curr = [[inf] * (n + 1) for _ in range(target + 2)]
         for i in reversed(range(m + 1)):
             for nhs in reversed(range(target + 1)):
                 for pcolor in reversed(range(n + 1)):
                     if i == m:
-                        dp[i][nhs][pcolor] = inf if nhs != target else 0
+                        curr[nhs][pcolor] = inf if nhs != target else 0
                         continue
                         
                     if houses[i] != 0:
-                        dp[i][nhs][pcolor] = dp[i + 1][
+                        curr[nhs][pcolor] = prev[
                             nhs + 1 if pcolor != houses[i] else nhs
                         ][houses[i]]
                         continue
 
                     for j in range(n):
-                        dp[i][nhs][pcolor] = min(
-                            dp[i][nhs][pcolor], 
-                            cost[i][j] + dp[i + 1][
+                        curr[nhs][pcolor] = min(
+                            curr[nhs][pcolor], 
+                            cost[i][j] + prev[
                                 nhs + 1 if pcolor != j + 1 else nhs
                             ][j + 1]
                         )
-        return dp[0][0][0] if dp[0][0][0] != inf else -1
+            prev, curr = curr, [[inf] * (n + 1) for _ in range(target + 2)]
+        return prev[0][0] if prev[0][0] != inf else -1
