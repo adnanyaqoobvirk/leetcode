071d9c037354c72d1a74cdb884dfbe843c2925d7
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-09 18:27:38 +0800

    Time: 1257 ms (78.78%), Space: 34.6 MB (23.74%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.py
+++ 1696-jump-game-vi/1696-jump-game-vi.py
@@ -2,13 +2,11 @@ class Solution:
     def maxResult(self, nums: List[int], k: int) -> int:
         n = len(nums)
         
-        score = [0] * n
-        score[-1] = nums[-1]
-        
+        ans = nums[-1]
         max_heap = [(-nums[-1], n - 1)]
         for i in reversed(range(n - 1)):
             while max_heap and max_heap[0][1] > i + k:
                 heappop(max_heap)
-            score[i] = nums[i] + score[max_heap[0][1]]
-            heappush(max_heap, (-score[i], i))
-        return score[0]
+            ans = nums[i] - max_heap[0][0]
+            heappush(max_heap, (-ans, i))
+        return ans
