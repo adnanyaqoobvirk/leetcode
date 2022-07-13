c91b23b9a6e1d9e3e4a13ac97cc4a0468614818c
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-13 22:39:45 +0800

    Time: 4 ms (18.72%), Space: 42.8 MB (23.48%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
@@ -1,10 +1,25 @@
 class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
-        Arrays.sort(arr);
-        int diff = arr[1] - arr[0];
-        for(int i = 2; i < arr.length; i++)
-            if(arr[i] - arr[i - 1] != diff)
+        Map<Integer, Integer> counts = new HashMap<>();
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for(int num : arr){
+            counts.put(num, counts.getOrDefault(num, 0) + 1);
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+        
+        int diff = (max - min) / (arr.length - 1);
+        
+        if(diff == 0 && counts.size() == 1)
+            return true;
+        
+        int curr = min + diff;
+        while(curr <= max){
+            Integer count = counts.get(curr);
+            if(count == null || count > 1)
                 return false;
+            curr += diff;
+        }
         return true;
     }
 }
