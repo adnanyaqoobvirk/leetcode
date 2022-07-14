c8082ea44273d790d8ef07144662dc88aee8f5f1
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-14 15:49:02 +0800

    Time: 812 ms (9.35%), Space: 0B (100.00%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -10,14 +10,15 @@ from
             sum(amount) over(
                 order by visited_on asc
                 range between interval 6 day preceding and current row
-            ) as amount
+            ) as amount,
+            dense_rank() over(
+                order by visited_on asc
+            ) as drank
         from
             Customer
     ) as t
+where
+    drank >= 7
 order by
-    visited_on
-limit 
-    18446744073709551615
-offset 
-    6
+    visited_on asc
     
