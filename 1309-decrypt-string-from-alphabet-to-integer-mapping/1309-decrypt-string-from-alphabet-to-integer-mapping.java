11aba68a932f52b7690d5b1aef4d00eed3160dce
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-14 17:54:14 +0800

    Time: 1 ms (88.75%), Space: 41.7 MB (82.13%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.java
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.java
@@ -5,16 +5,16 @@ class Solution {
         int j = 0;
         for(int i = 2; i < s.length(); i++){
             if(s.charAt(i) == '#'){
-                sb.append((char) ((s.charAt(i - 2) - '0') * 10 + s.charAt(i - 1) - '0' + 96));
+                sb.append((char) ((s.charAt(i - 2) - '0') * 10 + s.charAt(i - 1) - '1' + 'a'));
                 j = i + 1;
             } else if (i - j >= 2){
-                sb.append((char) (s.charAt(j) - '0' + 96));
+                sb.append((char) (s.charAt(j) - '1' + 'a'));
                 j++;
             }
         }
         
         for(; j < s.length(); j++){
-            sb.append((char) (s.charAt(j) - '0' + 96));
+            sb.append((char) (s.charAt(j) - '1' + 'a'));
         }
         
         return sb.toString();
