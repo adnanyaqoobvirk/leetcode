9525ae58d4cea56179451fbeb8718e2876a9244b
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-14 16:30:50 +0800

    Time: 1 ms (77.89%), Space: 42.1 MB (58.86%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.java
+++ 709-to-lower-case/709-to-lower-case.java
@@ -1,9 +1,8 @@
 class Solution {
     public String toLowerCase(String s) {
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            if(c <= 90 && c >= 65)
+        for(char c : s.toCharArray()){
+            if(c <= 'Z' && c >= 'A')
                 sb.append((char) (c + 32));
             else
                 sb.append(c);
