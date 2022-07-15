f4642cea540e41daee29533c3224ba3cc681d7a8
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-15 14:54:49 +0800

    Time: 3021 ms (44.45%), Space: 36.8 MB (93.02%) - LeetHub

--- 1182-shortest-distance-to-target-color/1182-shortest-distance-to-target-color.py
+++ 1182-shortest-distance-to-target-color/1182-shortest-distance-to-target-color.py
@@ -1,21 +1,24 @@
 class Solution:
     def shortestDistanceColor(self, colors: List[int], queries: List[List[int]]) -> List[int]:
-        color_indices = [[] for _ in range(3)]
-        for i, color in enumerate(colors):
-            color_indices[color - 1].append(i)
+        color_indices = [[inf] * 3 for _ in range(len(colors))]
         
+        indices = [inf] * 3
+        for i, c in enumerate(colors):
+            indices[c - 1] = i
+            for j in range(3):
+                color_indices[i][j] = min(color_indices[i][j], abs(i - indices[j]))
+        
+        indices = [inf] * 3
+        for i in reversed(range(len(colors))):
+            c = colors[i]
+            indices[c - 1] = i
+            for j in range(3):
+                color_indices[i][j] = min(color_indices[i][j], abs(i - indices[j]))
+            
         ans = []
         for i, c in queries:
-            indices = color_indices[c - 1]
-            if not indices:
+            if color_indices[i][c - 1] == inf:
                 ans.append(-1)
-                continue
-            
-            idx = bisect_left(indices, i)
-            ans.append(
-                min(
-                    abs(indices[min(idx, len(indices) - 1)] - i),
-                    abs(indices[max(idx - 1, 0)] - i)
-                )
-            )
+            else:
+                ans.append(color_indices[i][c - 1])
         return ans
