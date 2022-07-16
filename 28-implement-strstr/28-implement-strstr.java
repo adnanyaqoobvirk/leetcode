70bfc9ddf8f70c4b1918b433e0c2fb720487e2b9
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-16 23:03:30 +0800

    Time: 0 ms (100.00%), Space: 40.4 MB (89.30%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,23 +1,33 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        int m = haystack.length(), n = needle.length();
-        
-        if(n > m){
+        int h = haystack.length(), n = needle.length();
+        if(n > h){
             return -1;
         }
         
-        for(int i = 0; i < m; i++){
-            boolean brokeEarly = false;
-            for(int j = 0; j < n; j++){
-                if(i + j < m && needle.charAt(j) == haystack.charAt(i + j)){
-                    continue;
-                } else {
-                    brokeEarly = true;
-                    break;
+        long nhash = 0, hhash = 0;
+        for(int i = 0; i < n; i++){
+            nhash += needle.charAt(i) - 97;
+            hhash += haystack.charAt(i) - 97;
+        }
+        
+        for(int i = n; i <= h; i++){
+            int k = i - n;
+            if(nhash == hhash){
+                boolean exitEarly = false;
+                for(int j = 0; j < n; j++){
+                    if(needle.charAt(j) != haystack.charAt(j + k)){
+                        exitEarly = true;
+                        break;
+                    }
+                }
+                if(!exitEarly){
+                    return k;
                 }
             }
-            if(!brokeEarly){
-                return i;
+            
+            if(i < h){
+                hhash += haystack.charAt(i) - haystack.charAt(k);
             }
         }
         
