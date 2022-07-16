eb2aef768ef3deeb09fb556c77c70eb1b3643e98
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-16 22:50:55 +0800

    Time: 36 ms (82.04%), Space: 13.9 MB (63.66%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -1,16 +1,30 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        n, m = len(needle), len(haystack)
-        
-        if n > m:
+        n, h = len(needle), len(haystack)
+        if n > h:
             return -1
         
-        for i in range(m):
-            for j in range(n):
-                if i + j < m and needle[j] == haystack[i + j]:
-                    continue
+        MAX_CHARS = 26
+        
+        nhash = 0
+        for i, c in enumerate(needle):
+            nhash += MAX_CHARS**i + ord(c)
+        
+        hhash = 0
+        for i in range(n):
+            hhash += MAX_CHARS**i + ord(haystack[i])
+        
+        for i in range(n, h + 1):
+            k = i - n
+            if hhash == nhash:
+                for j in range(n):
+                    if haystack[k + j] != needle[j]:
+                        break
                 else:
-                    break
-            else:
-                return i
+                    return k
+            if i < h:
+                hhash -= ord(haystack[k])
+                hhash += ord(haystack[i])
+        
         return -1
+                
