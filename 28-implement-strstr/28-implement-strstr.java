03f2aed363c676a0b717f427df3ad0aa2f33a313
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-17 01:54:48 +0800

    Time: 1 ms (64.96%), Space: 42.5 MB (10.27%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,33 +1,44 @@
 class Solution {
+    public static final int MAX_CHARS = 26;
+    public static final int MOD = 900001;
+    
     public int strStr(String haystack, String needle) {
-        int h = haystack.length(), n = needle.length();
-        if(n > h){
+        int m = haystack.length(), n = needle.length();
+        if(n > m){
             return -1;
         }
         
-        long nhash = 0, hhash = 0;
-        for(int i = 0; i < n; i++){
-            nhash += needle.charAt(i) - 97;
-            hhash += haystack.charAt(i) - 97;
+        int h = 1;
+        for(int i = 0; i < n - 1; i++){
+            h = (MAX_CHARS * h) % MOD;
         }
         
-        for(int i = n; i <= h; i++){
+        int nhash = 0, mhash = 0;
+        for(int i = 0; i < n; i++){
+            nhash = (MAX_CHARS * nhash + needle.charAt(i)) % MOD;
+            mhash = (MAX_CHARS * mhash + haystack.charAt(i)) % MOD;
+        }
+        
+        for(int i = n; i <= m; i++){
             int k = i - n;
-            if(nhash == hhash){
-                boolean exitEarly = false;
-                for(int j = 0; j < n; j++){
+            if(nhash == mhash){
+                int j;
+                for(j = 0; j < n; j++){
                     if(needle.charAt(j) != haystack.charAt(j + k)){
-                        exitEarly = true;
                         break;
                     }
                 }
-                if(!exitEarly){
+                if(j == n){
                     return k;
                 }
             }
             
-            if(i < h){
-                hhash += haystack.charAt(i) - haystack.charAt(k);
+            if(i < m){
+                mhash = (MAX_CHARS * (mhash - haystack.charAt(k) * h) + haystack.charAt(i)) % MOD;
+                
+                if(mhash < 0){
+                    mhash = (MOD + mhash);
+                }
             }
         }
         
