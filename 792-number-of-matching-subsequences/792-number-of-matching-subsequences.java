f21ae1d4ef1d7a194f13a2457e00db715ef0db40
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-20 19:01:50 +0800

    Time: 243 ms (26.93%), Space: 117.2 MB (40.50%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
@@ -1,40 +1,32 @@
+import java.text.StringCharacterIterator;
+
 class Solution {
     public int numMatchingSubseq(String s, String[] words) {
-        Map<Character, List<Integer>> buckets = new HashMap<>();
-        for(int i = 0; i < s.length(); i++){
-            if(!buckets.containsKey(s.charAt(i)))
-                buckets.put(s.charAt(i), new ArrayList<>());
-            buckets.get(s.charAt(i)).add(i);
+        Map<Character, List<StringCharacterIterator>> buckets = new HashMap<>();
+        for(String word : words){
+            if(!buckets.containsKey(word.charAt(0)))
+                buckets.put(word.charAt(0), new ArrayList<>());
+            buckets.get(word.charAt(0)).add(new StringCharacterIterator(word));
         }
         
         int ans = 0;
-        for(String word : words){
-            int i = 0, j = 0;
-            for(; j < word.length(); j++){
-                if(!buckets.containsKey(word.charAt(j))){
-                    break;
-                }
+        for(char c : s.toCharArray()){
+            List<StringCharacterIterator> b = buckets.get(c);
+            if(b != null){
+                buckets.put(c, new ArrayList<>());
                 
-                List<Integer> b = buckets.get(word.charAt(j));
-                int lo = 0, hi = b.size();
-                while(lo < hi){
-                    int mid = lo + (hi - lo) / 2;
-                    if(b.get(mid) >= i){
-                        hi = mid;
+                for(StringCharacterIterator itr : b){
+                    char nc = itr.next();
+                    if(nc == itr.DONE){
+                        ans++;
                     } else{
-                        lo = mid + 1;
+                        if(!buckets.containsKey(nc))
+                            buckets.put(nc, new ArrayList<>());
+                        buckets.get(nc).add(itr);
                     }
                 }
-                if(lo == b.size()){
-                    break;
-                }
-                i = b.get(lo) + 1;
-            }
-            if(j == word.length()){
-                ans++;
             }
         }
-        
         return ans;
     }
 }
