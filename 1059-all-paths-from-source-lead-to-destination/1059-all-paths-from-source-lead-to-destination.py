b9bbea4a757677a808ed7d8121bff3c707b8a3c4
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-22 13:15:21 +0800

    Time: 421 ms (50.00%), Space: 18.8 MB (94.75%) - LeetHub

--- 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.py
+++ 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.py
@@ -1,45 +1,26 @@
-class Solution {
-    public boolean leadsToDestination(int n, int[][] edges, int source, int destination) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        for(int[] edge : edges){
-            int src = edge[0], dst = edge[1];
-            if(!graph.containsKey(src)){
-                graph.put(src, new ArrayList<>());
-            }
-            graph.get(src).add(dst);
-        }
+class Solution:
+    def leadsToDestination(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        graph = defaultdict(set)
+        indegrees = defaultdict(int)
+        for src, dst in edges:
+            graph[src].add(dst)
+            indegrees[dst] += 1
+            
+        if graph[destination]:
+            return False
         
-        return this.helper(new HashMap<>(), graph, destination, source);
-    }
-    
-    private boolean helper(
-        Map<Integer, Integer> nodeColor, 
-        Map<Integer, List<Integer>> graph, 
-        int destination, 
-        int curr
-    ){
-        if(nodeColor.containsKey(curr)){
-            return nodeColor.get(curr) == 1;
-        }
-        
-        if(!graph.containsKey(curr)){
-            if(curr != destination){
-                return false;
-            }
-            else {
-                nodeColor.put(curr, 1);
-                return true;
-            }
-        }
-        
-        nodeColor.put(curr, 0);
-        for(int child : graph.get(curr)){
-            if(!this.helper(nodeColor, graph, destination, child)){
-                return false;
-            }
-        }
-        nodeColor.put(curr, 1);
-        
-        return true;
-    }
-}
+        q = [source]
+        while q:
+            nq = []
+            for curr in q:
+                if not graph[curr] and curr != destination:
+                    return False
+            
+                if indegrees[curr] < 0 and curr != destination:
+                    return False
+                
+                for child in graph[curr]:
+                    indegrees[child] -= 1
+                    nq.append(child)
+            q = nq
+        return True
