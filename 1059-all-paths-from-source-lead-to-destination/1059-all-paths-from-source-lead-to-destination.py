a7c5a56f6b43778f631f0a1882f2985729926a7b
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-22 11:52:44 +0800

    Time: 302 ms (93.08%), Space: 18.8 MB (94.75%) - LeetHub

--- 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.py
+++ 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.py
@@ -1,23 +1,28 @@
 class Solution:
     def leadsToDestination(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        def helper(curr: int) -> bool:
-            if curr in node_color:
-                return node_color[curr] == 1
-            
-            if not graph[curr] and curr != destination:
-                return False
-            
-            node_color[curr] = 0
-            for child in graph[curr]:
-                if not helper(child):
-                    return False
-            node_color[curr] = 1
-            
-            return True
-            
         graph = defaultdict(list)
         for src, dst in edges:
             graph[src].append(dst)
             
         node_color = {}
-        return helper(source)
+        
+        stack = [source]
+        while stack:
+            curr = stack[-1]
+            
+            if curr in node_color:
+                if node_color[curr] == 0:
+                    for child in graph[curr]:
+                        if node_color.get(child, 0) != 1:
+                            return False
+                    node_color[curr] = 1
+                stack.pop()
+                continue
+                    
+            if not graph[curr] and curr != destination:
+                return False
+            
+            node_color[curr] = 0
+            for child in graph[curr]:
+                stack.append(child)
+        return True
