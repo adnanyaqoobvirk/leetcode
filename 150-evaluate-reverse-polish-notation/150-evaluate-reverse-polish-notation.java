8c1659379853966540aefbd075721af5773ef92d
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-27 21:29:06 +0800

    Time: 9 ms (56.00%), Space: 44.4 MB (33.79%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
@@ -1,25 +1,23 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Set<String> special = new HashSet<>();
-        special.add("+");
-        special.add("-");
-        special.add("*");
-        special.add("/");
-        
         ArrayList<String> stack = new ArrayList<>();
         for(String token : tokens){
-            if(special.contains(token)){
+            if (token.equals("+")){
                 Integer op1 = Integer.parseInt(stack.remove(stack.size() - 1));
                 Integer op2 = Integer.parseInt(stack.remove(stack.size() - 1));
-                if (token.equals("+")){
-                    stack.add(Integer.toString(op1 + op2));
-                } else if (token.equals("-")){
-                    stack.add(Integer.toString(op2 - op1));
-                } else if (token.equals("*")){
-                    stack.add(Integer.toString(op1 * op2));
-                } else {
-                    stack.add(Integer.toString(op2 / op1));
-                }
+                stack.add(Integer.toString(op1 + op2));
+            } else if (token.equals("-")){
+                Integer op1 = Integer.parseInt(stack.remove(stack.size() - 1));
+                Integer op2 = Integer.parseInt(stack.remove(stack.size() - 1));
+                stack.add(Integer.toString(op2 - op1));
+            } else if (token.equals("*")){
+                Integer op1 = Integer.parseInt(stack.remove(stack.size() - 1));
+                Integer op2 = Integer.parseInt(stack.remove(stack.size() - 1));
+                stack.add(Integer.toString(op1 * op2));
+            } else if (token.equals("/")){
+                Integer op1 = Integer.parseInt(stack.remove(stack.size() - 1));
+                Integer op2 = Integer.parseInt(stack.remove(stack.size() - 1));
+                stack.add(Integer.toString(op2 / op1));
             } else {
                 stack.add(token);
             }
