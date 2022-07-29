1875ab7096b37696127e171586b4740dec7d45b7
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-29 20:34:51 +0800

    Time: 2347 ms (73.78%), Space: 30.8 MB (86.12%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.py
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.py
@@ -2,60 +2,31 @@ class NumArray:
 
     def __init__(self, nums: List[int]):
         self.nums = nums
-        self.tree = [0] * (4 * len(nums))
-        
-        self.buildTree(0, 0, len(nums) - 1)
+        self.tree = [0] * (len(nums) + 1)
+        for i, num in enumerate(nums):
+            self.updateTree(i, num)
         
     def update(self, index: int, val: int) -> None:
-        self.updateTree(0, index, 0, len(self.nums) - 1, val)
+        diff = val - self.nums[index]
+        self.nums[index] = val
+        self.updateTree(index, diff)
 
     def sumRange(self, left: int, right: int) -> int:
-        return self.searchTree(0, 0, len(self.nums) - 1, left, right)
+        return self.getSum(right) - self.getSum(left - 1)
 
-    def updateTree(self, curr: int, index: int, lo: int, hi: int, val: int) -> None:
-        if lo == hi:
-            self.tree[curr] = val
-            return
-        
-        mid = lo + (hi - lo) // 2
-        
-        if index <= mid:
-            self.updateTree(2 * curr + 1, index, lo, mid, val)
-        elif index > mid:
-            self.updateTree(2 * curr + 2, index, mid + 1, hi, val)
-            
-        self.tree[curr] = self.tree[2 * curr + 1] + self.tree[2 * curr + 2]
-        
-    def searchTree(self, curr: int, lo: int, hi: int, left: int, right: int) -> int:
-        if hi < lo or lo > right:
-            return 0
-        
-        if left <= lo and right >= hi:
-            return self.tree[curr]
-        
-        mid = lo + (hi - lo) // 2
-        
-        if right <= mid:
-            return self.searchTree(2 * curr + 1, lo, mid, left, right)
-        elif left > mid:
-            return self.searchTree(2 * curr + 2, mid + 1, hi, left, right)
-        else:
-            l = self.searchTree(2 * curr + 1, lo, mid, left, right)
-            r = self.searchTree(2 * curr + 2, mid + 1, hi, left, right)
-            
-            return l + r
-        
-    def buildTree(self, curr: int, lo: int, hi: int) -> None:
-        if lo == hi:
-            self.tree[curr] = self.nums[lo]
-            return
-        
-        mid = lo + (hi - lo) // 2
-
-        self.buildTree(2 * curr + 1, lo, mid)
-        self.buildTree(2 * curr + 2, mid + 1, hi)
-
-        self.tree[curr] = self.tree[2 * curr + 1] + self.tree[2 * curr + 2]
+    def getSum(self, index) -> int:
+        ans = 0
+        index += 1
+        while index > 0:
+            ans += self.tree[index]
+            index -= index & -index
+        return ans
+    
+    def updateTree(self, index: int, val: int) -> None:
+        index += 1
+        while index < len(self.tree):
+            self.tree[index] += val
+            index += index & -index
             
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
