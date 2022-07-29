45613fa3d750efb35ee96203f5084a77372b8573
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-29 20:42:00 +0800

    Time: 130 ms (66.20%), Space: 129.9 MB (44.21%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.java
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.java
@@ -1,73 +1,42 @@
 class NumArray {
-    private int n;
+    private int[] nums;
     private int[] tree;
     
     public NumArray(int[] nums) {
-        this.n = nums.length;
-        this.tree = new int[nums.length * 4];
+        this.nums = nums;
+        this.tree = new int[nums.length + 1];
         
-        buildTree(tree, nums, 0, 0, this.n - 1);
+        for(int i = 0; i < this.nums.length; i++){
+            this.updateTree(i, this.nums[i]);
+        }
     }
     
     public void update(int index, int val) {
-        this.updateTree(0, index, val, 0, this.n - 1);
+        int diff = val - this.nums[index];
+        this.nums[index] = val;
+        this.updateTree(index, diff);
     }
     
     public int sumRange(int left, int right) {
-        return searchTree(0, 0, this.n - 1, left, right);
+        return this.getSum(right) - this.getSum(left - 1);
     }
     
-    private void buildTree(int[] tree, int[] nums, int tindex, int lo, int hi){
-        if(lo == hi){
-            tree[tindex] = nums[lo];
-            return;
+    private int getSum(int index){
+        int sum = 0;
+        index += 1;
+        while(index > 0){
+            sum += this.tree[index];
+            index -= index & -index;
         }
-        
-        int mid = lo + (hi - lo) / 2;
-        buildTree(tree, nums, 2 * tindex + 1, lo, mid);
-        buildTree(tree, nums, 2 * tindex + 2, mid + 1, hi);
-        
-        tree[tindex] = tree[2 * tindex + 1] + tree[2 * tindex + 2];
+        return sum;
     }
     
-    private int searchTree(int tindex, int lo, int hi, int left, int right){
-        if(right < lo || left > hi){
-            return 0;
+    private void updateTree(int index, int val){
+        index += 1;
+        while(index < this.tree.length){
+            this.tree[index] += val;
+            index += index & -index;
         }
-        
-        if(left <= lo && right >= hi){
-            return this.tree[tindex];
-        }
-        
-        int mid = lo + (hi - lo) / 2;
-        
-        if(right <= mid){
-            return this.searchTree(tindex * 2 + 1, lo, mid, left, right);
-        } else if(left > mid){
-            return this.searchTree(tindex * 2 + 2, mid + 1, hi, left, right);
-        } else {
-            int l = this.searchTree(tindex * 2 + 1, lo, mid, left, right);
-            int r = this.searchTree(tindex * 2 + 2, mid + 1, hi, left, right);
-            
-            return l + r;
-        }
-    }
-    
-    private void updateTree(int tindex, int index, int val, int lo, int hi){
-        if(lo == hi){
-            this.tree[tindex] = val;
-            return;
-        }
-        
-        int mid = lo + (hi - lo) / 2;
-        
-        if(index <= mid){
-            this.updateTree(tindex * 2 + 1, index, val, lo, mid);
-        } else if(index > mid){
-            this.updateTree(tindex * 2 + 2, index, val, mid + 1, hi);
-        }
-        
-        this.tree[tindex] = this.tree[tindex * 2 + 1] + this.tree[tindex * 2 + 2];
     }
 }
 
