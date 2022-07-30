b9140afd753f637bfc861fe26a336f287e4f2034
Adnan Yaqoob <adnan@yaqoob.org>
2022-07-30 13:35:54 +0800

    Time: 1811 ms (20.67%), Space: 18.5 MB (73.00%) - LeetHub

--- 916-word-subsets/916-word-subsets.py
+++ 916-word-subsets/916-word-subsets.py
@@ -1,15 +1,16 @@
 class Solution:
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
-        char_count = defaultdict(int)
+        max_char_count = [0] * 26
         for word in words2:
             for c, count in Counter(word).items():
-                char_count[c] = max(char_count[c], count)
+                i = ord(c) - ord('a')
+                max_char_count[i] = max(max_char_count[i], count)
         
         ans = []
         for word in words1:
             counts = Counter(word)
-            for c, count in char_count.items():
-                if counts[c] < count:
+            for i in range(26):
+                if counts[chr(ord('a') + i)] < max_char_count[i]:
                     break
             else:
                 ans.append(word)
