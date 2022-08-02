4dd568cd51d02c8f570b64ff5f8f58d9bdac91d8
Adnan Yaqoob <adnan@yaqoob.org>
2022-08-02 18:29:33 +0800

    Time: 81 ms (20.68%), Space: 118.4 MB (29.95%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,17 +1,13 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        List<int[]> distances = new ArrayList<>(); 
+        int[][] distances = new int[points.length][2]; 
         for(int i = 0; i < points.length; ++i){
-            distances.add(
-                new int[]{
-                    points[i][0] * points[i][0] + points[i][1] * points[i][1], 
-                    i
-                }
-            );
+            distances[i][0] = points[i][0] * points[i][0] + points[i][1] * points[i][1];
+            distances[i][1] = i;
         }
         
         Comparator<int[]> comparator = (d1, d2) -> Integer.compare(d1[0], d2[0]);
-        Collections.sort(
+        Arrays.sort(
             distances, 
             comparator.thenComparing(
              (d1, d2) -> Integer.compare(d1[1], d2[1])
@@ -20,7 +16,7 @@ class Solution {
         
         int[][] ans = new int[k][];
         for(int i = 0; i < k; ++i){
-            ans[i] = points[distances.get(i)[1]];
+            ans[i] = points[distances[i][1]];
         }
         return ans;
     }
