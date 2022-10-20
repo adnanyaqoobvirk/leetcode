1debdaa0ac77d74f78cf4a2ec7faf9be4e8fbc6f
Adnan Yaqoob <adnan@yaqoob.org>
2022-10-20 11:16:33 +1100

    Time: 130 ms (25.85%), Space: 14.1 MB (27.91%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,6 +1,22 @@
+class FWord:
+    def __init__(self, word, freq):
+        self.word = word
+        self.freq = freq
+        
+    def __lt__(self, other):
+        if self.freq < other.freq:
+            return True
+        elif self.freq > other.freq:
+            return False
+        else:
+            return self.word > other.word
+    
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        freqs = Counter(words)
-        swords = [(-freq, word) for word, freq in freqs.items()]
-        swords.sort()
-        return [swords[i][1] for i in range(k)]
+        h = []
+        for word, freq in Counter(words).items():
+            fword = FWord(word, freq)
+            heappush(h, fword)
+            if len(h) > k:
+                heappop(h)
+        return [fword.word for fword in sorted(h, key = lambda x: (-x.freq, x.word))]
