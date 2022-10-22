a4b363fcc8d5b3b8e0d281b7fbc1ec3f3d2f3f04
Adnan Yaqoob <adnan@yaqoob.org>
2022-10-22 18:31:29 +1100

    Time: 61 ms (49.87%), Space: 13.8 MB (67.21%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,11 +1,11 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         ans = 0
-        one_count_exists = False
-        for c, cnt in Counter(s).items():
-            if cnt & 1:
-                one_count_exists = True
-                cnt -= 1
-                
-            ans += cnt 
-        return ans + 1 if one_count_exists else ans
+        seen = set()
+        for c in s:
+            if c in seen:
+                ans += 2
+                seen.remove(c)
+            else:
+                seen.add(c)
+        return ans + 1 if seen else ans
