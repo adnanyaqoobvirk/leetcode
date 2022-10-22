13c67367e672dfbb2868b2055c2615707a8d67b9
Adnan Yaqoob <adnan@yaqoob.org>
2022-10-22 17:50:06 +1100

    Time: 3387 ms (5.02%), Space: 24.5 MB (97.43%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -3,15 +3,12 @@ class Solution:
         n = len(prices)
         dp = [[-inf] * (n + 1) for _ in range(2)]
         for i in reversed(range(n)):
-            for bought in range(2):
-                if bought:
-                    dp[1][i] = max(
-                        prices[i],
-                        dp[1][i + 1]
-                    )
-                else:
-                    dp[0][i] = max(
-                        dp[0][i + 1],
-                        dp[1][i + 1] - prices[i]
-                    )
+            dp[1][i] = max(
+                prices[i],
+                dp[1][i + 1]
+            )
+            dp[0][i] = max(
+                dp[0][i + 1],
+                dp[1][i + 1] - prices[i]
+            )
         return 0 if dp[0][0] < 0 else dp[0][0]
