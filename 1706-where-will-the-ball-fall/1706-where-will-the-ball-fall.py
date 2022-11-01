42e0642e7415cf1aaeef792db1a9ef52debccfa0
Adnan Yaqoob <adnan@yaqoob.org>
2022-11-01 16:52:46 +1100

    Time: 474 ms (45.93%), Space: 14.3 MB (84.52%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
@@ -1,20 +1,25 @@
 class Solution:
     def findBall(self, grid: List[List[int]]) -> List[int]:
-        @cache
-        def helper(r: int, c: int) -> int:
-            if r >= m:
-                return c
-            
-            if grid[r][c] == 1:
-                if c == n - 1 or grid[r][c + 1] == -1:
-                    return -1
-                else:
-                    return helper(r + 1, c + 1)
-            else:
-                if c == 0 or grid[r][c - 1] == 1:
-                    return -1
-                else:
-                    return helper(r + 1, c - 1)
-        
         m, n = len(grid), len(grid[0])
-        return [helper(0, col) for col in range(n)]
+        
+        ans = []
+        for col in range(n):
+            r, c = 0, col
+            while r < m:
+                if grid[r][c] == 1:
+                    if c == n - 1 or grid[r][c + 1] == -1:
+                        ans.append(-1)
+                        break
+                    else:
+                        r += 1
+                        c += 1
+                else:
+                    if c == 0 or grid[r][c - 1] == 1:
+                        ans.append(-1)
+                        break
+                    else:
+                        r += 1
+                        c -= 1
+            else:
+                ans.append(c)
+        return ans
