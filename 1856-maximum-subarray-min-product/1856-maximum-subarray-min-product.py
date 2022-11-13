dde480dccdba4f47c99fb4e43aa7f12516bea250
Adnan Yaqoob <adnan@yaqoob.org>
2022-11-14 08:53:09 +1100

    Time: 3478 ms (26.94%), Space: 29.9 MB (33.42%) - LeetHub

--- 1856-maximum-subarray-min-product/1856-maximum-subarray-min-product.py
+++ 1856-maximum-subarray-min-product/1856-maximum-subarray-min-product.py
@@ -3,9 +3,9 @@ class Solution:
         R = 10 ** 9 + 7
         n = len(nums)
         
-        smallest = [-1] * n
+        smallest = [0] * n
         stack = []
-        prefix = []
+        prefix = [0]
         total = 0
         for i in range(n):
             total += nums[i]
@@ -15,7 +15,7 @@ class Solution:
                 stack.pop()
             
             if stack:
-                smallest[i] = stack[-1]
+                smallest[i] = stack[-1] + 1
             
             stack.append(i)
         
@@ -25,8 +25,8 @@ class Solution:
             while stack and nums[stack[-1]] >= nums[i]:
                 stack.pop()
             
-            p = prefix[stack[-1] - 1] if stack else prefix[-1]
-            ans = max(ans, (p - (prefix[smallest[i]] if smallest[i] != -1 else 0)) * nums[i])
+            p = prefix[stack[-1]] if stack else prefix[-1]
+            ans = max(ans, (p - prefix[smallest[i]]) * nums[i])
             
             stack.append(i)
         return ans % R
