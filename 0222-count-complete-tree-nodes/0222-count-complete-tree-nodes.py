3f8ebcdf47bac87314190b1b5d80e353bfc819fe
Adnan Yaqoob <adnan@yaqoob.org>
2022-11-15 17:12:44 +1100

    Time: 154 ms (66.60%), Space: 21.4 MB (46.74%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -9,6 +9,28 @@ class Solution:
         if not root:
             return 0
         
-        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+        h = 0
+        curr = root
+        while curr.left:
+            curr = curr.left
+            h += 1
+        
+        hcount = 0
+        stack = [(root, 1)]
+        while stack:
+            curr, lvl = stack.pop()
             
-            
+            if lvl == h:
+                if not curr.left:
+                    return 2**h + hcount * 2 - 1
+                
+                if not curr.right:
+                    return 2**h + hcount * 2
+                
+                hcount += 1
+            elif lvl < h:
+                stack.append((curr.right, lvl + 1))
+                stack.append((curr.left, lvl + 1))
+                
+        return 2**(h + 1) - 1
+        
