85eaec324606e718ea1c3c3b8a28aa3e164656d4
Adnan Yaqoob <adnan@yaqoob.org>
2022-11-18 18:29:23 +1100

    Time: 781 ms (5.63%), Space: 18.7 MB (39.92%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,16 +1,20 @@
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        n = len(matrix)
+        def possible(guess):
+            count = 0
+            for i in range(n):
+                for j in range(n):
+                    if matrix[i][j] <= guess:
+                        count += 1
+            return count
         
-        sign = -1
-        if k > n**2//2:
-            sign = 1
-            k = n**2 - k + 1
-
-        h = []
-        for row in matrix:
-            for col in row:
-                heappush(h, sign * col)
-                if len(h) > k:
-                    heappop(h)
-        return sign * heappop(h)
+        n = len(matrix)
+        lo, hi = matrix[0][0] - 1, matrix[n - 1][n - 1]
+        while lo + 1 < hi:
+            mid = lo + (hi - lo) // 2
+            
+            if possible(mid) >= k:
+                hi = mid
+            else:
+                lo = mid
+        return hi
