1bdacf1f0ba6100ac1096667f4204bd4f40852f6
Adnan Yaqoob <adnan@yaqoob.org>
2022-11-18 16:35:19 +1100

    Time: 9098 ms (5.12%), Space: 16.6 MB (51.93%) - LeetHub

--- 1102-path-with-maximum-minimum-value/1102-path-with-maximum-minimum-value.py
+++ 1102-path-with-maximum-minimum-value/1102-path-with-maximum-minimum-value.py
@@ -1,26 +1,34 @@
 class Solution:
     def maximumMinimumPath(self, grid: List[List[int]]) -> int:
-        def pathExists(score: int) -> int:
+        def possible(score):
+            if grid[0][0] < score:
+                return False
+            
             q = [(0, 0)]
-            processed = {(0, 0)}
+            seen = {(0, 0)}
             while q:
                 nq = []
                 for i, j in q:
                     if i == m - 1 and j == n - 1:
                         return True
-                    for x, y in [(i - 1, j), (i, j - 1), (i, j + 1), (i + 1, j)]:
-                        if 0 <= x < m and 0 <= y < n and (x, y) not in processed and grid[x][y] >= score:
-                            processed.add((x, y))
+                    
+                    for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+                        if 0 <= x < m and 0 <= y < n and grid[x][y] >= score and (x, y) not in seen:
                             nq.append((x, y))
+                            seen.add((x, y))
                 q = nq
             return False
-                        
+        
         m, n = len(grid), len(grid[0])
-        lo, hi = 0, min(grid[0][0], grid[m - 1][n - 1])
-        while lo < hi:
+        
+        min_score = min(min(grid[i]) for i in range(m))
+        
+        lo, hi = min_score, min(grid[0][0], grid[m - 1][n - 1]) + 1
+        while lo + 1 < hi:
             mid = lo + (hi - lo) // 2
-            if pathExists(mid):
-                lo = mid + 1
+            
+            if possible(mid):
+                lo = mid
             else:
-                hi = mid - 1
-        return lo if pathExists(lo) else lo - 1
+                hi = mid
+        return lo
