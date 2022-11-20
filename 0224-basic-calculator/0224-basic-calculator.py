e255632ef9b8e61ecb112fccc6550576793d814c
Adnan Yaqoob <adnan@yaqoob.org>
2022-11-20 16:28:50 +1100

    Time: 149 ms (71.36%), Space: 15.3 MB (93.82%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -1,46 +1,44 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        digits = set("1234567890")
+        DIGITS = set("1234567890")
         
-        stack = []
-        i = len(s)
+        def calc_sub_expr():
+            r = 0
+            sign = 1
+            while stack and stack[-1] != ')':
+                t = stack.pop()
+                if t == '-':
+                    sign = -1
+                elif t == '+':
+                    sign = 1
+                else:
+                    r += sign * t
+            return r
+        
+        def get_num():
+            nonlocal i
+            
+            num = j = 0
+            while i >= 0 and s[i] in DIGITS:
+                num = int(s[i]) * 10**j + num
+                i -= 1
+                j += 1
+            i += 1
+            return num
+        
+        stack, i = [], len(s)
         while i > 0:
             i -= 1
             c = s[i]
+            
             if c == ' ':
                 continue
             elif c == ')' or c == '+' or c == '-':
                 stack.append(c)
             elif c == '(':
-                r = 0
-                sign = 1
-                while stack and stack[-1] != ')':
-                    t = stack.pop()
-                    if t == '-':
-                        sign = -1
-                    elif t == '+':
-                        sign = 1
-                    else:
-                        r += sign * t
+                r = calc_sub_expr()
                 stack.pop()
                 stack.append(r)
             else:
-                num = 0
-                j = 0
-                while i >= 0 and s[i] in digits:
-                    num = int(s[i]) * 10**j + num
-                    i -= 1
-                    j += 1
-                stack.append(num)
-                i += 1
-        r = 0
-        sign = 1
-        while stack:
-            t = stack.pop()
-            if t == '-':
-                sign = -1
-            elif t == '+':
-                sign = 1
-            else:
-                r += sign * t
-        return r
+                stack.append(get_num())
+        return calc_sub_expr()
