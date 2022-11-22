b7a368e9eeee5414df8e11b0d8df989d6d0c5fcc
Adnan Yaqoob <adnan@yaqoob.org>
2022-11-22 16:49:38 +1100

    Time: 2722 ms (52.61%), Space: 15.6 MB (18.88%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,21 +1,20 @@
 class Solution:
     def numSquares(self, n: int) -> int:
-        squares = [p * p for p in reversed(range(int(math.sqrt(n)) + 1))]
+        squares = [
+            p * p for p in reversed(range(int(math.sqrt(n)) + 1))
+        ]
         
-        q = [n]
-        seen = {n}
-        ans = 0
+        q = {n}
+        level = 0
         while q:
-            nq = []
+            nq = set()
             for r in q:
                 if r == 0:
-                    return ans
+                    return level
                 
                 for s in squares:
                     d = r - s
-                    if d >= 0 and d not in seen:
-                        seen.add(d)
-                        nq.append(d)
+                    if d >= 0:
+                        nq.add(d)
             q = nq
-            ans += 1
-        return ans
+            level += 1
