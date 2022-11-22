6f09f30bb706ca1458d73fcf6eb323e526a613b5
Adnan Yaqoob <adnan@yaqoob.org>
2022-11-22 12:16:24 +1100

    Time: 2047 ms (57.36%), Space: 14.8 MB (37.14%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        nsqrt = int(math.sqrt(n)) + 1
+        
+        q = [n]
+        seen = {n}
+        ans = 0
+        while q:
+            nq = []
+            for r in q:
+                if r == 0:
+                    return ans
+                
+                for p in range(nsqrt):
+                    d = r - p * p
+                    if d >= 0 and d not in seen:
+                        seen.add(d)
+                        nq.append(d)
+            q = nq
+            ans += 1
+        return ans
