6ff028708951ba74770b888a61c9b222a0f4df5e
Adnan Yaqoob <adnan@yaqoob.org>
2022-11-24 11:39:36 +1100

    Time: 4858 ms (66.39%), Space: 13.9 MB (52.49%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -7,20 +7,19 @@ class Solution:
             if i < 0 or j < 0 or i >= m or j >= n:
                 return False
             
-            if (i, j) in seen or board[i][j] != word[pos]:
+            if board[i][j] != word[pos]:
                 return False
             
-            seen.add((i, j))
+            board[i][j] = '#'
             for a, b in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
                 if helper(a, b, pos + 1):
                     return True
-            seen.remove((i, j))
+            board[i][j] = word[pos]
             return False
         
         m, n, k = len(board), len(board[0]), len(word)
         for x in range(m):
             for y in range(n):
-                seen = set()
                 if helper(x, y, 0):
                     return True
         return False
