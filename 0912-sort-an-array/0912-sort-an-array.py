1218ef7e4e939cd68f77541bff67ab99501063f9
Adnan Yaqoob <adnan@yaqoob.org>
2023-03-01 15:22:25 +1100

    Time: 1685 ms (72.35%), Space: 34 MB (5.24%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,9 +1,37 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        mn, mx = min(nums), max(nums)
-        counts = Counter(nums)
-        ans = []
-        for num in range(mn, mx + 1):
-            for cnt in range(counts[num]):
-                ans.append(num)
-        return ans
+        def mergeSort(lo, hi):
+            if lo > hi:
+                return []
+            
+            if lo == hi:
+                return [nums[lo]]
+            
+            mid = (lo + hi) // 2
+            left = mergeSort(lo, mid)
+            right = mergeSort(mid + 1, hi)
+            
+            if not left:
+                return right
+            
+            if not right:
+                return left
+            
+            ans = []
+            n, m = len(left), len(right)
+            p1 = p2 = 0
+            while p1 < n and p2 < m:
+                if left[p1] <= right[p2]:
+                    ans.append(left[p1])
+                    p1 += 1
+                else:
+                    ans.append(right[p2])
+                    p2 += 1
+            
+            p, arr = (p1, left) if p1 < n else (p2, right)
+            for i in range(p, len(arr)):
+                ans.append(arr[i])
+                
+            return ans
+        
+        return mergeSort(0, len(nums) - 1)
