c3daf4d11921e2cec135ad133f328d712b518583
Adnan Yaqoob <adnan@yaqoob.org>
2023-04-06 10:46:13 +1000

    Time: 1130 ms (28.57%), Space: 26.1 MB (84.38%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
@@ -1,7 +1,18 @@
 class Solution:
     def minimizeArrayValue(self, nums: List[int]) -> int:
-        ans = total = 0
-        for i in range(len(nums)):
-            total += nums[i]
-            ans = max(ans, ceil(total / (i + 1)))
-        return ans
+        def possible(k):
+            ops = 0
+            for num in nums:
+                ops += k - num
+                if ops < 0:
+                    return False
+            return True
+        
+        lo, hi = -1, max(nums)
+        while lo + 1 < hi:
+            mid = lo + hi >> 1
+            if possible(mid):
+                hi = mid
+            else:
+                lo = mid
+        return hi
