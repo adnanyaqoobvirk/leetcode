a6a76fd93bf5db19386b0bab3cd3a8c7de781781
Adnan Yaqoob <adnan@yaqoob.org>
2023-05-24 22:49:49 +1000

    Time: 105 ms (41.37%), Space: 16.4 MB (39.25%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,28 +1,19 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
-            return False
-        
-        d = {'a': 2, 'b': 3, 'c': 5, 'd': 7, 'e': 11, 'f': 13, 'g': 17, 'h': 19, 'i': 23, 'j': 29, 'k': 31, 'l': 37, 'm': 41, 'n': 43, 'o': 47, 'p': 53, 'q': 59, 'r': 61, 's': 67, 't': 71, 'u': 73, 'v': 79, 'w': 83, 'x': 89, 'y': 97, 'z': 101}
-        
-        product = 1
-        for x in s1:
-            product *= d[x]
-            
-        windowProduct = 1
-        for x in s2[:len(s1)]:
-            windowProduct *= d[x]
-            
-        if windowProduct == product:
+        cntr, w, match = Counter(s1), len(s1), 0     
+
+        for i in range(len(s2)):
+            if s2[i] in cntr:
+                if not cntr[s2[i]]: match -= 1
+                cntr[s2[i]] -= 1
+                if not cntr[s2[i]]: match += 1
+
+            if i >= w and s2[i-w] in cntr:
+                if not cntr[s2[i-w]]: match -= 1
+                cntr[s2[i-w]] += 1
+                if not cntr[s2[i-w]]: match += 1
+
+            if match == len(cntr):
                 return True
-            
-        nextchar = len(s1)
-        firstchar = 0
-        while nextchar < len(s2):
-            windowProduct = windowProduct // d[s2[firstchar]]
-            windowProduct *= d[s2[nextchar]]
-            if windowProduct == product:
-                return True
-            nextchar += 1
-            firstchar += 1
+
         return False
