a4309f239ef3ec3be5357442aaa31fded96512e8
Adnan Yaqoob <adnan@yaqoob.org>
2023-06-23 15:57:00 +1000

    Time: 1734 ms (50.95%), Space: 42.4 MB (62.69%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.py
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.py
@@ -1,15 +1,21 @@
 class Solution:
     def maxDistance(self, arrays: List[List[int]]) -> int:
-        vals = []
+        first = second = (inf, 0)
+        third = fourth = (-inf, 0)
         for i, arr in enumerate(arrays):
-            vals.append((arr[0], i))
-            if len(arr) > 1:
-                vals.append((arr[-1], i))
+            f, l = arr[0], arr[-1]
+            
+            if f < first[0]:
+                second, first = first, (f, i)
+            elif f < second[0]:
+                second = (f, i)
+            
+            if l > fourth[0]:
+                third, fourth = fourth, (l, i)
+            elif l > third[0]:
+                third = (l, i)
         
-        vals.sort(key=lambda x: x[0])
-        
-        if vals[0][1] == vals[-1][1]:
-            return max(abs(vals[1][0] - vals[-1][0]), abs(vals[0][0] - vals[-2][0]))
+        if first[1] == fourth[1]:
+            return max(abs(second[0] - fourth[0]), abs(first[0] - third[0]))
         else:
-            return abs(vals[0][0] - vals[-1][0])
-        
+            return abs(first[0] - fourth[0])
