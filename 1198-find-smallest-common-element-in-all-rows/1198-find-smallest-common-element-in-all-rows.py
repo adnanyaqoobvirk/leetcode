7bc3d0eb9cc897840375cbbcc8966f182b8f0a80
Adnan Yaqoob <adnan@yaqoob.org>
2023-06-27 09:26:29 +1000

    Time: 444 ms (100.00%), Space: 41.4 MB (85.88%) - LeetHub

--- 1198-find-smallest-common-element-in-all-rows/1198-find-smallest-common-element-in-all-rows.py
+++ 1198-find-smallest-common-element-in-all-rows/1198-find-smallest-common-element-in-all-rows.py
@@ -1,11 +1,11 @@
 class Solution:
     def smallestCommonElement(self, mat: List[List[int]]) -> int:
         m, n = len(mat) - 1, len(mat[0])
-        freq = defaultdict(int)
-        for i in range(m):
-            for j in range(n):
-                freq[mat[i][j]] += 1
         for num in mat[-1]:
-            if freq[num] == m:
+            for i in range(m):
+                pos = bisect_left(mat[i], num)
+                if pos == n or mat[i][pos] != num:
+                    break
+            else:
                 return num
         return -1
