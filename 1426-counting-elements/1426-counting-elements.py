34142d17d334d04049e156015c95969b6511a954
Adnan Yaqoob <adnan@yaqoob.org>
2023-06-27 08:41:28 +1000

    Time: 47 ms (87.18%), Space: 16.4 MB (77.54%) - LeetHub

--- 1426-counting-elements/1426-counting-elements.py
+++ 1426-counting-elements/1426-counting-elements.py
@@ -1,14 +1,10 @@
 class Solution:
     def countElements(self, arr: List[int]) -> int:
-        heapify(arr)
-        prev, count, duplicates = heappop(arr), 0, 1
-        while arr:
-            curr = heappop(arr)
-            if curr == prev:
-                duplicates += 1
-            else:
-                if curr == prev + 1:
-                    count += duplicates
-                duplicates = 1
-            prev = curr
+        freq, count = defaultdict(int), 0
+        for x in arr:
+            freq[x] += 1
+            if freq[x + 1] > 0:
+                count += 1
+            if freq[x] == 1:
+                count += freq[x - 1]
         return count
