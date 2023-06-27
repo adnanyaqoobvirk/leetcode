b0916f2a45d4e309da6af4e8d25761130597374b
Adnan Yaqoob <adnan@yaqoob.org>
2023-06-27 15:35:10 +1000

    Time: 488 ms (95.89%), Space: 18.2 MB (5.06%) - LeetHub

---
+++ 0531-lonely-pixel-i/0531-lonely-pixel-i.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findLonelyPixel(self, picture: List[List[str]]) -> int:
+        row_map, col_map, blacks = defaultdict(int), defaultdict(int), []
+        for i in range(len(picture)):
+            for j in range(len(picture[0])):
+                if picture[i][j] == "B":
+                    row_map[i] += 1
+                    col_map[j] += 1
+                    blacks.append((i, j))
+        
+        count = 0
+        for i, j in blacks:
+            if row_map[i] == 1 and col_map[j] == 1:
+                count += 1
+        return count
