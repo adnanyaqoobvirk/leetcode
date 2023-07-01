5087806be9b6c906105ae140d2eb81383f9885a1
Adnan Yaqoob <adnan@yaqoob.org>
2023-07-01 18:20:52 +1000

    Time: 397 ms (53.05%), Space: 23.8 MB (14.50%) - LeetHub

---
+++ 1272-remove-interval/1272-remove-interval.py
@@ -0,0 +1,18 @@
+class Solution:
+    def removeInterval(self, intervals: List[List[int]], toBeRemoved: List[int]) -> List[List[int]]:
+        rstart, rend = toBeRemoved
+        ans = []
+        for start, end in intervals:
+            if start >= rend or end <= rstart:
+                ans.append([start, end])
+                continue
+                
+            if start >= rstart and end <= rend:
+                continue
+            
+            if start < rstart:
+                ans.append([start, rstart])
+            
+            if end > rend:
+                ans.append([rend, end])
+        return ans
