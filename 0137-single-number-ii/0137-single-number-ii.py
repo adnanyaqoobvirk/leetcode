eb3bced00148391a3db5032611f876f1e6440e58
Adnan Yaqoob <adnan@yaqoob.org>
2023-07-04 21:07:06 +1000

    Time: 69 ms (87.06%), Space: 18 MB (98.16%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,21 +1,13 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        bcount, neg = [0] * 32, 0
-        for num in nums:
-            if num < 0:
-                neg += 1
-                num = -num
-                
-            i = 0
-            while num > 0:
-                if num & 1:
-                    bcount[i] += 1
-                num >>= 1
-                i += 1
-                
-        ans = 0
-        for i in range(32):
-            if bcount[i] % 3 != 0:
-                ans |= 1 << i
-                
-        return -ans if neg % 3 != 0 else ans
+        nums.sort()
+        
+        cnt = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                if cnt != 3:
+                    return nums[i - 1]
+                else:
+                    cnt = 0
+            cnt += 1
+        return nums[-1]
