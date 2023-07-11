924a0471449a3e3617d58835122e434226388a17
Adnan Yaqoob <adnan@yaqoob.org>
2023-07-11 23:40:32 +1000

    Time: 68 ms (47.25%), Space: 16.6 MB (36.26%) - LeetHub

--- 0439-ternary-expression-parser/0439-ternary-expression-parser.py
+++ 0439-ternary-expression-parser/0439-ternary-expression-parser.py
@@ -1,13 +1,13 @@
 class Solution:
     def parseTernary(self, e: str) -> str:
         stack = []
-        for i in reversed(range(len(e))):
+        for c in reversed(e):
             if len(stack) >= 5 and stack[-2] == "?" and stack[-4] == ":":
                 res = stack[-3] if stack[-1] == 'T' else stack[-5]
                 for _ in range(5):
                     stack.pop()
                 stack.append(res)
-            stack.append(e[i])
+            stack.append(c)
         
         if len(stack) > 1:
             return stack[-3] if stack[-1] == 'T' else stack[-5]
