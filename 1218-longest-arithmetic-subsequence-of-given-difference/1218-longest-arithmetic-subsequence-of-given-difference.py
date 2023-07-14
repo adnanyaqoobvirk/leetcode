c13a130d1a0668c82496a11ac797b93b9a8c6972
Adnan Yaqoob <adnan@yaqoob.org>
2023-07-14 18:19:09 +1000

    Time: 575 ms (50.00%), Space: 29.9 MB (99.28%) - LeetHub

--- 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.py
+++ 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.py
@@ -2,6 +2,6 @@ class Solution:
     def longestSubsequence(self, a: List[int], d: int) -> int:
         depths, ans = defaultdict(int), 0
         for n in reversed(a):
-            depths[n] = depths[n + d] + 1
-            ans = max(ans, depths[n])
+            ndepth = depths[n] = depths[n + d] + 1
+            ans = max(ans, ndepth)
         return ans
