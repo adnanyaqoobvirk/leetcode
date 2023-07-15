339265036124b49cbe1bb2fea074a1f1c8c76802
Adnan Yaqoob <adnan@yaqoob.org>
2023-07-16 00:29:34 +1000

    Time: 1275 ms (16.30%), Space: 249.5 MB (5.19%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
@@ -6,20 +6,25 @@ class Solution:
                 return 0
             
             if s:
-                l, r = i, n - 1
-                while l + 1 < r:
-                    m = l + r >> 1
-                    if events[m][0] > events[i][1]:
-                        r = m
-                    else:
-                        l = m
-                if events[r][0] <= events[i][1]:
+                if i not in nmap:
                     return events[i][2]
-                
-                return events[i][2] + max(dp(r, c + 1, True), dp(r, c + 1, False))
+                return events[i][2] + max(dp(nmap[i], c + 1, True), dp(nmap[i], c + 1, False))
             else:
                 return max(dp(i + 1, c, True), dp(i + 1, c, False))
         
         n = len(events)
         events.sort()
+        
+        nmap = {}
+        for i in range(n):
+            l, r = i, n - 1
+            while l + 1 < r:
+                m = l + r >> 1
+                if events[m][0] > events[i][1]:
+                    r = m
+                else:
+                    l = m
+            if events[r][0] > events[i][1]:
+                nmap[i] = r
+                
         return max(dp(0, 0, True), dp(0, 0, False))
