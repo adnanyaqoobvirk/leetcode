21bd61da08da138d8c1cbc277dcdf23baa70f904
Adnan Yaqoob <adnan@yaqoob.org>
2023-07-16 20:50:42 +1000

    Time: 77 ms (73.11%), Space: 19.6 MB (75.71%) - LeetHub

--- 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
@@ -1,16 +1,18 @@
 class MovingAverage:
 
     def __init__(self, size: int):
-        self.q, self.total, self.size = deque(), 0, size
+        self.q, self.total, self.pos = [inf] * size, 0, 0
         
     def next(self, val: int) -> float:
-        self.q.append(val)
+        if self.q[self.pos] != inf:
+            self.total -= self.q[self.pos]
+        
+        self.q[self.pos] = val
         self.total += val
         
-        if len(self.q) > self.size:
-            self.total -= self.q.popleft()
+        self.pos = (self.pos + 1) % len(self.q)
         
-        return self.total / len(self.q)
+        return self.total / (len(self.q) if self.q[self.pos] != inf else self.pos)
 
 # Your MovingAverage object will be instantiated and called as such:
 # obj = MovingAverage(size)
