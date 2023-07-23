ceaedea29268e0db7c411aeb25a7ecbf8cd8d62f
Adnan Yaqoob <adnan@yaqoob.org>
2023-07-23 12:18:09 +1000

    Time: 235 ms (73.37%), Space: 18.9 MB (62.23%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.py
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.py
@@ -1,20 +1,25 @@
 class Solution:
     def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
-        @cache
-        def dp(i: int, j: int, c: int) -> int:
+        def recurse(i: int, j: int, c: int) -> int:
             if c == k:
                 return 1
+            
+            if dp[i][j][c] != 0:
+                return dp[i][j][c]
             
             total = 0
             for dr, dc in moves:
                 ni, nj = i + dr, j + dc
                 if 0 <= ni < n and 0 <= nj < n:
-                    total += dp(ni, nj, c + 1)
-                    
+                    total += recurse(ni, nj, c + 1)
+            
+            dp[i][j][c] = total
+            
             return total
             
         moves = [
             (-1, -2), (-2, -1), (-2, 1), (-1, 2),
             (1, -2), (2, -1), (2, 1), (1, 2)
         ]
-        return dp(row, column, 0) / (8 ** k)
+        dp = [[[0] * k for _ in range(n)] for _ in range(n)]
+        return recurse(row, column, 0) / 8 ** k
