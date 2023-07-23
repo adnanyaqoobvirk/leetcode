6fc5710663d473f1d4eb64b47d257aae77d6e1e5
Adnan Yaqoob <adnan@yaqoob.org>
2023-07-23 12:53:55 +1000

    Time: 340 ms (39.23%), Space: 20 MB (54.24%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.py
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.py
@@ -1,25 +1,23 @@
 class Solution:
     def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
-        def recurse(i: int, j: int, c: int) -> int:
-            if c == k:
-                return 1
-            
-            if dp[i][j][c] != 0:
-                return dp[i][j][c]
-            
-            total = 0
-            for dr, dc in moves:
-                ni, nj = i + dr, j + dc
-                if 0 <= ni < n and 0 <= nj < n:
-                    total += recurse(ni, nj, c + 1)
-            
-            dp[i][j][c] = total
-            
-            return total
-            
         moves = [
             (-1, -2), (-2, -1), (-2, 1), (-1, 2),
             (1, -2), (2, -1), (2, 1), (1, 2)
         ]
-        dp = [[[0] * k for _ in range(n)] for _ in range(n)]
-        return recurse(row, column, 0) / 8 ** k
+        dp = [[[0] * (k + 1) for _ in range(n)] for _ in range(n)]
+        
+        for c in reversed(range(k + 1)):
+            for j in range(n):
+                for i in range(n):
+                    if c == k:
+                        dp[i][j][c] = 1
+                        continue
+                
+                    total = 0
+                    for dr, dc in moves:
+                        ni, nj = i + dr, j + dc
+                        if 0 <= ni < n and 0 <= nj < n:
+                            total += dp[ni][nj][c + 1]
+
+                    dp[i][j][c] = total
+        return dp[row][column][0] / (8 ** k)
