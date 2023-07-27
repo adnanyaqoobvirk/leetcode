3f91be7bec4df0cd9a6ece0ce4ad63e3e92fc9e3
Adnan Yaqoob <adnan@yaqoob.org>
2023-07-27 19:49:43 +1000

    Time: 2571 ms (19.28%), Space: 30.6 MB (40.96%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.py
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.py
@@ -1,15 +1,14 @@
 class Solution:
     def maxRunTime(self, n: int, batteries: List[int]) -> int:
-        def valid(minutes: int) -> bool:
-            extra = 0
-            for p in batteries:
-                extra += min(p, minutes)
-            return extra // n >= minutes
-        
         l, h = 0, sum(batteries) // n + 1
         while l + 1 < h:
             m = l + h >> 1
-            if valid(m):
+            
+            extra = 0
+            for p in batteries:
+                extra += min(p, m)
+                
+            if extra // n >= m:
                 l = m
             else:
                 h = m
