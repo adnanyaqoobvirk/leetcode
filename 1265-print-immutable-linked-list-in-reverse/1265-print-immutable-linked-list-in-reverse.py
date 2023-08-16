9d4a006f7fa7593d340f206e0f6a724914e2787a
Adnan Yaqoob <adnan@yaqoob.org>
2023-08-16 17:54:25 +1000

    Time: 47 ms (68.83%), Space: 16.6 MB (79.87%) - LeetHub

--- 1265-print-immutable-linked-list-in-reverse/1265-print-immutable-linked-list-in-reverse.py
+++ 1265-print-immutable-linked-list-in-reverse/1265-print-immutable-linked-list-in-reverse.py
@@ -8,10 +8,32 @@
 
 class Solution:
     def printLinkedListInReverse(self, head: 'ImmutableListNode') -> None:
-        tail = None
-        while not tail or tail != head:
-            curr = head
-            while curr.next and curr.next != tail:
+        size = 0
+        curr = head
+        while curr:
+            size += 1
+            curr = curr.next
+        
+        bsize = int(math.sqrt(size))
+        buckets = [head]
+        curr = head
+        count = 0
+        while curr:
+            if count < bsize:
+                count += 1
+            else:
+                count = 1
+                buckets.append(curr)
+            curr = curr.next
+        
+        for curr in reversed(buckets):
+            stack = []
+            for _ in range(bsize):
+                if not curr:
+                    break
+                stack.append(curr)
                 curr = curr.next
-            curr.printValue()
-            tail = curr
+            
+            for n in reversed(stack):
+                n.printValue()
+            
