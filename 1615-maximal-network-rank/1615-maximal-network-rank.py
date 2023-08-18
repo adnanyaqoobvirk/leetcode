cb28d2894ab120e90715be332a1aaa2ffda4c0fb
Adnan Yaqoob <adnan@yaqoob.org>
2023-08-19 06:49:14 +1000

    Time: 303 ms (87.16%), Space: 18.3 MB (63.64%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.py
+++ 1615-maximal-network-rank/1615-maximal-network-rank.py
@@ -1,16 +1,12 @@
 class Solution:
     def maximalNetworkRank(self, n: int, roads: List[List[int]]) -> int:
-        connections, sroads = defaultdict(int), set()
+        graph = defaultdict(set)
         for src, dst in roads:
-            sroads.add((min(src, dst), max(src, dst)))
-            connections[src] += 1
-            connections[dst] += 1
-        
-        max_rank = 0
+            graph[src].add(dst)
+            graph[dst].add(src)
+          
+        ans = 0
         for i in range(n):
             for j in range(i + 1, n):
-                rank = connections[i] + connections[j]
-                if (i, j) in sroads:
-                    rank -= 1
-                max_rank = max(max_rank, rank)
-        return max_rank
+                ans = max(ans, len(graph[i]) + len(graph[j]) - int(i in graph[j]))
+        return ans
