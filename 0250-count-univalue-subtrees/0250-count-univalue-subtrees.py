d3404084376c733f7f4c29923ae6eff13017ad13
Adnan Yaqoob <adnan@yaqoob.org>
2023-08-19 13:38:07 +1000

    Time: 45 ms (79.94%), Space: 16.6 MB (6.04%) - LeetHub

--- 0250-count-univalue-subtrees/0250-count-univalue-subtrees.py
+++ 0250-count-univalue-subtrees/0250-count-univalue-subtrees.py
@@ -8,29 +8,17 @@ class Solution:
     def countUnivalSubtrees(self, root: Optional[TreeNode]) -> int:
         def helper(curr: TreeNode) -> Tuple[int, int]:
             if not curr:
-                return 1001, 0
+                return -inf, 0
             
             if not curr.left and not curr.right:
                 return curr.val, 1
-            elif not curr.left:
-                right_val, right_cnt = helper(curr.right)
-                if right_val == curr.val:
-                    return curr.val, right_cnt + 1
-                else:
-                    return 1001, right_cnt
-            elif not curr.right:
-                left_val, left_cnt = helper(curr.left)
-                if left_val == curr.val:
-                    return curr.val, left_cnt + 1
-                else:
-                    return 1001, left_cnt
+
+            left_val, left_cnt = helper(curr.left)
+            right_val, right_cnt = helper(curr.right)
+
+            if (left_val == -inf or left_val == curr.val) and (right_val == -inf or right_val == curr.val):
+                return curr.val, left_cnt + right_cnt + 1
             else:
-                left_val, left_cnt = helper(curr.left)
-                right_val, right_cnt = helper(curr.right)
-            
-                if left_val == right_val == curr.val:
-                    return curr.val, left_cnt + right_cnt + 1
-                else:
-                    return 1001, left_cnt + right_cnt
+                return inf, left_cnt + right_cnt
         return helper(root)[1]
     
