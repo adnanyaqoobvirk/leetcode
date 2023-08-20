63751ba5e6e5d97f1a541efd27b335deb398f811
Adnan Yaqoob <adnan@yaqoob.org>
2023-08-20 19:50:01 +1000

    Time: 198 ms (78.44%), Space: 17.3 MB (100.00%) - LeetHub

---
+++ 0255-verify-preorder-sequence-in-binary-search-tree/0255-verify-preorder-sequence-in-binary-search-tree.py
@@ -0,0 +1,10 @@
+class Solution:
+    def verifyPreorder(self, preorder: List[int]) -> bool:
+        stack, minlimit = [], -inf
+        for v in preorder:
+            while stack and stack[-1] < v:
+                minlimit = max(minlimit, stack.pop())
+            if v <= minlimit:
+                return False
+            stack.append(v)
+        return True
