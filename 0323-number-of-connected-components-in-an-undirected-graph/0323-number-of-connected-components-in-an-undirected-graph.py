575947bba3bca1b148796a8f947e6d0831d89339
Adnan Yaqoob <adnan@yaqoob.org>
2023-08-23 21:29:30 +1000

    Time: 94 ms (94.20%), Space: 17.8 MB (74.00%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,21 +1,28 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        graph = defaultdict(list)
-        for src, dst in edges:
-            graph[src].append(dst)
-            graph[dst].append(src)
+        parent = [i for i in range(n)]
+        rank = [0] * n
+        
+        def find(node):
+            if parent[node] != node:
+                parent[node] = find(parent[node])
+            return parent[node]
+        
+        def union(node1, node2):
+            p1, p2 = find(node1), find(node2)
             
-        nodes = set(range(n))
-        res = 0
-        while nodes:
-            q = [nodes.pop()]
-            while q:
-                nq = []
-                for node in q:
-                    for nei in graph[node]:
-                        if nei in nodes:
-                            nq.append(nei)
-                            nodes.remove(nei)
-                q = nq
-            res += 1
-        return res
+            if p1 == p2:
+                return
+            
+            if rank[p1] == rank[p2]:
+                parent[p2] = p1
+                rank[p1] += 1
+            elif rank[p1] > rank[p2]:
+                parent[p2] = p1
+            else:
+                parent[p1] = p2
+        
+        for src, dst in edges:
+            union(src, dst)
+        
+        return len({find(node) for node in range(n)})
