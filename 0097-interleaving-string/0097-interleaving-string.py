d86eb825aaa5fdcfc16339025390510818982212
Adnan Yaqoob <adnan@yaqoob.org>
2023-08-26 07:29:55 +1000

    Time: 42 ms (91.55%), Space: 17.4 MB (19.84%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        @cache
+        def dp(p1: int, p2: int, p3: int) -> bool:
+            if p3 == n3:
+                return True
+            
+            if p1 == n1:
+                if s2[p2] == s3[p3]:
+                    return dp(p1, p2 + 1, p3 + 1)
+                else:
+                    return False
+                
+            if p2 == n2:
+                if s1[p1] == s3[p3]:
+                    return dp(p1 + 1, p2, p3 + 1)
+                else:
+                    return False
+            
+            res1 = dp(p1 + 1, p2, p3 + 1) if s1[p1] == s3[p3] else False
+            res2 = dp(p1, p2 + 1, p3 + 1) if s2[p2] == s3[p3] else False
+
+            return res1 or res2
+        
+        n1, n2, n3 = len(s1), len(s2), len(s3)
+        if n1 + n2 != n3:
+            return False
+        return dp(0, 0, 0)
