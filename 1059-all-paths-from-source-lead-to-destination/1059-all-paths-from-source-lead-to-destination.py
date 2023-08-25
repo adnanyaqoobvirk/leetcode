806476b0893d49a455577f9180efd6227bf6e09f
Adnan Yaqoob <adnan@yaqoob.org>
2023-08-25 19:53:49 +1000

    Time: 282 ms (83.23%), Space: 22.7 MB (47.51%) - LeetHub

--- 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.py
+++ 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.py
@@ -1,27 +1,29 @@
 class Solution:
     def leadsToDestination(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        GRAY = 1
+        BLACK = 2
+        
+        states = defaultdict(int)
+        graph = defaultdict(list)
+        for src, dst in edges:
+            graph[src].append(dst)
+    
         def helper(node: int) -> bool:
-            if seen[node] == 2:
+            if states[node] == BLACK:
                 return True
             
-            if seen[node] == 1:
+            if states[node] == GRAY:
                 return False
             
             if node != destination and not graph[node]:
                 return False
             
-            seen[node] = 1
+            states[node] = GRAY
             for nei in graph[node]:
                 if not helper(nei):
                     return False
-            seen[node] = 2
+            states[node] = BLACK
             
             return True
         
-        seen = defaultdict(int)
-        
-        graph = defaultdict(list)
-        for src, dst in edges:
-            graph[src].append(dst)
-            
         return helper(source)
