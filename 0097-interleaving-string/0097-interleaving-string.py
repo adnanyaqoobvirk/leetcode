4fe6601cd740cf8172cdb1a62bc089b843cf5419
Adnan Yaqoob <adnan@yaqoob.org>
2023-08-26 10:36:46 +1000

    Time: 52 ms (63.50%), Space: 16.5 MB (62.83%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,30 +1,31 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        @cache
-        def dp(p1: int, p2: int) -> bool:
-            p3 = p1 + p2
-            
-            if p3 == n3:
-                return True
-            
-            if p1 == n1:
-                if s2[p2] == s3[p3]:
-                    return dp(p1, p2 + 1)
-                else:
-                    return False
-                
-            if p2 == n2:
-                if s1[p1] == s3[p3]:
-                    return dp(p1 + 1, p2)
-                else:
-                    return False
-            
-            res1 = dp(p1 + 1, p2) if s1[p1] == s3[p3] else False
-            res2 = dp(p1, p2 + 1) if s2[p2] == s3[p3] else False
-
-            return res1 or res2
-        
         n1, n2, n3 = len(s1), len(s2), len(s3)
         if n1 + n2 != n3:
             return False
-        return dp(0, 0)
+        
+        dp = [[False] * (n2 + 1) for _ in range(n1 + 1)]
+        
+        for p1 in reversed(range(n1 + 1)):
+            for p2 in reversed(range(n2 + 1)):
+                p3 = p1 + p2
+            
+                if p3 == n3:
+                    dp[p1][p2] = True
+                elif p1 == n1:
+                    if s2[p2] == s3[p3]:
+                        dp[p1][p2] = dp[p1][p2 + 1]
+                    else:
+                        dp[p1][p2] = False
+                elif p2 == n2:
+                    if s1[p1] == s3[p3]:
+                        dp[p1][p2] = dp[p1 + 1][p2]
+                    else:
+                        dp[p1][p2] = False
+                else:
+                    res1 = dp[p1 + 1][p2] if s1[p1] == s3[p3] else False
+                    res2 = dp[p1][p2 + 1] if s2[p2] == s3[p3] else False
+
+                    dp[p1][p2] = res1 or res2
+                
+        return dp[0][0]
