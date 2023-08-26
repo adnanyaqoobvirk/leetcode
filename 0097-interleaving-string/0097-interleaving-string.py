7a9f39c46d7a50e9ca2d62f63c8785b83131f2dd
Adnan Yaqoob <adnan@yaqoob.org>
2023-08-26 10:42:59 +1000

    Time: 54 ms (55.99%), Space: 16.3 MB (92.88%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -4,28 +4,27 @@ class Solution:
         if n1 + n2 != n3:
             return False
         
-        dp = [[False] * (n2 + 1) for _ in range(n1 + 1)]
-        
+        prev, curr = [False] * (n2 + 1), [False] * (n2 + 1)
         for p1 in reversed(range(n1 + 1)):
             for p2 in reversed(range(n2 + 1)):
                 p3 = p1 + p2
             
                 if p3 == n3:
-                    dp[p1][p2] = True
+                    curr[p2] = True
                 elif p1 == n1:
                     if s2[p2] == s3[p3]:
-                        dp[p1][p2] = dp[p1][p2 + 1]
+                        curr[p2] = curr[p2 + 1]
                     else:
-                        dp[p1][p2] = False
+                        curr[p2] = False
                 elif p2 == n2:
                     if s1[p1] == s3[p3]:
-                        dp[p1][p2] = dp[p1 + 1][p2]
+                        curr[p2] = prev[p2]
                     else:
-                        dp[p1][p2] = False
+                        curr[p2] = False
                 else:
-                    res1 = dp[p1 + 1][p2] if s1[p1] == s3[p3] else False
-                    res2 = dp[p1][p2 + 1] if s2[p2] == s3[p3] else False
+                    res1 = prev[p2] if s1[p1] == s3[p3] else False
+                    res2 = curr[p2 + 1] if s2[p2] == s3[p3] else False
 
-                    dp[p1][p2] = res1 or res2
-                
-        return dp[0][0]
+                    curr[p2] = res1 or res2
+            prev, curr = curr, prev                
+        return prev[0]
