aa173896706950fa93c1c7c3162678039212106d
Adnan Yaqoob <adnan@yaqoob.org>
2023-08-30 10:36:13 +1000

    Time: 185 ms (42.26%), Space: 21.2 MB (32.44%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
@@ -1,21 +1,18 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
-        # Start with closing at hour 0, the penalty equals all 'Y' in closed hours.
-        cur_penalty = min_penalty = customers.count('Y')
-        earliest_hour = 0
+        n = len(customers)
         
-        for i, ch in enumerate(customers):
-            # If status in hour i is 'Y', moving it to open hours decrement
-            # penalty by 1. Otherwise, moving 'N' to open hours increment
-            # penatly by 1.
-            if ch == 'Y':
-                cur_penalty -= 1
-            else:
-                cur_penalty += 1
-
-            # Update earliest_hour if a smaller penatly is encountered
-            if cur_penalty < min_penalty:
-                earliest_hour = i + 1
-                min_penalty = cur_penalty
-                
-        return earliest_hour
+        ycounts = [0] * (n + 1)
+        for i in reversed(range(n)):
+            ycounts[i] = ycounts[i + 1] + (1 if customers[i] == 'Y' else 0)
+        
+        hour = max_penalty = n
+        penalty = 0
+        for i in range(n + 1):
+            if penalty + ycounts[i] < max_penalty:
+                hour = i
+                max_penalty = penalty + ycounts[i]
+            if i < n:
+                penalty += (1 if customers[i] == 'N' else 0)
+            
+        return hour
