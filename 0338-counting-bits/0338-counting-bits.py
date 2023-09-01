aede84920d9ad4eec2390df42b571473eef2ca57
Adnan Yaqoob <adnan@yaqoob.org>
2023-09-02 07:45:25 +1000

    Time: 62 ms (96.66%), Space: 23.2 MB (23.18%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,11 +1,11 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        res, p = [0], 0
+        res, p, lp1, lp2 = [0], 0, 1, 0
         for num in range(1, n + 1):
-            if 2 ** p == num:
+            if lp1 == num:
                 res.append(1)
                 p += 1
+                lp1, lp2 = 2 ** p, 2 ** (p - 1)
             else:
-                lp = 2 ** (p - 1)
-                res.append(res[lp] + res[num - lp])
+                res.append(res[lp2] + res[num - lp2])
         return res
