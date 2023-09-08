936c5fd260a902f7511d224e51aed35e574eacdd
Adnan Yaqoob <adnan@yaqoob.org>
2023-09-09 09:01:28 +1000

    Time: 48 ms (20.37%), Space: 16.4 MB (45.46%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,22 +1,24 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        ans = []
-        pq = [(-count, char) for char, count in Counter(s).items()]
-        heapify(pq)
-
-        while pq:
-            count_first, char_first = heappop(pq)
-            if not ans or char_first != ans[-1]:
-                ans.append(char_first)
-                if count_first + 1 != 0: 
-                    heappush(pq, (count_first + 1, char_first))
-            else:
-                if not pq: return ''
-                count_second, char_second = heappop(pq)
-                ans.append(char_second)
-                if count_second + 1 != 0:
-                    heappush(pq, (count_second + 1, char_second))
-                heappush(pq, (count_first, char_first))
-
-        return ''.join(ans)
+        h = [(-count, char) for char, count in Counter(s).items()]
+        heapify(h)
+        
+        if -h[0][0] > math.ceil(len(s) / 2):
+            return ""
+        
+        res = []
+        while h:
+            count, char = heappop(h)
+            
+            if h and res and res[-1] == char:
+                count2, char2 = heappop(h)
                 
+                res.append(char2)
+                if count2 + 1 != 0:
+                    heappush(h, (count2 + 1, char2))
+                heappush(h, (count, char))
+            else:
+                res.append(char)
+                if count + 1 != 0:
+                    heappush(h, (count + 1, char))
+        return "".join(res)
