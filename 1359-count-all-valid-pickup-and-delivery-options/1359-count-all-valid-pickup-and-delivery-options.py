a5761f98d12487511eec17d731662a6437a9ae7f
Adnan Yaqoob <adnan@yaqoob.org>
2023-09-11 07:27:31 +1000

    Time: 953 ms (5.53%), Space: 617 MB (5.53%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -2,16 +2,11 @@ class Solution:
     def countOrders(self, n: int) -> int:
         @cache
         def helper(p: int, d: int) -> int:
-            if p < 0 or d < 0:
+            if p < 0 or d < 0 or d < p:
                 return 0
             
             if p == 0 and d == 0:
                 return 1
-            elif p == 0:
-                return math.factorial(d)
-            
-            if d < p:
-                return 0
             
             return p * helper(p - 1, d) + (d - p) * helper(p, d - 1)
         return helper(n, n) % (10 ** 9 + 7)
