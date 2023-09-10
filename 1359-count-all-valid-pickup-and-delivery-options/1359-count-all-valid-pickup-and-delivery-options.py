ae58f020ffce0db19e25adb34cf62e3d848820e5
Adnan Yaqoob <adnan@yaqoob.org>
2023-09-11 07:28:26 +1000

    Time: 662 ms (9.79%), Space: 197.3 MB (5.96%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -1,5 +1,7 @@
 class Solution:
     def countOrders(self, n: int) -> int:
+        MOD = 10 ** 9 + 7
+        
         @cache
         def helper(p: int, d: int) -> int:
             if p < 0 or d < 0 or d < p:
@@ -8,5 +10,5 @@ class Solution:
             if p == 0 and d == 0:
                 return 1
             
-            return p * helper(p - 1, d) + (d - p) * helper(p, d - 1)
-        return helper(n, n) % (10 ** 9 + 7)
+            return (p * helper(p - 1, d)) % MOD + ((d - p) * helper(p, d - 1)) % MOD
+        return helper(n, n) % MOD
