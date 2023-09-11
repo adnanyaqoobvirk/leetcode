045227c46141131a498dd379f49dbc592cf1e4e0
Adnan Yaqoob <adnan@yaqoob.org>
2023-09-12 06:44:43 +1000

    Time: 71 ms (78.85%), Space: 16.4 MB (64.79%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
@@ -1,13 +1,8 @@
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
-        groups = defaultdict(lambda: [[]])
+        res, groups = [], defaultdict(list)
         for i, gsize in enumerate(groupSizes):
-            glist = groups[gsize]
-            if len(glist[-1]) == gsize:
-                glist.append([])
-            glist[-1].append(i)
-        
-        res = []
-        for glist in groups.values():
-            res.extend(glist)
+            groups[gsize].append(i)
+            if len(groups[gsize]) == gsize:
+                res.append(groups.pop(gsize))
         return res
