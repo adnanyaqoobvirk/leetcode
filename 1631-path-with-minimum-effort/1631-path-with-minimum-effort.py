b0e35f8f647c01cca1d332bdd8eb72fa3ac06212
Adnan Yaqoob <adnan@yaqoob.org>
2023-09-16 12:13:39 +1000

    Time: 2597 ms (13.27%), Space: 18.4 MB (47.28%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,19 +1,33 @@
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        def valid(effort: int) -> bool:
+            seen = {(0, 0)}
+            q = [(0, 0)]
+            while q:
+                nq = []
+                for i, j in q:
+                    if i == m - 1 and j == n - 1:
+                        return True
+                    
+                    for di, dj in neighbors:
+                        x, y = i + di, j + dj
+                        
+                        if 0 <= x < m and 0 <= y < n and (x, y) not in seen and abs(heights[i][j] - heights[x][y]) <= effort:
+                            nq.append((x, y))
+                            seen.add((x, y))
+                q = nq
+            return False
+        
+        neighbors = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        
         m, n = len(heights), len(heights[0])
-        heap, nodes = [(0, 0, 0)], {}
-        while heap:
-            effort, i, j = heappop(heap)
-            if (i, j) not in nodes:
-                nodes[(i, j)] = effort
-                for x, y in [(i, j - 1), (i, j + 1), (i - 1, j), (i + 1, j)]:
-                    if 0 <= x < m and 0 <= y < n:
-                        heappush(
-                            heap, 
-                            (
-                                max(effort, abs(heights[i][j] - heights[x][y])),
-                                x,
-                                y
-                            )
-                        )
-        return nodes[(m - 1, n - 1)]
+        
+        lo, hi = -1, 10 ** 6
+        while lo + 1 < hi:
+            mid = lo + hi >> 1
+            
+            if valid(mid):
+                hi = mid
+            else:
+                lo = mid
+        return hi
