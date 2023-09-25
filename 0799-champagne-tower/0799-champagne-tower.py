5cdaa904a357394f972ded7937402522cc81a1cf
Adnan Yaqoob <adnan@yaqoob.org>
2023-09-25 11:59:37 +1000

    Time: 145 ms (17.57%), Space: 16.5 MB (48.21%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.py
+++ 0799-champagne-tower/0799-champagne-tower.py
@@ -1,10 +1,8 @@
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
         dp = [[0] * 101 for _ in range(101)]
-        for i in range(query_row + 1):
+        dp[0][0] = poured
+        for i in range(1, query_row + 1):
             for j in range(i + 1):
-                if i == 0 and j == 0:
-                    dp[i][j] = poured
-                else:
-                    dp[i][j] = max(0, dp[i - 1][j] - 1) / 2 + max(0, dp[i - 1][j - 1] - 1) / 2
+                dp[i][j] = max(0, dp[i - 1][j] - 1) / 2 + max(0, dp[i - 1][j - 1] - 1) / 2
         return min(1, dp[query_row][query_glass])
