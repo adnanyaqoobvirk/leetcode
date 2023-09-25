f7a19e1108e84a730fd382a1e8b8aec410463c78
Adnan Yaqoob <adnan@yaqoob.org>
2023-09-26 05:48:35 +1000

    Time: 61 ms (70.92%), Space: 18 MB (70.84%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,3 +1,13 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        return (3 * sum(set(nums)) - sum(nums)) // 2
+        nums.sort()
+        
+        p, pcount = -inf, 3
+        for c in nums:
+            if c != p:
+                if pcount < 3:
+                    return p
+                pcount = 0
+            p = c
+            pcount += 1
+        return p
