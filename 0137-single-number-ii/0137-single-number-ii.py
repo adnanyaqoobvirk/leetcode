4acf648675e2931572ae78d376dc2ebc3587bc6d
Adnan Yaqoob <adnan@yaqoob.org>
2023-09-26 06:13:02 +1000

    Time: 95 ms (36.08%), Space: 17.9 MB (98.63%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,8 +1,13 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        
-        for i in range(0, len(nums) - 1, 3):
-            if nums[i] != nums[i + 1]:
-                return nums[i]
-        return nums[-1]
+        res = 0
+        for pos in range(32):
+            mask = 1 << pos
+            
+            ones = 0
+            for num in nums:
+                if abs(num) & mask:
+                    ones += 1
+            if ones % 3:
+                res |= mask
+        return -res if sum(1 for num in nums if num < 0) % 3 else res
