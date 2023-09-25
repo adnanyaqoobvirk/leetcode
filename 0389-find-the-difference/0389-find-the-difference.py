68036cdb9bda5bea0d3d0c2e6ae2d779d92a8e9e
Adnan Yaqoob <adnan@yaqoob.org>
2023-09-26 05:12:46 +1000

    Time: 49 ms (17.79%), Space: 16.3 MB (76.19%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,8 +1,10 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        scounts = Counter(s)
-        tcounts = Counter(t)
+        s = sorted(s)
+        t = sorted(t)
         
-        for c, count in tcounts.items():
-            if scounts[c] != count:
-                return c
+        for i in range(len(s)):
+            if s[i] != t[i]:
+                return t[i]
+        
+        return t[-1]
