9e4a76cc5d16b4109555e26f36a7e4a130183bd4
Adnan Yaqoob <adnan@yaqoob.org>
2024-03-18 13:43:26 +1100

    Time: 121 ms (73.51%), Space: 17.5 MB (43.67%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.py
+++ 0986-interval-list-intersections/0986-interval-list-intersections.py
@@ -11,18 +11,14 @@ class Solution:
         p2 = 0
         
         while p1 < n and p2 < m:
+            lo = max(firstList[p1][0], secondList[p2][0])
+            hi = min(firstList[p1][1], secondList[p2][1])
+            if lo <= hi:
+                ans.append([lo, hi])
+                
             if firstList[p1][1] <= secondList[p2][1]:
-                if firstList[p1][0] >= secondList[p2][0]:
-                    ans.append(firstList[p1])
-                else:
-                    if secondList[p2][0] <= firstList[p1][1]:
-                        ans.append([secondList[p2][0], firstList[p1][1]])
                 p1 += 1
             else:
-                if secondList[p2][0] >= firstList[p1][0]:
-                    ans.append(secondList[p2])
-                else:
-                    if firstList[p1][0] <= secondList[p2][1]:
-                        ans.append([firstList[p1][0], secondList[p2][1]])
                 p2 += 1
+        
         return ans
