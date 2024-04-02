bf3c897cf2f7148a47fe35cf9fb0cf60bdaab196
Adnan Yaqoob <adnan@yaqoob.org>
2024-04-03 10:25:28 +1100

    Time: 42 ms (62.10%), Space: 16.7 MB (64.69%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,18 +1,15 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        mapping_s_t = {}
-        mapping_t_s = {}
+        s_ch_map = {}
+        t_ch_map = {}
         
-        for c1, c2 in zip(s, t):
+        for i in range(len(s)):
+            if s[i] not in s_ch_map:
+                s_ch_map[s[i]] = t[i]
+            if t[i] not in t_ch_map:
+                t_ch_map[t[i]] = s[i]
             
-            # Case 1: No mapping exists in either of the dictionaries
-            if (c1 not in mapping_s_t) and (c2 not in mapping_t_s):
-                mapping_s_t[c1] = c2
-                mapping_t_s[c2] = c1
-            
-            # Case 2: Ether mapping doesn't exist in one of the dictionaries or Mapping exists and
-            # it doesn't match in either of the dictionaries or both            
-            elif mapping_s_t.get(c1) != c2 or mapping_t_s.get(c2) != c1:
+            if s_ch_map[s[i]] != t[i] or s[i] != t_ch_map[t[i]]:
                 return False
-            
+        
         return True
