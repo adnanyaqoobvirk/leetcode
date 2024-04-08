9b5be8f5dc7fd63b0f0b89af59b5c7b9deb828cf
Adnan Yaqoob <adnan@yaqoob.org>
2024-04-09 07:17:24 +1000

    Time: 34 ms (90.57%), Space: 16.5 MB (95.21%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -1,15 +1,9 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        q = deque(students)
-        p = 0
-        n = len(sandwiches)
-        r = 0
-        while p < n and r < len(q):
-            if q[0] == sandwiches[p]:
-                p += 1
-                r = 0
-                q.popleft()
+        counts = Counter(students)
+        for s in sandwiches:
+            if counts[s] == 0:
+                return counts[not s]
             else:
-                q.append(q.popleft())
-                r += 1
-        return len(q)
+                counts[s] -= 1
+        return 0
