28d9cca6daefaddb8d17b68c71da4cf3b844ff5d
Adnan Yaqoob <adnan@yaqoob.org>
2024-04-16 07:54:29 +1000

    Time: 42 ms (14.22%), Space: 16.5 MB (81.07%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -6,12 +6,15 @@
 #         self.right = right
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        def helper(curr, num):
+        def recurse(curr, total):
             if not curr:
                 return 0
             
-            if not curr.left and not curr.right:
-                return 10 * num + curr.val
+            total = total * 10 + curr.val
             
-            return helper(curr.left, num * 10 + curr.val) + helper(curr.right, num * 10 + curr.val)
-        return helper(root, 0)
+            if not curr.left and not curr.right:
+                return total
+            
+            return recurse(curr.left, total) + recurse(curr.right, total)
+        
+        return recurse(root, 0)
