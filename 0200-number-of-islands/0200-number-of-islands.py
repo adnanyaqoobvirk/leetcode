3b90945a94a182bced6e9b6d0b2fc627451676e2
Adnan Yaqoob <adnan@yaqoob.org>
2024-04-20 08:13:52 +1000

    Time: 246 ms (58.37%), Space: 24 MB (39.48%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,18 +1,18 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m, n = len(grid), len(grid[0])
-        ones = set((i, j) for i in range(m) for j in range(n) if grid[i][j] == '1')
-        res = 0
+        ones = {(i, j) for i in range(m) for j in range(n) if grid[i][j] == "1"}
+        
+        islands = 0
         while ones:
             q = [ones.pop()]
             while q:
                 nq = []
                 for i, j in q:
-                    for x, y in [(i + 1, j), (i, j + 1), (i - 1, j), (i, j - 1)]:
-                        if 0 <= x < m and 0 <= y < n and (x, y) in ones:
-                            nq.append((x, y))
+                    for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+                        if 0 <= x < m and 0 <= y < n and (x, y) in ones and grid[x][y] == "1":
                             ones.remove((x, y))
+                            nq.append((x, y))
                 q = nq
-            res += 1
-        return res
-                
+            islands += 1
+        return islands
