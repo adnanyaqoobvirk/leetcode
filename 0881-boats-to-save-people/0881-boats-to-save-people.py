2ba2203eba323c8cba36fa7c16480f7678be4e6e
Adnan Yaqoob <adnan@yaqoob.org>
2024-05-05 09:55:41 +1000

    Time: 339 ms (90.68%), Space: 23.4 MB (25.27%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -1,6 +1,7 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
         people.sort()
+        
         i, j = 0, len(people) - 1
         ans = 0
         while i <= j:
@@ -9,3 +10,5 @@ class Solution:
                 i += 1
             j -= 1
         return ans
+            
+                
