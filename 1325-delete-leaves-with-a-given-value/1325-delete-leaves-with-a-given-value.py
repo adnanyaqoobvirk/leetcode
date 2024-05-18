e265ffbf5debfa142d7621456eb777b56f162131
Adnan Yaqoob <adnan@yaqoob.org>
2024-05-19 08:21:42 +1000

    Time: 45 ms (58.45%), Space: 17.1 MB (27.15%) - LeetHub

--- 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.py
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.py
@@ -6,26 +6,13 @@
 #         self.right = right
 class Solution:
     def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
-        stack = []
-        current = root
-        while current or stack:
-            if current:
-                if current.right:
-                    stack.append(current.right)
-                stack.append(current)
-                current = current.left
-            else:
-                node = stack.pop()
-                if stack and node.right == stack[-1]:
-                    current = stack.pop()
-                    stack.append(node)
-                else:
-                    if node.left is None and node.right is None and node.val == target:
-                        if stack:
-                            if stack[-1].left == node:
-                                stack[-1].left = None
-                            else:
-                                stack[-1].right = None
-                        else:
-                            return None
+        if not root:
+            return None
+        
+        root.left = self.removeLeafNodes(root.left, target)
+        root.right = self.removeLeafNodes(root.right, target)
+        
+        if not root.left and not root.right and root.val == target:
+            return None
+        
         return root
