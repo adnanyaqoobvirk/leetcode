97d5957e39703807e9c7376d74811d2fcc8105ab
Adnan Yaqoob <adnan@yaqoob.org>
2024-09-19 14:31:26 +1000

    Time: 463 ms (40%), Space: 17.7 MB (60.04%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -1,24 +1,21 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
-        n = len(s)
-
-        acount = []
-        count = 0
-        for i in reversed(range(n)):
-            acount.append(count)
-            if s[i] == 'a':
-                count += 1
-
-        bcount = []
-        count = 0
-        for i in range(n):
-            bcount.append(count)
-            if s[i] == 'b':
-                count += 1
+        a_count = 0
+        for c in s:
+            if c == 'a':
+                a_count += 1
         
+        b_count = 0
         min_dels = inf
-        for i in range(n):
-            min_dels = min(min_dels, acount[n - i - 1] + bcount[i])
+        for c in s:
+            min_dels = min(min_dels, b_count + a_count)
+
+            if c == 'a':
+                a_count -= 1
+            else:
+                b_count += 1
+        
+        min_dels = min(min_dels, b_count + a_count)
         
         return min_dels
         
