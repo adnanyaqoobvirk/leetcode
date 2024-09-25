df7441c4d987b566c71b4691119ece2c4df2cd6c
Adnan Yaqoob <adnan@yaqoob.org>
2024-09-25 19:20:45 +1000

    Time: 2861 ms (5%), Space: 23.2 MB (20.83%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,4 +1,34 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        points.sort(key = lambda x: x[0]**2 + x[1]**2)
-        return points[:k]
+        def partition(l: int, r: int) -> int:
+            if l >= r:
+                return l
+            
+            p = randint(l, r)
+            d[r], d[p] = d[p], d[r]
+
+            i = l
+            for j in range(l, r):
+                if d[j][1] < d[r][1]:
+                    d[i], d[j] = d[j], d[i]
+                    i += 1
+            d[i], d[r] = d[r], d[i]
+            return i
+        
+        d = []
+        for i, point in enumerate(points):
+            d.append([i, point[0]**2 + point[1]**2])
+
+        left, right = 0, len(points) - 1
+        while True:
+            pivot = partition(left, right)
+
+            if pivot == k:
+                ans = []
+                for i in range(pivot):
+                    ans.append(points[d[i][0]])
+                return ans
+            elif pivot < k:
+                left = pivot + 1
+            else:
+                right = pivot - 1
