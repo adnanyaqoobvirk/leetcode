a60422786783200cad4ce8bcc311a450399e69c6
Adnan Yaqoob <adnan@yaqoob.org>
2024-09-26 22:28:15 +1000

    Time: 254 ms (99.3%), Space: 16.9 MB (24.7%) - LeetHub

--- 0487-max-consecutive-ones-ii/0487-max-consecutive-ones-ii.py
+++ 0487-max-consecutive-ones-ii/0487-max-consecutive-ones-ii.py
@@ -5,8 +5,9 @@ class Solution:
         l = 0
         for r in range(len(nums)):
             if nums[r] == 0:
+                max_len = max(max_len, r - l)
                 if zero_i != -1:
                     l = zero_i + 1
                 zero_i = r
-            max_len = max(max_len, r - l + 1)
+        max_len = max(max_len, r - l + 1)
         return max_len
