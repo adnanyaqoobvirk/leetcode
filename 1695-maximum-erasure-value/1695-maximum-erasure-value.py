c3405d4120ce4e22f1e0708346204392adf15ee7
Adnan Yaqoob <adnan@yaqoob.org>
2024-09-29 16:49:37 +1000

    Time: 865 ms (71.8%), Space: 31.1 MB (16.65%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.py
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.py
@@ -1,13 +1,19 @@
 class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
-        num_idx_map, prefix_sums = defaultdict(lambda: -1), []
-        prefix = max_score = j = 0
-        for i, num in enumerate(nums):
-            if num_idx_map[num] >= j:
-                max_score = max(max_score, (prefix - prefix_sums[j - 1]) if j > 0 else prefix)
-                j = num_idx_map[num] + 1
-            prefix += num
-            prefix_sums.append(prefix)
-            num_idx_map[num] = i
-        max_score = max(max_score, (prefix - prefix_sums[j - 1]) if j > 0 else prefix)
+        seen = set()
+        score = 0
+        max_score = 0
+        l = 0
+        for r in range(len(nums)):
+            score += nums[r]
+
+            while l < r and nums[r] in seen:
+                score -= nums[l]
+                seen.remove(nums[l])
+                l += 1
+            
+            seen.add(nums[r])
+            
+            max_score = max(max_score, score)
         return max_score
+            
