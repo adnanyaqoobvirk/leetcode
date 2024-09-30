c7920427822fb9ac915433494e890e0d444f32b9
Adnan Yaqoob <adnan@yaqoob.org>
2024-10-01 02:11:11 +1000

    Time: 1042 ms (48.7%), Space: 31.1 MB (25.85%) - LeetHub

--- 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.py
+++ 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.py
@@ -1,32 +1,16 @@
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
-        def freq(t: int) -> int:
-            max_len = 0
-            ops = k
-            l = 0
-            for r in range(n):
-                ops -= abs(t - nums[r])
-
-                if ops < 0:
-                    ops += abs(t - nums[l])
-                    l += 1
-                
-                if ops >= 0:
-                    max_len = max(max_len, r - l + 1)
-            return max_len 
-
         nums.sort()
-        n = len(nums)
+        
+        max_freq = 0
+        curr_sum = 0
+        l = 0
+        for r in range(len(nums)):
+            curr_sum += nums[r]
 
-        lo, hi = min(nums), max(nums) + k + 1
-        ans = 0
-        while lo + 1 < hi:
-            mid = lo + (hi - lo) // 2
-
-            f1 = freq(mid)
-            f2 = freq(mid + 1)
-            if f2 > f1:
-                lo = mid
-            else:
-                hi = mid
-        return freq(lo)
+            while l < r and (r - l + 1) * nums[r] - curr_sum > k:
+                curr_sum -= nums[l]
+                l += 1
+            
+            max_freq = max(max_freq, r - l + 1)
+        return max_freq
