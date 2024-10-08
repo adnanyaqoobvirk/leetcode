6655b0c6de6c1afddad3ff5efa0398dc40253226
Adnan Yaqoob <adnan@yaqoob.org>
2024-10-08 14:34:52 +1100

    Time: 1091 ms (75.07%), Space: 35.1 MB (29.67%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.py
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.py
@@ -1,14 +1,22 @@
 class Solution:
     def getAverages(self, nums: List[int], k: int) -> List[int]:
         n = len(nums)
-        
-        psums = [0]
-        for num in nums:
-            psums.append(psums[-1] + num)
-
         avgs = [-1] * n
-        wsize = 2 * k + 1
-        for i in range(k, n - k):
-            avgs[i] = (psums[i + k + 1] - psums[i - k]) // wsize
 
+        if k >= math.ceil(n / 2):
+            return avgs
+        
+        wsize = 2 * k + 1
+        
+        wsum = 0
+        for i in range(wsize):
+            wsum += nums[i]
+
+        avgs[k] = wsum // wsize
+
+        for i in range(wsize, n):
+            wsum += nums[i]
+            wsum -= nums[i - wsize]
+
+            avgs[i - k] = wsum // wsize
         return avgs
