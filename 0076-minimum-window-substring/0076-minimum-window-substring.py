b216d911380d84ee6e937b5a54c322aadf2b4ab4
Adnan Yaqoob <adnan@yaqoob.org>
2024-10-08 18:05:40 +1100

    Time: 122 ms (54.3%), Space: 17.4 MB (30.94%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,27 +1,26 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        def isEqual():
-            for char, count in tcounts.items():
-                if scounts[char] < count:
-                    return False
-            return True
-
         n, m = len(s), len(t)
 
         if n < m:
             return ""
 
         tcounts = Counter(t)
-        scounts = Counter()
+        scounts = defaultdict(int)
+        matches = 0
         rl, rr = 0, inf
         l = 0
         for r in range(n):
             scounts[s[r]] += 1
+            if s[r] in tcounts and scounts[s[r]] == tcounts[s[r]]:
+                matches += 1
 
-            while l <= r and isEqual():
+            while l <= r and matches == len(tcounts):
                 if r - l < rr - rl:
                     rl, rr = l, r
                 scounts[s[l]] -= 1
+                if s[l] in tcounts and scounts[s[l]] + 1 == tcounts[s[l]]:
+                    matches -= 1
                 l += 1
         return s[rl:rr + 1] if rr != inf else ""
 
