7eee88209f6e8c48f8134a9d07658aa6c79dc0c4
Adnan Yaqoob <adnan@yaqoob.org>
2024-10-13 19:51:31 +1100

    Time: 758 ms (5.91%), Space: 16.7 MB (48.9%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -4,15 +4,13 @@ class Solution:
 
         n = len(nums)
         ans = inf
-        for i in range(n):
-            l, r = i + 1, n - 1
-            while l < r:
-                total = nums[i] + nums[l] + nums[r]
-                if abs(ans - target) > abs(total - target):
-                    ans = total
-
-                if total > target:
-                    r -= 1
-                else:
-                    l += 1
+        for i in range(n - 2):
+            for j in range(i + 1, n - 1):
+                psum = nums[i] + nums[j]
+                diff = target - psum
+                k = bisect_right(nums, diff, j + 1)
+                if k < n and abs(diff - nums[k]) < abs(target - ans):
+                    ans = psum + nums[k]
+                if k > j + 1 and abs(diff - nums[k - 1]) < abs(target - ans):
+                    ans = psum + nums[k - 1]
         return ans
