b1346fb1db03ed79b811086401e26a6863d1c369
Adnan Yaqoob <adnan@yaqoob.org>
2024-10-13 20:33:06 +1100

    Time: 72 ms (17.51%), Space: 17.8 MB (78.34%) - LeetHub

--- 0484-find-permutation/0484-find-permutation.py
+++ 0484-find-permutation/0484-find-permutation.py
@@ -1,12 +1,16 @@
 class Solution:
     def findPermutation(self, s: str) -> List[int]:
-        prev, curr, ans = 1, 1, []
+        ans = []
+        start = 1
+        count = 1
         for c in s:
             if c == "I":
-                for i in reversed(range(prev, curr + 1)):
-                    ans.append(i)
-                prev = curr + 1
-            curr += 1
-        for i in reversed(range(prev, curr + 1)):
-            ans.append(i)
+                for num in reversed(range(start, start + count)):
+                    ans.append(num)
+                start = start + count
+                count = 1
+            else:
+                count += 1
+        for num in reversed(range(start, start + count)):
+            ans.append(num)
         return ans
