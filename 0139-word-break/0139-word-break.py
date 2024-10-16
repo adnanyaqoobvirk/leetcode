6b42b6cb7c9d0946406cd03049974d9b0247383e
Adnan Yaqoob <adnan@yaqoob.org>
2024-10-17 01:09:26 +1100

    Time: 38 ms (69.62%), Space: 17.4 MB (6.88%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,20 +1,5 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        @cache
-        def helper(pos: int) -> bool:
-            if pos == n:
-                return True
-            
-            t = trie
-            for i in range(pos, n):
-                if s[i] not in t:
-                    return False
-                
-                t = t[s[i]]
-                if '#' in t and helper(i + 1):
-                    return True
-            return False
-                    
         trie = {}
         for word in wordDict:
             t = trie
@@ -22,7 +7,19 @@ class Solution:
                 if c not in t:
                     t[c] = {}
                 t = t[c]
-            t['#'] = True
-        
+            t['#'] = {}
+            
         n = len(s)
-        return helper(0)
+        dp = [False] * (n + 1)
+        dp[n] = True
+        for i in reversed(range(n)):
+            t = trie
+            for j in range(i, n):
+                if s[j] not in t:
+                    break
+                t = t[s[j]]
+                if '#' in t and dp[j + 1]:
+                    dp[i] = True
+                    break
+        return dp[0]
+                
