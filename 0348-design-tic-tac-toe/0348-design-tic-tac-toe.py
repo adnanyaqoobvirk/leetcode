06538e929bebca218788c872cc65a7b756128298
Adnan Yaqoob <adnan@yaqoob.org>
2024-10-18 19:03:53 +1100

    Time: 81 ms (34.82%), Space: 19.3 MB (40.67%) - LeetHub

--- 0348-design-tic-tac-toe/0348-design-tic-tac-toe.py
+++ 0348-design-tic-tac-toe/0348-design-tic-tac-toe.py
@@ -1,43 +1,25 @@
 class TicTacToe:
 
     def __init__(self, n: int):
-        self.p1counts = [[0] * n for _ in range(2)]
-        self.p1mdcounts = 0
-        self.p1sdcounts = 0
-        self.p2counts = [[0] * n for _ in range(2)]
-        self.p2mdcounts = 0
-        self.p2sdcounts = 0
+        self.counts = [[0] * n for _ in range(2)]
+        self.mdcount = self.sdcount = 0
         self.n = n
 
     def move(self, row: int, col: int, player: int) -> int:
-        if player == 1:
-            self.p1counts[0][row] += 1
-            self.p1counts[1][col] += 1
-            if row - col == 0:
-                self.p1mdcounts += 1
-            if row + col == self.n - 1:
-                self.p1sdcounts += 1
+        sign = -1 if player == 1 else 1
+        self.counts[0][row] += sign
+        self.counts[1][col] += sign
+        if row - col == 0:
+            self.mdcount += sign
+        if row + col == self.n - 1:
+            self.sdcount += sign
 
-            return 1 if (
-                self.p1counts[0][row] == self.n or
-                self.p1counts[1][col] == self.n or
-                self.p1mdcounts == self.n or
-                self.p1sdcounts == self.n
-            ) else 0
-        else:
-            self.p2counts[0][row] += 1
-            self.p2counts[1][col] += 1
-            if row - col == 0:
-                self.p2mdcounts += 1
-            if row + col == self.n - 1:
-                self.p2sdcounts += 1
-            
-            return 2 if (
-                self.p2counts[0][row] == self.n or
-                self.p2counts[1][col] == self.n or
-                self.p2mdcounts == self.n or
-                self.p2sdcounts == self.n
-            ) else 0
+        return player if sign * self.n in (
+            self.counts[0][row],
+            self.counts[1][col],
+            self.mdcount,
+            self.sdcount
+        ) else 0
 
 # Your TicTacToe object will be instantiated and called as such:
 # obj = TicTacToe(n)
