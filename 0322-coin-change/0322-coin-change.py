ff31c2db6096fd500875b9192066ec36765f93d6
Adnan Yaqoob <adnan@yaqoob.org>
2024-10-22 14:06:12 +1100

    Time: 885 ms (36.67%), Space: 17.6 MB (31.82%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,11 +1,12 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [inf] * (amount + 1)
+        n = len(coins)
+        dp = defaultdict(lambda: 10001)
         dp[0] = 0
-        for m in range(1, amount + 1):
-            ans = inf
+        for m in range(min(coins), amount + 1):
+            ans = 10001
             for c in coins:
                 if m - c >= 0:
                     ans = min(ans, dp[m - c] + 1)
             dp[m] = ans
-        return -1 if dp[amount] == inf else dp[amount]
+        return -1 if dp[amount] == 10001 else dp[amount]
