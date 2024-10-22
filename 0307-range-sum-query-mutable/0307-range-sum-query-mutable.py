346c05bee4d9c846c90463cba8590a1a921366fe
Adnan Yaqoob <adnan@yaqoob.org>
2024-10-22 17:57:24 +1100

    Time: 257 ms (100%), Space: 34.6 MB (58.02%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
@@ -1,42 +1,34 @@
-class FenwickTree:
-    def __init__(self, arr):
-        self.arr = arr
-        self.n = len(arr) + 1
-        self.t = [0] + arr
-        for i in range(1, self.n):
-            p = i + (i & -i)
-            if p < self.n:
-                self.t[p] += self.t[i]
-        
-    def update(self, i, v):
-        d = v - self.arr[i]
-        self.arr[i] = v
-        i += 1
-        while i < self.n:
-            self.t[i] += d
-            i += i & -i
-        
-    def query(self, i):
-        i += 1
-        ans = 0
-        while i > 0:
-            ans += self.t[i]
-            i -= i & -i
-        return ans
-    
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.t = FenwickTree(nums)
+        self.nums = nums
+        self.bit = [0] + nums
+        self.n = len(nums) + 1
+        for i in range(1, self.n):
+            p = i + (i & -i)
+            if p < self.n:
+                self.bit[p] += self.bit[i]
+
+    def query(self, index: int) -> int:
+        index += 1
+
+        total = 0
+        while index > 0:
+            total += self.bit[index]
+            index -= index & -index
+        return total
 
     def update(self, index: int, val: int) -> None:
-        self.t.update(index, val)
+        d = val - self.nums[index]
+        self.nums[index] = val
+        index += 1
+        while index < self.n:
+            self.bit[index] += d
+            index += index & -index
 
     def sumRange(self, left: int, right: int) -> int:
-        if left == 0:
-            return self.t.query(right)
-        else:
-            return self.t.query(right) - self.t.query(left - 1)
+        return self.query(right) - self.query(left - 1)
+
 
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
