51d6f25a237e00489d5f02a29262ac468d7bca06
Adnan Yaqoob <adnan@yaqoob.org>
2024-10-25 15:05:36 +1100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
@@ -1,35 +1,41 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.nums = nums
-        self.bit = [0] + nums
-        self.n = len(nums) + 1
-        for i in range(1, self.n):
-            p = i + (i & -i)
-            if p < self.n:
-                self.bit[p] += self.bit[i]
+        def build(lo: int, hi: int, i: int) -> int:
+            if lo == hi:
+                self.s[i] = nums[lo]
+            else:
+                mid = lo + (hi - lo) // 2
+                self.s[i] = build(lo, mid, i * 2 + 1) + build(mid + 1, hi, i * 2 + 2)
+            return self.s[i]
 
-    def query(self, index: int) -> int:
-        index += 1
-
-        total = 0
-        while index > 0:
-            total += self.bit[index]
-            index -= index & -index
-        return total
+        self.n = len(nums)
+        self.s = [0] * 2**(ceil(log(self.n, 2)) + 1)
+        build(0, self.n - 1, 0)
 
     def update(self, index: int, val: int) -> None:
-        d = val - self.nums[index]
-        self.nums[index] = val
-        index += 1
-        while index < self.n:
-            self.bit[index] += d
-            index += index & -index
-
+        def update(lo: int, hi: int, i: int) -> None:
+            if lo == hi:
+                self.s[i] = val
+            else:
+                mid = lo + (hi - lo) // 2
+                if index <= mid:
+                    update(lo, mid, i * 2 + 1)
+                else:
+                    update(mid + 1, hi, i * 2 + 2)
+                self.s[i] = self.s[i * 2 + 1] + self.s[i * 2 + 2]
+        update(0, self.n - 1, 0)
+                
     def sumRange(self, left: int, right: int) -> int:
-        return self.query(right) - self.query(left - 1)
-
-
+        def query(lo: int, hi: int, i: int, l: int, r: int) -> int:
+            if r < lo or l > hi:
+                return 0
+            elif l <= lo and r >= hi:
+                return self.s[i]
+            else:
+                mid = lo + (hi - lo) // 2
+                return query(lo, mid, i * 2 + 1, l, mid) + query(mid + 1, hi , i * 2 + 2, mid + 1, r)
+        return query(0, self.n - 1, 0, left, right)
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
 # obj.update(index,val)
