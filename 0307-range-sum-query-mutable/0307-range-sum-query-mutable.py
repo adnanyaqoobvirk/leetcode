1dd3328d9443d51b54dcaba8f3ce4c982140b338
Adnan Yaqoob <adnan@yaqoob.org>
2024-10-25 16:44:05 +1100

    Time: 503 ms (97.48%), Space: 36.2 MB (39.48%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
@@ -1,41 +1,35 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        def build(lo: int, hi: int, i: int) -> int:
-            if lo == hi:
-                self.s[i] = nums[lo]
-            else:
-                mid = lo + (hi - lo) // 2
-                self.s[i] = build(lo, mid, i * 2 + 1) + build(mid + 1, hi, i * 2 + 2)
-            return self.s[i]
-
         self.n = len(nums)
-        self.s = [0] * 2**(ceil(log(self.n, 2)) + 1)
-        build(0, self.n - 1, 0)
+        self.tree = [0] * self.n + nums
+        for i in reversed(range(1, self.n)):
+            self.tree[i] = self.tree[i << 1] + self.tree[i << 1 | 1]
 
     def update(self, index: int, val: int) -> None:
-        def update(lo: int, hi: int, i: int) -> None:
-            if lo == hi:
-                self.s[i] = val
-            else:
-                mid = lo + (hi - lo) // 2
-                if index <= mid:
-                    update(lo, mid, i * 2 + 1)
-                else:
-                    update(mid + 1, hi, i * 2 + 2)
-                self.s[i] = self.s[i * 2 + 1] + self.s[i * 2 + 2]
-        update(0, self.n - 1, 0)
-                
+        i = index + self.n
+        self.tree[i] = val
+        
+        p = i >> 1
+        while p > 0:
+            self.tree[p] = self.tree[p << 1] + self.tree[p << 1 | 1]
+            p >>= 1
+
     def sumRange(self, left: int, right: int) -> int:
-        def query(lo: int, hi: int, i: int, l: int, r: int) -> int:
-            if r < lo or l > hi:
-                return 0
-            elif l <= lo and r >= hi:
-                return self.s[i]
-            else:
-                mid = lo + (hi - lo) // 2
-                return query(lo, mid, i * 2 + 1, l, mid) + query(mid + 1, hi , i * 2 + 2, mid + 1, r)
-        return query(0, self.n - 1, 0, left, right)
+        total = 0
+        l, r = left + self.n, right + self.n + 1
+        while l < r:
+            if l & 1:
+                total += self.tree[l]
+                l += 1
+            l >>= 1
+            if r & 1:
+                r -= 1
+                total += self.tree[r]
+            r >>= 1
+        return total
+
+
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
 # obj.update(index,val)
