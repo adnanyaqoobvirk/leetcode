5c62c30981b609771b2f8ae2f8c4823ae12926c2
Adnan Yaqoob <adnan@yaqoob.org>
2024-10-27 17:40:39 +1100

    Time: 87 ms (63.64%), Space: 19.9 MB (20.16%) - LeetHub

--- 0308-range-sum-query-2d-mutable/0308-range-sum-query-2d-mutable.py
+++ 0308-range-sum-query-2d-mutable/0308-range-sum-query-2d-mutable.py
@@ -1,59 +1,46 @@
-class BIT2D:
-    def __init__(self, matrix: List[List[int]]) -> None:
-        self.m, self.n = len(matrix) + 1, len(matrix[0]) + 1
-        self.matrix = [matrix[i][::] for i in range(self.m - 1)]
-        self.tree = [[0] + (matrix[i - 1] if i > 0 else [0] * (self.n - 1)) for i in range(self.m)]
-        for i in range(1, self.m):
-            for j in range(1, self.n):
-                p = j + (j & -j)
-                if p < self.n:
-                    self.tree[i][p] += self.tree[i][j]
-        
-        for j in range(1, self.n):
-            for i in range(1, self.m):
-                p = i + (i & -i)
-                if p < self.m:
-                    self.tree[p][j] += self.tree[i][j]
-    
-    def query(self, i: int, c: int) -> int:
-        i += 1
-        c += 1
+class SegTree:
+    def __init__(self, nums: List[int]) -> None:
+        self.n = len(nums)
+        self.stree = [0] * self.n + nums
+        for i in reversed(range(self.n)):
+            self.stree[i] = self.stree[i << 1] + self.stree[i << 1 | 1]
+
+    def query(self, left: int, right: int) -> int:
+        l, r = left + self.n, right + self.n + 1
         total = 0
-        while i > 0:
-            j = c
-            while j > 0:
-                total += self.tree[i][j]
-                j -= j & -j
-            i -= i & -i
+        while l < r:
+            if l & 1:
+                total += self.stree[l]
+                l += 1
+            if r & 1:
+                r -= 1
+                total += self.stree[r]
+            l >>= 1
+            r >>= 1
         return total
 
-    def update(self, i: int, c: int, v: int) -> None:
-        d = v - self.matrix[i][c]
-        self.matrix[i][c] = v
-        i += 1
-        c += 1
-        while i < self.m:
-            j = c
-            while j < self.n:
-                self.tree[i][j] += d
-                j += j & -j
-            i += i & -i
-        
+    def update(self, idx: int, val: int) -> None:
+        i = idx + self.n
+        self.stree[i] = val
+        i >>= 1
+        while i > 0:
+            self.stree[i] = self.stree[i << 1] + self.stree[i << 1 | 1]
+            i >>= 1
+
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self.bit = BIT2D(matrix)
+        self.trees = [SegTree(row) for row in matrix]
 
     def update(self, row: int, col: int, val: int) -> None:
-        self.bit.update(row, col, val)
+        self.trees[row].update(col, val)
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        return (
-            self.bit.query(row2, col2) - 
-            self.bit.query(row1 - 1, col2) - 
-            self.bit.query(row2, col1 - 1) + 
-            self.bit.query(row1 - 1, col1 - 1)
-        )
+        total = 0
+        for r in range(row1, row2 + 1):
+            total += self.trees[r].query(col1, col2)
+        return total
+
 
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
