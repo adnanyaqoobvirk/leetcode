d9254f1c0fe7e10110c9d661eafdef0b568b42c5
Adnan Yaqoob <adnan@yaqoob.org>
2024-10-29 18:26:40 +1100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.py
+++ 0493-reverse-pairs/0493-reverse-pairs.py
@@ -1,11 +1,26 @@
-from sortedcontainers import SortedList
-
 class Solution:
     def reversePairs(self, nums: List[int]) -> int:
-        sl = SortedList(key=lambda x: -x)
+        def update(i):
+            while i <= n:
+                bit[i] += 1
+                i += i & -i
+        
+        def query(i):
+            res = 0
+            while i > 0:
+                res += bit[i]
+                i -= i & -i
+            return res
+        
+        nmap = {v: i for i, v in enumerate(sorted(set(nums)), 1)}
+        n = 2 * len(nmap) + 1
+        bit = [0] * (n + 1)
+
         ans = 0
         for num in nums:
-            i = sl.bisect_left(2 * num)
-            ans += i
-            sl.add(num)
+            if num <= 0:
+                ans += query(n) - query(nmap[num] // 2 - 1)
+            else:
+                ans += query(n) - query(2 * nmap[num])
+            update(nmap[num])
         return ans
