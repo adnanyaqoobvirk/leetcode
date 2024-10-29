3768bdd25a1c848e6d3f839d8a0eea2b21f6a576
Adnan Yaqoob <adnan@yaqoob.org>
2024-10-29 21:50:34 +1100

    Time: 2556 ms (75%), Space: 31.2 MB (64.14%) - LeetHub

--- 1649-create-sorted-array-through-instructions/1649-create-sorted-array-through-instructions.py
+++ 1649-create-sorted-array-through-instructions/1649-create-sorted-array-through-instructions.py
@@ -2,10 +2,26 @@ from sortedcontainers import SortedList
 
 class Solution:
     def createSortedArray(self, instructions: List[int]) -> int:
-        sl = SortedList()
+        def update(i):
+            while i < n:
+                bit[i] += 1
+                i += i & -i
+
+        def query(i):
+            res = 0
+            while i > 0:
+                res += bit[i]
+                i -= i & -i
+            return res
+
+        minv, maxv = min(instructions), max(instructions)
+        offset = minv - 1
+        n = maxv - minv + 2
+        bit = [0] * n
+
         cost = 0
         for num in instructions:
-            i, j = sl.bisect_left(num), sl.bisect_right(num)
-            cost += min(i, len(sl) - j)
-            sl.add(num)
+            num = num - offset
+            cost += min(query(num - 1), query(n - 1) - query(num))
+            update(num)
         return cost % (10**9 + 7)
