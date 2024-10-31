7151a99a49af3fcef474590541ee1d7d2da0add5
Adnan Yaqoob <adnan@yaqoob.org>
2024-10-31 17:38:14 +1100

    Time: 1235 ms (53.21%), Space: 52.1 MB (11.09%) - LeetHub

--- 0327-count-of-range-sum/0327-count-of-range-sum.py
+++ 0327-count-of-range-sum/0327-count-of-range-sum.py
@@ -2,26 +2,32 @@ class Solution:
     def countRangeSum(self, nums: List[int], lower: int, upper: int) -> int:
         def count(l: int, r: int) -> int:
             if l == r:
-                return 1 if lower <= nums[l] <= upper else 0
+                return [psums[l]]
 
             m = l + (r - l) // 2
             left = count(l, m)
             right = count(m + 1, r)
-        
-            ssums = []
-            total = 0
-            for i in reversed(range(l, m + 1)):
-                total += nums[i]
-                ssums.append(total)
-            ssums.sort()
 
-            cnt = total = 0
-            for i in range(m + 1, r + 1):
-                total += nums[i]
+            i = j = k = p = 0
+            ln, rn = len(left), len(right)
+            res = []
+            for i in range(ln):
+                while j < rn and right[j] - left[i] < lower: j += 1
+                while k < rn and right[k] - left[i] <= upper: k += 1
+                ans[0] += k - j
 
-                j = bisect_left(ssums, lower - total)
-                k = bisect_right(ssums, upper - total)
+                while p < rn and right[p] <= left[i]:
+                    res.append(right[p])
+                    p += 1
+                res.append(left[i])
+            for p in range(p, rn):
+                res.append(right[p])
+            return res
 
-                cnt += k - j
-            return cnt + left + right
-        return count(0, len(nums) - 1)
+        psums = [0]
+        for num in nums:
+            psums.append(psums[-1] + num)
+
+        ans = [0]
+        count(0, len(psums) - 1)
+        return ans[0]
