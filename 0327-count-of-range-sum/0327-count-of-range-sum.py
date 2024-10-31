8567157bfbf5549040d0622ce7f870068678a3f6
Adnan Yaqoob <adnan@yaqoob.org>
2024-11-01 01:51:47 +1100

    Time: 1184 ms (57.8%), Space: 31.9 MB (71.51%) - LeetHub

--- 0327-count-of-range-sum/0327-count-of-range-sum.py
+++ 0327-count-of-range-sum/0327-count-of-range-sum.py
@@ -1,33 +1,26 @@
 class Solution:
     def countRangeSum(self, nums: List[int], lower: int, upper: int) -> int:
-        def count(l: int, r: int) -> int:
-            if l == r:
-                return [psums[l]]
+        def update(i: int) -> None:
+            while i < n:
+                bit[i] += 1
+                i += i & -i
 
-            m = l + (r - l) // 2
-            left = count(l, m)
-            right = count(m + 1, r)
-
-            i = j = k = p = 0
-            ln, rn = len(left), len(right)
-            res = []
-            for i in range(ln):
-                while j < rn and right[j] - left[i] < lower: j += 1
-                while k < rn and right[k] - left[i] <= upper: k += 1
-                ans[0] += k - j
-
-                while p < rn and right[p] <= left[i]:
-                    res.append(right[p])
-                    p += 1
-                res.append(left[i])
-            for p in range(p, rn):
-                res.append(right[p])
+        def query(i: int) -> int:
+            res = 0
+            while i > 0:
+                res += bit[i]
+                i -= i & -i
             return res
 
         psums = [0]
         for num in nums:
             psums.append(psums[-1] + num)
-
-        ans = [0]
-        count(0, len(psums) - 1)
-        return ans[0]
+        
+        n = len(psums) + 1
+        bit = [0] * n
+        ssums = sorted(psums)
+        count = 0
+        for s in psums:
+            count += query(bisect_right(ssums, s - lower)) - query(bisect_left(ssums, s - upper))
+            update(bisect_left(ssums, s) + 1)
+        return count
