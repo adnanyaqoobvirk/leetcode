db2198da4c46ebff83c6e21785ecc3a0d8f0aa88
Adnan Yaqoob <adnan@yaqoob.org>
2024-11-01 18:20:06 +1100

    Time: 1943 ms (30.98%), Space: 34.7 MB (61.09%) - LeetHub

--- 2955-number-of-same-end-substrings/2955-number-of-same-end-substrings.py
+++ 2955-number-of-same-end-substrings/2955-number-of-same-end-substrings.py
@@ -1,13 +1,11 @@
 class Solution:
     def sameEndSubstringCount(self, s: str, queries: List[List[int]]) -> List[int]:
-        n = len(s) + 1
-        char_sums = {c: [0] * n for c in ascii_lowercase}
-        for i, ch in enumerate(s, 1):
+        char_sums = {c: [0] for c in ascii_lowercase}
+        for ch in s:
             for c in ascii_lowercase:
+                char_sums[c].append(char_sums[c][-1])
                 if c == ch:
-                    char_sums[c][i] = char_sums[c][i - 1] + 1
-                else:
-                    char_sums[c][i] = char_sums[c][i - 1]
+                    char_sums[c][-1] += 1
                     
         ans = []
         for l, r in queries:
