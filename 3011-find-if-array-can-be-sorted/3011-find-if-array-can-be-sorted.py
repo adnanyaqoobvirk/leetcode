417f7182bf6e235191a47408eb3588a90fb5daf2
Adnan Yaqoob <adnan@yaqoob.org>
2024-11-06 19:21:38 +1100

    Time: 1 ms (96.36%), Space: 16.5 MB (78.85%) - LeetHub

--- 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
@@ -1,22 +1,13 @@
 class Solution:
     def canSortArray(self, nums: List[int]) -> bool:
-        def bit_count(num: int) -> int:
-            count = 0
-            while num > 0:
-                count += 1
-                num -= num & -num
-            return count
-
-        has_swaps = True
-        while has_swaps:
-            has_swaps = False
-            for i in range(1, len(nums)):
-                if bit_count(nums[i]) == bit_count(nums[i - 1]) and nums[i] < nums[i - 1]:
-                    nums[i], nums[i - 1] = nums[i - 1], nums[i]
-                    has_swaps = True
-
+        bit_counts = {num: num.bit_count() for num in nums}
+        pseg, cseg = [], [nums[0]]
         for i in range(1, len(nums)):
-            if nums[i] < nums[i - 1]:
-                return False
-        
-        return True
+            if bit_counts[nums[i]] != bit_counts[cseg[0]]:
+                cseg.sort()
+                if pseg and pseg[-1] > cseg[0]:
+                    return False
+                pseg, cseg = cseg, []
+            cseg.append(nums[i])
+        cseg.sort()
+        return not pseg or pseg[-1] <= cseg[0]
