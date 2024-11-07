dc83dafb8ee1e69017788bae24de2f6e75275d88
Adnan Yaqoob <adnan@yaqoob.org>
2024-11-07 16:49:43 +1100

    Time: 1433 ms (58.65%), Space: 41.7 MB (34.58%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
@@ -1,40 +1,11 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
-        P, M = 127, 10**9 + 7
-
-        def valid(l: int) -> bool:
-            rhash1 = rhash2 = 0
-            pmax = 1
-            for i in range(l):
-                if i > 0:
-                    pmax = pmax * P % M
-                rhash1 = (rhash1 * P + nums1[i]) % M
-                rhash2 = (rhash2 * P + nums2[i]) % M
-            
-            seen = {rhash1}
-            for i in range(l, len(nums1)):
-                rhash1 -= nums1[i - l] * pmax
-                rhash1 = rhash1 * P + nums1[i]
-
-                seen.add(rhash1)
-            
-            if rhash2 in seen:
-                return True
-
-            for i in range(l, len(nums2)):
-                rhash2 -= nums2[i - l] * pmax
-                rhash2 = rhash2 * P + nums2[i]
-
-                if rhash2 in seen:
-                    return True
-            return False
-
-        lo, hi = 0, min(len(nums1), len(nums2)) + 1
-        while lo + 1 < hi:
-            mid = lo + (hi - lo) // 2
-
-            if valid(mid):
-                lo = mid
-            else:
-                hi = mid
-        return lo
+        m, n = len(nums1), len(nums2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        ans = 0
+        for i in range(m):
+            for j in range(n):
+                if nums1[i] == nums2[j]:
+                    dp[i + 1][j + 1] = 1 + dp[i][j]
+                    ans = max(ans, dp[i + 1][j + 1])
+        return ans
