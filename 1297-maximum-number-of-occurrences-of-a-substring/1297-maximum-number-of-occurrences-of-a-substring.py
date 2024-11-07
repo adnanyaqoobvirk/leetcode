ceb2197947fb0fdda6bf9fa1eb662bc24be5a2c0
Adnan Yaqoob <adnan@yaqoob.org>
2024-11-08 04:09:47 +1100

    Time: 103 ms (44.74%), Space: 18.4 MB (36.7%) - LeetHub

--- 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.py
+++ 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.py
@@ -3,32 +3,30 @@ class Solution:
         P, M = 127, 10**9 + 7
 
         hcounts = defaultdict(int)
-        for wsize in range(minSize, maxSize + 1):
-            uchars = defaultdict(int)
+        uchars = defaultdict(int)
+        rhash = 0
+        pmax = 1
+        for i in range(minSize):
+            if i > 0:
+                pmax = pmax * P % M
+            rhash = (rhash * P + ord(s[i])) % M
+            uchars[s[i]] += 1
+        
+        if len(uchars) <= maxLetters:
+            hcounts[rhash] += 1
+        
+        for i in range(minSize, len(s)):
+            prev, curr = s[i - minSize], s[i]
+            rhash -= pmax * ord(prev)
+            rhash = (rhash * P + ord(curr)) % M
 
-            rhash = 0
-            pmax = 1
-            for i in range(wsize):
-                if i > 0:
-                    pmax = pmax * P % M
-                rhash = (rhash * P + ord(s[i])) % M
-                uchars[s[i]] += 1
-            
+            uchars[prev] -= 1
+            if uchars[prev] == 0:
+                del uchars[prev]
+            uchars[curr] += 1
+
             if len(uchars) <= maxLetters:
                 hcounts[rhash] += 1
-            
-            for i in range(wsize, len(s)):
-                prev, curr = s[i - wsize], s[i]
-                rhash -= pmax * ord(prev)
-                rhash = (rhash * P + ord(curr)) % M
-
-                uchars[prev] -= 1
-                if uchars[prev] == 0:
-                    del uchars[prev]
-                uchars[curr] += 1
-
-                if len(uchars) <= maxLetters:
-                    hcounts[rhash] += 1
         return max(hcounts.values()) if len(hcounts) > 0 else 0
 
 
