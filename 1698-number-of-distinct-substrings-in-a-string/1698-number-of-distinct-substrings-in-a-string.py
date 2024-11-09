f9779d55975b2fb91d6aa87326b3093915be74dd
Adnan Yaqoob <adnan@yaqoob.org>
2024-11-09 22:05:18 +1100

    Time: 768 ms (31.37%), Space: 47.7 MB (57.25%) - LeetHub

--- 1698-number-of-distinct-substrings-in-a-string/1698-number-of-distinct-substrings-in-a-string.py
+++ 1698-number-of-distinct-substrings-in-a-string/1698-number-of-distinct-substrings-in-a-string.py
@@ -1,15 +1,16 @@
 class Solution:
     def countDistinct(self, s: str) -> int:
-        P, M = 127, 2**40 + 2**8 + 0xb3
-        h = ans = 0
-        pmax = 1
-        for i in range(len(s)):
-            h = (h * P + ord(s[i])) % M
-            r, seen = h, {h}
-            for j in range(i + 1, len(s)):
-                r -= pmax * ord(s[j - i - 1])
-                r = (r * P + ord(s[j])) % M
-                seen.add(r)
-            ans += len(seen)
-            pmax = pmax * P % M
-        return ans
+        n = len(s)
+        dup = 0
+        trie = {}
+        for i in range(n):
+            t = trie
+            for j in range(i, n):
+                if s[j] not in t:
+                    t[s[j]] = {}
+                t = t[s[j]]
+                if '#' in t:
+                    dup += 1
+                else:
+                    t["#"] = {}
+        return (n * (n + 1) // 2) - dup
