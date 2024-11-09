37a2aae63e4ff2ea31ea56f5f7d29f3904244eaf
Adnan Yaqoob <adnan@yaqoob.org>
2024-11-09 15:40:18 +1100

    Time: 216 ms (61.6%), Space: 33.2 MB (29.96%) - LeetHub

--- 1062-longest-repeating-substring/1062-longest-repeating-substring.py
+++ 1062-longest-repeating-substring/1062-longest-repeating-substring.py
@@ -1,31 +1,11 @@
 class Solution:
     def longestRepeatingSubstring(self, s: str) -> int:
-        def valid(l: int) -> bool:
-            P, M = 127, 10**9 + 7
-
-            rhash, pmax = 0, 1
-            for i in range(l):
-                if i > 0:
-                    pmax = pmax * P % M
-                rhash = (rhash * P + ord(s[i])) % M
-            
-            seen = {rhash}
-            for i in range(l, n):
-                rhash -= pmax * ord(s[i - l])
-                rhash = (rhash * P + ord(s[i])) % M
-
-                if rhash in seen:
-                    return True
-                seen.add(rhash)
-            return False
-
-        n = len(s)
-        lo, hi = 0, len(s)
-        while lo + 1 < hi:
-            mid = lo + (hi - lo) // 2
-
-            if valid(mid):
-                lo = mid
-            else:
-                hi = mid
-        return lo
+        n = len(s) + 1
+        dp = [[0] * n for _ in range(n)]
+        ans = 0
+        for i in reversed(range(n - 1)):
+            for j in reversed(range(i)):
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i + 1][j + 1] + 1
+                    ans = max(ans, dp[i][j])
+        return ans
