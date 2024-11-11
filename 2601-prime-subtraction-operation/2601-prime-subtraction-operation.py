dc32d746320e4dea3ae0092f140e6b4732a7bb9e
Adnan Yaqoob <adnan@yaqoob.org>
2024-11-11 16:53:51 +1100

    Time: 35 ms (54.19%), Space: 16.7 MB (66.41%) - LeetHub

--- 2601-prime-subtraction-operation/2601-prime-subtraction-operation.py
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.py
@@ -1,6 +1,6 @@
 class Solution:
-    def primeSubOperation(self, nums: List[int]) -> bool:
-        n = max(nums) + 1
+    def __init__(self):
+        n = 1001
         primes = [True] * n
         primes[1] = False
         p = 2
@@ -9,8 +9,10 @@ class Solution:
                 for i in range(p * p, n, p):
                     primes[i] = False
             p += 1
-        primes = [p for p in range(len(primes)) if primes[p]]
-        
+        self.primes = [p for p in range(len(primes)) if primes[p]]
+
+    def primeSubOperation(self, nums: List[int]) -> bool:
+        primes = self.primes
         prev = 0
         for num in nums:
             i = bisect_left(primes, num - prev)
