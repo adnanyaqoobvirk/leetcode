f9c67adce4bb154f396b2c84e0e841f08430738c
Adnan Yaqoob <adnan@yaqoob.org>
2024-11-13 20:27:51 +1100

    Time: 1234 ms (15.99%), Space: 17.3 MB (40.38%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -1,18 +1,15 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
-        P1, M1 = 31, 2**24 + 2**8 + 0x93
-        P2, M2 = 29, 10**9 + 7
-        lhash1 = rhash1 = lhash2 = rhash2 = r = 0
-        pmax1 = pmax2 = 1
-        for i in range(len(s)):
-            lhash1 = (lhash1 * P1 + ord(s[i]) - ord('a') + 1) % M1
-            rhash1 = (rhash1 + (ord(s[i]) - ord('a') + 1) * pmax1) % M1
-            pmax1 = pmax1 * P1 % M1
-
-            lhash2 = (lhash2 * P2 + ord(s[i]) - ord('a') + 1) % M2
-            rhash2 = (rhash2 + (ord(s[i]) - ord('a') + 1) * pmax2) % M2
-            pmax2 = pmax2 * P2 % M2
-
-            if lhash1 == rhash1 and lhash2 == rhash2:
-                r = i
+        P, M = 127, 2**24 + 2**8 + 0x93
+        n = len(s)
+        lhash = rhash = r = 0
+        pmax = 1
+        for i in range(n):
+            lhash = (lhash * P + ord(s[i])) % M
+            rhash = (rhash + ord(s[i]) * pmax) % M
+            pmax = pmax * P % M
+            if lhash == rhash:
+                ps = s[:i + 1]
+                if ps == ps[::-1]:
+                    r = i
         return s[r + 1:][::-1] + s
