4c5d55ab2fc016beb9f9b37d2d8ab218d4b75577
Adnan Yaqoob <adnan@yaqoob.org>
2024-11-13 20:19:52 +1100

    Time: 61 ms (47.71%), Space: 17.2 MB (44.28%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -1,26 +1,13 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
         P, M = 127, 2**40 + 2**8 + 0xb3
-        
         n = len(s)
-        lhashes, pmaxes = [], []
-        lhash = rhash = 0
+        lhash = rhash = r = 0
         pmax = 1
         for i in range(n):
             lhash = (lhash * P + ord(s[i])) % M
-            lhashes.append(lhash)
-
-            rhash = (rhash * P + ord(s[n - i - 1])) % M
-            
+            rhash = (rhash + ord(s[i]) * pmax) % M
             pmax = pmax * P % M
-            pmaxes.append(pmax)
-
-        if lhash == rhash:
-            return s
-        
-        for i in reversed(range(1, n)):
-            rhash -= pmaxes[i - 1] * ord(s[i])
-            rhash %= M
-            if lhashes[i - 1] == rhash:
-                return s[i:][::-1] + s
-        return s[1:][::-1] + s
+            if lhash == rhash:
+                r = i
+        return s[r + 1:][::-1] + s
