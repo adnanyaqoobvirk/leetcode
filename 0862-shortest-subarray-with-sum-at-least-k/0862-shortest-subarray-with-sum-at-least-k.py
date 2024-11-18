d02852d22440af86ebcdba238a18cb1ab8d152ae
Adnan Yaqoob <adnan@yaqoob.org>
2024-11-18 20:19:32 +1100

    Time: 404 ms (14.79%), Space: 36.9 MB (10.19%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -5,12 +5,10 @@ class Solution:
         psum = 0
         for i in range(len(nums)):
             psum += nums[i]
+            if psum >= k:
+                ans = min(ans, i + 1)
             while h and psum - h[0][0] >= k:
                 _, j = heappop(h)
                 ans = min(ans, i - j)
             heappush(h, (psum, i))
-        while h:
-            s, j = heappop(h)
-            if s >= k:
-                ans = min(ans, j + 1)
         return -1 if ans == inf else ans
