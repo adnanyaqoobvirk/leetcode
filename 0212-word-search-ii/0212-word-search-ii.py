ac5744ecb82c90f7cf02ca73ffc0db058a8d225e
Adnan Yaqoob <adnan@yaqoob.org>
2024-11-27 17:01:02 +1100

    Time: 5472 ms (52.2%), Space: 18.6 MB (66.52%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -1,24 +1,16 @@
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        def helper(i: int, j: int, t: dict) -> None:
-            if '#' in t:
-                ans.append(t['#'])
-                del t['#']
+        def backtrack(a: int, b: int, t: dict) -> None:
+            if "#" in t:
+                ans.append(t["#"])
+                del t["#"]
             
-            if i < 0 or j < 0 or i >= m or j >= n:
-                return
-            
-            if (i, j) in seen or board[i][j] not in t:
-                return
-            
-            seen.add((i, j))
-            for a, b in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
-                helper(a, b, t[board[i][j]])
-            seen.remove((i, j))
-            
-            if not t[board[i][j]]:
-                del t[board[i][j]]
-        
+            for x, y in [(a + 1, b), (a - 1, b), (a, b + 1), (a, b - 1)]:
+                if 0 <= x < m and 0 <= y < n and board[x][y] in t and (x, y) not in seen:
+                    seen.add((x, y))
+                    backtrack(x, y, t[board[x][y]])
+                    seen.remove((x, y))
+
         trie = {}
         for word in words:
             t = trie
@@ -26,12 +18,13 @@ class Solution:
                 if c not in t:
                     t[c] = {}
                 t = t[c]
-            t['#'] = word
+            t["#"] = word
         
-        m, n = len(board), len(board[0])
         ans = []
-        for x in range(m):
-            for y in range(n):
-                seen = set()
-                helper(x, y, trie)
+        m, n = len(board), len(board[0])
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] in trie:
+                    seen = {(i, j)}
+                    backtrack(i, j, trie[board[i][j]])
         return ans
