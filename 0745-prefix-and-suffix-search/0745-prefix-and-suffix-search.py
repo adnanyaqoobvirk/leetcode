fe76e4cf00c37474e969cc59a595f60a224dd4c6
Adnan Yaqoob <adnan@yaqoob.org>
2024-11-28 19:04:28 +1100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0745-prefix-and-suffix-search/0745-prefix-and-suffix-search.py
@@ -0,0 +1,39 @@
+class WordFilter:
+
+    def __init__(self, words: List[str]):
+        self.pt = {"#": []}
+        for i, word in enumerate(words):
+            t = self.pt
+            for c in word:
+                if c not in t:
+                    t[c] = {"#": []}
+                t = t[c]
+                t["#"].append(i)
+        self.st = {"#": set()}
+        for i, word in enumerate(words):
+            t = self.st
+            for c in reversed(word):
+                if c not in t:
+                    t[c] = {"#": set()}
+                t = t[c]
+                t["#"].add(i)
+
+    def f(self, pref: str, suff: str) -> int:
+        t1 = self.pt
+        for c in pref:
+            if c not in t1:
+                break
+            t1 = t1[c]
+        t2 = self.st
+        for c in reversed(suff):
+            if c not in t2:
+                break
+            t2 = t2[c]
+        for i in reversed(t1["#"]):
+            if i in t2["#"]:
+                return i
+        return -1
+
+# Your WordFilter object will be instantiated and called as such:
+# obj = WordFilter(words)
+# param_1 = obj.f(pref,suff)
