e93016c2313ada091df750530fa7219f115e611a
Adnan Yaqoob <adnan@yaqoob.org>
2024-11-28 14:02:27 +1100

    Time: 2093 ms (28.4%), Space: 53 MB (14.13%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
@@ -1,21 +1,15 @@
 class Solution:
     def minimumObstacles(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        tx, ty = m - 1, n - 1
-        cells = m * n + 1
         h = [(0, 0, 0)]
         d = defaultdict(lambda: inf)
         d[(0, 0)] = 0
         while h:
             ud, ux, uy = heappop(h)
-
-            if ux == tx and uy == ty:
-                return floor(ud / cells)
-
             for vx, vy in [(ux + 1, uy), (ux - 1, uy), (ux, uy + 1), (ux, uy - 1)]:
                 if 0 <= vx < m and 0 <= vy < n:
-                    w = d[(ux, uy)] + (1 if grid[vx][vy] == 0 else cells)
+                    w = d[(ux, uy)] + grid[vx][vy]
                     if w < d[(vx, vy)]:
                         d[(vx, vy)] = w
                         heappush(h, (w, vx, vy))
-        return -1
+        return d[(m - 1, n - 1)]
