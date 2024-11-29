32164477c7a0bda23acc4bb1bea92b2d7d8a2b4d
Adnan Yaqoob <adnan@yaqoob.org>
2024-11-29 20:26:11 +1100

    Time: 1712 ms (11.5%), Space: 59.8 MB (5.47%) - LeetHub

--- 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.py
+++ 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.py
@@ -5,18 +5,16 @@ class Solution:
 
         m, n = len(grid), len(grid[0])
         dis = defaultdict(lambda: inf)
+        dis[(0, 0)] = 0
+        seen = {(0, 0)}
         h = [(0, 0, 0)]
         while h:
             s, i, j = heappop(h)
             for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
-                if 0 <= x < m and 0 <= y < n:
-                    if s >= grid[x][y] - 1:
-                        if s + 1 < dis[(x, y)]:
-                            dis[(x, y)] = s + 1
-                            heappush(h, (s + 1, x, y))
-                    else:
-                        ns = grid[x][y] if s & 1 ^ grid[x][y] & 1 else grid[x][y] + 1
-                        if ns < dis[(x, y)]:
-                            dis[(x, y)] = ns
-                            heappush(h, (ns, x, y))
+                if 0 <= x < m and 0 <= y < n and (x, y) not in seen:
+                    ns = max(s + 1, grid[x][y] if s & 1 ^ grid[x][y] & 1 else grid[x][y] + 1)
+                    if ns < dis[(x, y)]:
+                        dis[(x, y)] = ns
+                        heappush(h, (ns, x, y))
+                        seen.add((x, y))
         return dis[(m - 1, n - 1)]
