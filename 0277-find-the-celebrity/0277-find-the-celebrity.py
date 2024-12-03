465923fbfff5359247c83836c3d3f2bf73d10827
Adnan Yaqoob <adnan@yaqoob.org>
2024-12-03 20:48:18 +1100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0277-find-the-celebrity/0277-find-the-celebrity.py
+++ 0277-find-the-celebrity/0277-find-the-celebrity.py
@@ -4,17 +4,11 @@
 
 class Solution:
     def findCelebrity(self, n: int) -> int:
-        x, seen = 0, {}
-        for y in range(1, n):
-            seen[(x, y)] = knows(x, y)
-            if seen[(x, y)]:
-                x = y
-        
-        count = 0
-        for y in range(n):
-            xy = seen[(x, y)] if (x, y) in seen else knows(x, y)
-            yx = seen[(y, x)] if (y, x) in seen else knows(y, x)
-            if not xy and yx:
-                count += 1
-        
-        return -1 if count != n - 1 else x
+        nodes = {node for node in range(n)}
+        for a in range(n):
+            for b in range(a + 1, n):
+                if knows(b, a):
+                    nodes.discard(b)
+                if knows(a, b):
+                    nodes.discard(a)
+        return next(iter(nodes)) if len(nodes) == 1 else -1
