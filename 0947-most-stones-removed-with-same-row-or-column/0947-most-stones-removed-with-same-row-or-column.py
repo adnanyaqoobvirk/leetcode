5c02eed5e09a00059f17a42f251c93db94c17b92
Adnan Yaqoob <adnan@yaqoob.org>
2025-01-21 16:46:54 +1100

    Time: 20 ms (69.98%), Space: 18.8 MB (32.59%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
@@ -1,20 +1,31 @@
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
-        n = len(stones)
+        rmap = defaultdict(list)
+        cmap = defaultdict(list)
+        for i, (r, c) in enumerate(stones):
+            rmap[r].append(i)
+            cmap[c].append(i)
         
-        UF = {}
+        nodes = set(tuple(e) for e in stones)
+        ans = 0
+        while nodes:
+            q = [nodes.pop()]
+            count = 0
+            while q:
+                nq = []
+                for node in q:
+                    for i in rmap[node[0]]:
+                        stone = tuple(stones[i])
+                        if node != stone and stone in nodes:
+                            nq.append(stone)
+                            nodes.remove(stone)
+                    for i in cmap[node[1]]:
+                        stone = tuple(stones[i])
+                        if node != stone and stone in nodes:
+                            nq.append(stone)
+                            nodes.remove(stone)
+                    count += 1
+                q = nq
+            ans += count - 1
+        return ans
         
-        def find(x):
-            if x != UF[x]:
-                UF[x] = find(UF[x])
-            return UF[x]
-        
-        def union(x, y):
-            UF.setdefault(x, x)
-            UF.setdefault(y, y)
-            UF[find(x)] = find(y)
-            
-        for i, j in stones:
-            union(i, j + 10001)
-        
-        return n - len({find(i) for i in UF})
