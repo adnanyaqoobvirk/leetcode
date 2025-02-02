3c322482ad1d6af6844bcf500852de51d87591e1
Adnan Yaqoob <adnan@yaqoob.org>
2025-02-03 01:28:44 +1100

    Time: 51 ms (33.17%), Space: 55.8 MB (5.04%) - LeetHub

--- 3152-special-array-ii/3152-special-array-ii.py
+++ 3152-special-array-ii/3152-special-array-ii.py
@@ -1,16 +1,9 @@
 class Solution:
     def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
-        psums = [0]
+        smap = {0: 0}
+        sindex = 0
         for i in range(1, len(nums)):
-            psums.append(psums[-1])
-            if nums[i] & 1 == nums[i - 1] & 1:
-                continue
-            psums[i] += 1
-        ans = []
-        for i, j in queries:
-            count = psums[j] - psums[i] 
-            if count == j - i:
-                ans.append(True)
-            else:
-                ans.append(False)
-        return ans
+            if nums[i - 1] & 1 == nums[i] & 1:
+                sindex += 1
+            smap[i] = sindex
+        return [smap[start] == smap[end] for start, end in queries]
