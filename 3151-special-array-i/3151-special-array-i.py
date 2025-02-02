0ec74441c3a02f1d00a9bf522b494d1ad81a0e9f
Adnan Yaqoob <adnan@yaqoob.org>
2025-02-02 23:33:36 +1100

    Time: 0 ms (100%), Space: 17.7 MB (34.27%) - LeetHub

--- 3151-special-array-i/3151-special-array-i.py
+++ 3151-special-array-i/3151-special-array-i.py
@@ -1,6 +1,6 @@
 class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
         for i in range(1, len(nums)):
-            if not (nums[i - 1] ^ nums[i]) & 1:
+            if not ((nums[i - 1] ^ nums[i]) & 1):
                 return False
         return True
