ca35e6fcf78e066067410a9c637bb126082d04a9
Adnan Yaqoob <adnan@yaqoob.org>
2025-02-06 00:39:21 +1100

    Time: 868 ms (82.71%), Space: 37.8 MB (81.47%) - LeetHub

--- 1852-distinct-numbers-in-each-subarray/1852-distinct-numbers-in-each-subarray.py
+++ 1852-distinct-numbers-in-each-subarray/1852-distinct-numbers-in-each-subarray.py
@@ -1,17 +1,13 @@
 class Solution:
     def distinctNumbers(self, nums: List[int], k: int) -> List[int]:
-        counts = {}
-        for i in range(0, k):
-            counts[nums[i]] = counts.get(nums[i], 0) + 1
-            
-        ans = [len(counts)]
-        for i in range(1, len(nums) - k + 1):
-            left, right = nums[i - 1], nums[i + k - 1]
-            if counts[left] == 1:
-                del counts[left]
-            else:
-                counts[left] -= 1
-            counts[right] = counts.get(right, 0) + 1
-            ans.append(len(counts))
-            
+        freq = {}
+        for i in range(k):
+            freq[nums[i]] = freq.get(nums[i], 0) + 1
+        ans = [len(freq)]
+        for i in range(k, len(nums)):
+            freq[nums[i]] = freq.get(nums[i], 0) + 1
+            freq[nums[i - k]] -= 1
+            if freq[nums[i - k]] == 0:
+                del freq[nums[i - k]]
+            ans.append(len(freq))
         return ans
