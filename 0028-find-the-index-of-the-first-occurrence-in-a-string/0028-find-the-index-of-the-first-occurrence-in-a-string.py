3fce6b964c94d9b67e0629787eeae756065eea79
Adnan Yaqoob <adnan@yaqoob.org>
2025-05-31 23:22:31 +1000

    Time: 3 ms (8.63%), Space: 17.8 MB (32.53%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,9 +1,41 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        for i in range(len(haystack) - len(needle) + 1):
-            for j in range(len(needle)):
-                if haystack[i + j] != needle[j]:
-                    break
-            else:
-                return i
+        MOD = 10**9 + 7
+        P = 29
+
+        m, n = len(haystack), len(needle)
+
+        if n == 0 or n > m:
+            return -1
+
+        h = 1
+        thash = 0
+        phash = 0
+        for i in range(n):
+            if i > 0:
+                h = h * P % MOD
+            thash = (thash * P + ord(haystack[i])) % MOD
+            phash = (phash * P + ord(needle[i])) % MOD
+        
+        for i in range(n, m + 1):
+            if thash == phash:
+                for j in range(n):
+                    if haystack[i - n + j] != needle[j]:
+                        break
+                else:
+                    return i - n
+            
+            if i < m:
+                thash -= h * ord(haystack[i - n])
+                thash %= MOD
+
+                if thash < 0:
+                    thash += MOD
+                    
+                thash *= P
+                thash += ord(haystack[i])
+                thash %= MOD
+
+                if thash < 0:
+                    thash += MOD
         return -1
