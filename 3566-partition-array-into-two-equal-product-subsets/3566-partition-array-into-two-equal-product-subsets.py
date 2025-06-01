a561f5466c110c1c8705ab2ea3715f1b9bef35b6
Adnan Yaqoob <adnan@yaqoob.org>
2025-06-01 21:17:01 +1000

    Time: 3 ms (84.61%), Space: 18 MB (38.1%) - LeetHub

--- 3566-partition-array-into-two-equal-product-subsets/3566-partition-array-into-two-equal-product-subsets.py
+++ 3566-partition-array-into-two-equal-product-subsets/3566-partition-array-into-two-equal-product-subsets.py
@@ -1,6 +1,5 @@
 class Solution:
     def checkEqualPartitions(self, nums: List[int], target: int) -> bool:
-        @cache
         def dp(i: int, t1: int, t2: int) -> bool:
             if i == n: 
                 return t1 == 1 and t2 == 1
