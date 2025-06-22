f78f096414c1ea6b1c92c75e73c574aa8aa9c69a
Adnan Yaqoob <adnan@yaqoob.org>
2025-06-22 17:44:34 +1000

    Time: 143 ms (53.09%), Space: 78.3 MB (77.22%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,26 +1,27 @@
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.c = capacity
-        self.d = {}
+        self.cap = capacity
+        self.items = {}
 
     def get(self, key: int) -> int:
-        if key not in self.d:
+        if key not in self.items:
             return -1
         
-        v = self.d[key]
-        del self.d[key]
-        self.d[key] = v
-        return v
+        value = self.items[key]
+        del self.items[key]
+        self.items[key] = value
+
+        return value
 
     def put(self, key: int, value: int) -> None:
-        if key in self.d:
-            del self.d[key]
-            
-        self.d[key] = value
-        
-        if len(self.d) > self.c:
-            del self.d[next(iter(self.d))]
+        if key in self.items:
+            del self.items[key]
+        self.items[key] = value
+
+        if len(self.items) > self.cap:
+            k = next(iter(self.items.keys()))
+            del self.items[k]
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
