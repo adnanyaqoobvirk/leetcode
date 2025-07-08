6271b7887ff41ff73470d57ec543f3eef278a8a6
Adnan Yaqoob <adnan@yaqoob.org>
2025-07-08 17:01:46 +1000

    Time: 3057 ms (5.49%), Space: 287.4 MB (8.24%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
@@ -1,25 +1,23 @@
 class Solution:
     def maxValue(self, events: List[List[int]], k: int) -> int:
+        @cache
+        def dp(i: int, r: int) -> int:
+            if r <= 0 or i >= n:
+                return 0
+
+            lo, hi = i, n - 1
+            while lo + 1 < hi:
+                mid = lo + (hi - lo) // 2
+
+                if events[mid][0] > events[i][1]: 
+                    hi = mid
+                else:
+                    lo = mid
+
+            return max(
+                events[i][2] + dp(hi if events[hi][0] > events[i][1] else n, r - 1),
+                dp(i + 1, r)
+            )
         n = len(events)
         events.sort()
-        
-        nmap = [-1] * n
-        for i in range(n):
-            l, r = i, n - 1
-            while l + 1 < r:
-                m = l + r >> 1
-                if events[m][0] > events[i][1]:
-                    r = m
-                else:
-                    l = m
-            if events[r][0] > events[i][1]:
-                nmap[i] = r
-        
-        dp = [[0] * (k + 1) for _ in range(n + 1)]
-        for i in reversed(range(n)):
-            for j in reversed(range(k)):
-                dp[i][j] = max(
-                    dp[i + 1][j],
-                    events[i][2] + (0 if nmap[i] == -1 else dp[nmap[i]][j + 1])
-                )
-        return dp[0][0]
+        return dp(0, k)
