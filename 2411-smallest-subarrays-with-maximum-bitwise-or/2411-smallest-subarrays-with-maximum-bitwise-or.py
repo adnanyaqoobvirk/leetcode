b7edfd393ee1536b37b6a9e22cb36eacc50271ab
Adnan Yaqoob <adnan@yaqoob.org>
2025-07-30 09:30:29 +1000

    Time: 1511 ms (14.92%), Space: 28.9 MB (87.31%) - LeetHub

--- 2411-smallest-subarrays-with-maximum-bitwise-or/2411-smallest-subarrays-with-maximum-bitwise-or.py
+++ 2411-smallest-subarrays-with-maximum-bitwise-or/2411-smallest-subarrays-with-maximum-bitwise-or.py
@@ -1,26 +1,14 @@
 class Solution:
     def smallestSubarrays(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        mor = [0] * (n + 1)
+        mor = 0
+        minBitLen = [n] * 31
+        ans = [1] * n
         for i in reversed(range(n)):
-            mor[i] = mor[i + 1] | nums[i]
-        
-        m = mor[0].bit_length()
-        minLenPerBit = [[0] * n for _ in range(m)]
-        for i in range(m):
-            stack = []
-            for r in range(n):
-                stack.append(r)
-                if nums[r] & (1 << i):
-                    while stack:
-                        l = stack.pop()
-                        minLenPerBit[i][l] = r - l + 1
-        
-        ans = []
-        for i in range(n):
-            imaxLen = 1
-            for j in range(m):
-                if mor[i] & (1 << j):
-                    imaxLen = max(imaxLen, minLenPerBit[j][i])
-            ans.append(imaxLen)
+            mor |= nums[i]
+            for j in range(31):
+                if nums[i] & (1 << j):
+                    minBitLen[j] = min(minBitLen[j], i)
+                if mor & (1 << j):
+                    ans[i] = max(ans[i], minBitLen[j] - i + 1)
         return ans
