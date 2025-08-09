f025cc1f3cb7d93050eb5d1e497ce28561fde04e
Adnan Yaqoob <adnan@yaqoob.org>
2025-08-10 08:55:44 +1000

    Time: 15 ms (49.83%), Space: 21.3 MB (20%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.py
+++ 0808-soup-servings/0808-soup-servings.py
@@ -1,20 +1,20 @@
 class Solution:
     def soupServings(self, n: int) -> float:
-        if n >= 1000:
+        if n >= 10000:
             return 1
 
         @cache
-        def dp(a, b, p):
+        def dp(a, b):
             if a <= 0 and b <= 0:
-                return p / 2
+                return 0.5
             if a <= 0 and b > 0:
-                return p
+                return 1
             if a <= 0 or b <= 0:
                 return 0
             
             res = 0
             for da, db in turns:
-                res += dp(a - da, b - db, p * 0.25)
-            return res
+                res += dp(a - da, b - db)
+            return res * 0.25
         turns = [(100, 0), (75, 25), (50, 50), (25, 75)]
-        return dp(n, n, 1)
+        return dp(n, n)
