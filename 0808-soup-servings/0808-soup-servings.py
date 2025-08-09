1b7061ef135b5b2ae0c4ec36ad88ef88470e6649
Adnan Yaqoob <adnan@yaqoob.org>
2025-08-10 08:48:51 +1000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.py
+++ 0808-soup-servings/0808-soup-servings.py
@@ -1,20 +1,20 @@
 class Solution:
     def soupServings(self, n: int) -> float:
-        m = ceil(n / 25)
-        dp = {}
+        if n >= 1000:
+            return 1
 
-        def calculate_dp(i, j):
-            return (dp[max(0, i - 4)][j] + dp[max(0, i - 3)][j - 1] +
-                    dp[max(0, i - 2)][max(0, j - 2)]
-                    + dp[i - 1][max(0, j - 3)]) / 4
-
-        dp[0] = {0: 0.5}
-        for k in range(1, m + 1):
-            dp[0][k] = 1
-            dp[k] = {0: 0}
-            for j in range(1, k + 1):
-                dp[j][k] = calculate_dp(j, k)
-                dp[k][j] = calculate_dp(k, j)
-            if dp[k][k] > 1 - 1e-5:
-                return 1
-        return dp[m][m]
+        @cache
+        def dp(a, b, p):
+            if a <= 0 and b <= 0:
+                return p / 2
+            if a <= 0 and b > 0:
+                return p
+            if a <= 0 or b <= 0:
+                return 0
+            
+            res = 0
+            for da, db in turns:
+                res += dp(a - da, b - db, p * 0.25)
+            return res
+        turns = [(100, 0), (75, 25), (50, 50), (25, 75)]
+        return dp(n, n, 1)
