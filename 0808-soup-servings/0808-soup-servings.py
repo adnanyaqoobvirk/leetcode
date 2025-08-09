0de666839085afbc6b81f8d4ca87936c85a4ad88
Adnan Yaqoob <adnan@yaqoob.org>
2025-08-10 09:59:50 +1000

    Time: 100 ms (30.74%), Space: 19.5 MB (57.86%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.py
+++ 0808-soup-servings/0808-soup-servings.py
@@ -3,18 +3,15 @@ class Solution:
         if n >= 10000:
             return 1
 
-        @cache
-        def dp(a, b):
-            if a <= 0 and b <= 0:
-                return 0.5
-            if a <= 0 and b > 0:
-                return 1
-            if a <= 0 or b <= 0:
-                return 0
-            
-            res = 0
-            for da, db in turns:
-                res += dp(a - da, b - db)
-            return res * 0.25
+        n = ceil(n / 25) + 1
         turns = [(4, 0), (3, 1), (2, 2), (1, 3)]
-        return dp(ceil(n / 25), ceil(n / 25))
+        dp = [[0] * n for _ in range(n)]
+        dp[0][0] = 0.5
+        for i in range(1, n):
+            dp[0][i] = 1
+        for a in range(1, n):
+            for b in range(1, n):
+                for da, db in turns:
+                    dp[a][b] += dp[max(a - da, 0)][max(b - db, 0)]
+                dp[a][b] *= 0.25
+        return dp[n - 1][n - 1]
