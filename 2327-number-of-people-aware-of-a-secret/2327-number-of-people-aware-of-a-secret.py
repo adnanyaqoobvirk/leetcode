9faa828b7f7bebda1f0d3a9a2ae1b1f3d0d5aa5a
Adnan Yaqoob <adnan@yaqoob.org>
2025-09-10 08:59:52 +1000

    Time: 3 ms (92.03%), Space: 18.3 MB (13.04%) - LeetHub

--- 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.py
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.py
@@ -1,10 +1,25 @@
 class Solution:
     def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
         MOD = 10**9 + 7
-        dp = [0] * n
-        dp[0] = 1
-        for i in range(n):
-            for j in range(min(n, i + delay), min(n, i + forget)):
-                dp[j] += dp[i]
-            dp[i] %= MOD
-        return sum(dp[i] for i in range(n - forget, n)) % MOD
+
+        knows = deque([(1, 1)])
+        knows_cnt = 1
+        shares = deque()
+        shares_cnt = 0
+
+        for day in range(2, n + 1):
+            if knows and day - delay >= knows[0][0]:
+                kday, kcnt = knows.popleft()
+                shares.append((kday, kcnt))
+                shares_cnt += kcnt
+                knows_cnt -= kcnt
+
+            if shares and day - forget >= shares[0][0]:
+                sday, scnt = shares.popleft()
+                shares_cnt -= scnt
+
+            if shares_cnt > 0:
+                knows.append((day, shares_cnt))
+                knows_cnt += shares_cnt
+
+        return (knows_cnt + shares_cnt) % MOD
