1157e62f8bd127b945e93795e8008988f42f5eca
Adnan Yaqoob <adnan@yaqoob.org>
2025-09-15 08:07:57 +1000

    Time: 87 ms (9.19%), Space: 28 MB (6.32%) - LeetHub

---
+++ 0966-vowel-spellchecker/0966-vowel-spellchecker.py
@@ -0,0 +1,40 @@
+class Solution:
+    def spellchecker(self, wordlist: List[str], queries: List[str]) -> List[str]:
+        vowels = set(list("aeiouAEIOU"))
+        wmap = {}
+        for i, word in enumerate(wordlist):
+            wmap[word] = i
+            t1, t2 = [], []
+            for j, c in enumerate(word):
+                t1.append((j, c.lower()))
+                if c not in vowels:
+                    t2.append((j, c.lower()))
+            t1, t2 = tuple(t1), tuple(t2)
+            if t1 not in wmap:
+                wmap[t1] = i
+            if t2 not in wmap:
+                wmap[t2] = i
+        
+        ans = []
+        for q in queries:
+            if q in wmap:
+                ans.append(q)
+                continue
+
+            t1, t2 = [], []
+            for j, c in enumerate(q):
+                t1.append((j, c.lower()))
+                if c not in vowels:
+                    t2.append((j, c.lower()))
+            
+            t1, t2 = tuple(t1), tuple(t2)
+            if t1 in wmap:
+                ans.append(wordlist[wmap[t1]])
+                continue
+            
+            if t2 in wmap:
+                ans.append(wordlist[wmap[t2]])
+                continue
+            
+            ans.append("")
+        return ans
