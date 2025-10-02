42be6039bd7c0dc423ea4ca753b327b67d0f81fa
Adnan Yaqoob <adnan@yaqoob.org>
2025-10-03 08:00:23 +1000

    Time: 63 ms (72.26%), Space: 22.2 MB (87.44%) - LeetHub

--- 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.py
+++ 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.py
@@ -1,27 +1,42 @@
+class UF:
+    def __init__(self, n: int) -> None:
+        self.roots = list(range(n + 1))
+        self.rank = [0] * (n + 1)
+    
+    def find(self, x) -> int:
+        if self.roots[x] != x:
+            self.roots[x] = self.find(self.roots[x])
+        return self.roots[x]
+
+    def union(self, x, y) -> bool:
+        rootX = self.find(x)
+        rootY = self.find(y)
+
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.roots[rootY] = rootX
+                self.rank[rootX] += 1
+            else:
+                self.roots[rootX] = rootY
+                self.rank[rootY] += 1
+            return True
+        
+        return False
+
 class Solution:
     def minimumCost(self, n: int, connections: List[List[int]]) -> int:
-        graph = defaultdict(list)
-        for x, y, cost in connections:
-            graph[x].append((y, cost))
-            graph[y].append((x, cost))
-        
-        h = [(0, 1)]
-        visited = set()
+        connections.sort(key=lambda x: x[2])
+
+        uf = UF(n)
         min_cost = 0
-        while h:
-            cost, x = heappop(h)
-            
-            if x in visited:
-                continue
-
-            visited.add(x)
-            min_cost += cost
-
-            for y, cost in graph[x]:
-                if y not in visited:
-                    heappush(h, (cost, y))
-
-        if len(visited) == n:
+        edge_count = 0
+        
+        for x, y, cost in connections:
+            if uf.union(x, y):
+                min_cost += cost
+                edge_count += 1
+        
+        if edge_count == n - 1:
             return min_cost
         
         return -1
