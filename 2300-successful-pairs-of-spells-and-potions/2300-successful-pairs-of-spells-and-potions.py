4b6b72d1947e458866fecfeca74af36c8e4ac90f
Adnan Yaqoob <adnan@yaqoob.org>
2025-10-08 11:32:38 +1100

    Time: 143 ms (97.92%), Space: 42.7 MB (25.48%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -1,20 +1,10 @@
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        n = len(potions)
         potions.sort()
-        
-        m = len(potions)
-        
-        pairs = []
-        for spell in spells:
-            min_multiple = ceil(success / spell) - 1
-            
-            lo, hi = 0, m
-            while lo < hi:
-                mid = lo + (hi - lo) // 2
-                if potions[mid] > min_multiple:
-                    hi = mid
-                else:
-                    lo = mid + 1
-                    
-            pairs.append(m - lo)
-        return pairs
+
+        ans = []
+        for s in spells:
+            i = bisect_left(potions, ceil(success / s))
+            ans.append(0 if i == n else n - i)
+        return ans
