3272d4dd633ef2e3337c1f944543e47f8c84098a
Adnan Yaqoob <adnan@yaqoob.org>
2025-10-14 08:27:43 +1100

    Time: 19 ms (25.36%), Space: 18.2 MB (7.47%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
@@ -1,8 +1,9 @@
 class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
+        counts = {word: Counter(word) for word in words}
         stack = []
         for word in words:
-            if stack and Counter(word) == Counter(stack[-1]):
+            if stack and counts[word] == counts[stack[-1]]:
                 continue
             stack.append(word)
         return stack
