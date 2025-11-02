894105d9f90130e740f0719ab6fa6a330bdf84ad
Adnan Yaqoob <adnan@yaqoob.org>
2025-11-02 11:36:57 +1100

    Time: 571 ms (32.65%), Space: 43.4 MB (34.69%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.py
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.py
@@ -1,35 +1,23 @@
 class Solution:
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
-        grid = [[0] * n for _ in range(m)]
-        for i, j in walls:
-            grid[i][j] = 1
-        
+        seen = set()
+        walls = set(tuple(w) for w in walls)
+        guards = set(tuple(g) for g in guards)
         for i, j in guards:
-            grid[i][j] = 2
-            for y in range(j - 1, -1, -1):
-                if grid[i][y] > 0:
+            for r in range(i - 1, -1, -1):
+                if (r, j) in walls or (r, j) in guards:
                     break
-                grid[i][y] = -1
-                
-            for y in range(j + 1, n):
-                if grid[i][y] > 0:
+                seen.add((r, j))
+            for r in range(i + 1, m):
+                if (r, j) in walls or (r, j) in guards:
                     break
-                grid[i][y] = -1
-                
-            for x in range(i - 1, -1, -1):
-                if grid[x][j] > 0:
+                seen.add((r, j))
+            for c in range(j - 1, -1, -1):
+                if (i, c) in walls or (i, c) in guards:
                     break
-                grid[x][j] = -1
-                
-            for x in range(i + 1, m):
-                if grid[x][j] > 0:
+                seen.add((i, c))
+            for c in range(j + 1, n):
+                if (i, c) in walls or (i, c) in guards:
                     break
-                grid[x][j] = -1
-        
-        ans = 0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 0:
-                    ans += 1
-        return ans
-            
+                seen.add((i, c))
+        return m * n - len(walls) - len(seen) - len(guards)
