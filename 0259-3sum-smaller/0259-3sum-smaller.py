7c135ddce8b27bc868f1235086e47318b437cc8c
Adnan Yaqoob <adnan@yaqoob.org>
2025-11-15 15:14:19 +1100

    Time: 412 ms (88.79%), Space: 18 MB (59.65%) - LeetHub

--- 0259-3sum-smaller/0259-3sum-smaller.py
+++ 0259-3sum-smaller/0259-3sum-smaller.py
@@ -4,8 +4,13 @@ class Solution:
         nums.sort()
 
         triplets = 0
-        for i in reversed(range(n)):
-            for j in reversed(range(i)):
-                k = bisect_left(nums, target - nums[i] - nums[j], 0, j)
-                triplets += k
+        for i in range(n - 2):
+            remaining = target - nums[i]
+            l, r = i + 1, n - 1
+            while l < r:
+                if nums[l] + nums[r] < remaining:
+                    triplets += r - l
+                    l += 1
+                else:
+                    r -= 1
         return triplets
