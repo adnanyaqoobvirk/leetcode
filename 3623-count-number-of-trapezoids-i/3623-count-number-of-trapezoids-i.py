c857a3ee10745dffbab6dba22e5594c45b0e47a4
Adnan Yaqoob <adnan@yaqoob.org>
2025-12-03 10:43:44 +1100

    Time: 87 ms (41.49%), Space: 63.5 MB (91.05%) - LeetHub

--- 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.py
+++ 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.py
@@ -7,17 +7,10 @@ class Solution:
             ycounts[y] += 1
         
         total_lines = 0
-        line_counts = []
+        trap_count = 0
         for y, count in ycounts.items():
             line_count = count * (count - 1) // 2
-            line_counts.append(line_count)
+            trap_count = (trap_count + line_count * total_lines) % MOD
             total_lines += line_count
-        
-        trap_count = 0
-        remaining_lines = total_lines
-        for i in range(len(line_counts) - 1):
-            line_count = line_counts[i]
-            remaining_lines -= line_count
-            trap_count = (trap_count + line_count * remaining_lines) % MOD
         return trap_count
 
