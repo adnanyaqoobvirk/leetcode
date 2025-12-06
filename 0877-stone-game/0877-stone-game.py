2159ab15e3221301e51eaa010838fda694d46102
Adnan Yaqoob <adnan@yaqoob.org>
2025-12-06 12:02:58 +1100

    Time: 0 ms (100%), Space: 17.6 MB (92.55%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,10 +1,3 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        def dp(i: int, j: int) -> int:
-            if i == j:
-                return 0
-            
-            return max(piles[i] + dp(i + 1, j), piles[j] + dp(i, j - 1))
-        n = len(piles)
-        total_half = sum(piles) // 2
-        return dp(0, n - 1) > total_half
+        return True
