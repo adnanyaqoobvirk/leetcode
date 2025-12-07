64e8b9d4117a042fa36a6bd730f517782e02c986
Adnan Yaqoob <adnan@yaqoob.org>
2025-12-07 11:48:15 +1100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.py
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.py
@@ -1,6 +1,4 @@
 class Solution:
     def countOdds(self, low: int, high: int) -> int:
-        if low & 1 and high & 1:
-            return (high - low) // 2 + 1
-        else:
-            return (high - low + 1) // 2
+        odds = (high - low) // 2
+        return odds + 1 if low & 1 and high & 1 else odds
