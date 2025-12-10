4bd05ac8bc711318b740dfd91481a1a0be8ed9b5
Adnan Yaqoob <adnan@yaqoob.org>
2025-12-11 05:41:52 +1100

    Time: 190 ms (29.8%), Space: 21.7 MB (18.1%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -2,24 +2,18 @@ class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         m, n = len(mat), len(mat[0])
 
-        distances = [[inf] * n for _ in range(m)]
-        for i in range(m):
-            for j in range(n):
-                if mat[i][j] == 0:
-                    distances[i][j] = 0
-                    continue
-
-                for x, y in [(i - 1, j), (i, j - 1)]:
-                    if 0 <= x < m and 0 <= y < n:
-                        distances[i][j] = min(distances[i][j], distances[x][y] + 1)
-        
-        for i in reversed(range(m)):
-            for j in reversed(range(n)):
-                if mat[i][j] == 0:
-                    continue
-
-                for x, y in [(i + 1, j), (i, j + 1)]:
-                    if 0 <= x < m and 0 <= y < n:
-                        distances[i][j] = min(distances[i][j], distances[x][y] + 1)
-
+        distances = [[0] * n for _ in range(m)]
+        q = [(i, j) for i in range(m) for j in range(n) if mat[i][j] == 0]
+        seen = set(q)
+        steps = 0
+        while q:
+            steps += 1
+            nq = []
+            for i, j in q:
+                for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+                    if 0 <= x < m and 0 <= y < n and (x, y) not in seen and mat[x][y] != 0:
+                        seen.add((x, y))
+                        nq.append((x, y))
+                        distances[x][y] = steps
+            q = nq
         return distances
