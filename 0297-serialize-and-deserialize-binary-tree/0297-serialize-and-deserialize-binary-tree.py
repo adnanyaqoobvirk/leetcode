46f975ae7c3b8f593cd3fd84058a0916c03cb80a
Adnan Yaqoob <adnan@yaqoob.org>
2025-12-10 16:23:47 +1100

    Time: 88 ms (44.63%), Space: 21.3 MB (58.8%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -13,18 +13,16 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        def helper(curr):
+        encodes = []
+        def encode(curr: TreeNode) -> None:
             if not curr:
-                ans.append("n")
+                encodes.append("#")
             else:
-                helper(curr.right)
-                helper(curr.left)
-                ans.append(str(curr.val))
-
-
-        ans = []
-        helper(root)
-        return ",".join(ans)
+                encode(curr.right)
+                encode(curr.left)
+                encodes.append(str(curr.val))
+        encode(root)
+        return "|".join(encodes)
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -32,23 +30,20 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        def helper():
-            if nodes:
-                val = nodes.pop()
+        encodes = data.split("|")
+        def decode() -> None:
+            if not encodes:
+                return None
+            
+            encode = encodes.pop()
+            if encode == "#":
+                return None
 
-                if val == "n":
-                    return None
-                
-                node = TreeNode(int(val))
-                node.left = helper()
-                node.right = helper()
-                
-                return node
-
-        nodes = data.split(",")
-
-        return helper()
-        
+            curr = TreeNode(int(encode))
+            curr.left = decode()
+            curr.right = decode()
+            return curr
+        return decode()
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
