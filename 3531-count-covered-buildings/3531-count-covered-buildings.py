60cb6dc8c3baf9f11c6091ed76e565912052bc5c
Adnan Yaqoob <adnan@yaqoob.org>
2025-12-11 12:58:44 +1100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3531-count-covered-buildings/3531-count-covered-buildings.py
+++ 3531-count-covered-buildings/3531-count-covered-buildings.py
@@ -1,7 +1,5 @@
 class Solution:
     def countCoveredBuildings(self, n: int, buildings: List[List[int]]) -> int:
-        buildings.sort()
-
         column_buildings = defaultdict(list)
         row_buildings = defaultdict(list)
 
@@ -10,13 +8,33 @@ class Solution:
             column_buildings[y].append(i)
         
         column_candidates = set()
-        for column, buildings in column_buildings.items():
-            for j in range(1, len(buildings) - 1):
-                column_candidates.add(buildings[j])
+        for _, candidates in column_buildings.items():
+            if len(candidates) < 3:
+                continue
+
+            min_row, max_row = inf, -inf
+            for j in range(len(candidates)):
+                i = candidates[j]
+                x = buildings[i][0]
+                column_candidates.add(x)
+                min_row = min(min_row, x)
+                max_row = max(max_row, x)
+            column_candidates.discard(min_row)
+            column_candidates.discard(max_row)
         
         row_candidates = set()
-        for row, buildings in row_buildings.items():
-            for j in range(1, len(buildings) - 1):
-                row_candidates.add(buildings[j])
-        
+        for _, candidates in row_buildings.items():
+            if len(candidates) < 3:
+                continue
+
+            min_col, max_col = inf, -inf
+            for j in range(len(candidates)):
+                i = candidates[j]
+                y = buildings[i][1]
+                row_candidates.add(y)
+                min_col = min(min_col, y)
+                max_col = max(max_col, y)
+            row_candidates.discard(min_col)
+            row_candidates.discard(max_col)
+
         return len(row_candidates & column_candidates)
