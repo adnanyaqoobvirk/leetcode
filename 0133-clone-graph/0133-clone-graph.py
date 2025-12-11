5f8fb532a13eef01ecba28470731431c634f1838
Adnan Yaqoob <adnan@yaqoob.org>
2025-12-12 07:33:19 +1100

    Time: 38 ms (85.6%), Space: 18.1 MB (81.79%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -9,21 +9,27 @@ class Node:
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        def dfs(curr: Optional['Node']) -> Optional['Node']:
-            if not curr:
-                return None
-
-            new_curr = Node(curr.val)
-            node_map[curr] = new_curr
-            
-            for nei in curr.neighbors:
-                if nei not in node_map:
-                    new_nei = dfs(nei)
-                    node_map[nei] = new_nei
-                
-                new_curr.neighbors.append(node_map[nei])
-            
-            return new_curr
+        if not node:
+            return None
         
-        node_map = {}
-        return dfs(node)
+        SUB_GRAPH_DONE = True
+        SUB_GRAPH_NOT_DONE = False
+
+        node_map = {node: Node(node.val)}
+        stack = [(node, SUB_GRAPH_NOT_DONE)]
+        while stack:
+            curr, is_sub_graph_done = stack.pop()
+
+            if is_sub_graph_done:
+                new_curr = node_map[curr]
+                for nei in curr.neighbors:
+                    new_curr.neighbors.append(node_map[nei])
+            else:
+                stack.append((curr, SUB_GRAPH_DONE))
+                for nei in curr.neighbors:
+                    if nei in node_map:
+                        continue
+                    node_map[nei] = Node(nei.val)
+                    stack.append((nei, SUB_GRAPH_NOT_DONE))
+        
+        return node_map[node]
