ee35d25e075fb2bca25757e4571f1684618b9101
Adnan Yaqoob <adnan@yaqoob.org>
2025-12-12 05:45:56 +1100

    Time: 3 ms (77.06%), Space: 20.1 MB (6.96%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,20 +1,34 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        def dfs(course: int) -> bool:
+            visiting.add(course)
+            processed.add(course)
+            
+            for nei in adj[course]:
+                if nei in visiting:
+                    return False
+                if nei in processed:
+                    continue
+                if not dfs(nei):
+                    return False
+
+            visiting.remove(course)
+            ordered_courses.append(course)
+            return True
+        
         adj = defaultdict(list)
-        indegree = defaultdict(int)
         for dst, src in prerequisites:
             adj[src].append(dst)
-            indegree[dst] += 1
         
+        visiting = set()
+        processed = set()
         ordered_courses = []
-        q = deque([course for course in range(numCourses) if indegree[course] == 0])
-        while q:
-            course = q.popleft()
-            ordered_courses.append(course)
-
-            for nei in adj[course]:
-                indegree[nei] -= 1
-                if indegree[nei] == 0:
-                    q.append(nei)
+        unprocessed_courses = set(range(numCourses))
+        while unprocessed_courses:
+            course = unprocessed_courses.pop()
+            if course in processed:
+                continue
+            if not dfs(course):
+                return []
         
-        return ordered_courses if len(ordered_courses) == numCourses else []
+        return ordered_courses[::-1]
