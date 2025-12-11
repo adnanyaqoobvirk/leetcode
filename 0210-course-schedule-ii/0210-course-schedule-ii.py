0dbfd9a631ca8b62f6c19ba989567496f59e3976
Adnan Yaqoob <adnan@yaqoob.org>
2025-12-12 05:35:16 +1100

    Time: 5 ms (35.7%), Space: 19 MB (70.62%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,24 +1,20 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        indegrees = defaultdict(int)
-        graph = defaultdict(list)
+        adj = defaultdict(list)
+        indegree = defaultdict(int)
         for dst, src in prerequisites:
-            indegrees[dst] += 1
-            graph[src].append(dst)
+            adj[src].append(dst)
+            indegree[dst] += 1
         
-        res = []
-        q = [i for i in range(numCourses) if indegrees[i] == 0]
+        ordered_courses = []
+        q = deque([course for course in range(numCourses) if indegree[course] == 0])
         while q:
-            nq = []
-            
-            for src in q:
-                res.append(src)
-                
-                for dst in graph[src]:
-                    indegrees[dst] -= 1
-                    
-                    
-                    if indegrees[dst] == 0:
-                        nq.append(dst)
-            q = nq
-        return res if len(res) == numCourses else []
+            course = q.popleft()
+            ordered_courses.append(course)
+
+            for nei in adj[course]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    q.append(nei)
+        
+        return ordered_courses if len(ordered_courses) == numCourses else []
