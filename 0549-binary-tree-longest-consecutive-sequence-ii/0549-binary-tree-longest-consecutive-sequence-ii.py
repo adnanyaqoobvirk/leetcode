e852ebc795d5aeca605661951e21050a7d118948
Adnan Yaqoob <adnan@yaqoob.org>
2025-12-24 11:58:31 +1100

    Time: 6 ms (41.67%), Space: 18.5 MB (66.04%) - LeetHub

--- 0549-binary-tree-longest-consecutive-sequence-ii/0549-binary-tree-longest-consecutive-sequence-ii.py
+++ 0549-binary-tree-longest-consecutive-sequence-ii/0549-binary-tree-longest-consecutive-sequence-ii.py
@@ -6,41 +6,30 @@
 #         self.right = right
 class Solution:
     def longestConsecutive(self, root: Optional[TreeNode]) -> int:
-        def helper(curr: TreeNode) -> Tuple[int, int]:
+        def dfs(curr: Optional[TreeNode]) -> Tuple[int, int]:
             if not curr:
                 return 0, 0
             
-            nonlocal ans
-            
-            left_pmax, left_nmax = (v + 1 for v in helper(curr.left))
-            right_pmax, right_nmax = (v + 1 for v in helper(curr.right))
-            
-            if curr.left and curr.right:
-                if curr.left.val == curr.val - 1 and curr.right.val == curr.val + 1:
-                    ans = max(ans, left_nmax + right_pmax - 1)
-            
-                if curr.left.val == curr.val + 1 and curr.right.val == curr.val - 1:
-                    ans = max(ans, left_pmax + right_nmax - 1)
-            
+            left_inc, left_dec = dfs(curr.left)
+            right_inc, right_dec = dfs(curr.right)
+        
+            curr_inc = 1
+            curr_dec = 1
             if curr.left:
-                if curr.left.val != curr.val + 1:
-                    left_pmax = 1
-
-                if curr.left.val != curr.val - 1:
-                    left_nmax = 1
+                if curr.val == curr.left.val + 1:
+                    curr_dec = max(curr_dec, left_dec + 1)
+                elif curr.val == curr.left.val - 1:
+                    curr_inc = max(curr_inc, left_inc + 1)
             
             if curr.right:
-                if curr.right.val != curr.val + 1:
-                    right_pmax = 1
+                if curr.val == curr.right.val + 1:
+                    curr_dec = max(curr_dec, right_dec + 1)
+                elif curr.val == curr.right.val - 1:
+                    curr_inc = max(curr_inc, right_inc + 1)
 
-                if curr.right.val != curr.val - 1:
-                    right_nmax = 1
-            
-            pmax, nmax = max(left_pmax, right_pmax), max(left_nmax, right_nmax)
-            ans = max(ans, pmax, nmax)
-            
-            return pmax, nmax
+            max_length[0] = max(max_length[0], curr_inc + curr_dec - 1)
+            return curr_inc, curr_dec
         
-        ans = 1
-        helper(root)
-        return ans
+        max_length = [0]
+        dfs(root)
+        return max_length[0]
