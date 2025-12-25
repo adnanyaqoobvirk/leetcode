fa50f711f42ab008fa1c558c3b2dc7458a4cda2f
Adnan Yaqoob <adnan@yaqoob.org>
2025-12-25 13:57:33 +1100

    Time: 4 ms (4.42%), Space: 17.3 MB (96.7%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -1,36 +1,22 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        m, n = len(s), len(t)
+        def nextChar(inputStr: str) -> str:
+            bspace_cnt = 0
+            for c in reversed(inputStr):
+                if c == "#":
+                    bspace_cnt += 1
+                elif bspace_cnt > 0:
+                    bspace_cnt -= 1
+                else:
+                    yield c
 
-        s_bspace_cnt = 0
-        t_bspace_cnt = 0
-        j = n - 1
-        i = m - 1
+        s_iter, t_iter = nextChar(s), nextChar(t)
         while True:
-            while i >= 0:
-                if s[i] == "#":
-                    s_bspace_cnt += 1
-                elif s_bspace_cnt > 0:
-                    s_bspace_cnt -= 1
-                else:
-                    break
-                i -= 1
-
-            while j >= 0:
-                if t[j] == "#":
-                    t_bspace_cnt += 1
-                elif t_bspace_cnt > 0:
-                    t_bspace_cnt -= 1
-                else:
-                    break
-                j -= 1
-            
-            if i < 0 or j < 0:
-                break
-            
-            if s[i] != t[j]:
+            schar = next(s_iter, None)
+            tchar = next(t_iter, None)
+        
+            if schar != tchar:
                 return False
             
-            i -= 1
-            j -= 1
-        return i < 0 and j < 0
+            if schar is None and tchar is None:
+                return True
