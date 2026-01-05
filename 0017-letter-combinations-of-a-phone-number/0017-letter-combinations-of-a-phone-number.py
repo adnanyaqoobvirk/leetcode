506e31dde198e864f9a7e251b0a1f30ce83cac92
Adnan Yaqoob <adnan@yaqoob.org>
2026-01-05 14:02:05 +1100

    Time: 0 ms (100%), Space: 17.1 MB (98.28%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,16 +1,41 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        def backtrack(i: int, comb: list) -> None:
-            if i >= n:
-                if comb:
-                    ans.append("".join(comb))
-            else:
-                for c in mapping[digits[i]]:
-                    comb.append(c)
-                    backtrack(i + 1, comb)
-                    comb.pop()       
-        
-        mapping = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
-        ans, n = [], len(digits)
-        backtrack(0, [])
-        return ans
+        def nextChar(i: int) -> str:
+            for ch in char_map[digits[i]]:
+                yield ch
+
+        char_map = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+
+        n = len(digits)
+        combs = []
+        comb = []
+        stack = [(0, None)]
+        while stack:
+            idx, itr = stack.pop()
+
+            if idx >= n:
+                combs.append("".join(comb))
+                comb.pop()
+                continue
+            
+            if not itr:
+                itr = nextChar(idx)
+            
+            ch = next(itr, None)
+            if ch:
+                comb.append(ch)
+                stack.append((idx, itr))
+                stack.append((idx + 1, None))
+            elif comb:
+                comb.pop()
+
+        return combs
