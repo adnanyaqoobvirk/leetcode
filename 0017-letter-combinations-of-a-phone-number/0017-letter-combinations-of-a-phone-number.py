a1a830e8ace707dae603fc935ab72659bec45bec
Adnan Yaqoob <adnan@yaqoob.org>
2026-01-16 18:14:21 +1100

    Time: 0 ms (100%), Space: 19.4 MB (6.99%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,41 +1,35 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        def nextChar(i: int) -> str:
-            for ch in char_map[digits[i]]:
-                yield ch
-
-        char_map = {
-            "2": "abc",
-            "3": "def",
-            "4": "ghi",
-            "5": "jkl",
-            "6": "mno",
-            "7": "pqrs",
-            "8": "tuv",
-            "9": "wxyz"
+        if not digits:
+            return []
+        
+        phone = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
         }
-
-        n = len(digits)
-        combs = []
-        comb = []
-        stack = [(0, None)]
-        while stack:
-            idx, itr = stack.pop()
-
-            if idx >= n:
-                combs.append("".join(comb))
-                comb.pop()
-                continue
+        
+        # Generator for combinations
+        def generate() -> Iterator[str]:
+            n = len(digits)
+            indices = [0] * n
             
-            if not itr:
-                itr = nextChar(idx)
-            
-            ch = next(itr, None)
-            if ch:
-                comb.append(ch)
-                stack.append((idx, itr))
-                stack.append((idx + 1, None))
-            elif comb:
-                comb.pop()
-
-        return combs
+            while True:
+                # Build current combination
+                yield ''.join(phone[digits[i]][indices[i]] for i in range(n))
+                
+                # Increment like an odometer
+                for pos in reversed(range(n)):
+                    indices[pos] += 1
+                    if indices[pos] < len(phone[digits[pos]]):
+                        break
+                    indices[pos] = 0
+                else:
+                    break
+        
+        return list(generate())
