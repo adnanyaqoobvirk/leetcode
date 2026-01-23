8b90c197670160f2a77f2ec8a31ad60c78ea190b
Adnan Yaqoob <adnan@yaqoob.org>
2026-01-23 11:15:22 +1100

    Time: 54 ms (9.87%), Space: 19.9 MB (6.14%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -11,25 +11,14 @@ class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if not node:
             return None
-        
-        SUB_GRAPH_DONE = True
-        SUB_GRAPH_NOT_DONE = False
 
         node_map = {node: Node(node.val)}
-        stack = [(node, SUB_GRAPH_NOT_DONE)]
+        stack = [node]
         while stack:
-            curr, is_sub_graph_done = stack.pop()
-
-            if is_sub_graph_done:
-                new_curr = node_map[curr]
-                for nei in curr.neighbors:
-                    new_curr.neighbors.append(node_map[nei])
-            else:
-                stack.append((curr, SUB_GRAPH_DONE))
-                for nei in curr.neighbors:
-                    if nei in node_map:
-                        continue
+            curr = stack.pop()
+            for nei in curr.neighbors:
+                if nei not in node_map:
                     node_map[nei] = Node(nei.val)
-                    stack.append((nei, SUB_GRAPH_NOT_DONE))
-        
+                    stack.append(nei)
+                node_map[curr].neighbors.append(node_map[nei])
         return node_map[node]
