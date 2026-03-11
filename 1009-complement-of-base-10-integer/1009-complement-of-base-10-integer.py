da4911585b7ad55a7de9b26c19487c92cb73fdc2
Adnan Yaqoob <adnan@yaqoob.org>
2026-03-11 11:46:30 +1100

    Time: 0 ms (100%), Space: 19.3 MB (58.78%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.py
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.py
@@ -2,11 +2,4 @@ class Solution:
     def bitwiseComplement(self, n: int) -> int:
         if n == 0:
             return 1
-            
-        res = 0
-        mask = 1
-        while mask <= n:
-            if n & mask == 0:
-                res |= mask
-            mask <<= 1
-        return res
+        return n ^ (1 << n.bit_length()) - 1
