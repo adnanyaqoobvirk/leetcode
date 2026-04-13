235e25733b6cced3a0c96cc39b7fb5b98d9f32b9
Adnan Yaqoob <adnan@yaqoob.org>
2026-04-13 13:29:39 +1000

    Time: 11 ms (89.27%), Space: 19.5 MB (14.54%) - LeetHub

--- 0755-pour-water/0755-pour-water.py
+++ 0755-pour-water/0755-pour-water.py
@@ -2,35 +2,24 @@ class Solution:
     def pourWater(self, heights: List[int], volume: int, k: int) -> List[int]:
         n = len(heights)
         for _ in range(volume):
-            min_idx = -1
+            min_idx = k
             for i in reversed(range(k)):
-                if heights[i] > heights[k]:
+                if heights[i] > heights[min_idx]:
                     break
 
-                if min_idx != -1 and heights[i] > heights[min_idx]:
-                    break
-
-                if min_idx == -1 or heights[i] < heights[min_idx]:
+                if heights[i] < heights[min_idx]:
                     min_idx = i
             
-            if min_idx != -1 and heights[min_idx] != heights[k]:
+            if min_idx != k:
                 heights[min_idx] += 1
                 continue
-            
-            min_idx = -1
+
             for i in range(k + 1, n):
-                if heights[i] > heights[k]:
+                if heights[i] > heights[min_idx]:
                     break
 
-                if min_idx != -1 and heights[i] > heights[min_idx]:
-                    break
-
-                if min_idx == -1 or heights[i] < heights[min_idx]:
+                if heights[i] < heights[min_idx]:
                     min_idx = i
             
-            if min_idx != -1 and heights[min_idx] != heights[k]:
-                heights[min_idx] += 1
-                continue
-            
-            heights[k] += 1
+            heights[min_idx] += 1
         return heights
